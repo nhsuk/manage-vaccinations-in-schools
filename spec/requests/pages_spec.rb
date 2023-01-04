c4b98442417446049988f1c5ddf691cc4dfb51a1
Misha Gorodnitzky <misaka@pobox.com>
2023-01-04 23:14:02 +0000

    add github test action

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Pages", type: :request do
   describe "GET /home" do
     it "returns http success" do
-      get "/pages/home"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
