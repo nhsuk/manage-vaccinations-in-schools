d154d68fbe974b5317b7c090e88b7016bd8ada0c
Misha Gorodnitzky <misaka@pobox.com>
2023-01-09 10:28:49 +0000

    Create routeRequest for fetch handler
    Move logic out of serviceworker.js to keep things tidier, we don't want
    to end up with a single-source-file application.

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,6 +1,9 @@
 var CACHE_VERSION = 'v1';
 var CACHE_NAME = CACHE_VERSION + ':sw-cache-';
 
+// import routeRequest from 'router.js';
+importScripts('/router.js');
+
 function onInstall(event) {
   console.debug('[Serviceworker]', "Installing!", event);
   event.waitUntil(
@@ -36,49 +39,16 @@ function onActivate(event) {
   );
 }
 
+
 // Borrowed from https://github.com/TalAter/UpUp
 function onFetch(event) {
   event.respondWith(
-    // try to return untouched request from network first
-    fetch(event.request).then((response) => {
-      console.debug('[Serviceworker]', "Fetch received response", response);
-
-      // cache the response, but as the response can only be used once, clone it first
-      let responseClone = response.clone();
-
-      caches.open(CACHE_NAME).then((cache) => {
-        console.debug('[Serviceworker]', "Cacheing response for request", event.request);
-        cache.put(event.request, responseClone)
-      }).catch((err) => {
-        console.error('[Serviceworker]', "Could not open cache", CACHE_NAME, err);
-      });
-
-      return response;
-    }).catch((err) => {
-      console.debug('[Serviceworker]', "Failed to fetch response for request", event.request, err);
-
-      // if it fails, try to return request from the cache
-      return caches.match(event.request).then(function(response) {
-        if (response != undefined) {
-          console.debug('[Serviceworker]', "Returning cached response", response);
-          return response;
-        }
-
-        // if not found in cache, return default offline content for navigate requests
-        console.debug('[Serviceworker]', "Request not in cache", event.request);
-
-        if (event.request.mode === 'navigate' ||
-              (event.request.method === 'GET' &&
-               event.request.headers.get('accept').includes('text/html'))) {
-          console.debug('[Serviceworker]', "Request not in cache", event.request);
-
-          return caches.match('/offline.html');
-        }
-      })
-    })
-  );
+    routeRequest(event.request)
+  )
 }
+
 
 self.addEventListener('install', onInstall);
 self.addEventListener('activate', onActivate);
 self.addEventListener('fetch', onFetch);
+
