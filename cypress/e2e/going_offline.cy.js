bfe9d56d7babb6dcdfb7500011867a47ac66050b
Misha Gorodnitzky <misaka@pobox.com>
2023-01-13 12:04:47 +0000

    Add test for toggling offline and online.
    Simple proof-of-concept test to test the online/offline switching
    functionality. This DOES NOT fully test the functionality, however, as
    only tests what's displayed on the page without testing that the browser
    actually behaves as offline. That exercise has been punted down the road
    for now.
    
    This change also includes the decision to use `data-testid` property to
    select page elements for use in tests, and adds a Cypress command to get
    these easily in tests.

--- cypress/e2e/going_offline.cy.js
+++ cypress/e2e/going_offline.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/children')
+describe('Offline functionality', () => {
+  it('switches between going online and offline', () => {
+    cy.visit('/children');
+
+    cy.getBySelector('online-status').should('contain', 'Online');
+
+    cy.getBySelector('switch-online-offline')
+      .contains('Go Offline')
+      .click();
+    cy.getBySelector('online-status').should('contain', 'Offline');
+
+    cy.getBySelector('switch-online-offline')
+      .contains('Go Online')
+      .click();
+    cy.getBySelector('online-status').should('contain', 'Online');
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('getBySelector', (selector, ...args) => {
+  return cy.get(`[data-testid=${selector}]`, ...args);
+})
