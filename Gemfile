c138778d5872674ed32e59e9ae27160683b4426e
Misha Gorodnitzky <misaka@pobox.com>
2023-01-25 15:16:15 +0000

    Build FHIR record for immunization
    First step towards being able to send vaccine records to an external
    FHIR server.
    
    The FHIR builder here is simplistic and achieves the stepping-stone goal
    of replicating the JSON that comes out of the POC POC, which is our
    reference. As we have library support for FHIR we try to implement some
    things there, but will need to continue adoption of the library's
    conventions.
    
    We also currently use many hard-coded values which will have to come
    from the vaccination record.

--- Gemfile
+++ Gemfile
@@ -54,6 +54,9 @@ gem "awesome_print"
 # Add support to Rails for serviceworker
 gem "serviceworker-rails"
 
+# FHIR
+gem "fhir_client"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri mingw x64_mingw]
@@ -84,6 +87,7 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem "webmock"
 end
 gem "govuk-components"
 gem "govuk_design_system_formbuilder"
---
+++ spec/fhir_builders/immunization_fhir_builder_spec.rb
@@ -0,0 +1,105 @@
+require "rails_helper"
+
+RSpec.describe ImmunizationFHIRBuilder do
+  describe "JSON produced" do
+    let(:target_json) { <<~EOJSON.freeze }
+        {
+          "resourceType": "Immunization",
+          "meta": {
+            "profile": [
+              "https://fhir.hl7.org.uk/StructureDefinition/UKCore-Immunization"
+            ]
+          },
+          "extension": [
+            {
+              "url": "https://fhir.hl7.org.uk/StructureDefinition/Extension-UKCore-VaccinationProcedure",
+              "valueCodeableConcept": {
+                "coding": [
+                  {
+                    "system": "http://snomed.info/sct",
+                    "code": "734152003",
+                    "display": "Administration of vaccine product containing only Human papillomavirus 6, 11, 16 and 18 antigens"
+                  }
+                ]
+              }
+            }
+          ],
+          "identifier": [
+            {
+              "system": "https://supplierABC/identifiers/vacc",
+              "value": "5c1c14a6-37c2-45d3-9e0c-bffea36e13c7"
+            }
+          ],
+          "status": "completed",
+          "vaccineCode": {
+            "coding": [
+              {
+                "system": "http://snomed.info/sct",
+                "code": "10880211000001104",
+                "display": "Gardasil vaccine suspension for injection 0.5ml pre-filled syringes (Merck Sharp & Dohme (UK) Ltd)"
+              }
+            ]
+          },
+          "patient": {
+            "reference": "https://sandbox.api.service.nhs.uk/personal-demographics/FHIR/R4/Patient/9000000009",
+            "type": "Patient",
+            "identifier": {
+              "system": "https://fhir.nhs.uk/Id/nhs-number",
+              "value": "9000000009"
+            }
+          },
+          "occurrenceDateTime": "2023-01-25T00:00:00.000+00:00",
+          "recorded": "2023-01-25",
+          "primarySource": true,
+          "lotNumber": "808",
+          "expirationDate": "2023-01-31",
+          "site": {
+            "coding": [
+              {
+                "system": "http://snomed.info/sct",
+                "code": "368209003",
+                "display": "Right upper arm structure (body structure)"
+              }
+            ]
+          },
+          "route": {
+            "coding": [
+              {
+                "system": "http://snomed.info/sct",
+                "code": "78421000",
+                "display": "Intramuscular route (qualifier value)"
+              }
+            ]
+          },
+          "doseQuantity": {
+            "value": 0.5,
+            "unit": "Millilitre",
+            "system": "http://snomed.info/sct",
+            "code": "258773002"
+          },
+          "reasonCode": [
+            {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "443684005",
+                  "display": "Disease outbreak"
+                }
+              ]
+            }
+          ]
+        }
+      EOJSON
+    let(:app_identity) { "5c1c14a6-37c2-45d3-9e0c-bffea36e13c7" }
+
+    before { allow(Random).to receive(:uuid).and_return(app_identity) }
+
+    it "should match the JSON produced from the POC POC" do
+      imm = described_class.new(patient_identifier: "9000000009")
+
+      expect(JSON.parse(imm.immunization.to_json)).to eq(
+        JSON.parse(target_json)
+      )
+    end
+  end
+end
