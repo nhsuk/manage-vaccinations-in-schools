c01efbae1c2b13eae9fc9ae5082ab672c3740bf0
Misha Gorodnitzky <misaka@pobox.com>
2023-01-26 15:38:37 +0000

    Add offline banner

--- cypress/e2e/going_offline.cy.js
+++ cypress/e2e/going_offline.cy.js
@@ -1,13 +1,17 @@
 describe("Offline functionality", () => {
   it("switches between going online and offline", () => {
-    cy.visit("/children");
+    cy.visit("/");
+    cy.getBySelector("campaigns").first().click();
 
     cy.getBySelector("online-status").should("contain", "Online");
+    cy.getBySelector("offline-indicator").should("not.be.visible");
 
     cy.getBySelector("switch-online-offline").contains("Go Offline").click();
     cy.getBySelector("online-status").should("contain", "Offline");
+    cy.getBySelector("offline-indicator").should("be.visible");
 
     cy.getBySelector("switch-online-offline").contains("Go Online").click();
     cy.getBySelector("online-status").should("contain", "Online");
+    cy.getBySelector("offline-indicator").should("not.be.visible");
   });
 });
