4a8de33518cae747e513ac0451055cdb499104da
Theodor Vararu <theo@vararu.org>
2023-02-02 16:03:28 +0000

    Remove logging from serviceworker-companion
    This can be added back in if it's necessary, possibly in a more
    togglable fashion, so that it's only active in development mode.

--- app/javascript/serviceworker-companion.js
+++ app/javascript/serviceworker-companion.js
@@ -5,30 +5,6 @@ export let wb;
 export function initServiceWorker() {
   if ("serviceWorker" in navigator) {
     wb = new Workbox("/serviceworker.js");
-    console.log("[ServiceWorker Companion] registering service worker: ", wb);
-
-    wb.addEventListener("activated", (event) => {
-      console.log("[Service Worker Companion] service worker activated", event);
-    });
-
-    wb.addEventListener("waiting", (event) => {
-      console.log(
-        "[Service Worker Companion] service worker is waiting for current version to be fully unloaded",
-        event
-      );
-    });
-
-    wb.addEventListener("controlling", (event) => {
-      console.log(
-        "[Service Worker Companion] service worker is IN CONTROL",
-        event
-      );
-    });
-
-    wb.addEventListener("installed", (event) => {
-      console.log("[Service Worker Companion] service worker installed", event);
-    });
-
     wb.register();
   }
 }
