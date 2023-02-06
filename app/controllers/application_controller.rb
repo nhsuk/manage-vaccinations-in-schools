9a8f0f87783c06261034cfcab741247e0bd6271e
Theodor Vararu <theo@vararu.org>
2023-02-06 16:37:15 +0000

    Set up basic authentication in production mode
    This should be set up on all prototypes and services that look like a
    UK public sector website, to avoid confusing users who might stumble on
    it and could think it's a real service.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.production?
+    http_basic_authenticate_with name: Settings.support_username,
+                                 password: Settings.support_password,
+                                 message:
+                                   "THIS IS NOT A PRODUCTION NHS.UK SERVICE"
+  end
+
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
 end
