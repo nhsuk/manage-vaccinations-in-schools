5465b7ec21b2a5d143d94237547adc65dbb81cd7
Theodor Vararu <theo@vararu.org>
2023-02-10 16:14:08 +0000

    Extract messageHandler to messages module

---
+++ app/javascript/serviceworker/messages.js
@@ -0,0 +1,32 @@
+import { checkOnlineStatus, toggleOnlineStatus } from "./online-status";
+import { addAll } from "./cache";
+
+const messageHandlers = {
+  TOGGLE_CONNECTION: (event) => {
+    event.ports[0].postMessage(toggleOnlineStatus());
+  },
+
+  GET_CONNECTION_STATUS: (event) => {
+    event.ports[0].postMessage(checkOnlineStatus());
+  },
+
+  SAVE_CAMPAIGN_FOR_OFFLINE: async ({ data }) => {
+    const campaignId = data.payload["campaignId"];
+
+    addAll([
+      `/campaigns/${campaignId}/children`,
+      `/campaigns/${campaignId}/children.json`,
+      `/campaigns/${campaignId}/children/show-template`,
+    ]);
+  },
+};
+
+export const handler = (event) => {
+  if (event.data && event.data.type) {
+    console.debug(
+      "[Service Worker Message Listener] received message event:",
+      event.data
+    );
+    messageHandlers[event.data.type](event);
+  }
+};
