eba4901833d674ea73cfa4ffff42cf0e25b25895
Theodor Vararu <theo@vararu.org>
2023-02-10 16:05:44 +0000

    Replace lookupCachedResponse with match
    This function is really just a wrapper around `match`, which is more
    generic and can work with any URL and not just request objects.

--- app/javascript/serviceworker/cache.js
+++ app/javascript/serviceworker/cache.js
@@ -14,10 +14,3 @@ export const put = async (request, response) => {
   const cache = await caches.open(cacheNames.runtime);
   return await cache.put(request, response);
 };
-
-export const lookupCachedResponse = async (request) => {
-  const cache = await caches.open(cacheNames.runtime);
-  const response = await cache.match(request.url);
-
-  return response;
-};
