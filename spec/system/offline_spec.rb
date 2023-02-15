3c30e2b929467caf994599c02a3c34168302e87e
Theodor Vararu <theo@vararu.org>
2023-02-15 09:29:13 +0000

    WIP offline spec

--- spec/system/offline_spec.rb
+++ spec/system/offline_spec.rb
@@ -5,24 +5,18 @@ RSpec.describe "Offline mode" do
     @campaign = create(:campaign)
     visit root_path
     click_link @campaign.location.name
-    # click_button "Save offline"
-    when_i_go_offline
-    visit campaign_vaccinations_path(@campaign)
-    # visit root_path
-    # click_link "Record vaccinations"
-    expect(page).to have_content "foo"
-    # click_link @campaign.children.first.full_name
+    click_button "Save offline"
+    # binding.irb
+    # page.driver.wait_for_network_idle
+    # binding.irb
+    page.driver.browser.network.offline_mode
+    WebMock.disable_net_connect!(allow_localhost: false)
+    # binding.irb
+    click_link "Record vaccinations"
+    click_link @campaign.children.first.full_name
+    binding.irb
+    # page.driver.debug(binding)
   end
 
   private
-
-  def when_i_go_offline
-    page.driver.browser.page.command(
-      "Network.emulateNetworkConditions",
-      offline: true,
-      latency: 0,
-      downloadThroughput: 0,
-      uploadThroughput: 0
-    )
-  end
 end
