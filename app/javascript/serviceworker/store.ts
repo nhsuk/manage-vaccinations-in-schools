c095830c79633b517fca732a48f7504849b209be
Theodor Vararu <theo@vararu.org>
2023-02-17 17:53:58 +0000

    Add store module
    This instantiates an indexedDB for storing offline PUT/POST requests in.
    
    The module exposes methods to add, query, and delete items from the
    database.
    
    The module is written in TypeScript, and specifies a schema, which means
    that errors in respecting the schema should be caught at compile time.
    
    We don't currently include a typescript linting step. We can add this
    later.

---
+++ app/javascript/serviceworker/store.ts
@@ -0,0 +1,60 @@
+import { openDB, DBSchema } from "idb";
+
+interface RequestObject {
+  id?: number;
+  url: string;
+  data: any;
+}
+
+interface OfflineDatabase extends DBSchema {
+  requests: {
+    key: number;
+    value: RequestObject;
+  };
+}
+
+interface Db {
+  createObjectStore: (
+    arg0: string,
+    arg1: { keyPath: string; autoIncrement: boolean }
+  ) => void;
+}
+
+const DB_NAME = "offline";
+const DB_VERSION = 1;
+
+const openTx = async (mode: "readwrite" | "readonly") => {
+  const db = await openDB<OfflineDatabase>(DB_NAME, DB_VERSION, {
+    upgrade(db: Db) {
+      db.createObjectStore("requests", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+    },
+  });
+
+  return db.transaction("requests", mode);
+};
+
+export const saveRequest = async (request: Request) => {
+  const requestData = await request.formData();
+  const data = Object.fromEntries(requestData);
+
+  const tx = await openTx("readwrite");
+  await tx.store.add({ url: request.url, data });
+  await tx.done;
+};
+
+export const deleteRequest = async (id: number) => {
+  const tx = await openTx("readwrite");
+  await tx.store.delete(id);
+  await tx.done;
+};
+
+export const getAllRequests = async (): Promise<RequestObject[]> => {
+  const tx = await openTx("readonly");
+  const requests = await tx.store.getAll();
+  await tx.done;
+
+  return requests;
+};
