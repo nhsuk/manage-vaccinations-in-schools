78a83fe0f7a23c961d1cd1b46b707ee3e7b3cb27
Theodor Vararu <theo@vararu.org>
2023-02-17 17:53:58 +0000

    Add store module
    This instantiates an indexedDB for storing offline PUT/POST requests in.
    
    The module exposes methods to add, query, and delete items from the
    database.
    
    The module is written in TypeScript, and specifies a schema, which means
    that errors in respecting the schema should be caught at compile time.
    
    We don't currently include a typescript linting step. We can add this
    later.

---
+++ app/javascript/serviceworker/store.spec.ts
@@ -0,0 +1,61 @@
+import "jest-fetch-mock";
+import "fake-indexeddb/auto";
+import FDBFactory from "fake-indexeddb/lib/FDBFactory";
+import { saveRequest, getAllRequests, deleteRequest } from "./store";
+
+const formDataMock = jest.fn(() => {
+  const form = new FormData();
+  form.append("name", "John");
+  return Promise.resolve(form);
+});
+
+beforeEach(() => {
+  // Reset database https://github.com/dumbmatter/fakeIndexedDB/issues/40
+  indexedDB = new FDBFactory();
+});
+
+describe("saveRequest and getAllRequests", () => {
+  it("save and get the requests", async () => {
+    const request = new Request("/api", {
+      method: "POST",
+      body: JSON.stringify({ name: "John" }),
+    });
+
+    request.formData = formDataMock;
+
+    await saveRequest(request);
+
+    const requests = await getAllRequests();
+
+    expect(requests).toMatchInlineSnapshot(`
+      [
+        {
+          "data": {
+            "name": "John",
+          },
+          "id": 1,
+          "url": "/api",
+        },
+      ]
+    `);
+  });
+});
+
+describe("deleteRequest", () => {
+  it("delete a request", async () => {
+    const request = new Request("/api", {
+      method: "POST",
+      body: JSON.stringify({ name: "John" }),
+    });
+
+    request.formData = formDataMock;
+
+    await saveRequest(request);
+
+    await deleteRequest(1);
+
+    const requests = await getAllRequests();
+
+    expect(requests).toMatchInlineSnapshot(`[]`);
+  });
+});
