24a418c68e1c167a54043a1d6cc6f1f140714bcc
Theodor Vararu <theo@vararu.org>
2023-02-20 15:30:11 +0000

    Add refreshOnlineStatus to online status module
    This checks if the user is online by attempting to ping a /health
    endpoint on the server in a loop.

--- app/javascript/serviceworker/online-status.spec.js
+++ app/javascript/serviceworker/online-status.spec.js
@@ -1,8 +1,10 @@
+require("jest-fetch-mock").enableMocks();
 import {
   isOnline,
   setOfflineMode,
   setOnlineMode,
   toggleOnlineStatus,
+  refreshOnlineStatus,
 } from "./online-status";
 
 describe("isOnline", () => {
@@ -32,5 +34,27 @@ describe("setOnlineMode", () => {
     setOfflineMode();
     setOnlineMode();
     expect(isOnline()).toBe(true);
+  });
+});
+
+describe("refreshOnlineStatus", () => {
+  const cb = jest.fn();
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  test("works", async () => {
+    refreshOnlineStatus(cb);
+
+    jest.advanceTimersToNextTimer(2);
+    await Promise.resolve();
+    expect(cb).toHaveBeenCalledTimes(2);
+
+    fetch.mockReject(new Error("Offline"));
+
+    jest.advanceTimersToNextTimer(2);
+    await Promise.resolve();
+    expect(cb).toHaveBeenCalledTimes(2);
   });
 });
