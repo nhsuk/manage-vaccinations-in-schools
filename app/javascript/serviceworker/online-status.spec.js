30b72df237eab802456aa96fd04944c08529f43a
Theodor Vararu <theo@vararu.org>
2023-02-22 16:11:47 +0000

    Use a fetchWithTimeout in refreshOnlineStatus
    We don't want these requests to wait indefinitely; rather, we want to
    cancel them and try again at a later point.

--- app/javascript/serviceworker/online-status.spec.js
+++ app/javascript/serviceworker/online-status.spec.js
@@ -5,6 +5,7 @@ import {
   setOnlineMode,
   toggleOnlineStatus,
   refreshOnlineStatus,
+  fetchWithTimeout,
 } from "./online-status";
 
 describe("isOnline", () => {
@@ -37,11 +38,30 @@ describe("setOnlineMode", () => {
   });
 });
 
+describe("fetchWithTimeout", () => {
+  test("works if request is shorter than timeout", async () => {
+    fetch.mockResponseOnce(
+      () =>
+        new Promise((resolve) => setTimeout(() => resolve({ body: "ok" }), 1))
+    );
+    await expect(fetchWithTimeout("/")).resolves.not.toThrow();
+  });
+
+  test("times out if request takes longer than timeout", async () => {
+    fetch.mockResponseOnce(
+      () =>
+        new Promise((resolve) => setTimeout(() => resolve({ body: "ok" }), 2))
+    );
+    await expect(fetchWithTimeout("/", { timeout: 1 })).rejects.toThrow();
+  });
+});
+
 describe("refreshOnlineStatus", () => {
   const cb = jest.fn();
 
   const tick = async () => {
     jest.advanceTimersToNextTimer(1); // Wait the sleep
+    await Promise.resolve(); // Resolve the fetchWithTimeout
     await Promise.resolve(); // Resolve the fetch
     await Promise.resolve(); // Resolve the cb
   };
