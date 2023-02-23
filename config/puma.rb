e69928408c74edb872073bc8d815a8bfab2adff6
Theodor Vararu <theo@vararu.org>
2023-02-23 16:35:17 +0000

    Set the Puma to port 4444 if tmp/offline.txt exists
    This is a simple "offline-mode" implementation for Rails. The idea is to
    take down the server and restart it on a different port when we want to
    switch from offline to online mode and vice versa.
    
    With this config, you can take down the server with a script line:
    
    ```
    touch tmp/offline.txt && kill -USR2 $(cat tmp/pids/server.pid)
    ```
    
    And take it back online with:
    
    ```
    rm -f tmp/offline.txt && kill -USR2 $(cat tmp/pids/server.pid)
    ```

--- config/puma.rb
+++ config/puma.rb
@@ -13,9 +13,11 @@ threads min_threads_count, max_threads_count
 #
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch("PORT", 4000)
+if File.exist? "tmp/offline.txt"
+  port 4444
+else
+  port ENV.fetch("PORT", 4000)
+end
 
 # Specifies the `environment` that Puma will run in.
 #
