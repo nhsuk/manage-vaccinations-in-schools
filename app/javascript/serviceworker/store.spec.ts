928b41aa9d5d61a5b193b8428fc0acaf138c7186
Theodor Vararu <theo@vararu.org>
2023-04-06 17:18:55 +0100

    Dedupe requests when added to idb store
    When we cache a request by its URL, we want to only have 1 entry per
    URL, and keep those updated with the last fetched version.

--- app/javascript/serviceworker/store.spec.ts
+++ app/javascript/serviceworker/store.spec.ts
@@ -7,11 +7,54 @@ beforeEach(() => {
   indexedDB = new FDBFactory();
 });
 
-describe("add and getAll", () => {
-  it("save and get the requests", async () => {
+describe("add", () => {
+  it("saves the request", async () => {
     await add("delayedRequests", "/api", { name: "John" });
 
     const requests = await getAll("delayedRequests");
+
+    expect(requests).toMatchInlineSnapshot(`
+      [
+        {
+          "body": {
+            "name": "John",
+          },
+          "id": 1,
+          "url": "/api",
+        },
+      ]
+    `);
+  });
+
+  it("dedupes requests by url", async () => {
+    await add("cachedResponses", "/api", { name: "John" });
+    await add("cachedResponses", "/api", { name: "John" });
+
+    const requests = await getAll("cachedResponses");
+
+    expect(requests).toMatchInlineSnapshot(`
+      [
+        {
+          "body": {
+            "name": "John",
+          },
+          "id": 1,
+          "url": "/api",
+        },
+      ]
+    `);
+  });
+});
+
+describe("getAll", () => {
+  it("returns all requests", async () => {
+    let requests = await getAll("delayedRequests");
+
+    expect(requests).toMatchInlineSnapshot(`[]`);
+
+    await add("delayedRequests", "/api", { name: "John" });
+
+    requests = await getAll("delayedRequests");
 
     expect(requests).toMatchInlineSnapshot(`
       [
@@ -46,7 +89,7 @@ describe("getByUrl", () => {
 });
 
 describe("destroy", () => {
-  it("delete a request", async () => {
+  it("deletes a request", async () => {
     await add("delayedRequests", "/api", { name: "John" });
 
     await destroy("delayedRequests", 1);
