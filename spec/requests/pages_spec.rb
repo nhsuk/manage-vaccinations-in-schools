633652ed4292a8ce5ed0a14274f5d631edccb2d2
Theodor Vararu <theo@vararu.org>
2023-04-17 10:17:51 +0100

    Disable browser caching for children.json
    This prevents the browser from saving a request for this file to the
    Cache_Data folder.

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe "Pages", type: :request do
       expect(response).to redirect_to("/dashboard")
     end
   end
+
+  describe "GET /campaigns/:campaign_id/children.json" do
+    it "has a no-store cache header" do
+      campaign = create(:campaign)
+      get "/campaigns/#{campaign.id}/children.json"
+      expect(response.headers["Cache-Control"]).to eq("no-store")
+      expect(response.headers["Pragma"]).to eq("no-cache")
+      expect(response.headers["Expires"]).to eq("0")
+    end
+  end
 end
