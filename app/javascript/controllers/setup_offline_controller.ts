5fe9b810a2c2eba7d491e9a867de3fe1a0624add
Theodor Vararu <theo@vararu.org>
2023-05-03 12:26:45 +0100

    Save campaign for offline after submitting password
    Take the user-supplied password and use it to populate the encrypted
    cache.

--- app/javascript/controllers/setup_offline_controller.ts
+++ app/javascript/controllers/setup_offline_controller.ts
@@ -1,10 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
+import { wb } from "../serviceworker-companion.js";
 
 // Connects to data-controller="setup-offline"
 export default class extends Controller {
-  static targets = ["form"];
+  static targets = ["form", "password"];
 
   declare readonly formTarget: HTMLFormElement;
+  declare readonly passwordTarget: HTMLInputElement;
 
   connect() {
     this.formTarget.addEventListener("submit", this.handleSubmit.bind(this));
@@ -13,7 +15,21 @@ export default class extends Controller {
   async handleSubmit(event: Event) {
     event.preventDefault();
 
-    // TODO: Do async stuff
+    // Pass in paths to CSS and JS to save for offline use, because document
+    // methods are not available in serviceWorker
+    const css = (
+      document.querySelector("link[rel=stylesheet]") as HTMLLinkElement
+    ).href;
+    const js = (document.querySelector("script[src]") as HTMLScriptElement).src;
+
+    await wb.messageSW({
+      type: "SAVE_CAMPAIGN_FOR_OFFLINE",
+      payload: {
+        password: this.passwordTarget.value,
+        campaignId: 1,
+        additionalItems: [css, js],
+      },
+    });
 
     this.formTarget.submit();
   }
