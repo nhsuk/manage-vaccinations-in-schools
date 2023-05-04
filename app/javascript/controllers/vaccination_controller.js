b28df9163df9ffda9446302fc28b6d69088022e5
Theodor Vararu <theo@vararu.org>
2023-05-04 09:32:59 +0100

    WIP set tag to Vaccinated by reading from db

--- app/javascript/controllers/vaccination_controller.js
+++ app/javascript/controllers/vaccination_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
+import { getAll } from "../serviceworker/store";
 
 // Connects to data-controller="vaccinations"
 export default class extends Controller {
@@ -7,5 +8,12 @@ export default class extends Controller {
     id: String,
   };
 
-  connect() {}
+  async connect() {
+    const reqs = await getAll("delayedRequests");
+    const childId = this.idValue;
+
+    if (reqs.find((req) => req.url.includes(`children/${childId}`))) {
+      this.statusTarget.innerHTML = `<strong class="nhsuk-tag">Vaccinated</stong`;
+    }
+  }
 }
