3b4470c0e1eb3b127f38c38738b619feae46c8b0
Theodor Vararu <theo@vararu.org>
2023-05-25 12:22:01 +0100

    Add missing OfflinePasswordsController
    Should have been in https://github.com/nhsuk/record-childrens-vaccinations/pull/212

---
+++ app/controllers/offline_passwords_controller.rb
@@ -0,0 +1,24 @@
+class OfflinePasswordsController < ApplicationController
+  def new
+    @password = OfflinePassword.new
+  end
+
+  def create
+    @password = OfflinePassword.new(password_params)
+
+    if @password.save
+      redirect_to root_path,
+                  flash: {
+                    success: "Campaign saved, you can now go offline",
+                  }
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:offline_password).permit(:password, :password_confirmation)
+  end
+end
