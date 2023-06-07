6175e41bdd0b2bf8a265000e7e6aed9d41750c88
Theodor Vararu <theo@vararu.org>
2023-06-07 18:23:28 +0100

    Remove record checkboxes and set required fields
    Make sure users have to enter certain fields if they wish to create or
    edit new records. Also remove the record selector checkboxes which don't
    do anything currently.

--- app/avo/resources/child_resource.rb
+++ app/avo/resources/child_resource.rb
@@ -1,22 +1,23 @@
 class ChildResource < Avo::BaseResource
   self.title = :full_name
   self.includes = []
+  self.record_selector = false
   # self.search_query = -> do
   #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
 
   field :id, as: :id
   # Fields generated from the model
-  field :dob, as: :date
-  field :nhs_number, as: :number
+  field :dob, as: :date, required: true
+  field :nhs_number, as: :number, required: true
   field :sex, as: :select, enum: ::Child.sexes
-  field :first_name, as: :text
-  field :last_name, as: :text
+  field :first_name, as: :text, required: true
+  field :last_name, as: :text, required: true
   field :preferred_name, as: :text
-  field :gp, as: :select, enum: ::Child.gps
-  field :screening, as: :select, enum: ::Child.screenings
-  field :consent, as: :select, enum: ::Child.consents
-  field :seen, as: :select, enum: ::Child.seens
+  field :gp, as: :select, enum: ::Child.gps, required: true
+  field :screening, as: :select, enum: ::Child.screenings, required: true
+  field :consent, as: :select, enum: ::Child.consents, required: true
+  field :seen, as: :select, enum: ::Child.seens, required: true
   field :sessions, as: :has_and_belongs_to_many
   field :location, as: :belongs_to
   # add fields here
--- app/avo/resources/location_resource.rb
+++ app/avo/resources/location_resource.rb
@@ -1,13 +1,14 @@
 class LocationResource < Avo::BaseResource
   self.title = :name
   self.includes = []
+  self.record_selector = false
   # self.search_query = -> do
   #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
 
   field :id, as: :id
   # Fields generated from the model
-  field :name, as: :text
+  field :name, as: :text, required: true
   field :address, as: :text
   field :locality, as: :text
   field :town, as: :text
