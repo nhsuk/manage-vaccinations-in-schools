4b61ed96f0723223f777829cfda4f54c4cc68fe7
Theodor Vararu <theo@vararu.org>
2023-06-07 18:23:28 +0100

    Remove record checkboxes and set required fields
    Make sure users have to enter certain fields if they wish to create or
    edit new records. Also remove the record selector checkboxes which don't
    do anything currently.

--- app/avo/resources/campaign_resource.rb
+++ app/avo/resources/campaign_resource.rb
@@ -1,13 +1,14 @@
 class CampaignResource < Avo::BaseResource
   self.title = :name
   self.includes = []
+  self.record_selector = false
   # self.search_query = -> do
   #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
 
   field :id, as: :id
   # Fields generated from the model
-  field :name, as: :text
+  field :name, as: :text, required: true
   field :sessions, as: :has_many
   # add fields here
 end
--- app/avo/resources/session_resource.rb
+++ app/avo/resources/session_resource.rb
@@ -1,14 +1,15 @@
 class SessionResource < Avo::BaseResource
   self.title = :name
   self.includes = []
+  self.record_selector = false
   # self.search_query = -> do
   #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
 
   field :id, as: :id
   # Fields generated from the model
-  field :date, as: :date_time
-  field :name, as: :text
+  field :name, as: :text, required: true
+  field :date, as: :date_time, required: true
   field :campaign, as: :belongs_to
   field :location, as: :belongs_to
   field :children, as: :has_and_belongs_to_many
