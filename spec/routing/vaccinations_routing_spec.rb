d5ec8de34f867702828d7f4d49c7d2a98fc9c768
Theodor Vararu <theo@vararu.org>
2023-06-08 13:31:43 +0100

    Set path to /vaccinations for VaccinationsController
    This matches our newly designed routes.

--- spec/routing/vaccinations_routing_spec.rb
+++ spec/routing/vaccinations_routing_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe VaccinationsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/sessions/1/patients").to route_to(
+      expect(get: "/sessions/1/vaccinations").to route_to(
         "vaccinations#index",
         session_id: "1"
       )
