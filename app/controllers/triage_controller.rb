7a649adab12e50b403bd23a77729f8dd25ecdcd9
Misha Gorodnitzky <misaka@pobox.com>
2023-06-14 17:06:49 +0100

    Show statuses on triage page

--- app/controllers/triage_controller.rb
+++ app/controllers/triage_controller.rb
@@ -2,7 +2,13 @@ class TriageController < ApplicationController
   before_action :set_session, only: [:index]
 
   def index
-    @patients = @session.patients
+    @patient_triages =
+      @session
+        .patients
+        .includes(:triage)
+        .map do |patient|
+          [patient, patient.triage_for_campaign(@session.campaign)]
+        end
   end
 
   private
