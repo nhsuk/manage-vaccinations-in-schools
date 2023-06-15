d6d574c498f0594bab1dfa9e09385c6e6919721a
Misha Gorodnitzky <misaka@pobox.com>
2023-06-15 16:51:40 +0100

    Add backlink component

---
+++ app/components/app_backlink_component.rb
@@ -0,0 +1,11 @@
+class AppBacklinkComponent < ViewComponent::Base
+  def initialize(href:, name:, classes: nil, attributes: {})
+    super
+
+    @href = href
+    @name = name
+    classes = classes.join(" ") if classes.is_a? Array
+    @classes = "nhsuk-width-container#{classes.present? ? " #{classes}" : ""}"
+    @attributes = attributes.merge(class: @classes)
+  end
+end
