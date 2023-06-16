f5dcdf5890cf9c759367d6fc5ebae8f88e4d3557
Misha Gorodnitzky <misaka@pobox.com>
2023-06-16 15:44:07 +0100

    Add consent responses table & model

---
+++ app/models/consent_response.rb
@@ -0,0 +1,59 @@
+# == Schema Information
+#
+# Table name: consent_responses
+#
+#  id                          :bigint           not null, primary key
+#  address_line_1              :text
+#  address_line_2              :text
+#  address_postcode            :text
+#  address_town                :text
+#  childs_common_name          :text
+#  childs_dob                  :date
+#  childs_name                 :text
+#  consent                     :integer
+#  gp_name                     :text
+#  gp_response                 :integer
+#  parent_contact_method       :integer
+#  parent_contact_method_other :text
+#  parent_email                :text
+#  parent_name                 :text
+#  parent_phone                :text
+#  parent_relationship         :integer
+#  parent_relationship_other   :text
+#  reason_for_refusal          :integer
+#  reason_for_refusal_other    :text
+#  route                       :integer          not null
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
+#  campaign_id                 :bigint           not null
+#  patient_id                  :bigint           not null
+#
+# Indexes
+#
+#  index_consent_responses_on_campaign_id  (campaign_id)
+#  index_consent_responses_on_patient_id   (patient_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (campaign_id => campaigns.id)
+#  fk_rails_...  (patient_id => patients.id)
+#
+
+class ConsentResponse < ApplicationRecord
+  belongs_to :patient
+  belongs_to :campaign
+
+  enum :parent_relationship, %w[mother father guardian other], prefix: true
+  enum :consent, %w[given refused], prefix: true
+  enum :reason_for_refusal,
+       %w[
+         already_vaccinated
+         will_be_vaccinated_elsewhere
+         medical
+         personal_choice
+         other
+       ],
+       prefix: true
+  enum :gp_response, %w[yes no dont_know]
+  enum :route, %i[website phone paper in_person self_consent], prefix: "via"
+end
---
+++ db/migrate/20230616103904_create_consent_responses.rb
@@ -0,0 +1,31 @@
+class CreateConsentResponses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :consent_responses do |t|
+      t.references :patient, null: false, foreign_key: true
+      t.references :campaign, null: false, foreign_key: true
+
+      t.text :childs_name
+      t.text :childs_common_name
+      t.date :childs_dob
+      t.text :address_line_1
+      t.text :address_line_2
+      t.text :address_town
+      t.text :address_postcode
+      t.text :parent_name
+      t.integer :parent_relationship
+      t.text :parent_relationship_other
+      t.text :parent_email
+      t.text :parent_phone
+      t.integer :parent_contact_method
+      t.text :parent_contact_method_other
+      t.integer :consent
+      t.integer :reason_for_refusal
+      t.text :reason_for_refusal_other
+      t.integer :gp_response
+      t.text :gp_name
+      t.integer :route, null: false
+
+      t.timestamps
+    end
+  end
+end
