d75f95074246c714f46c4bc0fcf07f1f139313c1
Misha Gorodnitzky <misaka@pobox.com>
2023-06-20 16:34:55 +0100

    Add spec

---
+++ spec/components/app_backlink_component_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe AppBacklinkComponent, type: :component do
+  before { render_inline(component) }
+
+  subject { page }
+
+  let(:component) { described_class.new(href:, name:, classes:, attributes:) }
+  let(:href) { "/previous_page" }
+  let(:name) { "Previous Page" }
+  let(:classes) { "additional-class" }
+  let(:attributes) { { id: "back-link", data: { controller: "back-link" } } }
+
+  it { should have_css(".nhsuk-back-link") }
+  it { should have_css(".nhsuk-back-link__link") }
+  it { should have_link(href:) }
+  it { should have_css(".nhsuk-width-container.additional-class") }
+  it { should have_css('[id="back-link"]') }
+  it { should have_css(".nhsuk-u-visually-hidden", text: "to #{name}") }
+end
