7175f360a6bf7db02f51912a9b5651d5ceabf880
Misha Gorodnitzky <misaka@pobox.com>
2023-06-21 22:57:18 +0100

    Update vaccination index page to use new tags

--- app/helpers/vaccinations_helper.rb
+++ app/helpers/vaccinations_helper.rb
@@ -1,2 +1,25 @@
 module VaccinationsHelper
+  def vaccination_status_colour(status)
+    case status
+    when "no_outcome"
+      :white
+    when "vaccinated"
+      :green
+    when "no_consent"
+      :red
+    when "could_not_vaccinate"
+      :orange
+    else
+      :white
+    end
+  end
+
+  def vaccination_status_icon(status)
+    case status
+    when "no_consent", "couldnt_vaccinate"
+      "cross"
+    when "vaccinated"
+      "tick"
+    end
+  end
 end
