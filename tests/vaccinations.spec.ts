02aabc9994687dd9cc124d8376cf79ca6215c1cd
Theodor Vararu <theo@vararu.org>
2023-06-22 17:34:09 +0100

    Update vaccinations end to end test

--- tests/vaccinations.spec.ts
+++ tests/vaccinations.spec.ts
@@ -1,10 +1,36 @@
 import { test, expect } from "@playwright/test";
 
-test("Records vaccinations", async ({ page }) => {
-  await page.goto("/reset");
+let p = null;
 
-  await page.goto("/sessions/1/vaccinations");
-  await expect(page.getByTestId("child-status").nth(0)).toContainText(
+test("Records vaccinations", async ({ page }) => {
+  p = page;
+  await given_the_app_is_setup();
+
+  await when_i_go_to_the_vaccinations_page();
+  await then_i_should_see_no_outcome_yet();
+
+  await when_i_click_on_the_first_patient();
+  await then_i_should_see_the_vaccinations_page();
+});
+
+async function given_the_app_is_setup() {
+  await p.goto("/reset");
+}
+
+async function when_i_go_to_the_vaccinations_page() {
+  await p.goto("/sessions/1/vaccinations");
+}
+
+async function then_i_should_see_no_outcome_yet() {
+  await expect(p.getByTestId("child-status").nth(0)).toContainText(
     "No outcome yet"
   );
-});
+}
+
+async function when_i_click_on_the_first_patient() {
+  await p.getByTestId("child-link").nth(0).click();
+}
+
+async function then_i_should_see_the_vaccinations_page() {
+  await expect(p.locator("h1")).toContainText("Child details");
+}
