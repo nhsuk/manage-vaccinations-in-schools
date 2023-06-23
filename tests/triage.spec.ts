8e09143a0734fa654cdbb85f0e0d58753219ad59
Misha Gorodnitzky <misaka@pobox.com>
2023-06-23 19:23:32 +0100

    Full Future Learn For The Win! FFLFTW!

--- tests/triage.spec.ts
+++ tests/triage.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
+let p = null;
+
 const patients = {
   "Aaron Pfeffer": {
     row: 1,
@@ -94,73 +96,89 @@ const patients = {
 };
 
 test("Performing triage", async ({ page }) => {
-  await page.goto("/reset");
+  p = page;
 
-  await page.goto("/sessions/1");
-  await page.getByRole("link", { name: "Triage" }).click();
-
-  await expect(page.locator("h1")).toContainText("Triage");
-
-  await then_i_should_see_the_correct_breadcrumbs(page);
-  await then_i_should_see_patients_with_their_triage_info(page);
+  await given_the_app_is_setup();
+  await when_i_go_to_the_triage_page_for_the_first_session();
+  await then_i_should_see_the_triage_index_page();
+  await then_i_should_see_the_correct_breadcrumbs();
+  await then_i_should_see_patients_with_their_triage_info();
 
   for (let name in patients) {
-    await page.getByRole("link", { name: name }).click();
-    await then_i_should_see_the_triage_page_for_the_patient(page, name);
-    await page.getByRole("link", { name: "Back to triage" }).click();
+    await when_i_click_on_the_patient(name);
+    await then_i_should_see_the_triage_page_for_the_patient(name);
+    await when_i_go_back_to_the_triage_index_page();
+    await then_i_should_see_the_triage_index_page();
   }
 });
 
-async function then_i_should_see_the_correct_breadcrumbs(page) {
-  await expect(
-    page.locator(".nhsuk-breadcrumb__item:last-of-type")
-  ).toContainText(
+async function given_the_app_is_setup() {
+  await p.goto("/reset");
+}
+
+async function when_i_go_to_the_triage_page_for_the_first_session() {
+  await p.goto("/sessions/1");
+  await p.getByRole("link", { name: "Triage" }).click();
+}
+
+async function when_i_click_on_the_patient(name) {
+  await p.getByRole("link", { name: name }).click();
+}
+
+async function when_i_go_back_to_the_triage_index_page() {
+  await p.getByRole("link", { name: "Back to triage" }).click();
+}
+
+async function then_i_should_see_the_triage_index_page() {
+  await expect(p.locator("h1")).toContainText("Triage");
+}
+
+async function then_i_should_see_the_correct_breadcrumbs() {
+  await expect(p.locator(".nhsuk-breadcrumb__item:last-of-type")).toContainText(
     "HPV campaign at St Andrew's Benn CofE (Voluntary Aided) Primary School"
   );
 }
 
-async function then_i_should_see_patients_with_their_triage_info(page) {
+async function then_i_should_see_patients_with_their_triage_info() {
   for (let name in patients) {
     let patient = patients[name];
 
     await expect(
-      page.locator(`#patients tr:nth-child(${patient.row}) td:first-child`),
+      p.locator(`#patients tr:nth-child(${patient.row}) td:first-child`),
       `Name for patient row: ${patient.row} name: ${name}`
     ).toContainText(name);
 
     if (patient.note) {
       await expect(
-        page.locator(`#patients tr:nth-child(${patient.row}) td:nth-child(2)`),
+        p.locator(`#patients tr:nth-child(${patient.row}) td:nth-child(2)`),
         `Note for patient row: ${patient.row} name: ${name}`
       ).toContainText(patient.note);
     } else {
       await expect(
-        page.locator(`#patients tr:nth-child(${patient.row}) td:nth-child(2)`),
+        p.locator(`#patients tr:nth-child(${patient.row}) td:nth-child(2)`),
         `Empty note patient row: ${patient.row} name: ${name}`
       ).toBeEmpty();
     }
     await expect(
-      page.locator(`#patients tr:nth-child(${patient.row}) td:nth-child(3)`),
+      p.locator(`#patients tr:nth-child(${patient.row}) td:nth-child(3)`),
       `Status text for patient row: ${patient.row} name: ${name}`
     ).toContainText(patient.status);
 
     await expect(
-      page.locator(
-        `#patients tr:nth-child(${patient.row}) td:nth-child(3) div`
-      ),
+      p.locator(`#patients tr:nth-child(${patient.row}) td:nth-child(3) div`),
       `Status colour for patient row: ${patient.row} name: ${name}`
     ).toHaveClass(new RegExp(patient.class));
 
     if (patient.icon) {
       await expect(
-        page.locator(
+        p.locator(
           `#patients tr:nth-child(${patient.row}) td:nth-child(3) div svg`
         ),
         `Status icon patient row: ${patient.row} name: ${name}`
       ).toHaveClass(new RegExp(patient.icon));
     } else {
       expect(
-        await page
+        await p
           .locator(
             `#patients tr:nth-child(${patient.row}) td:nth-child(3) div svg`
           )
@@ -171,13 +189,13 @@ async function then_i_should_see_patients_with_their_triage_info(page) {
   }
 }
 
-async function then_i_should_see_the_triage_page_for_the_patient(page, name) {
+async function then_i_should_see_the_triage_page_for_the_patient(name) {
   let patient = patients[name];
-  await expect(page.locator("h1")).toContainText(name);
+  await expect(p.locator("h1")).toContainText(name);
 
   let consentResponse = patient["consent_response"];
 
-  await expect(page.locator("#consent")).toContainText(consentResponse);
+  await expect(p.locator("#consent")).toContainText(consentResponse);
 
   if (consentResponse == "No response given") return;
 
@@ -189,18 +207,18 @@ async function then_i_should_see_the_triage_page_for_the_patient(page, name) {
   }
 
   if (parentRelationship) consentResponse += " " + parentRelationship;
-  await expect(page.locator("#consent")).toContainText(
+  await expect(p.locator("#consent")).toContainText(
     parentRelationship + " " + patient["parent_name"]
   );
 
   if (patient["type_of_consent"]) {
-    await expect(page.locator("#consent")).toContainText(
+    await expect(p.locator("#consent")).toContainText(
       "Type of consent " + patient["type_of_consent"]
     );
   }
 
   if (patient["reason_for_refusal"]) {
-    await expect(page.locator("#consent")).toContainText(
+    await expect(p.locator("#consent")).toContainText(
       "Reason for refusal " + patient["reason_for_refusal"]
     );
   }
