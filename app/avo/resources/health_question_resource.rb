85cec330cd910397f4e30b9ea3afb3671c43a588
Misha Gorodnitzky <misaka@pobox.com>
2023-06-28 12:51:53 +0100

    Add health_questions

---
+++ app/avo/resources/health_question_resource.rb
@@ -0,0 +1,11 @@
+class HealthQuestionResource < Avo::BaseResource
+  self.title = :id
+  self.includes = []
+  # self.search_query = -> do
+  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  # end
+
+  field :id, as: :id
+  # add fields here
+  field :vaccine, as: :belongs_to
+end
