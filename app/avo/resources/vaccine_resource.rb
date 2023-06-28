8bbe376492ee0ad5db414b6eda56db6e75799a3a
Misha Gorodnitzky <misaka@pobox.com>
2023-06-28 12:11:44 +0100

    Add vaccine table and model

---
+++ app/avo/resources/vaccine_resource.rb
@@ -0,0 +1,13 @@
+class VaccineResource < Avo::BaseResource
+  self.title = :id
+  self.includes = []
+  # self.search_query = -> do
+  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  # end
+
+  field :id, as: :id
+  # Fields generated from the model
+  field :name, as: :text
+  # add fields here
+  field :campaigns, as: :has_many
+end
