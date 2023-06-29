a1b44aa9158bb2732f1884081b486c18e4a777af
Misha Gorodnitzky <misaka@pobox.com>
2023-06-29 17:11:18 +0100

    Add missing annotations for consent_response spec

--- spec/models/consent_response_spec.rb
+++ spec/models/consent_response_spec.rb
@@ -1,3 +1,44 @@
+# == Schema Information
+#
+# Table name: consent_responses
+#
+#  id                          :bigint           not null, primary key
+#  address_line_1              :text
+#  address_line_2              :text
+#  address_postcode            :text
+#  address_town                :text
+#  childs_common_name          :text
+#  childs_dob                  :date
+#  childs_name                 :text
+#  consent                     :integer
+#  gp_name                     :text
+#  gp_response                 :integer
+#  health_questions            :jsonb
+#  parent_contact_method       :integer
+#  parent_contact_method_other :text
+#  parent_email                :text
+#  parent_name                 :text
+#  parent_phone                :text
+#  parent_relationship         :integer
+#  parent_relationship_other   :text
+#  reason_for_refusal          :integer
+#  reason_for_refusal_other    :text
+#  route                       :integer          not null
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
+#  campaign_id                 :bigint           not null
+#  patient_id                  :bigint           not null
+#
+# Indexes
+#
+#  index_consent_responses_on_campaign_id  (campaign_id)
+#  index_consent_responses_on_patient_id   (patient_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (campaign_id => campaigns.id)
+#  fk_rails_...  (patient_id => patients.id)
+#
 require "rails_helper"
 
 RSpec.describe ConsentResponse do
