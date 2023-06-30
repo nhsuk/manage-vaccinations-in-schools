9536608a6619638d9cd23d73e95d1ca8d88fb96f
Jake Benilov <benilov@gmail.com>
2023-06-30 15:51:35 +0100

    Standardise formatting of dates and times, make more Rails-idiomatic

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
-Date::DATE_FORMATS[:long_ordinal_uk] = "%-d %B %Y"
-Time::DATE_FORMATS[:long_ordinal_uk] = "%-d %B %Y"
+Date::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y"
+Date::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y"
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
-Time::DATE_FORMATS[:long_ordinal_uk] = "%d %B %Y at %l:%M%P"
-Time::DATE_FORMATS[:date_uk] = "%d %B %Y"
+Time::DATE_FORMATS[:long_ordinal_uk] = "-%d %B %Y at %l:%M%P"
+Time::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y"
+Time::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y"
