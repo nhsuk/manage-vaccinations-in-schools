0e9970b3c4bb4690985871dc8d7f5acb733c213d
Jake Benilov <benilov@gmail.com>
2023-06-30 15:02:07 +0100

    Remove all unused status tag code and styles

--- app/helpers/vaccinations_helper.rb
+++ app/helpers/vaccinations_helper.rb
@@ -1,19 +1,4 @@
 module VaccinationsHelper
-  def vaccination_status_colour(status)
-    case status
-    when :no_outcome
-      :white
-    when :vaccinated
-      :green
-    when :no_consent
-      :red
-    when :could_not_vaccinate
-      :orange
-    else
-      :white
-    end
-  end
-
   def vaccination_action_colour(action_or_outcome)
     case action_or_outcome
     when :get_consent
@@ -36,15 +21,6 @@ module VaccinationsHelper
       :white
     else
       :white
-    end
-  end
-
-  def vaccination_status_icon(status)
-    case status
-    when :no_consent, :could_not_vaccinate
-      "cross"
-    when :vaccinated
-      "tick"
     end
   end
 
