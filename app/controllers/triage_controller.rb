937a3f72f72e369485565ee8d91e960bb0a95781
Jake Benilov <benilov@gmail.com>
2023-06-30 13:14:20 +0100

    Display an empty state when there is nothing to triage.

--- app/controllers/triage_controller.rb
+++ app/controllers/triage_controller.rb
@@ -4,7 +4,7 @@ class TriageController < ApplicationController
   before_action :set_triage, only: %i[show]
   before_action :set_consent_response, only: %i[show]
 
-  layout "two_thirds", except: %i[index]
+  layout "two_thirds"
 
   def index
     @session = Session.find_by(id: params[:id])
