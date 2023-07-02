9196297a924a11a4ec02afe80029329d970e113d
Theodor Vararu <theo@vararu.org>
2023-07-02 13:21:56 +0100

    Change recorded_at to datetime in VaccinationRecord
    We need the time of the recording, not just the date.

---
+++ db/migrate/20230702121826_change_date_to_date_time_in_vaccination_record.rb
@@ -0,0 +1,9 @@
+class ChangeDateToDateTimeInVaccinationRecord < ActiveRecord::Migration[7.0]
+  def up
+    change_column :vaccination_records, :recorded_at, :datetime
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
