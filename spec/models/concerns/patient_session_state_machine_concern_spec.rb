b5643121aba697fc18e125aaddc3d50cb949032d
Misha Gorodnitzky <misaka@pobox.com>
2023-07-10 12:34:32 +0100

    Refactor patient sessions state names
    State names should reflect the condition or state the record is in. For
    example, "Awaiting triage" talks about the action that will be taken on
    the record, not about what state it's actually in, which is that a
    consent response has been received. Displaying a record that has consent
    as needing triage should be a part of the logic of how we display this
    state to the user.

--- spec/models/concerns/patient_session_state_machine_concern_spec.rb
+++ spec/models/concerns/patient_session_state_machine_concern_spec.rb
@@ -33,21 +33,21 @@ RSpec.describe PatientSessionStateMachineConcern do
 
   let(:state) { nil }
 
-  it "starts in the awaiting_consent state" do
-    expect(fsm).to be_awaiting_consent_response
+  it "starts in the added_to_session state" do
+    expect(fsm).to be_added_to_session
   end
 
-  context "in awaiting_consent_response state" do
-    let(:state) { :awaiting_consent_response }
+  context "in added_to_session state" do
+    let(:state) { :added_to_session }
 
     describe "#received_consent_response" do
-      it "transitions to awaiting_triage when consent is refused" do
+      it "transitions to consent_response_received when consent is refused" do
         allow(consent_response).to receive(:consent_given?).and_return(false)
         allow(consent_response).to receive(:consent_refused?).and_return(true)
         allow(consent_response).to receive(:triage_needed?).and_return(false)
 
         fsm.received_consent_response
-        expect(fsm).to be_awaiting_triage
+        expect(fsm).to be_consent_response_received
       end
 
       it "transitions to ready_to_vaccinate when consent is given and needs no triage" do
@@ -59,19 +59,19 @@ RSpec.describe PatientSessionStateMachineConcern do
         expect(fsm).to be_ready_to_vaccinate
       end
 
-      it "transitions to awaiting_triage when consent is given and needs triage" do
+      it "transitions to consent_response_received when consent is given and needs triage" do
         allow(consent_response).to receive(:consent_given?).and_return(true)
         allow(consent_response).to receive(:consent_refused?).and_return(false)
         allow(consent_response).to receive(:triage_needed?).and_return(true)
 
         fsm.received_consent_response
-        expect(fsm).to be_awaiting_triage
+        expect(fsm).to be_consent_response_received
       end
     end
   end
 
-  context "in awaiting_triage state" do
-    let(:state) { :awaiting_triage }
+  context "in consent_response_received state" do
+    let(:state) { :consent_response_received }
 
     describe "#triaged" do
       it "transitions to ready_to_vaccinate when triage is ready to vaccinate" do
@@ -83,13 +83,13 @@ RSpec.describe PatientSessionStateMachineConcern do
         expect(fsm).to be_ready_to_vaccinate
       end
 
-      it "transitions to not_vaccinated when triage is do_not_vaccinate" do
+      it "transitions to unable_to_vaccinate when triage is do_not_vaccinate" do
         allow(triage).to receive(:ready_to_vaccinate?).and_return(false)
         allow(triage).to receive(:do_not_vaccinate?).and_return(true)
         allow(triage).to receive(:needs_follow_up?).and_return(false)
 
         fsm.triaged
-        expect(fsm).to be_not_vaccinated
+        expect(fsm).to be_unable_to_vaccinate
       end
     end
   end
@@ -108,12 +108,12 @@ RSpec.describe PatientSessionStateMachineConcern do
     end
 
     describe "#did_not_vaccinate" do
-      it "transitions to not_vaccinated when vaccination_record is not administered" do
+      it "transitions to unable_to_vaccinate when vaccination_record is not administered" do
         allow(vaccination_record).to receive(:administered?).and_return(false)
         allow(vaccination_record).to receive(:administered).and_return(false)
 
         fsm.did_not_vaccinate
-        expect(fsm).to be_not_vaccinated
+        expect(fsm).to be_unable_to_vaccinate
       end
     end
   end
