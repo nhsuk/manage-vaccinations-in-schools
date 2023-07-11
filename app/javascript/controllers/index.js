df8e8cf36b417ef4749a197f62cf3261e7b36e96
Theodor Vararu <theo@vararu.org>
2023-07-11 18:46:06 +0100

    Replace GOVUK Frontend initAll with stimulus shims
    initAll is an easy way to initialise all GOVUK Frontend elements on the
    page with their custom JS. However, it doesn't allow initialisation of
    individual modules, and doesn't work well with Rails Turbo, because the
    elements are only initialised once and not on every page navigation.
    
    The main reason to do this refactoring is to allow us to replace the JS
    for GOVUK Radios with a shim that's cross-compatible with the NHSUK
    Radios.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,3 +3,39 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application";
+
+import GovukAccordionController from "./govuk_accordion_controller";
+application.register("govuk-accordion", GovukAccordionController);
+
+import GovukButtonController from "./govuk_button_controller";
+application.register("govuk-button", GovukButtonController);
+
+import GovukCharacterCountController from "./govuk_character_count_controller";
+application.register("govuk-character-count", GovukCharacterCountController);
+
+import GovukCheckboxesController from "./govuk_checkboxes_controller";
+application.register("govuk-checkboxes", GovukCheckboxesController);
+
+import GovukDetailsController from "./govuk_details_controller";
+application.register("govuk-details", GovukDetailsController);
+
+import GovukErrorSummaryController from "./govuk_error_summary_controller";
+application.register("govuk-error-summary", GovukErrorSummaryController);
+
+import GovukHeaderController from "./govuk_header_controller";
+application.register("govuk-header", GovukHeaderController);
+
+import GovukNotificationBannerController from "./govuk_notification_banner_controller";
+application.register(
+  "govuk-notification-banner",
+  GovukNotificationBannerController,
+);
+
+import GovukRadiosController from "./govuk_radios_controller";
+application.register("govuk-radios", GovukRadiosController);
+
+import GovukSkipLinkController from "./govuk_skip_link_controller";
+application.register("govuk-skip-link", GovukSkipLinkController);
+
+import GovukTabsController from "./govuk_tabs_controller";
+application.register("govuk-tabs", GovukTabsController);
