62272286b71a9c6dda44d7257b2a9edefabdf0e0
Misha Gorodnitzky <misaka@pobox.com>
2023-07-13 09:55:40 +0100

    Update triage tests to use tabs and new data

--- tests/triage.spec.ts
+++ tests/triage.spec.ts
@@ -4,96 +4,57 @@ import { example_patient } from "./example_data";
 let p = null;
 
 const patients = {
-  "Aaron Pfeffer": {
-    row: 1,
-    note: "",
-    tab: "Triage complete",
-    status: "Vaccinate",
-    status_colour: "purple",
-    consent_response: "Given by",
-    parent_name: "Betty Pfeffer",
-    parent_email: "Betty_Pfeffer62@yahoo.com",
-    parent_relationship: "Father",
-    type_of_consent: "Paper",
-  },
-  "Alaia Lakin": {
-    row: 2,
-    note: "",
-    tab: "Triage complete",
-    status: "Vaccinate",
-    status_colour: "purple",
-    consent_response: "Given by",
-    parent_name: "Garret Lakin",
-    parent_email: "Garret57@hotmail.com",
-    parent_relationship: "Grandmother",
-    type_of_consent: "Website",
-  },
-  "Aliza Kshlerin": {
-    row: 3,
-    note: "Notes from nurse",
-    tab: "Triage complete",
-    status: "Vaccinate",
-    status_colour: "purple",
-    consent_response: "Given by",
-    parent_name: "Georgianna Kshlerin",
-    parent_email: "Georgianna.Kshlerin0@hotmail.com",
-    parent_relationship: "Mother",
-    type_of_consent: "Phone",
-  },
-  "Amara Rodriguez": {
-    row: 6,
-    note: "",
+  "Blaine DuBuque": {
     tab: "Needs triage",
-    status: "Triage: follow up",
-    status_colour: "aqua-green",
-    banner_title: "Triage follow-up needed",
-    consent_response: "Given by",
-    parent_relationship: "Father",
-    parent_name: "Lloyd Rodriguez",
-    parent_email: "Lloyd.Rodriguez61@yahoo.com",
-    type_of_consent: "Self consent",
-  },
-  "Amaya Sauer": {
-    row: 7,
-    note: "",
-    tab: "Triage complete",
-    status: "Get consent",
-    status_colour: "yellow",
-    banner_title: "No-one responded to our requests for consent",
-    consent_response: "No response given",
-  },
-  "Annabel Morar": {
-    row: 8,
-    note: "",
-    tab: "No triage needed",
-    status: "Check refusal",
-    status_colour: "orange",
-    banner_title: "Their father has refused to give consent",
-    consent_response: "Refused by",
-    reason_for_refusal: "Personal choice",
-    parent_relationship: "Father",
-    parent_name: "Meggie Morar",
-    parent_email: "Meggie19@hotmail.com",
-    type_of_consent: "Website",
-  },
-  "Archie Simonis": {
-    row: 9,
-    note: "",
-    tab: "Needs triage",
-    triage_reasons: ["Notes need triage"],
-    status: "Triage",
-    status_colour: "blue",
+    action: "Triage",
+    action_colour: "blue",
     banner_title: "Triage needed",
-    banner_content: ["Notes need triage"],
+    triage_reasons: ["Notes need triage"],
     consent_response: "Given by",
     parent_relationship: "Mother",
-    parent_name: "Erika Simonis",
-    parent_email: "Erika54@hotmail.com",
-    type_of_consent: "Website",
+  },
+  "Caridad Sipes": {
+    tab: "Needs triage",
+    action: "Triage: follow up",
+    action_colour: "aqua-green",
+    banner_title: "Triage follow-up needed",
+    triage_reasons: ["Notes need triage"],
+  },
+  "Jessika Lindgren": {
+    tab: "Triage complete",
+    action: "Vaccinate",
+    action_colour: "purple",
+  },
+  "Kristal Schumm": {
+    tab: "Triage complete",
+    action: "Do not vaccinate",
+    action_colour: "red",
+    banner_title: "Do not vaccinate",
+  },
+  "Alexandra Sipes": {
+    tab: "Get consent",
+    action: "Get consent",
+    action_colour: "yellow",
+    banner_title: "No-one responded to our requests for consent",
+  },
+  "Ernie Funk": {
+    tab: "No triage needed",
+    action: /.*/,
+    action_colour: "purple",
+  },
+  "Fae Skiles": {
+    tab: "No triage needed",
+    action: "Check refusal",
+    action_colour: "orange",
+  },
+  "Man Swaniawski": {
+    tab: "No triage needed",
+    action: "Vaccinate",
+    action_colour: "purple",
   },
 };
 
-test.skip("Performing triage", async ({ page }) => {
+test("Performing triage", async ({ page }) => {
   p = page;
 
   await given_the_app_is_setup();
@@ -107,6 +68,7 @@ test.skip("Performing triage", async ({ page }) => {
 
     await when_i_click_on_the_tab(patients[name].tab);
     await then_i_should_see_a_triage_row_for_the_patient(name);
+
     await when_i_click_on_the_patient(name);
     await then_i_should_see_the_triage_page_for_the_patient(name);
     await and_i_should_see_a_banner_for_the_patient(name);
@@ -117,25 +79,25 @@ test.skip("Performing triage", async ({ page }) => {
     await then_i_should_see_the_triage_index_page();
   }
 
-  await when_i_click_on_the_patient("Aaron Pfeffer");
-  await when_i_enter_the_note("Notes from nurse");
-  await when_i_click_on_the_option("Do not vaccinate");
-  await when_i_click_on_the_submit_button();
-  await then_i_should_see_a_triage_row_for_the_patient("Aaron Pfeffer", {
-    note: "Notes from nurse",
-    status: "Do not vaccinate",
-    status_colour: "red",
-  });
+  // await when_i_click_on_the_patient("Aaron Pfeffer");
+  // await when_i_enter_the_note("Notes from nurse");
+  // await when_i_click_on_the_option("Do not vaccinate");
+  // await when_i_click_on_the_submit_button();
+  // await then_i_should_see_a_triage_row_for_the_patient("Aaron Pfeffer", {
+  //   note: "Notes from nurse",
+  //   status: "Do not vaccinate",
+  //   status_colour: "red",
+  // });
 
-  await when_i_click_on_the_patient("Aaron Pfeffer");
-  await when_i_clear_the_note();
-  await when_i_click_on_the_option("Ready to vaccinate");
-  await when_i_click_on_the_submit_button();
-  await then_i_should_see_a_triage_row_for_the_patient("Aaron Pfeffer", {
-    note: null,
-    status: "Vaccinate",
-    status_colour: "purple",
-  });
+  // await when_i_click_on_the_patient("Aaron Pfeffer");
+  // await when_i_clear_the_note();
+  // await when_i_click_on_the_option("Ready to vaccinate");
+  // await when_i_click_on_the_submit_button();
+  // await then_i_should_see_a_triage_row_for_the_patient("Aaron Pfeffer", {
+  //   note: null,
+  //   status: "Vaccinate",
+  //   status_colour: "purple",
+  // });
 });
 
 async function given_the_app_is_setup() {
@@ -181,130 +143,159 @@ async function then_i_should_see_the_triage_index_page() {
 
 async function then_i_should_see_the_correct_breadcrumbs() {
   await expect(p.locator(".nhsuk-breadcrumb__item:last-of-type")).toContainText(
-    "HPV session at St Andrew's Benn CofE (Voluntary Aided) Primary School",
+    "HPV session at St Andrew's Benn CofE (Voluntary Aided) Primary School"
   );
 }
 
 async function then_i_should_be_on_the_tab(name: string) {
   await expect(p.getByRole("tab", { name: name, exact: true })).toHaveAttribute(
     "aria-selected",
-    "true",
+    "true"
   );
 }
 
 async function then_i_should_see_a_triage_row_for_the_patient(
   name,
-  attributes = {},
+  attributes = {}
 ) {
-  let patient = { ...patients[name], ...attributes };
-
-  let id = patient.tab.toLowerCase().replace(" ", "-");
-  await expect(
-    p.locator(`#${id} tr:nth-child(${patient.row}) td:first-child`),
-    `Name for patient row: ${patient.row} name: ${name}`,
-  ).toContainText(name);
+  const patient = { ...patients[name], ...attributes };
+  const id = patient.tab.toLowerCase().replace(/ /g, "-");
+  const row = p.locator(`#${id} tr`, { hasText: name });
 
   if (patient.triage_reasons) {
     for (let reason of patient.triage_reasons) {
-      await expect(
-        p.locator(`#${id} tr:nth-child(${patient.row}) td:nth-child(2)`),
-        `Triage reason for patient row: ${patient.row} name: ${name}`,
-      ).toContainText(reason);
+      await expect(row, `[${name}] Triage reason`).toContainText(reason);
     }
   }
 
   await expect(
-    p.locator(`#${id} tr:nth-child(${patient.row}) td:nth-child(3)`),
-    `Status text for patient row: ${patient.row} name: ${name}`,
-  ).toContainText(patient.status);
+    row,
+    `[${name}] Status text should be ${patient.action}`
+  ).toContainText(patient.action);
 
-  let colourClass = "nhsuk-tag--" + patient.status_colour;
+  const colourClass = "nhsuk-tag--" + patient.action_colour;
   await expect(
-    p.locator(`#${id} tr:nth-child(${patient.row}) td:nth-child(3) div`),
-    `Status colour for patient row: ${patient.row} name: ${name}`,
+    // p.locator(`#${id} tr:nth-child(${patient.row}) td:nth-child(3) div`),
+    row.locator("td:nth-child(3) div"),
+    `[${name}] Status colour`
   ).toHaveClass(new RegExp(colourClass));
 }
 
 async function then_i_should_see_the_triage_page_for_the_patient(name) {
-  let patient = patients[name];
-  await expect(p.locator("h1")).toContainText(name);
+  await expect(p.locator("h1"), `[${name}] Triage page title`).toContainText(
+    name
+  );
 }
 
 async function and_i_should_see_a_banner_for_the_patient(name) {
-  let patient = patients[name];
-  let title = patient["banner_title"];
-  let colourClass = "app-consent-banner--" + patient["status_colour"];
-  let content = patient["banner_content"];
+  const patient = patients[name];
+  const title = patient["banner_title"];
+  const colourClass = "app-consent-banner--" + patient["action_colour"];
+  const content = patient["banner_content"];
 
   if (title == null) return;
 
-  await expect(p.locator(".app-consent-banner > span")).toHaveText(title);
-  await expect(p.locator("div.app-consent-banner")).toHaveClass(
-    new RegExp(colourClass),
-  );
+  await expect(
+    p.locator(".app-consent-banner > span"),
+    `[${name}] Banner title`
+  ).toHaveText(title);
+  await expect(
+    p.locator("div.app-consent-banner"),
+    `[${name}] Banner colour`
+  ).toHaveClass(new RegExp(colourClass));
 
   if (content != null)
     for (let text of content) await expect(p.getByText(text)).toBeVisible();
 }
 
 async function and_i_should_see_the_consent_section_for_the_patient(name) {
-  let patient = patients[name];
-  let consentResponse = patient["consent_response"];
+  const patient = patients[name];
+  const consentResponse = example_patient(name).consent;
 
-  await expect(p.locator("#consent")).toContainText(consentResponse);
-
-  if (consentResponse == "No response given") return;
+  if (!consentResponse) {
+    await expect(
+      p.locator("#consent"),
+      `[${name}] Consent response`
+    ).toContainText("No response given");
+    return;
+  }
 
   let parentRelationship;
-  if (patient["parent_relationship"]) {
-    if (patient["parent_relationship"] == "Other")
-      parentRelationship = patient["parent_relationship_other"];
-    else parentRelationship = patient["parent_relationship"];
-  }
+  if (consentResponse.parentRelationship == "other")
+    parentRelationship = consentResponse.parentRelationshipOther;
+  else if (consentResponse.parentRelationship)
+    parentRelationship = consentResponse.parentRelationship;
+  parentRelationship = capitalise(parentRelationship);
 
-  if (parentRelationship) consentResponse += " " + parentRelationship;
-  await expect(p.locator("#consent")).toContainText(
-    parentRelationship + " " + patient["parent_name"],
+  await expect(
+    p.locator("#consent"),
+    `[${name}] Consent response`
+  ).toContainText(
+    capitalise(`${consentResponse.consent} by ${parentRelationship}`)
   );
 
-  if (patient["type_of_consent"]) {
-    await expect(p.locator("#consent")).toContainText(
-      "Type of consent " + patient["type_of_consent"],
-    );
-  }
+  await expect(
+    p.locator("#consent"),
+    `[${name}] Consent parent relationship`
+  ).toContainText(`${parentRelationship} ${consentResponse.parentName}`);
 
-  if (patient["reason_for_refusal"]) {
-    await expect(p.locator("#consent")).toContainText(
-      "Reason for refusal " + patient["reason_for_refusal"],
+  const route = capitalise(consentResponse.route);
+  await expect(
+    p.locator("#consent", `[${name}] Consent type should be be: ${route}`)
+  ).toContainText(`Type of consent ${route}`);
+
+  if (consentResponse.consent == "refused")
+    await expect(
+      p.locator("#consent"),
+      `[${name}] Reason for refusal`
+    ).toContainText(
+      `Reason for refusal ${humanise(consentResponse.reasonForRefusal)}`
     );
-  }
 }
 
 async function and_i_should_see_health_question_responses_if_present(name) {
-  let patient = example_patient(name);
-  let consent = patient["consent"];
+  let consent = example_patient(name).consent;
 
-  if (consent && consent["healthQuestionResponses"]) {
+  if (
+    consent &&
+    consent.healthQuestionResponses &&
+    consent.healthQuestionResponses.length > 0
+  ) {
     await expect(
       p.getByRole("heading", { name: "Health questions" }),
+      `[${name}] Health questions heading`
     ).toBeVisible();
 
     for (const example_question of consent["healthQuestionResponses"]) {
       await expect(
         p.locator("h3:text('" + example_question.question + "')"),
+        `[${name}] Health question`
       ).toContainText(example_question.question);
 
       if (example_question["response"].toLowerCase() == "yes") {
         await expect(
           p.locator("h3:text('" + example_question.question + "') + p"),
+          `[${name}] Health question response`
         ).toContainText("Yes – " + example_question.notes);
       } else {
         await expect(
           p.locator("h3:text('" + example_question.question + "') + p"),
+          `[${name}] Health question response`
         ).toContainText("No");
       }
     }
   } else {
-    expect(await p.textContent("body")).not.toContain("Health questions");
+    expect(
+      await p.textContent("body"),
+      `[${name}] No health questions`
+    ).not.toContain("Health questions");
   }
+}
+
+function humanise(str: string) {
+  return capitalise(str.replace(/_/g, " "));
+}
+
+function capitalise(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
