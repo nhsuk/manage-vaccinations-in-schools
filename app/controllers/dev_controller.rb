661f9b9cfbfd594e0913a96b5cb8e15c616f50ca
Misha Gorodnitzky <misaka@pobox.com>
2023-07-13 18:44:22 +0100

    Ensure rake tasks are always executed on /reset
    When using "invoke" on rake tasks (from inside Ruby), they will only be run
    once (it was originally a build tool after all). To ensure we always run
    the given tasks we need to use "execute".

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -4,9 +4,9 @@ Rails.application.load_tasks
 
 class DevController < ApplicationController
   def reset
-    Rake::Task["db:schema:load"].invoke
-    Rake::Task["load_campaign_example"].invoke(
-      "db/sample_data/example-test-campaign.json"
+    Rake::Task["db:schema:load"].execute
+    Rake::Task["load_campaign_example"].execute(
+      example_file: "db/sample_data/example-test-campaign.json"
     )
     redirect_to root_path
   end
