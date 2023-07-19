dd26e5ff48e74f7f26741374d1404df2c895daf4
Theodor Vararu <theo@vararu.org>
2023-07-19 16:17:49 +0100

    Add record action to consent controller
    This takes the latest draft consent, sets the recorded_at timestamp,
    moves the state machine forward, and redirects the user back to the
    triage index.

--- app/controllers/consent_responses_controller.rb
+++ app/controllers/consent_responses_controller.rb
@@ -1,14 +1,12 @@
 class ConsentResponsesController < ApplicationController
   before_action :set_session
   before_action :set_patient
+  before_action :set_patient_session
+  before_action :set_draft_consent_response
 
   layout "two_thirds"
 
   def confirm
-    @draft_consent_response = @patient
-      .consent_responses
-      .find_or_initialize_by(recorded_at: nil)
-
     @draft_consent_response.update!(
       campaign: @session.campaign,
       parent_name: "Test Parent",
@@ -30,6 +28,22 @@ class ConsentResponsesController < ApplicationController
     )
   end
 
+  def record
+    @draft_consent_response.update!(recorded_at: Time.zone.now)
+    @patient_session.do_consent!
+
+    redirect_to triage_session_path(@session),
+                flash: {
+                  success: {
+                    title: "Consent saved for #{@patient.full_name}",
+                    body: ActionController::Base.helpers.link_to(
+                      "View child record",
+                      session_patient_triage_path(@session, @patient)
+                    ),
+                  },
+                }
+  end
+
   private
 
   def set_session
@@ -38,5 +52,15 @@ class ConsentResponsesController < ApplicationController
 
   def set_patient
     @patient = @session.patients.find_by(id: params[:id])
+  end
+
+  def set_patient_session
+    @patient_session = @patient.patient_sessions.find_by(session: @session)
+  end
+
+  def set_draft_consent_response
+    @draft_consent_response = @patient
+      .consent_responses
+      .find_or_initialize_by(recorded_at: nil)
   end
 end
