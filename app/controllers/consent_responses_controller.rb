e0ed9006dc64030ebef6994ac7fd4a14ec236b3f
Theodor Vararu <theo@vararu.org>
2023-07-19 15:30:20 +0100

    Render Consent card in consent#confirm page
    This is different from AppConsentCardComponent, so we can't use that
    component.

--- app/controllers/consent_responses_controller.rb
+++ app/controllers/consent_responses_controller.rb
@@ -2,7 +2,32 @@ class ConsentResponsesController < ApplicationController
   before_action :set_session
   before_action :set_patient
 
+  layout "two_thirds"
+
   def confirm
+    @draft_consent_response = @patient
+      .consent_responses
+      .find_or_initialize_by(recorded_at: nil)
+
+    @draft_consent_response.update!(
+      campaign: @session.campaign,
+      parent_name: "Test Parent",
+      parent_phone: "07412 345678",
+      parent_email: "test.parent@example.com",
+      parent_relationship: "mother",
+      consent: "given",
+      route: "website",
+      health_questions: [
+        {"question"=>"Does the child have any severe allergies that have led to an anaphylactic reaction?",
+         "response"=>"no"},
+        {"question"=>"Does the child have any existing medical conditions?",
+         "response"=>"no"},
+        {"question"=>"Does the child take any regular medication?",
+         "response"=>"no"},
+        {"question"=>"Is there anything else we should know?",
+         "response"=>"no"}
+      ],
+    )
   end
 
   private
