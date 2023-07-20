98d5ba116ba09cf5435374bf5edf31fa10ec3b64
Misha Gorodnitzky <misaka@pobox.com>
2023-07-20 15:18:46 +0100

    Add status banner specs

---
+++ spec/factories/patient_sessions.rb
@@ -0,0 +1,74 @@
+# == Schema Information
+#
+# Table name: patient_sessions
+#
+#  id         :bigint           not null, primary key
+#  state      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  patient_id :bigint           not null
+#  session_id :bigint           not null
+#
+# Indexes
+#
+#  index_patient_sessions_on_patient_id_and_session_id  (patient_id,session_id) UNIQUE
+#  index_patient_sessions_on_session_id_and_patient_id  (session_id,patient_id) UNIQUE
+#
+FactoryBot.define do
+  factory :patient_session do
+    patient { create :patient }
+    session { create :session }
+
+    trait :added_to_session do
+      state { "added_to_session" }
+    end
+
+    trait :consent_given_triage_not_needed do
+      state { "consent_given_triage_not_needed" }
+      patient do
+        create :patient, :consent_given_triage_not_needed, session:
+      end
+    end
+
+    trait :consent_given_triage_needed do
+      state { "consent_given_triage_needed" }
+      patient { create :patient, :consent_given_triage_needed, session: }
+    end
+
+    trait :consent_refused do
+      state { "consent_refused" }
+      patient { create :patient, :consent_refused, session: }
+    end
+
+    trait :triaged_ready_to_vaccinate do
+      state { "triaged_ready_to_vaccinate" }
+      patient { create :patient, :triaged_ready_to_vaccinate, session: }
+    end
+
+    trait :triaged_do_not_vaccinate do
+      state { "triaged_do_not_vaccinate" }
+      patient { create :patient, :triaged_do_not_vaccinate, session: }
+    end
+
+    trait :triaged_kept_in_triage do
+      state { "triaged_kept_in_triage" }
+      patient { create :patient, :triaged_kept_in_triage, session: }
+    end
+
+    trait :unable_to_vaccinate do
+      state { "unable_to_vaccinate" }
+      patient { create :patient, :triaged_ready_to_vaccinate, session: }
+
+      after :create do |patient_session|
+        create :vaccination_record,
+               reason: :contraindications,
+               administered: false,
+               patient_session:
+      end
+    end
+
+    trait :vaccinated do
+      state { "vaccinated" }
+    end
+  end
+end
--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -21,7 +21,14 @@
 #
 FactoryBot.define do
   factory :patient do
-    nhs_number { rand(10**10) }
+    transient do
+      # Used for associations like consent_response and triage that need to be
+      # associated with a campaign
+      session { create :session }
+      campaign { session.campaign }
+    end
+
+    nhs_number { rand(10 ** 10) }
     sex { %w[Male Female].sample }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
@@ -29,9 +36,44 @@ FactoryBot.define do
     consent { "Parental consent (digital)" }
     seen { "Not yet" }
     dob { Faker::Date.birthday(min_age: 3, max_age: 9) }
+    patient_sessions { [] }
 
     trait :of_hpv_vaccination_age do
       dob { Faker::Date.birthday(min_age: 12, max_age: 13) }
+    end
+
+    trait :consent_given_triage_not_needed do
+      consent_responses do
+        [create(:consent_response, :given, campaign:)]
+      end
+    end
+
+    trait :consent_given_triage_needed do
+      consent_responses do
+        [create(:consent_response, :given, :health_question_notes, campaign:)]
+      end
+    end
+
+    trait :consent_refused do
+      consent_responses { [create(:consent_response, :refused, :from_mum, campaign:)] }
+    end
+
+    trait :triaged_ready_to_vaccinate do
+      consent_given_triage_needed
+
+      triage { [create(:triage, campaign:)] }
+    end
+
+    trait :triaged_do_not_vaccinate do
+      consent_given_triage_needed
+
+      triage { [create(:triage, status: :do_not_vaccinate, campaign: session.campaign)] }
+    end
+
+    trait :triaged_kept_in_triage do
+      consent_given_triage_needed
+
+      triage { [create(:triage, status: :needs_follow_up, campaign: session.campaign)] }
     end
   end
 end
