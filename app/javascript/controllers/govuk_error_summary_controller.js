879dcb490150bf40d4820eaf63683678b98e9b3f
Theodor Vararu <theo@vararu.org>
2023-07-24 18:12:13 +0100

    Fix nhsuk-error-summary
    The summary isn't getting focused because the GOVUK Error Summary JS
    doesn't match the NHSUK Frontend HTML.

--- app/javascript/controllers/govuk_error_summary_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-import { ErrorSummary } from "govuk-frontend";
-
-// Connects to data-module="govuk-error-summary"
-export default class extends Controller {
-  connect() {
-    new ErrorSummary(this.element).init();
-  }
-}
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -19,9 +19,6 @@ application.register("govuk-checkboxes", GovukCheckboxesController);
 import GovukDetailsController from "./govuk_details_controller";
 application.register("govuk-details", GovukDetailsController);
 
-import GovukErrorSummaryController from "./govuk_error_summary_controller";
-application.register("govuk-error-summary", GovukErrorSummaryController);
-
 import GovukHeaderController from "./govuk_header_controller";
 application.register("govuk-header", GovukHeaderController);
 
@@ -33,6 +30,9 @@ application.register(
 
 import GovukSkipLinkController from "./govuk_skip_link_controller";
 application.register("govuk-skip-link", GovukSkipLinkController);
+
+import NhsukErrorSummaryController from "./nhsuk_error_summary_controller";
+application.register("nhsuk-error-summary", NhsukErrorSummaryController);
 
 import NhsukRadiosController from "./nhsuk_radios_controller";
 application.register("nhsuk-radios", NhsukRadiosController);
