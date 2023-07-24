de1589b8d3405f9bf67f753f0111157d2a1e9007
Misha Gorodnitzky <misaka@pobox.com>
2023-07-24 16:53:58 +0100

    Add tests for parent info on patient card

--- spec/components/app_patient_card_component_spec.rb
+++ spec/components/app_patient_card_component_spec.rb
@@ -35,5 +35,45 @@ RSpec.describe AppPatientCardComponent, type: :component do
     )
   end
 
-  it "should"
+  describe "rendered parent info" do
+    it "should include the parent name" do
+      expect(page).to have_css(
+        '.nhsuk-summary-list__value[data-testid="parent-info"]',
+        text: patient.parent_name
+      )
+    end
+
+    it "should include the parent telephone number" do
+      expect(page).to have_css(
+        '.nhsuk-summary-list__value[data-testid="parent-info"]',
+        text: patient.parent_phone
+      )
+    end
+
+    it "should include the information source" do
+      expect(page).to have_css(
+        '.nhsuk-summary-list__value[data-testid="parent-info"]',
+        text: "(Info from #{patient.parent_info_source})"
+      )
+    end
+
+    context "when the parent relationship is 'other'" do
+      it "should include the parent relationship" do
+        expect(page).to have_css(
+          '.nhsuk-summary-list__value[data-testid="parent-info"]',
+          text: patient.parent_relationship_other
+        )
+      end
+    end
+
+    context "no parent info is provided" do
+      let(:patient) { FactoryBot.create(:patient, :no_parent_info) }
+
+      it "should not render the parent info" do
+        expect(page).not_to have_css(
+          '.nhsuk-summary-list__value[data-testid="parent-info"]'
+        )
+      end
+    end
+  end
 end
