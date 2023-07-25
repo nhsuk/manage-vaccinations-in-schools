a30d8a1ee584e99aa3b7c0e97e2b3a1f1b28b780
Theodor Vararu <theo@vararu.org>
2023-07-25 17:07:40 +0100

    Add Gillick competence card component

---
+++ app/components/app_gillick_card_component.rb
@@ -0,0 +1,9 @@
+class AppGillickCardComponent < ViewComponent::Base
+  def initialize(session:, patient:, consent_response:)
+    super
+
+    @session = session
+    @patient = patient
+    @consent_response = consent_response
+  end
+end
