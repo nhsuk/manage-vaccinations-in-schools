1f729f1baca8397b4a4267b0fbc4102c8544e16e
Misha Gorodnitzky <misaka@pobox.com>
2023-07-25 11:51:08 +0100

    Add #human_enum_name and move to concern
    Using #human_enum_name is much more concise than the class method,
    although both are handy. Moving these to a concern is tidier and
    facilitates testing (#human_enum_name was tricky to test on
    ApplicationRecord.

---
+++ app/models/concerns/human_enum_name_concern.rb
@@ -0,0 +1,21 @@
+module HumanEnumNameConcern
+  extend ActiveSupport::Concern
+
+  included do
+    def human_enum_name(attribute)
+      enum_name = attribute.to_s.pluralize
+      enum_value = self[attribute]
+      self.class.human_enum_name(enum_name, enum_value)
+    end
+  end
+
+  class_methods do
+    def human_enum_name(enum_name, enum_value)
+      enum_i18n_key = enum_name.to_s.pluralize
+      I18n.t(
+        "activerecord.attributes.#{model_name.i18n_key}.#{enum_i18n_key}.#{enum_value}",
+        default: ->(_key) { enum_value.to_s.humanize }
+      )
+    end
+  end
+end
---
+++ spec/models/concerns/human_enum_name_concern_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe HumanEnumNameConcern do
+  let(:dummy_class) do
+      Class.new do
+        include HumanEnumNameConcern
+
+        attr_reader :status
+
+        def initialize(status:)
+          @status = status
+        end
+
+        def self.model_name
+          ActiveModel::Name.new(self, nil, "Dummy")
+        end
+
+        def [](attr)
+          send(attr)
+        end
+      end
+    end
+
+  describe ".human_enum_name" do
+    subject { dummy_class.human_enum_name(enum_name, enum_value) }
+
+    let(:enum_name) { :status }
+    let(:enum_value) { :test_status }
+
+    context "when no translation is available" do
+      it { should eq "Test status" }
+    end
+
+    context "when a translation is available" do
+      before do
+        I18n.backend.store_translations(
+          :en,
+          activerecord: {
+            attributes: {
+              dummy: {
+                statuses: {
+                  test_status: "Test status"
+                }
+              }
+            }
+          }
+        )
+      end
+
+      it { should eq "Test status" }
+    end
+  end
+
+  describe "#human_enum_name" do
+    subject(:record) do
+      dummy_class.new(status: :test_status).human_enum_name(:status)
+    end
+
+   before do
+      allow(dummy_class).to receive(:human_enum_name).and_return("Test status")
+    end
+
+    # Cannot instantiate ActiveRecord
+   it { should eq "Test status" }
+  end
+
+end
