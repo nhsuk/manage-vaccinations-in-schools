7bb5a25c1218d0ce8b6845b06301a772609cab22
Theodor Vararu <theo@vararu.org>
2023-08-02 18:20:15 +0100

    Add intitializer for production checks
    We want to ensure that when we release the application, we don't forget
    to configure certain things, like Active Record Encryption. We can
    codify these to reduce the chance of this occuring in production mode.

---
+++ config/initializers/check_production.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  if credentials.dig(:active_record_encryption, :primary_key).blank?
+    raise "Application is running in production mode but Active Record
+    Encryption is not set up. Please set up Active Record Encryption. See
+    https://edgeguides.rubyonrails.org/active_record_encryption.html"
+  end
+
+  if ENV.fetch("SENTRY_DSN", "").blank?
+    raise "Application is running in production mode but SENTRY_DSN is not set
+    up. Please set up Sentry."
+  end
+end
