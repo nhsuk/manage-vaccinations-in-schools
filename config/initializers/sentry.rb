9f2bebb998657944e14c01f531053dde09c59950
Theodor Vararu <theo@vararu.org>
2023-08-02 11:14:02 +0100

    Filter sensitive env var values from Sentry logs

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,8 +5,30 @@ Sentry.init do |config|
 
   config.traces_sample_rate = 1.0
 
-  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+  rails_filter_parameters =
+    Rails.application.config.filter_parameters.map(&:to_s)
+
+  sensitive_env_vars = ENV.select do |key, _v|
+    rails_filter_parameters.any? do |parameter|
+      key.downcase.include?(parameter)
+    end
+  end
+
+  sensitive_values_pattern = Regexp.union(sensitive_env_vars.values)
+
+  sensitive_value_filter = ->(key, value) do
+    key_is_relevant = %i[value title].include?(key)
+
+    if key_is_relevant && sensitive_values_pattern.match?(value)
+      value.gsub!(sensitive_values_pattern, "[FILTERED]")
+    end
+  end
+
+  combined_filter = ActiveSupport::ParameterFilter.new(
+    Rails.application.config.filter_parameters + [sensitive_value_filter],
+  )
+
   config.before_send = lambda do |event, _hint|
-    filter.filter(event.to_hash)
+    combined_filter.filter(event.to_hash)
   end
 end
