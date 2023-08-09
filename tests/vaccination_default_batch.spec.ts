7540e26e03fdb45867f7de062bba6050d8806239
Theodor Vararu <theo@vararu.org>
2023-08-09 17:32:55 +0100

    Simplify vaccination default batch spec

--- tests/vaccination_default_batch.spec.ts
+++ tests/vaccination_default_batch.spec.ts
@@ -2,14 +2,15 @@ import { test, expect, Page } from "@playwright/test";
 
 let p: Page;
 
-test("Sets default batch for today", async ({ page }) => {
+test("Vaccination default batch", async ({ page }) => {
   p = page;
   await given_the_app_is_setup();
 
+  // Setting the batch
   await when_i_go_to_the_vaccinations_page();
-  await and_i_click_on_the_patient("Ernie Funk");
+  await and_i_click_on_a_patient();
   await and_i_record_a_vaccination();
-  await and_i_select_the_batch("IE5343");
+  await and_i_select_a_batch();
   await and_i_set_the_batch_to_be_the_default_for_today();
   await and_i_press_continue();
   await then_i_should_see_the_check_answers_page();
@@ -17,11 +18,12 @@ test("Sets default batch for today", async ({ page }) => {
   await when_i_press_confirm();
   await then_i_should_see_a_success_message();
 
-  await when_i_click_on_the_tab("Action needed (7)");
-  await and_i_click_on_the_patient("Man Swaniawski");
+  // Default batch is displayed
+  await when_i_click_on_the_action_needed_tab();
+  await and_i_click_on_another_patient();
   await and_i_record_a_vaccination();
   await then_i_should_see_the_check_answers_page();
-  await and_the_batch_should_be_displayed("IE5343");
+  await and_the_batch_should_be_displayed();
 });
 
 async function given_the_app_is_setup() {
@@ -32,8 +34,8 @@ async function when_i_go_to_the_vaccinations_page() {
   await p.goto("/sessions/1/vaccinations");
 }
 
-async function when_i_click_on_the_tab(name: string) {
-  await p.getByRole("tab", { name: name, exact: true }).click();
+async function when_i_click_on_the_action_needed_tab() {
+  await p.getByRole("tab", { name: "Action needed (7)", exact: true }).click();
 }
 
 async function and_i_record_a_vaccination() {
@@ -42,8 +44,8 @@ async function and_i_record_a_vaccination() {
   await p.click("text=Continue");
 }
 
-async function and_i_select_the_batch(batch: string) {
-  await p.click(`text=${batch}`);
+async function and_i_select_a_batch() {
+  await p.click("text=IE5343");
 }
 
 async function and_i_set_the_batch_to_be_the_default_for_today() {
@@ -66,11 +68,14 @@ async function and_i_press_continue() {
   await p.click("text=Continue");
 }
 
-async function when_i_click_on_the_patient(name: string) {
-  await p.getByRole("link", { name: name }).click();
+async function and_i_click_on_a_patient() {
+  await p.getByRole("link", { name: "Ernie Funk" }).click();
 }
-const and_i_click_on_the_patient = when_i_click_on_the_patient;
 
-async function and_the_batch_should_be_displayed(batch: string) {
-  await expect(p.getByText(batch)).toBeVisible();
+async function and_i_click_on_another_patient() {
+  await p.getByRole("link", { name: "Man Swaniawski" }).click();
+}
+
+async function and_the_batch_should_be_displayed() {
+  await expect(p.getByText("IE5343")).toBeVisible();
 }
