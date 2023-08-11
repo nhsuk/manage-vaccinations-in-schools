93be373dd3b284f74b52729a502afa495da4a886
Misha Gorodnitzky <misaka@pobox.com>
2023-08-11 17:07:11 +0100

    Add consent_refused feature spec

--- tests/README.md
+++ tests/README.md
@@ -27,6 +27,21 @@ The diagrams below show what steps and transitions are used for each test.
 Test of the flow when a nurse attempts to get consent during triage, but doesn't
 get a response, followed by a successful attempt to get consent.
 
+### Feature: Consent - parental consent during triage
+
+[Source file - consent.spec.ts](consent.spec.ts)
+
+![Flow diagram - consent during triage](images/consent.png)
+
+Test of the flow when a nurse attempts to get consent during triage, but gets a
+negative response, followed by a successful attempt to get consent, also during triage.
+
+### Feature: Consent - parental consent refused during triage
+
+[Source file - consent_refused.spec.ts](consent_refused.spec.ts)
+
+![Flow diagram - consent refused during triage](images/consent_refused.png)
+
 ### Feature: Consent - parental consent obtained during vaccination
 
 [Source file - consent_during_vaccination.spec.ts](consent_during_vaccination.spec.ts)
---
+++ tests/consent_refused.spec.ts
@@ -0,0 +1,75 @@
+import { test, expect, Page } from "@playwright/test";
+
+let p: Page;
+
+test("Consent", async ({ page }) => {
+  p = page;
+  await given_the_app_is_setup();
+
+  await given_i_am_doing_triage();
+  await when_i_select_a_child_with_no_consent_response();
+  await and_i_click_get_consent();
+  await then_the_consent_form_is_empty();
+
+  await given_i_call_the_parent_and_they_refuse_consent();
+  await when_i_record_the_consent_refused();
+  await and_i_record_the_reason_for_refusal();
+  // TODO: This is a bug in the app, their status should be do not vaccinate.
+  await then_i_see_that_the_child_needs_their_refusal_checked();
+});
+
+async function given_the_app_is_setup() {
+  await p.goto("/reset");
+}
+
+async function given_i_am_doing_triage() {
+  await p.goto("/sessions/1/triage");
+}
+
+async function when_i_select_a_child_with_no_consent_response() {
+  await p.getByRole("tab", { name: "Get consent" }).click();
+  await p.getByRole("link", { name: "Alexandra Sipes" }).click();
+}
+
+async function when_i_click_get_consent() {
+  await p.getByRole("button", { name: "Get consent" }).click();
+}
+const and_i_click_get_consent = when_i_click_get_consent;
+
+async function then_the_consent_form_is_empty() {
+  await expect(p.locator('[name="consent[parent_name]"]')).toBeEmpty();
+  await expect(p.locator('[name="consent[parent_phone]"]')).toBeEmpty();
+  await expect(p.locator("text=Mum")).not.toBeChecked();
+}
+
+async function given_i_call_the_parent_and_they_refuse_consent() {}
+
+async function when_i_record_the_consent_refused() {
+  // Who
+  await p.fill('[name="consent[parent_name]"]', "Jane Doe");
+  await p.fill('[name="consent[parent_phone]"]', "07700900000");
+  await p.click("text=Mum");
+  await p.getByRole("button", { name: "Continue" }).click();
+
+  // Do they agree
+  await p.getByRole("radio", { name: "No, they do not agree" }).click();
+  await p.getByRole("button", { name: "Continue" }).click();
+}
+
+async function and_i_record_the_reason_for_refusal() {
+  // Why do they not agree
+  await p.getByRole("radio", { name: "Personal choice" }).click();
+
+  await p.getByRole("button", { name: "Continue" }).click();
+
+  await p.getByRole("button", { name: "Confirm" }).click();
+}
+
+async function then_i_see_that_the_child_needs_their_refusal_checked() {
+  await expect(p.locator(".nhsuk-notification-banner__content")).toContainText(
+    "Consent saved for Alexandra Sipes",
+  );
+  await p.getByRole("tab", { name: "Triage complete" }).click();
+  const row = p.locator(`tr`, { hasText: "Alexandra Sipes" });
+  await expect(row.getByTestId("child-action")).toContainText("Check refusal");
+}
