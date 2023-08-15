e94b345b475d4dce6c9fc885f25fecf8a95df4bd
Misha Gorodnitzky <misaka@pobox.com>
2023-08-15 11:07:22 +0100

    Add validations specs
    These tests that validations errors are displayed on forms. With the
    complexity of the various form journeys we have, and given the setting
    that this tool will be used, these are useful to ensure that the service
    communicates errors in the correct way to nurses.

---
+++ tests/consent_gillick_validations.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect, Page } from "@playwright/test";
+
+let p: Page;
+
+test("Consent via Gillick competence validations", async ({ page }) => {
+  p = page;
+  await given_the_app_is_setup();
+
+  await given_i_am_assessing_a_child_for_gillick_competence();
+  await when_i_continue_without_entering_anything();
+  await then_the_gillick_assessment_validation_errors_are_displayed();
+});
+
+async function given_the_app_is_setup() {
+  await p.goto("/reset");
+}
+
+async function given_i_am_assessing_a_child_for_gillick_competence() {
+  await p.goto("/sessions/1/vaccinations");
+  await p.getByRole("tab", { name: "Action needed" }).click();
+  await p.getByRole("link", { name: "Alexandra Sipes" }).click();
+
+  await p.getByRole("radio", { name: "Gillick competence" }).click();
+  await p.getByRole("button", { name: "Continue" }).click();
+
+  await p.getByRole("button", { name: "Give your assessment" }).click();
+}
+
+async function when_i_continue_without_entering_anything() {
+  await p.getByRole("button", { name: "Continue" }).click();
+}
+
+async function then_the_gillick_assessment_validation_errors_are_displayed() {
+  const alert = p.getByRole("alert");
+  await expect(alert).toBeVisible();
+  await expect(alert).toContainText("Choose if they are Gillick competent");
+  await expect(alert).toContainText("Enter details of your assessment");
+}
---
+++ tests/consent_validations.spec.ts
@@ -0,0 +1,83 @@
+import { test, expect, Page } from "@playwright/test";
+
+let p: Page;
+
+test("Consent validations", async ({ page }) => {
+  p = page;
+  await given_the_app_is_setup();
+
+  // Who are you getting consent from validations
+  await given_i_am_on_the_who_am_i_contacting_for_consent_page();
+  await when_i_continue_without_entering_anything();
+  await then_i_see_the_who_i_am_contacting_validation_errors();
+
+  await given_i_select_other_relationship();
+  await when_i_continue_without_entering_anything();
+  await then_i_see_the_other_relationship_validation_errors();
+
+  // Consent response validations
+  await given_i_move_on_to_the_consent_response_page();
+  // TODO: There is currently a bug where submitting this returns an empty response.
+  // await when_i_continue_without_entering_anything();
+  // await then_i_see_the_consent_response_form_validation_errors();
+
+  // Triage details validations
+  await given_i_move_on_to_the_triage_details_page();
+  await when_i_continue_without_entering_anything();
+  await then_i_see_the_triage_details_validation_errors();
+});
+
+async function given_the_app_is_setup() {
+  await p.goto("/reset");
+}
+
+async function given_i_am_on_the_who_am_i_contacting_for_consent_page() {
+  await p.goto("/sessions/1/triage");
+  await p.getByRole("tab", { name: "Get consent" }).click();
+  await p.getByRole("link", { name: "Alexandra Sipes" }).click();
+  await p.getByRole("button", { name: "Get consent" }).click();
+}
+
+async function when_i_continue_without_entering_anything() {
+  await p.getByRole("button", { name: "Continue" }).click();
+}
+
+async function then_i_see_the_consent_response_form_validation_errors() {
+  await expect(
+    p.getByRole("alert").getByText("Choose yes or no"),
+  ).toBeVisible();
+}
+
+async function then_i_see_the_who_i_am_contacting_validation_errors() {
+  const alert = p.getByRole("alert");
+  await expect(alert).toBeVisible();
+  await expect(alert).toContainText("Enter a name");
+  await expect(alert).toContainText("Enter a phone number");
+  await expect(alert).toContainText("Choose a relationship");
+}
+
+async function given_i_select_other_relationship() {
+  await p.getByRole("radio", { name: "Other" }).click();
+}
+
+async function then_i_see_the_other_relationship_validation_errors() {
+  await expect(
+    p.getByRole("alert").getByText("Enter a relationship"),
+  ).toBeVisible();
+}
+
+async function given_i_move_on_to_the_consent_response_page() {
+  await p.fill('[name="consent[parent_name]"]', "Carl Sipes");
+  await p.fill('[name="consent[parent_phone]"]', "07700900000");
+  await p.getByRole("radio", { name: "Dad" }).click();
+  await p.getByRole("button", { name: "Continue" }).click();
+}
+
+async function given_i_move_on_to_the_triage_details_page() {
+  await p.getByRole("radio", { name: "Yes, they agree" }).click();
+  await p.getByRole("button", { name: "Continue" }).click();
+}
+
+async function then_i_see_the_triage_details_validation_errors() {
+  await expect(p.getByRole("alert").getByText("Choose a status")).toBeVisible();
+}
