08aaa9bb48224a6c0ab55cec428aa1b8b48921a7
Misha Gorodnitzky <misaka@pobox.com>
2023-08-18 16:49:29 +0100

    Raise unprocessable_entity with exception handling
    This makes rendering the correct template and status code a little
    tidier, and makes it easier to display the standard dev error page with
    stack trace in dev mode.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :set_disable_cache_headers
 
+  class UnprocessableEntity < StandardError
+  end
+
+  unless Rails.configuration.consider_all_requests_local
+    rescue_from UnprocessableEntity, with: :handle_unprocessable_entity
+  end
+
   if Rails.env.staging? || Rails.env.production?
     http_basic_authenticate_with name: Settings.support_username,
                                  password: Settings.support_password,
@@ -16,5 +23,9 @@ class ApplicationController < ActionController::Base
     response.headers["Cache-Control"] = "no-store"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "0"
+  end
+
+  def handle_unprocessable_entity(_exception)
+    render "errors/unprocessable_entity", status: :unprocessable_entity
   end
 end
