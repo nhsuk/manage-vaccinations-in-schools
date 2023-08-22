61c5c885b7d7c1da20dbb03f1932642afd469ca9
Theodor Vararu <theo@vararu.org>
2023-08-22 13:14:38 +0100

    Pass in route to AppConsentCardComponent
    This component is used in both the triage and vaccinations view, so we
    need to tell it which route it should send users on when starting a
    consent journey.

--- app/components/app_consent_card_component.rb
+++ app/components/app_consent_card_component.rb
@@ -1,9 +1,10 @@
 class AppConsentCardComponent < ViewComponent::Base
-  def initialize(session:, patient:, consent:)
+  def initialize(session:, patient:, consent:, route:)
     super
 
     @session = session
     @patient = patient
     @consent = consent
+    @route = route
   end
 end
