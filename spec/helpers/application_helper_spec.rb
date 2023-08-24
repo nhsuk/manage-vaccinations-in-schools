3515fee4537a36585f6e99180d68fb1aed0b0908
Theodor Vararu <theo@vararu.org>
2023-08-24 15:06:44 +0100

    Populate page titles based on h1 content
    This introduces a h1 helper that automatically sets
    `content_for(:page_title)`. Currently none of our pages have custom
    titles, which is an accessibility issue. Every page must also have one
    (and preferably only one) h1 element, and in most cases the content of
    the custom title prefix and the h1 will coincide.

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+
+describe ApplicationHelper do
+  describe "#h1" do
+    context "when called with only text" do
+      it "returns an h1 tag with the text" do
+        expect(helper.h1("Title")).to eq("<h1>Title</h1>")
+      end
+
+      it "sets the page title" do
+        helper.h1("Title")
+        expect(helper.content_for(:page_title)).to eq("Title")
+      end
+    end
+
+    context "when called with text and page_title option" do
+      it "returns an h1 tag with the text" do
+        expect(helper.h1("Title", page_title: "Custom")).to eq("<h1>Title</h1>")
+      end
+
+      it "sets the page title from the option" do
+        helper.h1("Title", page_title: "Custom")
+        expect(helper.content_for(:page_title)).to eq("Custom")
+      end
+    end
+
+    context "when called with a block" do
+      it "raises an error if the title option is not provided" do
+        expect { helper.h1 { "Title" } }.to raise_error(ArgumentError)
+      end
+
+      it "returns an h1 tag with the block content" do
+        output = helper.h1(page_title: "Custom Title") { "Block Content" }
+        expect(output).to eq("<h1>Block Content</h1>")
+      end
+
+      it "sets the page title from the title option" do
+        helper.h1(page_title: "Custom Title") { "Block Content" }
+        expect(helper.content_for(:page_title)).to eq("Custom Title")
+      end
+    end
+
+    context "when page title is already set" do
+      before { helper.content_for(:page_title, "Existing Title") }
+
+      it "does not override the existing page title" do
+        helper.h1("New Title")
+        expect(helper.content_for(:page_title)).to eq("Existing Title")
+      end
+    end
+  end
+end
