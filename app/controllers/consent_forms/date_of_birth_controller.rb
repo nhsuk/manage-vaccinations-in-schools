c8691ec35b2ad47514a8d88e3cc028a40cb2ab75
Misha Gorodnitzky <misaka@pobox.com>
2023-08-25 22:41:26 +0100

    Return to confirm page on consent_form steps

--- app/controllers/consent_forms/date_of_birth_controller.rb
+++ app/controllers/consent_forms/date_of_birth_controller.rb
@@ -1,13 +1,18 @@
 class ConsentForms::DateOfBirthController < ApplicationController
   before_action :set_session, only: %i[edit update]
   before_action :set_consent_form, only: %i[edit update]
+  before_action :set_return_to, only: %i[edit update]
 
   layout "two_thirds"
 
   def update
     @consent_form.assign_attributes(update_params)
     if @consent_form.save(context: :edit_date_of_birth)
-      redirect_to session_consent_form_confirm_path(@session)
+      if @return_to.present?
+        redirect_to @return_to
+      else
+        redirect_to session_consent_form_confirm_path(@session)
+      end
     else
       render action: :edit
     end
@@ -20,6 +25,10 @@ class ConsentForms::DateOfBirthController < ApplicationController
 
   def set_consent_form
     @consent_form = ConsentForm.find(params.fetch(:consent_form_id))
+  end
+
+  def set_return_to
+    @return_to = params[:return_to]
   end
 
   def set_session
