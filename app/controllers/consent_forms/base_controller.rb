484b9c0a0526a97611d2d8c7c779c278d958d47a
Theodor Vararu <theo@vararu.org>
2023-08-25 15:17:50 +0100

    Set custom header path based on controller
    Similar to the custom service name, we also need to update the place
    where the header takes you.

--- app/controllers/consent_forms/base_controller.rb
+++ app/controllers/consent_forms/base_controller.rb
@@ -1,6 +1,10 @@
 class ConsentForms::BaseController < ApplicationController
   private
 
+  def set_header_path
+    @header_path = start_session_consent_forms_path
+  end
+
   def set_service_name
     @service_name = "Give or refuse consent for vaccinations"
   end
