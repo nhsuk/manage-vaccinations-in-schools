5185046fb5edbdc6217ae8708496af8dd0c18d85
Theodor Vararu <theo@vararu.org>
2023-09-01 17:13:00 +0100

    Add Wicked EditController for ConsentForms
    This is a single controller that allows us to more easily create
    wizard-style flows. Adding a new step involves the following:
    
    - Appending another symbol to the `step` controller helper
    - Defining params for it and adding a case to the `update` method
    - Defining a translation for the route in en.yml
    - Adding the view
    - Adding validations to the model if necessary

---
+++ app/controllers/consent_forms/edit_controller.rb
@@ -0,0 +1,71 @@
+class ConsentForms::EditController < ConsentForms::BaseController
+  include Wicked::Wizard
+  include Wicked::Wizard::Translated # Defined custom URLs, see en.yml wicked
+
+  layout "two_thirds"
+
+  steps :name, :date_of_birth, :school
+
+  before_action :set_session
+  before_action :set_consent_form
+
+  def show
+    render_wizard
+  end
+
+  def update
+    case current_step
+    when :name
+      @consent_form.assign_attributes(name_params)
+    when :date_of_birth
+      @consent_form.assign_attributes(date_of_birth_params)
+    when :school
+      @consent_form.assign_attributes(school_params)
+
+      if @consent_form.is_this_their_school == "no"
+        return(
+          redirect_to session_consent_form_cannot_consent_path(
+                        @session,
+                        @consent_form
+                      )
+        )
+      end
+    end
+
+    render_wizard @consent_form, context: current_step
+  end
+
+  private
+
+  def current_step
+    wizard_value(step).to_sym
+  end
+
+  def finish_wizard_path
+    session_consent_form_confirm_path(@session, @consent_form)
+  end
+
+  def name_params
+    params.fetch(:consent_form, {}).permit(
+      %i[first_name last_name use_common_name common_name]
+    )
+  end
+
+  def date_of_birth_params
+    params.fetch(:consent_form, {}).permit(
+      %i[date_of_birth(3i) date_of_birth(2i) date_of_birth(1i)]
+    )
+  end
+
+  def school_params
+    params.fetch(:consent_form, {}).permit(%i[is_this_their_school])
+  end
+
+  def set_session
+    @session = Session.find(params.fetch(:session_id))
+  end
+
+  def set_consent_form
+    @consent_form = ConsentForm.find(params.fetch(:consent_form_id))
+  end
+end
