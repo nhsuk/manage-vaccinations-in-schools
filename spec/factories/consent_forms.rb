ce399e0f4fd1cb4b11d61d5f923c077e11aa2e10
Theodor Vararu <theo@vararu.org>
2023-09-06 13:32:42 +0100

    Add consent field to ConsentForm

--- spec/factories/consent_forms.rb
+++ spec/factories/consent_forms.rb
@@ -1,3 +1,34 @@
+# == Schema Information
+#
+# Table name: consent_forms
+#
+#  id                        :bigint           not null, primary key
+#  common_name               :text
+#  contact_method            :integer
+#  contact_method_other      :text
+#  date_of_birth             :date
+#  first_name                :text
+#  last_name                 :text
+#  parent_email              :string
+#  parent_name               :string
+#  parent_phone              :string
+#  parent_relationship       :integer
+#  parent_relationship_other :string
+#  recorded_at               :datetime
+#  response                  :integer
+#  use_common_name           :boolean
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#  session_id                :bigint           not null
+#
+# Indexes
+#
+#  index_consent_forms_on_session_id  (session_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (session_id => sessions.id)
+#
 FactoryBot.define do
   factory :consent_form do
   end
