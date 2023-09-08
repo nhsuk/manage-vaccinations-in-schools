b3507d97886f05c85b2e9d8574ed8ef4d441483e
Theodor Vararu <theo@vararu.org>
2023-09-08 16:33:07 +0100

    Add reason and reason_notes to ConsentForm
    These fields are required for the reason page.

---
+++ db/migrate/20230908152505_add_reason_and_notes_to_consent_form.rb
@@ -0,0 +1,8 @@
+class AddReasonAndNotesToConsentForm < ActiveRecord::Migration[7.0]
+  def change
+    change_table :consent_forms, bulk: true do |t|
+      t.integer :reason
+      t.text :reason_notes
+    end
+  end
+end
