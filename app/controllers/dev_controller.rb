06a0917fa72aed3ffb7a6f1de9c09f233d4606da
Misha Gorodnitzky <misaka@pobox.com>
2023-09-12 15:48:26 +0100

    Don't truncate users table on /reset
    At the moment we want user accounts to be independent of the changes we
    do to the example data for qa, testing and local dev work.

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -4,10 +4,20 @@ Rails.application.load_tasks
 
 class DevController < ApplicationController
   def reset
-    Rake::Task["db:schema:load"].execute
-    Rake::Task["load_campaign_example"].execute(
-      example_file: "db/sample_data/example-test-campaign.json"
-    )
+    ActiveRecord::Base.connection.transaction do
+      data_tables =
+        ActiveRecord::Base.connection.tables -
+          %w[users schema_migrations ar_internal_metadata]
+      data_tables.each do |table|
+        ActiveRecord::Base.connection.execute(
+          "TRUNCATE #{table} RESTART IDENTITY CASCADE"
+        )
+      end
+
+      Rake::Task["load_campaign_example"].execute(
+        example_file: "db/sample_data/example-test-campaign.json"
+      )
+    end
     redirect_to root_path
   end
 end
