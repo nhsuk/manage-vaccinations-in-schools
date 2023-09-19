31df5febc7933df39130eabd7dc6936d22aef3c0
Misha Gorodnitzky <misaka@dmfg.io>
2023-09-19 15:34:19 +0100

    Add e2e tests for sessions authorisation

--- tests/shared/sign_in.ts
+++ tests/shared/sign_in.ts
@@ -1,6 +1,10 @@
-export async function signInTestUser(page) {
+export async function signInTestUser(
+  page,
+  username = "nurse@test",
+  password = "nurse@test",
+) {
   await page.goto("/users/sign-in");
-  await page.getByLabel("Email address").fill("nurse@test");
-  await page.getByLabel("Password").fill("nurse@test");
+  await page.getByLabel("Email address").fill(username);
+  await page.getByLabel("Password").fill(password);
   await page.getByRole("button", { name: "Sign in" }).click();
 }
