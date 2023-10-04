8fa627889e63a769d699db7a01c7ead521ff39a5
Misha Gorodnitzky <misaka@dmfg.io>
2023-10-04 20:51:02 +0100

    Add patient names to triage and vacc auth tests

--- tests/triage_authorisation.spec.ts
+++ tests/triage_authorisation.spec.ts
@@ -9,6 +9,9 @@ test("Triage authorisation", async ({ page }) => {
   await given_the_app_is_setup();
   await when_i_sign_in_as_a_nurse_from_another_team();
   await and_i_go_to_the_triage_page();
+  await then_i_should_only_see_my_patients();
+
+  await when_i_go_to_the_triage_page_of_another_team();
   await then_i_should_get_an_error();
 
   await when_i_go_to_triage_a_patient_belonging_to_another_team();
@@ -24,6 +27,20 @@ async function when_i_sign_in_as_a_nurse_from_another_team() {
 }
 
 async function and_i_go_to_the_triage_page() {
+  await p.goto("/sessions/2/triage");
+}
+
+async function then_i_should_only_see_my_patients() {
+  await expect(
+    p.locator("#needs-triage-4 .nhsuk-table__body .nhsuk-table__row"),
+  ).toHaveCount(4);
+  await expect(p.getByText("Odelia Barton")).toBeVisible();
+  await expect(p.getByText("Rich Schaden")).toBeVisible();
+  await expect(p.getByText("Sung Boyle")).toBeVisible();
+  await expect(p.getByText("Ted Swift")).toBeVisible();
+}
+
+async function when_i_go_to_the_triage_page_of_another_team() {
   await p.goto("/sessions/1/triage");
 }
 
