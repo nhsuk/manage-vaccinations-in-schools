d8ff06b9d97fb4f82cbad0809de3d966bda83d4a
Misha Gorodnitzky <misaka@dmfg.io>
2023-10-04 20:51:02 +0100

    Add patient names to triage and vacc auth tests

--- tests/vaccination_authorisation.spec.ts
+++ tests/vaccination_authorisation.spec.ts
@@ -9,9 +9,12 @@ test("Vaccination authorisation", async ({ page }) => {
   await given_the_app_is_setup();
   await when_i_sign_in_as_a_nurse_from_another_team();
   await and_i_go_to_the_vaccinations_page();
+  await then_i_should_only_see_my_patients();
+
+  await when_i_go_to_the_vaccinations_page_of_another_team();
   await then_i_should_get_an_error();
 
-  await when_i_go_to_the_record_vaccination_page_belonging_to_another_team();
+  await when_i_go_to_the_vaccination_record_page_belonging_to_another_team();
   await then_i_should_get_an_error();
 });
 
@@ -24,6 +27,27 @@ async function when_i_sign_in_as_a_nurse_from_another_team() {
 }
 
 async function and_i_go_to_the_vaccinations_page() {
+  await p.goto("/sessions/2/vaccinations");
+}
+
+async function then_i_should_only_see_my_patients() {
+  await expect(
+    p.locator("#action-needed-11 .nhsuk-table__body .nhsuk-table__row"),
+  ).toHaveCount(11);
+  await expect(p.getByText("Brittany Klocko")).toBeVisible();
+  await expect(p.getByText("Bruce Reynolds")).toBeVisible();
+  await expect(p.getByText("Evette Muller")).toBeVisible();
+  await expect(p.getByText("Farah Welch")).toBeVisible();
+  await expect(p.getByText("John Grady")).toBeVisible();
+  await expect(p.getByText("Mckinley Marvin")).toBeVisible();
+  await expect(p.getByText("Odelia Barton")).toBeVisible();
+  await expect(p.getByText("Rich Schaden")).toBeVisible();
+  await expect(p.getByText("Sung Boyle")).toBeVisible();
+  await expect(p.getByText("Ted Swift")).toBeVisible();
+  await expect(p.getByText("Wonda Schuster")).toBeVisible();
+}
+
+async function when_i_go_to_the_vaccinations_page_of_another_team() {
   await p.goto("/sessions/1/vaccinations");
 }
 
@@ -33,6 +57,6 @@ async function then_i_should_get_an_error() {
   ).toBeVisible();
 }
 
-async function when_i_go_to_the_record_vaccination_page_belonging_to_another_team() {
+async function when_i_go_to_the_vaccination_record_page_belonging_to_another_team() {
   await p.goto("/sessions/1/patients/1/vaccinations");
 }
