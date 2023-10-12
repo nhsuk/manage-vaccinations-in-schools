9b00ce60790b1d4e5cd2e5c8f423bb86bf1660c3
Theodor Vararu <theo@vararu.org>
2023-10-12 13:13:27 +0300

    Add password reset fields to User model
    The token and date it was sent at need to be stored.

---
+++ db/migrate/20231012101034_add_reset_password_token_to_users.rb
@@ -0,0 +1,9 @@
+class AddResetPasswordTokenToUsers < ActiveRecord::Migration[6.0]
+  def change
+    change_table :users, bulk: true do |t|
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.index :reset_password_token, unique: true
+    end
+  end
+end
