c69278d457d8f42f7311219de7dfd69ebf8be908
Theodor Vararu <theo@vararu.org>
2023-10-20 17:25:40 +0100

    Implement email validator
    This is an email validator that's implemented to a similar spec as the
    GOVUK Notify one. This reduces the chance that emails entered by our
    users will be rejected by Notify.
    
    It follows their implementation and implements their test cases:
    
    - https://github.com/alphagov/notifications-utils/blob/9311851c105237dc5d770c9f1c0a5ad4c2acb380/notifications_utils/recipients.py#L622
    - https://github.com/alphagov/notifications-utils/blob/9311851c105237dc5d770c9f1c0a5ad4c2acb380/notifications_utils/__init__.py#L14
    - https://github.com/alphagov/notifications-utils/blob/9311851c105237dc5d770c9f1c0a5ad4c2acb380/tests/test_recipient_validation.py#L118
    
    International Domain Names (IDNs) are not currently implemented.
    
    The two test cases with non-UTF-8 characters are commented out.

---
+++ app/validators/email_validator.rb
@@ -0,0 +1,34 @@
+class EmailValidator < ActiveModel::EachValidator
+  VALID_LOCAL_CHARS = 'a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-'.freeze
+  EMAIL_REGEX_PATTERN = /^[#{VALID_LOCAL_CHARS}]+@([^.@][^@\s]+)$/
+  HOSTNAME_PART = /^(xn|[a-z0-9]+)(-?-[a-z0-9]+)*$/i
+  TLD_PART = /^([a-z]{2,63}|xn--([a-z0-9]+-)*[a-z0-9]+)$/i
+
+  def validate_each(record, attribute, value)
+    match = EMAIL_REGEX_PATTERN.match(value)
+
+    if !match || value.length > 320 || value.include?("..")
+      record.errors.add(attribute, :invalid)
+      return
+    end
+
+    hostname = match[1]
+
+    parts = hostname.split(".")
+
+    if hostname.length > 253 || parts.length < 2
+      record.errors.add(attribute, :invalid)
+      return
+    end
+
+    unless parts.all? { |part| HOSTNAME_PART.match?(part) }
+      record.errors.add(attribute, :invalid)
+      return
+    end
+
+    unless TLD_PART.match?(parts.last)
+      record.errors.add(attribute, :invalid)
+      return # rubocop:disable Style/RedundantReturn
+    end
+  end
+end
