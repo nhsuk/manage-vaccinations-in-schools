96b79622bb6aa5ed90e4f9cd3643ed8bd626d99d
Misha Gorodnitzky <misaka@dmfg.io>
2023-10-24 13:02:06 +0100

    Add follow_up question to health questions
    These are added as database columns instead of a JSONB attribute.
    next_question will be converted to the same in a following PR. These
    will be used as standard relationship attributes to make the Rails app
    simpler.

--- spec/models/health_question_spec.rb
+++ spec/models/health_question_spec.rb
@@ -2,20 +2,23 @@
 #
 # Table name: health_questions
 #
-#  id         :bigint           not null, primary key
-#  hint       :string
-#  metadata   :jsonb            not null
-#  question   :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  vaccine_id :bigint           not null
+#  id                    :bigint           not null, primary key
+#  hint                  :string
+#  metadata              :jsonb            not null
+#  question              :string
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  follow_up_question_id :bigint
+#  vaccine_id            :bigint           not null
 #
 # Indexes
 #
-#  index_health_questions_on_vaccine_id  (vaccine_id)
+#  index_health_questions_on_follow_up_question_id  (follow_up_question_id)
+#  index_health_questions_on_vaccine_id             (vaccine_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (follow_up_question_id => health_questions.id)
 #  fk_rails_...  (vaccine_id => vaccines.id)
 #
 require "rails_helper"
