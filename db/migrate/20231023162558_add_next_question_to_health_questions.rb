8f937c62a41856ab7d0244215a0fe6a8b2cfa02a
Misha Gorodnitzky <misaka@dmfg.io>
2023-10-24 13:28:28 +0100

    Move next_question out of jsonb field
    In order to work consistently the next_question field should always be
    present, so it makes more sense for this to be a traditional Rails
    reference attribute.

---
+++ db/migrate/20231023162558_add_next_question_to_health_questions.rb
@@ -0,0 +1,10 @@
+class AddNextQuestionToHealthQuestions < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :health_questions,
+                  :next_question,
+                  null: true,
+                  foreign_key: {
+                    to_table: :health_questions
+                  }
+  end
+end
