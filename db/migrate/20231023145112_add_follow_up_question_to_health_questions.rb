10512cff400665b5d50729a932590f2bfa3d51ed
Misha Gorodnitzky <misaka@dmfg.io>
2023-10-24 13:02:06 +0100

    Add follow_up question to health questions
    These are added as database columns instead of a JSONB attribute.
    next_question will be converted to the same in a following PR. These
    will be used as standard relationship attributes to make the Rails app
    simpler.

---
+++ db/migrate/20231023145112_add_follow_up_question_to_health_questions.rb
@@ -0,0 +1,10 @@
+class AddFollowUpQuestionToHealthQuestions < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :health_questions,
+                  :follow_up_question,
+                  null: true,
+                  foreign_key: {
+                    to_table: :health_questions
+                  }
+  end
+end
