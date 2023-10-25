72993b4d2fb8a49f24e75534aa1b0f296a1c4a64
Theodor Vararu <theo@vararu.org>
2023-10-25 11:11:29 +0100

    Tweak postcode and address validation messages
    Based on feedback from standup.

--- app/validators/postcode_validator.rb
+++ app/validators/postcode_validator.rb
@@ -2,7 +2,7 @@ class PostcodeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     ukpc = UKPostcode.parse(value.to_s)
     unless ukpc.full_valid?
-      record.errors.add(attribute, "Enter a valid postcode")
+      record.errors.add(attribute, "Enter a valid postcode, such as SW1A 1AA")
     end
   end
 end
