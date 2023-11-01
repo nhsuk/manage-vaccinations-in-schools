71ed244e446a0b6544e774165dd1110c2b068d68
Theodor Vararu <theo@vararu.org>
2023-11-01 21:34:43 +0000

    Remove unused steps in consent change answers spec
    Since we're setting up a random consent form using the generator link,
    we can skip these steps.

--- tests/parental_consent_change_answers.spec.ts
+++ tests/parental_consent_change_answers.spec.ts
@@ -6,10 +6,7 @@ test("Parental consent change answers", async ({ page }) => {
   p = page;
 
   await given_the_app_is_setup();
-  await when_i_go_to_the_consent_start_page();
-  await then_i_see_the_consent_start_page();
-
-  await when_i_fill_in_the_all_the_consent_questions();
+  await when_i_go_to_a_prefilled_consent_form();
   await then_i_see_the_consent_confirm_page();
 
   await when_i_click_on_the_change_link_of_the_first_answer();
@@ -33,17 +30,7 @@ async function given_the_app_is_setup() {
   await p.goto("/reset");
 }
 
-async function when_i_go_to_the_consent_start_page() {
-  await p.goto("/sessions/2/consents/start");
-}
-
-async function then_i_see_the_consent_start_page() {
-  await expect(p.locator("h1")).toContainText(
-    "Give or refuse consent for a flu vaccination",
-  );
-}
-
-async function when_i_fill_in_the_all_the_consent_questions() {
+async function when_i_go_to_a_prefilled_consent_form() {
   await p.goto("/random_consent_form?session_id=2");
 }
 
