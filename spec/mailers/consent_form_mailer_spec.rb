f8fc12f8cb704cfa9717d6da3c98d18af9396b4b
Theodor Vararu <theo@vararu.org>
2023-11-09 15:38:54 +0000

    Add confirmation_needs_triage email
    This is used when the parent answers "Yes" to any of the health
    questions.

--- spec/mailers/consent_form_mailer_spec.rb
+++ spec/mailers/consent_form_mailer_spec.rb
@@ -1,29 +1,29 @@
 require "rails_helper"
 
 RSpec.describe ConsentFormMailer, type: :mailer do
+  def consent_form(overrides = {})
+    @consent_form ||=
+      build(
+        :consent_form,
+        parent_email: "harry@hogwarts.edu",
+        parent_name: "Harry Potter",
+        first_name: "Albus",
+        last_name: "Potter",
+        common_name: "Severus",
+        **overrides
+      )
+  end
+
+  before do
+    allow_any_instance_of(Mail::Notify::Mailer).to receive(
+      :template_mail
+    ).with(notify_template_id, ->(options) { @template_options = options })
+  end
+
   describe "#confirmation" do
     let(:notify_template_id) { "7cda7ae5-99a2-4c40-9a3e-1863e23f7a73" }
     let(:team_email) { "england.manage-childrens-vaccinations@nhs.net" }
     let(:team_phone) { "01900 705 045" }
-
-    def consent_form(overrides = {})
-      @consent_form ||=
-        build(
-          :consent_form,
-          parent_email: "harry@hogwarts.edu",
-          parent_name: "Harry Potter",
-          first_name: "Albus",
-          last_name: "Potter",
-          common_name: "Severus",
-          **overrides
-        )
-    end
-
-    before do
-      allow_any_instance_of(Mail::Notify::Mailer).to receive(
-        :template_mail
-      ).with(notify_template_id, ->(options) { @template_options = options })
-    end
 
     it "calls template_mail with correct personalisation" do
       described_class.confirmation(consent_form).deliver_now
@@ -62,6 +62,27 @@ RSpec.describe ConsentFormMailer, type: :mailer do
       expect(@template_options[:personalisation]).to include(
         short_patient_name: "Harry",
         short_patient_name_apos: "Harry's"
+      )
+    end
+  end
+
+  describe "#confirmation_needs_triage" do
+    let(:notify_template_id) { "604ee667-c996-471e-b986-79ab98d0767c" }
+
+
+    it "calls template_mail with correct personalisation" do
+      described_class.confirmation_needs_triage(consent_form).deliver_now
+
+      expect(@template_options).to include(
+        personalisation: {
+          full_and_preferred_patient_name: "Albus Potter (known as Severus)",
+          location_name: consent_form.session.location.name,
+          long_date: consent_form.session.date.strftime("%A %-d %B"),
+          short_date: consent_form.session.date.strftime("%-d %B"),
+          parent_name: "Harry Potter",
+          short_patient_name: "Severus"
+        },
+        to: "harry@hogwarts.edu"
       )
     end
   end
