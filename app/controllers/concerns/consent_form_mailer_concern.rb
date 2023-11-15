c868c383335a1428e8d1a4c92a72b17738ce4739
Theodor Vararu <theo@vararu.org>
2023-11-15 13:41:43 +0100

    Add ConsentFormMailerConcern
    This abstracts the logic responsible for deciding which email to send at
    the end of the parental consent form transaction, allowing us to test it
    more easily.

---
+++ app/controllers/concerns/consent_form_mailer_concern.rb
@@ -0,0 +1,15 @@
+module ConsentFormMailerConcern
+  extend ActiveSupport::Concern
+
+  def send_record_mail(consent_form)
+    if consent_form.contact_injection?
+      ConsentFormMailer.confirmation_injection(consent_form).deliver_later
+    elsif consent_form.consent_refused?
+      ConsentFormMailer.confirmation_refused(consent_form).deliver_later
+    elsif consent_form.needs_triage?
+      ConsentFormMailer.confirmation_needs_triage(consent_form).deliver_later
+    else
+      ConsentFormMailer.confirmation(consent_form).deliver_later
+    end
+  end
+end
