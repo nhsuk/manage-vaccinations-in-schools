137d5763f449f6efec1597b97b396382a1aead23
Theodor Vararu <theo@vararu.org>
2023-11-21 15:28:13 +0100

    Display basic tabs in consents index page
    This queries the list of patient_sessions and splits them into the
    respective categories that match the tabs in the design.

--- app/controllers/consents_controller.rb
+++ app/controllers/consents_controller.rb
@@ -1,7 +1,16 @@
 class ConsentsController < ApplicationController
   before_action :set_session
+  before_action :set_patient_sessions, only: %i[index]
 
   def index
+    methods = %i[consent_given? consent_refused? consent_conflicts? no_consent?]
+
+    @tabs =
+      @patient_sessions.group_by do |patient_session|
+        methods.find { |m| patient_session.send(m) }
+      end
+
+    methods.each { |m| @tabs[m] ||= [] }
   end
 
   private
@@ -11,5 +20,13 @@ class ConsentsController < ApplicationController
       policy_scope(Session).find(
         params.fetch(:session_id) { params.fetch(:id) }
       )
+  end
+
+  def set_patient_sessions
+    @patient_sessions =
+      @session
+        .patient_sessions
+        .includes(patient: :consents)
+        .order("patients.first_name", "patients.last_name")
   end
 end
