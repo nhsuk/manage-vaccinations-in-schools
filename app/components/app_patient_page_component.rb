81980a30fb89f40e15e69d626838d4e226ade47c
Misha Gorodnitzky <misaka@dmfg.io>
2023-11-25 20:38:03 +0000

    Add basic patient page to triage and vaccinations
    We will slowly add other bits to the patient page a little at a time.

---
+++ app/components/app_patient_page_component.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class AppPatientPageComponent < ViewComponent::Base
+  include ApplicationHelper
+
+  attr_reader :patient_session
+
+  def initialize(patient_session:)
+    super
+
+    @patient_session = patient_session
+  end
+
+  delegate :patient, to: :patient_session
+
+  delegate :session, to: :patient_session
+end
