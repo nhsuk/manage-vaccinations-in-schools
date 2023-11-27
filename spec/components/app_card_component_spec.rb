11a7744cfb1fe80b4f5839541dd7c1bcf9d0feff
Misha Gorodnitzky <misaka@dmfg.io>
2023-11-27 16:03:58 +0000

    Add AppCardComponent and use for patient details

---
+++ spec/components/app_card_component_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe AppCardComponent, type: :component do
+  let(:heading) { "A Heading" }
+  let(:body) { "A Body" }
+  let(:component) { described_class.new(heading:) }
+
+  subject { page }
+
+  before { render_inline(component) { body } }
+
+  it { should have_css(".nhsuk-card") }
+  it { should have_css("h2.nhsuk-card__heading", text: "A Heading") }
+  it { should have_css(".nhsuk-card__content", text: "A Body") }
+end
