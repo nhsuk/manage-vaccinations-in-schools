ec93ec33315878be0cd21786725780f9b02937e9
Misha Gorodnitzky <misaka@dmfg.io>
2023-11-29 12:09:35 +0000

    Run rails app:update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "manage_vaccinations_production"
 
   config.action_mailer.perform_caching = false
