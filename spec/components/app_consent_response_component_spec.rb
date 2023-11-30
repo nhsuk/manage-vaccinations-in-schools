6eaa8c5654be7df2bae01029bf222ef197bf06e4
Misha Gorodnitzky <misaka@dmfg.io>
2023-11-30 13:05:33 +0000

    Add AppConsentResponseComponent
    The rendering of the response field of the consent details needs to
    accomodate raw HTML and the logic in case there are multiple responses
    to list.

---
+++ spec/components/app_consent_response_component_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe AppConsentResponseComponent, type: :component do
+  let(:consents) { [] }
+  let(:component) { described_class.new(consents:) }
+
+  subject { page }
+
+  before { render_inline(component) { body } }
+
+  context "with a single consent" do
+    let(:consents) { [create(:consent, :given)] }
+
+    it { should have_css("p", text: "Consent given (online)") }
+
+    it "displays the correct date and time" do
+      date = consents.first.created_at.to_fs(:nhsuk_date_short_month)
+      time = consents.first.created_at.strftime("%-l:%M%P")
+
+      should have_css("p", text: "#{date} at #{time}")
+    end
+
+    it { should_not have_css("ul") }
+  end
+
+  context "with multiple consents" do
+    let(:consent1) { create(:consent, :given) }
+    let(:consent2) { create(:consent, :given) }
+    let(:consents) { [consent1, consent2] }
+
+    it { should have_css("ul li p", text: "Consent given (online)", count: 2) }
+  end
+end
