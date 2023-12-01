cd9bd2c9227d5e7bb30c1b0d68b112da5cf9c45a
Theodor Vararu <theo@vararu.org>
2023-12-01 13:53:14 +0000

    Add consents#show page
    This reuses the same component from the triage and vaccination views.

--- tests/consent.spec.ts
+++ tests/consent.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect, Page } from "@playwright/test";
-import { signInTestUser } from "./shared/sign_in";
+import { signInTestUser, fixtures } from "./shared";
 
 let p: Page;
 
@@ -8,7 +8,10 @@ test("Check consent responses", async ({ page }) => {
   await given_the_app_is_setup();
   await and_i_am_signed_in();
   await when_i_go_to_the_consent_page();
-  await then_i_see_the_consent_page();
+  await then_i_see_the_consents_page();
+
+  await when_i_click_on_a_patient();
+  await then_i_see_the_patient_page();
 });
 
 async function given_the_app_is_setup() {
@@ -23,6 +26,14 @@ async function when_i_go_to_the_consent_page() {
   await p.goto("/sessions/1/consents");
 }
 
-async function then_i_see_the_consent_page() {
+async function then_i_see_the_consents_page() {
   await expect(p.locator("h1")).toHaveText("Check consent responses");
+}
+
+async function when_i_click_on_a_patient() {
+  await p.getByRole("link", { name: fixtures.patientThatNeedsTriage }).click();
+}
+
+async function then_i_see_the_patient_page() {
+  await expect(p.locator("h1")).toHaveText(fixtures.patientThatNeedsTriage);
 }
