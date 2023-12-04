907ce4f81e516b8801ed1c56ac59b34cccc4559b
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-04 12:20:06 +0000

    Render triage notes on patient page

--- spec/components/app_patient_page_component_spec.rb
+++ spec/components/app_patient_page_component_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
 RSpec.describe AppPatientPageComponent, type: :component do
-  let(:patient_session) { FactoryBot.create(:patient_session) }
+  let(:triage) { [FactoryBot.create(:triage, notes: "Patient notes")] }
+  let(:patient_session) { FactoryBot.create(:patient_session, triage:) }
   let(:consent) { FactoryBot.create(:consent, patient_session:) }
   let(:component) do
     described_class.new(patient_session:, consent:, route: "triage")
@@ -14,5 +15,6 @@ RSpec.describe AppPatientPageComponent, type: :component do
 
     it { should have_css(".nhsuk-card", text: "Child details") }
     it { should have_css(".nhsuk-card", text: "Consent") }
+    it { should have_css(".nhsuk-card", text: "Triage notes") }
   end
 end
