e720ca0ffe1439d54790cb7360d9eed62ebed78d
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-04 12:21:20 +0000

    Add triage notes test to journey spec

--- tests/triage.spec.ts
+++ tests/triage.spec.ts
@@ -23,6 +23,7 @@ test("Triage", async ({ page }) => {
 
   await when_i_click_on_a_patient();
   await then_i_should_see_the_triage_details();
+  await and_i_should_see_triage_notes();
 
   // Triage - not ready to vaccinate
   await given_i_click_on_back();
@@ -37,6 +38,7 @@ test("Triage", async ({ page }) => {
 
   await when_i_click_on_the_other_patient();
   await then_i_should_see_their_do_not_vaccinate_status();
+  await and_i_should_see_the_do_not_vaccinate_triage_notes();
 });
 
 async function given_the_app_is_setup() {
@@ -95,6 +97,27 @@ async function then_i_should_see_the_triage_details() {
   ).toBeChecked();
 }
 
+async function and_i_should_see_triage_notes() {
+  const today = new Date();
+  // Partial test. We're going to avoid wrestling with DateTimeFormat to get
+  // this 100% right, it's not useful for this test.
+  const formattedDate = today.toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  await expect(
+    p.locator("div.nhsuk-card", {
+      has: p.locator('h2:has-text("Triage notes")'),
+    }),
+  ).toHaveText(
+    new RegExp(
+      `Reached mother, able to proceed\\s*Nurse Joy, ${formattedDate}`,
+    ),
+  );
+}
+
 async function given_i_click_on_back() {
   await p.click("text=Back");
 }
@@ -113,7 +136,7 @@ const and_i_click_on_another_patient = when_i_click_on_the_other_patient;
 async function and_i_enter_a_note_and_select_do_not_vaccinate() {
   await p
     .getByLabel("Triage notes")
-    .fill("Father adament he does not want to vaccine");
+    .fill("Father adament he does not want to vaccinate");
   await p.getByRole("radio", { name: "Do not vaccinate" }).click();
   await p.getByRole("button", { name: "Save triage" }).click();
 }
@@ -136,9 +159,30 @@ async function then_i_should_see_the_other_patient() {
 
 async function then_i_should_see_their_do_not_vaccinate_status() {
   await expect(p.getByRole("textbox", { name: "Triage notes" })).toHaveValue(
-    "Father adament he does not want to vaccine",
+    "Father adament he does not want to vaccinate",
   );
   await expect(
     p.getByRole("radio", { name: "Do not vaccinate" }),
   ).toBeChecked();
+}
+
+async function and_i_should_see_the_do_not_vaccinate_triage_notes() {
+  const today = new Date();
+  // Partial test. We're going to avoid wrestling with DateTimeFormat to get
+  // this 100% right, it's not useful for this test.
+  const formattedDate = today.toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  await expect(
+    p.locator("div.nhsuk-card", {
+      has: p.locator('h2:has-text("Triage notes")'),
+    }),
+  ).toHaveText(
+    new RegExp(
+      `Father adament he does not want to vaccinate\\s*Nurse Joy, ${formattedDate}`,
+    ),
+  );
 }
