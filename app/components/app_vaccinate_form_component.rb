646d6c6537594fb8dc5007a9282c7a50423d3902
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-05 00:58:33 +0000

    Add vaccinate form card component

---
+++ app/components/app_vaccinate_form_component.rb
@@ -0,0 +1,35 @@
+class AppVaccinateFormComponent < ViewComponent::Base
+  def initialize(patient_session:, vaccination_record:, url: nil)
+    super
+
+    @patient_session = patient_session
+    @url = url
+    @vaccination_record = vaccination_record
+  end
+
+  def before_render
+    @url ||=
+      session_patient_vaccinations_path(
+        session_id: session.id,
+        patient_id: patient.id
+      )
+  end
+
+  private
+
+  def patient
+    @patient_session.patient
+  end
+
+  def session
+    @patient_session.session
+  end
+
+  def campaign_name
+    @patient_session.campaign.name
+  end
+
+  def vaccination_initial_delivery_sites
+    %w[left_arm right_arm other]
+  end
+end
