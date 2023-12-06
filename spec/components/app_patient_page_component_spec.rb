49fa3e43fe1035f63769effd0d277e3d6a18d0ba
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-06 23:33:39 +0000

    Move rendering of triage card to patient page comp
    This has the side-effect of making the triage form show up on all show
    pages. The fix to the vaccination refused validation journey is valid
    independent of this change since it just anchors the "No" to the
    beginning of the radio label.

--- spec/components/app_patient_page_component_spec.rb
+++ spec/components/app_patient_page_component_spec.rb
@@ -1,23 +1,46 @@
 require "rails_helper"
 
 RSpec.describe AppPatientPageComponent, type: :component do
-  let(:patient_session) do
-    FactoryBot.create(
-      :patient_session,
-      :triaged_ready_to_vaccinate,
-      :session_in_progress
-    )
+  let(:component) do
+    described_class.new(patient_session:, route: "triage", triage:)
   end
-  let(:component) { described_class.new(patient_session:, route: "triage") }
+  let(:triage) { nil }
+  let!(:rendered) { render_inline(component) }
 
-  describe "rendering" do
-    before { render_inline(component) }
+  subject { page }
 
-    subject { page }
+  context "patient in triage" do
+    let(:patient_session) do
+      FactoryBot.create(
+        :patient_session,
+        :consent_given_triage_needed,
+        :session_in_progress
+      )
+    end
+    let(:triage) { FactoryBot.create(:triage, patient_session:) }
 
     it { should have_css(".nhsuk-card", text: "Child details") }
     it { should have_css(".nhsuk-card", text: "Consent") }
     it { should have_css(".nhsuk-card", text: "Triage notes") }
+    it { should have_css(".nhsuk-card", text: "Triage") }
+    it do
+      should_not have_css(".nhsuk-card", text: "Did they get the HPV vaccine?")
+    end
+  end
+
+  context "patient ready to vaccinate" do
+    let(:patient_session) do
+      FactoryBot.create(
+        :patient_session,
+        :triaged_ready_to_vaccinate,
+        :session_in_progress
+      )
+    end
+
+    it { should have_css(".nhsuk-card", text: "Child details") }
+    it { should have_css(".nhsuk-card", text: "Consent") }
+    it { should have_css(".nhsuk-card", text: "Triage notes") }
+    it { should_not have_css(".nhsuk-card", text: /^Triage$/) }
     it { should have_css(".nhsuk-card", text: "Did they get the HPV vaccine?") }
   end
 end
