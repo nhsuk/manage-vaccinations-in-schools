671bd1656dd2193c5bfb71506995276b712919a7
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-06 14:45:07 +0000

    Conditionally render vaccination forms
    The patient must be ready to vaccinate and the session must be in
    progress for the vaccination form to be shown.

--- spec/components/app_vaccinate_form_component_spec.rb
+++ spec/components/app_vaccinate_form_component_spec.rb
@@ -3,17 +3,18 @@ require "rails_helper"
 RSpec.describe AppVaccinateFormComponent, type: :component do
   let(:heading) { "A Heading" }
   let(:body) { "A Body" }
-  let(:patient_session) { create :patient_session }
+  let(:session) { create :session, :in_progress }
+  let(:patient_session) do
+    create :patient_session, :consent_given_triage_not_needed, session:
+  end
   let(:vaccination_record) { VaccinationRecord.new }
   let(:url) { "/vaccinate" }
   let(:component) do
     described_class.new(patient_session:, url:, vaccination_record:)
   end
-  let(:rendered) { render_inline(component) }
+  let!(:rendered) { render_inline(component) }
 
   subject { page }
-
-  before { rendered }
 
   it { should have_css(".nhsuk-card") }
 
@@ -26,4 +27,46 @@ RSpec.describe AppVaccinateFormComponent, type: :component do
 
   it { should have_field("Yes, they got the HPV vaccine") }
   it { should have_field("No, they did not get it") }
+
+  describe "render?" do
+    subject { component.render? }
+
+    context "patient is not ready for vaccination" do
+      before do
+        allow(patient_session).to receive(:ready_to_vaccinate?).and_return(
+          false
+        )
+      end
+
+      context "session is in progress" do
+        let(:session) { create :session, :in_progress }
+
+        it { should be_falsey }
+      end
+
+      context "session is in the future" do
+        let(:session) { create :session, :in_future }
+
+        it { should be_falsey }
+      end
+    end
+
+    context "patient is ready for vaccination" do
+      before do
+        allow(patient_session).to receive(:ready_to_vaccinate?).and_return(true)
+      end
+
+      context "session is progress" do
+        let(:session) { create :session, :in_progress }
+
+        it { should be_truthy }
+      end
+
+      context "session is in the future" do
+        let(:session) { create :session, :in_future }
+
+        it { should be_falsey }
+      end
+    end
+  end
 end
