b481f7158eaea6598a57ae97d417ee3d9ff37154
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-06 23:23:43 +0000

    Add AppTriageFormComponent
    This is the component that will be used to render the triage form on the
    patient page.

---
+++ app/components/app_triage_form_component.rb
@@ -0,0 +1,34 @@
+class AppTriageFormComponent < ViewComponent::Base
+  def initialize(patient_session:, triage:, url: nil)
+    super
+
+    @patient_session = patient_session
+    @triage = triage
+    @url = url
+  end
+
+  # rubocop:disable Naming/MemoizedInstanceVariableName
+  def before_render
+    @url ||=
+      session_patient_triage_path(
+                    session, patient, @triage
+                 )
+  end
+  # rubocop:enable Naming/MemoizedInstanceVariableName
+
+  private
+
+  def patient
+    @patient_session.patient
+  end
+
+  def session
+    @patient_session.session
+  end
+
+  def triage_status_options
+    Triage.statuses.keys.map do |status|
+      [status, Triage.human_enum_name(:status, status)]
+    end
+  end
+end
