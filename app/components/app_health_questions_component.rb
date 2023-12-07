c560896e368f1bc076f996fe3e378408c8bf3c5e
Theodor Vararu <theo@vararu.org>
2023-12-07 17:12:38 +0000

    Display health questions correctly
    If there is only one consent, we should just display the formatted
    answer.
    
    If there are two or more consents, but they are matching, we should
    display "All responded:" followed by the formatted answer.
    
    If there are two or more consents, but they are conflicting, we should
    display "X responded: <answer>" for each consent.

--- app/components/app_health_questions_component.rb
+++ app/components/app_health_questions_component.rb
@@ -2,27 +2,71 @@ class AppHealthQuestionsComponent < ViewComponent::Base
   erb_template <<-ERB
     <% health_questions.each do |health_question| %>
       <h3 class="nhsuk-heading-xs nhsuk-u-margin-bottom-0">
-        <%= health_question["question"] %>
+        <%= health_question[:question] %>
       </h3>
-      <% if health_question["response"].downcase == "yes" %>
-        <p>
-          <%= health_question["response"].humanize %> &ndash;
-          <%= health_question["notes"] %>
-        </p>
-      <% else %>
-        <p><%= health_question["response"].humanize %></p>
-      <% end %>
+      <p><%= safe_join(health_question[:answers], tag.br) %></p>
     <% end %>
   ERB
 
   def initialize(consents:)
     super
 
-    # HACK: This needs to work with multiple consents
-    @consent = consents.first
+    @consents = consents
   end
 
   def health_questions
-    @consent&.health_questions
+    # Generates a hash like:
+    # {
+    #   "First question?" => {
+    #     "Mum" => "No",
+    #     "Dad" => "No"
+    #   },
+    #   "Second question?" => {
+    #     "Mum" => "No",
+    #     "Dad" =>" Yes – Notes"
+    #   }
+    # }
+    dict =
+      @consents.each_with_object({}) do |consent, acc|
+        consent.health_questions.each do |health_question|
+          question = health_question["question"]
+          response = health_question["response"]
+          notes = health_question["notes"]
+
+          formatted_answer = response.humanize
+          formatted_answer += " – #{notes}" if notes.present?
+
+          acc[question] ||= {}
+          acc[question][consent.who_responded] = formatted_answer
+        end
+      end
+
+    # Generates an array of hashes like:
+    # [
+    #   {
+    #     "question" => "First question?",
+    #     "answer" => ["All responded: No"]
+    #   }
+    #   {
+    #     "question" => "Second question?",
+    #     "answer" => ["Mum responded: No", "Dad responded: Yes – Notes"]}
+    #   }
+    # }
+    dict.map do |question, answers|
+      only_one_consent = @consents.length == 1
+      answers_are_all_identical =
+        @consents.length == answers.length && answers.values.uniq.length == 1
+
+      formatted_answers =
+        if only_one_consent
+          [answers.values.first]
+        elsif answers_are_all_identical
+          ["All responded: #{answers.values.first}"]
+        else
+          answers.map { |who, response| "#{who} responded: #{response}" }
+        end
+
+      { question:, answers: formatted_answers }
+    end
   end
 end
--- spec/components/app_health_questions_component_spec.rb
+++ spec/components/app_health_questions_component_spec.rb
@@ -1,59 +1,80 @@
 require "rails_helper"
 
 RSpec.describe AppHealthQuestionsComponent, type: :component do
-  health_questions = [
-    {
-      question:
-        "Does the child have a disease or treatment that severely affects their immune system?",
-      response: "no"
-    },
-    {
-      question:
-        "Is anyone in your household having treatment that severely affects their immune system?",
-      response: "no"
-    },
-    { question: "Has your child been diagnosed with asthma?", response: "no" },
-    {
-      question:
-        "Has your child been admitted to intensive care because of a severe egg allergy?",
-      response: "no"
-    },
-    {
-      question: "Is there anything else we should know?",
-      response: "yes",
-      notes:
-        "Please be aware my daughter likes to play with needles and will try to grab them"
-    }
-  ].freeze
-
   before { render_inline(component) }
 
   subject { page }
 
-  let(:patient) { FactoryBot.create(:patient) }
-  let(:session) { FactoryBot.create(:session) }
-  let(:consents) do
-    [create(:consent, patient:, campaign: session.campaign, health_questions:)]
-  end
-
   let(:component) { described_class.new(consents:) }
 
-  # Copy pasted from the consents factory
-  health_questions.each_with_index do |health_question, i|
-    it "should have the health question #{i}" do
-      expect(page.find_css("h3:nth(#{i + 1})").text).to(
-        match(/^\s*#{Regexp.escape(health_question[:question])}\s*$/s)
-      )
+  context "with one consent" do
+    let(:consents) do
+      [
+        create(
+          :consent,
+          :given,
+          :from_mum,
+          health_questions: [
+            { question: "First question?", response: "no" },
+            { question: "Second question?", response: "yes", notes: "Notes" }
+          ]
+        )
+      ]
     end
 
-    it "should have the answer to health question #{i}" do
-      if health_question[:response].downcase == "yes"
-        expect(page.find_css("p:nth(#{i + 1})").text).to(
-          match(/^\s*Yes – #{health_question[:notes]}\s*$/)
+    it { should have_content(/First question\?\s*No/) }
+    it { should have_content(/Second question\?\s*Yes –\s*Notes/) }
+  end
+
+  context "with two consents given" do
+    let(:consents) do
+      [
+        create(
+          :consent,
+          :given,
+          :from_mum,
+          health_questions: [
+            { question: "First question?", response: "no" },
+            { question: "Second question?", response: "no" }
+          ]
+        ),
+        create(
+          :consent,
+          :given,
+          :from_dad,
+          health_questions: [
+            { question: "First question?", response: "no" },
+            { question: "Second question?", response: "yes", notes: "Notes" }
+          ]
         )
-      else
-        expect(page.find_css("p:nth(#{i + 1})").text).to(match(/^\s*No\s*$/))
-      end
+      ]
     end
+
+    it { should have_content(/First question\?\s*All responded: No/) }
+    it do
+      should have_content(
+               /Second question\?\s*Mum responded: No\s*Dad responded: Yes –\s*Notes/
+             )
+    end
+  end
+
+  context "with two consents, one refused" do
+    let(:consents) do
+      [
+        create(
+          :consent,
+          :given,
+          :from_mum,
+          health_questions: [
+            { question: "First question?", response: "no" },
+            { question: "Second question?", response: "no" }
+          ]
+        ),
+        create(:consent, :refused, :from_dad)
+      ]
+    end
+
+    it { should have_content(/First question\?\s*Mum responded: No/) }
+    it { should have_content(/Second question\?\s*Mum responded: No/) }
   end
 end
