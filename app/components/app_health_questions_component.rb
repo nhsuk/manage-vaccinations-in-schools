d59810e79c046f24b694ff12d2c05df715bce994
Theodor Vararu <theo@vararu.org>
2023-12-07 11:52:40 +0000

    Use erb_template instead of html.erb file
    This component is easier to edit when it's in one place.

--- app/components/app_health_questions_component.rb
+++ app/components/app_health_questions_component.rb
@@ -1,4 +1,20 @@
 class AppHealthQuestionsComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <% health_questions.each do |health_question| %>
+      <h3 class="nhsuk-heading-xs nhsuk-u-margin-bottom-0">
+        <%= health_question["question"] %>
+      </h3>
+      <% if health_question["response"].downcase == "yes" %>
+        <p>
+          <%= health_question["response"].humanize %> &ndash;
+          <%= health_question["notes"] %>
+        </p>
+      <% else %>
+        <p><%= health_question["response"].humanize %></p>
+      <% end %>
+    <% end %>
+  ERB
+
   def initialize(consents:)
     super
 
