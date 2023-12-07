8887c9ece42d7cb7df9ec9136f21926cd1328577
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-07 23:31:19 +0000

    Update status banner component

--- spec/components/app_card_component_spec.rb
+++ spec/components/app_card_component_spec.rb
@@ -18,4 +18,36 @@ RSpec.describe AppCardComponent, type: :component do
 
     it { should_not have_css(".nhsuk-card__content") }
   end
+
+  context "feature card" do
+    let(:component) { described_class.new(heading:, feature: true) }
+
+    describe "card_classes" do
+      subject { component.send(:card_classes) }
+
+      it { should include "nhsuk-card--feature" }
+    end
+
+    describe "content_classes" do
+      subject { component.send(:content_classes) }
+
+      it { should include "nhsuk-card__content--feature" }
+    end
+
+    describe "heading_classes" do
+      subject { component.send(:heading_classes) }
+
+      it { should include "nhsuk-card__heading--feature" }
+    end
+  end
+
+  context "coloured card" do
+    let(:component) { described_class.new(heading:, colour: "purple") }
+
+    describe "card_classes" do
+      subject { component.send(:card_classes) }
+
+      it { should include "nhsuk-card--purple" }
+    end
+  end
 end
