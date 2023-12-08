7f53f021faea20841bc1a4e20f9f63b1a171c623
Theodor Vararu <theo@vararu.org>
2023-12-08 19:19:36 +0000

    Fill in counts on session show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,31 @@ class SessionsController < ApplicationController
   end
 
   def show
+    @patient_sessions = @session.patient_sessions
+      .includes(:campaign, patient: :consents)
+
+    @counts = {
+      with_consent_given: 0,
+      with_consent_refused: 0,
+      without_a_response: 0,
+      needing_triage: 0,
+      ready_to_vaccinate: 0
+    }
+
+    @patient_sessions.each do |s|
+      @counts[:with_consent_given] += 1 if s.consent_given?
+      @counts[:with_consent_refused] += 1 if s.consent_refused?
+      @counts[:without_a_response] += 1 if s.no_consent?
+
+      if s.consent_given_triage_needed? || s.triaged_kept_in_triage?
+        @counts[:needing_triage] += 1
+        @counts[:ready_to_vaccinate] += 1
+      end
+
+      @counts[:ready_to_vaccinate] += 1 if s.triaged_ready_to_vaccinate? ||
+                                            s.added_to_session? ||
+                                            s.consent_given_triage_not_needed?
+    end
   end
 
   private
