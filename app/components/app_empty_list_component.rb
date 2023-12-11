b124b292307a18ef0e19701d02e4e05563501c4c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2023-12-11 12:32:51 +0000

    Use card component within empty list component

--- app/components/app_empty_list_component.rb
+++ app/components/app_empty_list_component.rb
@@ -1,16 +1,2 @@
 class AppEmptyListComponent < ViewComponent::Base
-  erb_template <<-ERB
-    <div class="app-empty-list">
-      <div class="nhsuk-card__content">
-        <h2 class="nhsuk-heading-l">No results</h2>
-        <p><%= @message %></p>
-      </div>
-    </div>
-  ERB
-
-  def initialize(message:)
-    super
-
-    @message = message
-  end
 end
