0600f7ff25b052d1a801b54dfa3a3c3393cf0016
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-11 15:03:28 +0000

    Add user_id to vaccination_record

--- app/models/vaccination_record.rb
+++ app/models/vaccination_record.rb
@@ -12,16 +12,19 @@
 #  updated_at         :datetime         not null
 #  batch_id           :bigint
 #  patient_session_id :bigint           not null
+#  user_id            :bigint
 #
 # Indexes
 #
 #  index_vaccination_records_on_batch_id            (batch_id)
 #  index_vaccination_records_on_patient_session_id  (patient_session_id)
+#  index_vaccination_records_on_user_id             (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (batch_id => batches.id)
 #  fk_rails_...  (patient_session_id => patient_sessions.id)
+#  fk_rails_...  (user_id => users.id)
 #
 class VaccinationRecord < ApplicationRecord
   audited associated_with: :patient_session
@@ -30,6 +33,7 @@ class VaccinationRecord < ApplicationRecord
 
   belongs_to :patient_session
   belongs_to :batch, optional: true
+  belongs_to :user
   has_one :vaccine, through: :batch
 
   enum :delivery_method, %w[intramuscular subcutaneous], prefix: true
--- spec/factories/vaccination_records.rb
+++ spec/factories/vaccination_records.rb
@@ -12,16 +12,19 @@
 #  updated_at         :datetime         not null
 #  batch_id           :bigint
 #  patient_session_id :bigint           not null
+#  user_id            :bigint
 #
 # Indexes
 #
 #  index_vaccination_records_on_batch_id            (batch_id)
 #  index_vaccination_records_on_patient_session_id  (patient_session_id)
+#  index_vaccination_records_on_user_id             (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (batch_id => batches.id)
 #  fk_rails_...  (patient_session_id => patient_sessions.id)
+#  fk_rails_...  (user_id => users.id)
 #
 FactoryBot.define do
   factory :vaccination_record do
