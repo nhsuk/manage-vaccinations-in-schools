03e14540b85025df48e4ad9a32f9867409b12b7f
Theodor Vararu <theo@vararu.org>
2023-12-13 16:14:34 +0000

    Wrap ParentInterface controllers with module

--- app/controllers/parent_interface/consent_forms_controller.rb
+++ app/controllers/parent_interface/consent_forms_controller.rb
@@ -1,43 +1,45 @@
-class ConsentFormsController < ConsentForms::BaseController
-  include ConsentFormMailerConcern
+module ParentInterface
+  class ConsentFormsController < ConsentForms::BaseController
+    include ConsentFormMailerConcern
 
-  layout "two_thirds"
+    layout "two_thirds"
 
-  skip_before_action :set_consent_form, only: %i[start create]
-  skip_before_action :authenticate_consent_form_user!, only: %i[start create]
+    skip_before_action :set_consent_form, only: %i[start create]
+    skip_before_action :authenticate_consent_form_user!, only: %i[start create]
 
-  before_action :clear_session_edit_variables, only: %i[confirm]
+    before_action :clear_session_edit_variables, only: %i[confirm]
 
-  def start
-  end
+    def start
+    end
 
-  def create
-    vaccine = @session.campaign.vaccines.first
-    health_questions = vaccine.health_questions.in_order
-    consent_form = @session.consent_forms.create!(health_questions:)
+    def create
+      vaccine = @session.campaign.vaccines.first
+      health_questions = vaccine.health_questions.in_order
+      consent_form = @session.consent_forms.create!(health_questions:)
 
-    session[:consent_form_id] = consent_form.id
+      session[:consent_form_id] = consent_form.id
 
-    redirect_to session_consent_form_edit_path(@session, consent_form, :name)
-  end
+      redirect_to session_consent_form_edit_path(@session, consent_form, :name)
+    end
 
-  def cannot_consent
-  end
+    def cannot_consent
+    end
 
-  def confirm
-  end
+    def confirm
+    end
 
-  def record
-    @consent_form.update!(recorded_at: Time.zone.now)
+    def record
+      @consent_form.update!(recorded_at: Time.zone.now)
 
-    session.delete(:consent_form_id)
+      session.delete(:consent_form_id)
 
-    send_record_mail(@consent_form)
-  end
+      send_record_mail(@consent_form)
+    end
 
-  private
+    private
 
-  def clear_session_edit_variables
-    session.delete(:follow_up_changes_start_page)
+    def clear_session_edit_variables
+      session.delete(:follow_up_changes_start_page)
+    end
   end
 end
