e43bbc9f8555161aaba995df01d97095ef9feb19
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-14 11:05:09 +0000

    Add Flipper feature flag gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_11_152827) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_14_105751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -119,6 +119,22 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_11_152827) do
     t.datetime "recorded_at"
     t.index ["campaign_id"], name: "index_consents_on_campaign_id"
     t.index ["patient_id"], name: "index_consents_on_patient_id"
+  end
+
+  create_table "flipper_features", force: :cascade do |t|
+    t.string "key", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_flipper_features_on_key", unique: true
+  end
+
+  create_table "flipper_gates", force: :cascade do |t|
+    t.string "feature_key", null: false
+    t.string "key", null: false
+    t.text "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
   create_table "good_job_batches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
