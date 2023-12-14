84cffd892edade6adb6978e5ff22cf1e02d3f059
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-14 16:15:46 +0000

    Make dev tools component
    Allows us to do testing around whether this renders in production mode
    or not.

---
+++ spec/components/app_dev_tools_component_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe AppDevToolsComponent, type: :component do
+  let(:consent) { create(:consent, :refused, :from_dad, parent_name: "Harry") }
+  let(:consents) { [consent] }
+  let(:component) { described_class.new }
+  let!(:rendered) { render_inline(component) { body } }
+  let(:body) { "Hello dev tools!" }
+
+  subject { page }
+
+  it "renders in development" do
+    allow(Rails).to receive(:env).and_return("development".inquiry)
+    expect(render_inline(component).to_html).to include("Hello dev tools!")
+  end
+
+  it "does not render in production" do
+    allow(Rails).to receive(:env).and_return("production".inquiry)
+    expect(render_inline(component).to_html).to be_blank
+  end
+end
