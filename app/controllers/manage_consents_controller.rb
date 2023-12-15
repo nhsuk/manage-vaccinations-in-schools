db755b374e5cac386da2e68a86485f407b706c8c
Theodor Vararu <theo@vararu.org>
2023-12-15 17:07:35 +0000

    Add ManageConsentsController
    This is a Wicked rewrite of the NurseConsentsController. The new
    controller is hidden behind a feature flag and will be built piecemeal,
    in parallel to the old one.

---
+++ app/controllers/manage_consents_controller.rb
@@ -0,0 +1,33 @@
+class ManageConsentsController < ApplicationController
+  include Wicked::Wizard
+  include Wicked::Wizard::Translated # For custom URLs, see en.yml wicked
+
+  layout "two_thirds"
+
+  before_action :set_consent
+  before_action :set_steps
+  before_action :setup_wizard_translated
+
+  def show
+    render_wizard
+  end
+
+  def update
+    render_wizard @consent
+  end
+
+  private
+
+  def current_step
+    wizard_value(step).to_sym
+  end
+
+  def set_consent
+    @consent = Consent.find_or_initialize_by(id: params[:consent_id])
+  end
+
+  def set_steps
+    # self.steps = @consent.form_steps
+    self.steps = %i[who]
+  end
+end
