f230afe34f54fc2a2959f6179c8c2d02d91b621e
Theodor Vararu <theo@vararu.org>
2023-12-19 11:19:20 +0000

    Update avo class names
    Done by `rails avo:upgrade:2_to_3` from the `avo_upgrade` library.

--- app/avo/resources/triage.rb
+++ app/avo/resources/triage.rb
@@ -1,8 +1,8 @@
-class TriageResource < Avo::BaseResource
+class Avo::Resources::Triage < Avo::BaseResource
   self.title = :id
   self.includes = []
   # self.search_query = -> do
-  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  #   query.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
 
   field :id, as: :id
