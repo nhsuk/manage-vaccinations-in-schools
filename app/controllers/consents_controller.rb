dfe70175604bdee2e684b37060db3f5631b1885c
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-22 15:41:55 +0000

    Move consents show to patient_sessions show

--- app/controllers/consents_controller.rb
+++ app/controllers/consents_controller.rb
@@ -1,8 +1,6 @@
 class ConsentsController < ApplicationController
   before_action :set_session
   before_action :set_patient_sessions, only: %i[index]
-  before_action :set_patient, only: %i[show]
-  before_action :set_patient_session, only: %i[show]
 
   layout "two_thirds", except: :index
 
@@ -17,9 +15,6 @@ class ConsentsController < ApplicationController
     methods.each { |m| @tabs[m] ||= [] }
   end
 
-  def show
-  end
-
   private
 
   def set_session
@@ -27,14 +22,6 @@ class ConsentsController < ApplicationController
       policy_scope(Session).find(
         params.fetch(:session_id) { params.fetch(:id) }
       )
-  end
-
-  def set_patient
-    @patient = Patient.find(params.fetch(:patient_id) { params.fetch(:id) })
-  end
-
-  def set_patient_session
-    @patient_session = @patient.patient_sessions.find_by(session: @session)
   end
 
   def set_patient_sessions
