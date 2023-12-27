dcd0b209797bec451515bcfb0bf9cb35e5da5f1f
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-27 13:31:12 +0000

    Add specs for AppTabComponent
    specs and helpers copied over from govuk_components. Hopefully this can
    be temporary, but for now it's the easiest way to customise the govuk
    component.

---
+++ spec/govuk_helper.rb
@@ -0,0 +1,15 @@
+require "view_component/test_helpers"
+
+# require File.expand_path("dummy/config/environment", __dir__)
+
+Dir[File.join("./spec", "govuk_shared", "*.rb")].sort.each do |file|
+  require file
+end
+
+RSpec.configure do |config|
+  config.include ViewComponent::TestHelpers, type: :component
+  config.include RSpecHtmlMatchers
+
+  # config.include_context "helpers"
+  config.include_context "setup"
+end
