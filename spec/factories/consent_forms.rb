cbdef3b8d42e7b645532384e7478334b0608d0ce
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-28 16:25:35 +0000

    Add ConsentForm.belongs_to :consent & migration
    A ConsentForm will be linked to a Consent object when it is matched to a
    patient.

--- spec/factories/consent_forms.rb
+++ spec/factories/consent_forms.rb
@@ -29,14 +29,17 @@
 #  use_common_name           :boolean
 #  created_at                :datetime         not null
 #  updated_at                :datetime         not null
+#  consent_id                :bigint
 #  session_id                :bigint           not null
 #
 # Indexes
 #
+#  index_consent_forms_on_consent_id  (consent_id)
 #  index_consent_forms_on_session_id  (session_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (consent_id => consents.id)
 #  fk_rails_...  (session_id => sessions.id)
 #
 FactoryBot.define do
