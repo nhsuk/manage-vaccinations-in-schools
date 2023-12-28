c966d5e8dafad0639a2d4b29b5843c50affe93fe
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-28 10:23:20 +0000

    Add missing patient_session annotation

--- spec/models/patient_session_spec.rb
+++ spec/models/patient_session_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: patient_sessions
+#
+#  id                       :bigint           not null, primary key
+#  gillick_competence_notes :text
+#  gillick_competent        :boolean
+#  state                    :string
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  patient_id               :bigint           not null
+#  session_id               :bigint           not null
+#
+# Indexes
+#
+#  index_patient_sessions_on_patient_id_and_session_id  (patient_id,session_id) UNIQUE
+#  index_patient_sessions_on_session_id_and_patient_id  (session_id,patient_id) UNIQUE
+#
 require "rails_helper"
 
 RSpec.describe PatientSession do
