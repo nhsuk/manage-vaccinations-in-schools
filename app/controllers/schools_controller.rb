337561bac1bd71a5bd6c59bbdebe32d33f822f41
Misha Gorodnitzky <misaka@dmfg.io>
2023-12-28 17:42:31 +0000

    Add basic school show page

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,18 @@
+class SchoolsController < ApplicationController
+  before_action :set_school, only: [:show]
+  before_action :set_unmatched_consent_responses, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_school
+    # The only kind of locations we have currently are schools.
+    @school = Location.find(params[:id])
+  end
+
+  def set_unmatched_consent_responses
+    @unmatched_consent_responses = @school.consent_forms.unmatched
+  end
+end
