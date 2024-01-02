bc14633ebd509ece4ffb50a70bf1d2d92f79b530
Theodor Vararu <theo@vararu.org>
2024-01-02 12:57:18 +0000

    Set dummy keys for AR Encryption in dev/tes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -65,4 +65,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost:4000" }
   config.action_mailer.default_options = { from: "no-reply@nhs.net" }
   config.action_mailer.delivery_method = :file
+
+  # Set up Active Record Encryption in test mode
+  config.active_record.encryption.primary_key = "test"
+  config.active_record.encryption.deterministic_key = "test"
+  config.active_record.encryption.key_derivation_salt = "test"
 end
