46413be96356803120816acc6c54d83e811768cb
Theodor Vararu <theo@vararu.org>
2024-01-03 16:26:14 +0000

    Display errors correctly when not signed in
    Errors should not require the user to be signed in to display correctly.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class ErrorsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
 
   layout "two_thirds"
 
