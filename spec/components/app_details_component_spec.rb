fad0829a1a4ea7567136aef2b73db36796abe7dc
Theodor Vararu <theo@vararu.org>
2024-01-05 12:14:31 +0000

    Reuse variable

--- spec/components/app_details_component_spec.rb
+++ spec/components/app_details_component_spec.rb
@@ -10,17 +10,17 @@ RSpec.describe AppDetailsComponent, type: :component do
   before { render_inline(component) { content } }
 
   it { should have_css(".nhsuk-details") }
-  it { should have_css(".nhsuk-details__summary", text: "A summary") }
+  it { should have_css(".nhsuk-details__summary", text: summary) }
 
   it "defaults to being closed" do
-    should have_css(".nhsuk-details__text", text: "A content", visible: false)
+    should have_css(".nhsuk-details__text", text: content, visible: false)
   end
 
   context "open flag is true" do
     let(:component) { described_class.new(summary:, open: true) }
 
     it "displays the content section" do
-      should have_css(".nhsuk-details__text", text: "A content", visible: true)
+      should have_css(".nhsuk-details__text", text: content, visible: true)
     end
   end
 end
