7e1891923e3992ea2c10ce9630f86a65c0a44ff1
Misha Gorodnitzky <misaka@dmfg.io>
2024-01-08 13:24:17 +0000

    Change patients columns to align with consent

---
+++ db/migrate/20240108131124_rename_patient_columns.rb
@@ -0,0 +1,31 @@
+class RenamePatientColumns < ActiveRecord::Migration[7.1]
+  def up
+    change_table :patients, bulk: true do |t|
+      t.change :first_name, :string
+      t.change :last_name, :string
+      t.change :preferred_name, :string
+      t.change :parent_email, :string
+      t.change :parent_name, :string
+      t.change :parent_phone, :string
+      t.change :parent_relationship_other, :string
+
+      t.rename :dob, :date_of_birth
+      t.rename :preferred_name, :common_name
+    end
+  end
+
+  def down
+    change_table :patients, bulk: true do |t|
+      t.change :first_name, :text
+      t.change :last_name, :text
+      t.change :common_name, :text
+      t.change :parent_email, :text
+      t.change :parent_name, :text
+      t.change :parent_phone, :text
+      t.change :parent_relationship_other, :text
+
+      t.rename :date_of_birth, :dob
+      t.rename :common_name, :preferred_name
+    end
+  end
+end
