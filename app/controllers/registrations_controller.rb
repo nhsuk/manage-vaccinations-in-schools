813242d81ea6e60b0e7092361736f539bd88f686
Misha Gorodnitzky <misaka@dmfg.io>
2024-01-12 15:00:36 +0000

    Add parent fields to registration form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,9 +8,33 @@ class RegistrationsController < ApplicationController
     @registration_form = Registration.new
   end
 
+  def create
+    @registration_form = Registration.new(registration_params)
+    @registration_form.location = @school
+
+    if @registration_form.save
+      redirect_to confirmation_registration_path
+    else
+      render :new
+    end
+  end
+
+  def confirmation
+  end
+
   private
 
   def set_school
     @school = Location.find(params[:school_id])
+  end
+
+  def registration_params
+    params.require(:registration).permit(
+      :parent_name,
+      :parent_relationship,
+      :parent_relationship_other,
+      :parent_email,
+      :parent_phone
+    )
   end
 end
