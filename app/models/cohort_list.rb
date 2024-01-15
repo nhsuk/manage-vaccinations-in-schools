4938a03ba6a47495c76ecd043978debd164e5e35
Theodor Vararu <theo@vararu.org>
2024-01-15 12:39:27 +0000

    Add valid CSV validation
    This checks that at a basic level the CSV is properly formatted.

--- app/models/cohort_list.rb
+++ app/models/cohort_list.rb
@@ -1,11 +1,26 @@
+require "csv"
+
 class CohortList
   include ActiveModel::Model
 
-  attr_accessor :csv
+  attr_accessor :csv, :data
 
   validates :csv, presence: true
+  validate :csv_is_valid
 
   def generate_cohort!
     # Parse the CSV and create a Patient for each row
+  end
+
+  private
+
+  def csv_is_valid
+    return if csv.blank?
+
+    self.data = CSV.parse(csv.read, headers: true, skip_blanks: true)
+  rescue CSV::MalformedCSVError
+    errors.add(:csv, :invalid)
+  ensure
+    csv.close if csv.respond_to?(:close)
   end
 end
