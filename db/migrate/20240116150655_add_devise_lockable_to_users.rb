56301f0fd81e08bbef87e80d7e33fdf4563a6327
Misha Gorodnitzky <misaka@dmfg.io>
2024-01-16 15:16:03 +0000

    Add fields for and configure devise lockable

---
+++ db/migrate/20240116150655_add_devise_lockable_to_users.rb
@@ -0,0 +1,11 @@
+class AddDeviseLockableToUsers < ActiveRecord::Migration[7.1]
+  def change
+    change_table :users, bulk: true do |t|
+      t.integer :failed_attempts, default: 0, null: false
+      t.string :unlock_token
+      t.datetime :locked_at
+
+      t.index :unlock_token, unique: true
+    end
+  end
+end
