1e4191199a14243c37ebfae4252f6ce40d507a38
Misha Gorodnitzky <misaka@dmfg.io>
2024-01-16 10:19:17 +0000

    Add confirmation fields to registrations

---
+++ db/migrate/20240116100526_add_confirmation_to_registration.rb
@@ -0,0 +1,9 @@
+class AddConfirmationToRegistration < ActiveRecord::Migration[7.1]
+  def change
+    change_table :registrations, bulk: true do |t|
+      t.boolean :terms_and_conditions_agreed
+      t.boolean :data_processing_agreed
+      t.boolean :consent_response_confirmed
+    end
+  end
+end
