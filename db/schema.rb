381f6fdfd11ac0e34e2d0015717d92c32fe1675f
Misha Gorodnitzky <misaka@dmfg.io>
2024-01-16 15:16:03 +0000

    Add fields for and configure devise lockable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_16_100526) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_16_150655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -349,8 +349,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_100526) do
     t.string "full_name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: nil
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   create_table "vaccination_records", force: :cascade do |t|
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,13 +7,16 @@
 #  current_sign_in_ip     :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
 #  full_name              :string
 #  last_sign_in_at        :datetime
 #  last_sign_in_ip        :string
+#  locked_at              :datetime
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
 #  sign_in_count          :integer          default(0), not null
+#  unlock_token           :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
@@ -21,6 +24,7 @@
 #
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_unlock_token          (unlock_token) UNIQUE
 #
 FactoryBot.define do
   factory :user do
