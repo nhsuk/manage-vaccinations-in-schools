fdaa01a9ac259411cc1564efce2d0daa8014e755
Theodor Vararu <theo@vararu.org>
2024-01-17 10:11:23 +0000

    Refactor #create method for clarity
    Use less nesting and restructure if's to be more guard-y.

--- app/controllers/pilot_controller.rb
+++ app/controllers/pilot_controller.rb
@@ -10,19 +10,18 @@ class PilotController < ApplicationController
 
   def create
     @cohort_list = CohortList.new(cohort_list_params)
+
     @cohort_list.load_data!
-
-    if @cohort_list.valid?
-      @cohort_list.parse_rows!
-
-      if @cohort_list.errors.any?
-        render :errors, status: :unprocessable_entity, layout: "application"
-      else
-        redirect_to action: :success
-      end
-    else
-      render :cohort, status: :unprocessable_entity
+    if @cohort_list.invalid?
+      return render :cohort, status: :unprocessable_entity
     end
+
+    @cohort_list.parse_rows!
+    if @cohort_list.errors.any?
+      return render :errors, status: :unprocessable_entity, layout: "application"
+    end
+
+    redirect_to action: :success
   end
 
   def success
