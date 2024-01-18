30c0b463c8f36c968003e6897d1d405338163b43
Theodor Vararu <theo@vararu.org>
2024-01-18 14:59:15 +0000

    Split out CohortListsController
    Extract this functionality from the PilotController to slim it down.

--- app/controllers/pilot_controller.rb
+++ app/controllers/pilot_controller.rb
@@ -3,39 +3,4 @@ class PilotController < ApplicationController
 
   def manage
   end
-
-  def cohort
-    @cohort_list = CohortList.new
-  end
-
-  def create
-    @cohort_list = CohortList.new(cohort_list_params)
-
-    @cohort_list.load_data!
-    if @cohort_list.invalid?
-      render :cohort, status: :unprocessable_entity
-      return
-    end
-
-    @cohort_list.parse_rows!
-    if @cohort_list.invalid?
-      render :errors, status: :unprocessable_entity, layout: "application"
-      return
-    end
-
-    @cohort_list.generate_patients!
-    session[:last_cohort_upload_count] = @cohort_list.rows.count
-
-    redirect_to action: :success
-  end
-
-  def success
-    @count = session.delete(:last_cohort_upload_count)
-  end
-
-  private
-
-  def cohort_list_params
-    params.fetch(:cohort_list, {}).permit(:csv)
-  end
 end
