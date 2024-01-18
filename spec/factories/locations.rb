b4bbcccd1c08f4dc3242059879e7cda2443a0415
Theodor Vararu <theo@vararu.org>
2024-01-18 14:13:29 +0000

    Associate team with locations when generating data

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -26,11 +26,7 @@ FactoryBot.define do
     town { Faker::Address.city }
     county { Faker::Address.county }
     postcode { Faker::Address.postcode }
-    # minimum_age { 1 }
-    # maximum_age { 1 }
     url { Faker::Internet.url }
-    # phase { "Primary" }
-    # type { "Local authority maintained schools" }
-    # detailed_type { "Voluntary aided school" }
+    team { Team.first || association(:team) }
   end
 end
