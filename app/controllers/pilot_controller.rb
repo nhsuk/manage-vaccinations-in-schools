d7331d6d9944b17b176d6784c1c1cffdd5719ecc
Theodor Vararu <theo@vararu.org>
2024-01-22 16:06:23 +0000

    Add button to download CSV of registered parent data
    Users can download data to modify and re-upload it to generate a new
    cohort.

--- app/controllers/pilot_controller.rb
+++ app/controllers/pilot_controller.rb
@@ -13,4 +13,10 @@ class PilotController < ApplicationController
         .where(location_id: current_user.team.locations.map(&:id))
         .group_by { |r| r.location.name }
   end
+
+  def download
+    registrations = Registration.where(location: current_user.team.locations)
+    csv = CohortList.from_registrations(registrations).to_csv
+    send_data(csv, filename: "registered_parents.csv")
+  end
 end
