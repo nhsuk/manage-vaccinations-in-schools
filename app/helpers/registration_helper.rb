09dd1d4d2b36cea2588cefc02fd9ecbe8c770ed2
Misha Gorodnitzky <misaka@dmfg.io>
2024-01-29 16:50:30 +0000

    Add Location#registrations association
    This allows us to turn the registrations page from an O(n) to an O(1)
    operation.

--- app/helpers/registration_helper.rb
+++
@@ -1,5 +0,0 @@
-module RegistrationHelper
-  def school_registrations(school)
-    Registration.where(location_id: school.id)
-  end
-end
--- app/models/location.rb
+++ app/models/location.rb
@@ -25,6 +25,7 @@ class Location < ApplicationRecord
   has_many :sessions
   has_many :patients
   has_many :consent_forms, through: :sessions
+  has_many :registrations
   belongs_to :team
 
   validates :name, presence: true
