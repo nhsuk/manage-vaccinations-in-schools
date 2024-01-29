4e7fea8abf6cf5c5a17cd37f625aa0f498eb7565
Misha Gorodnitzky <misaka@dmfg.io>
2024-01-29 13:13:43 +0000

    Validate session location

---
+++ app/policies/location_policy.rb
@@ -0,0 +1,12 @@
+class LocationPolicy
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      @scope.where(team: @user.team)
+    end
+  end
+end
