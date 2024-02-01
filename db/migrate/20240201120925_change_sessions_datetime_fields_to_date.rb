e0d1e78bf5c216b68e0b3cc8a91e0dc1ce97838c
Misha Gorodnitzky <misaka@dmfg.io>
2024-02-01 12:17:40 +0000

    Change sessions dates to be date (not datetime)
    datetime here in unnecessary and was causing issues with how the
    DateParamsValidator works, blocking adding advanced validations.

---
+++ db/migrate/20240201120925_change_sessions_datetime_fields_to_date.rb
@@ -0,0 +1,19 @@
+class ChangeSessionsDatetimeFieldsToDate < ActiveRecord::Migration[7.1]
+  def up
+    change_table :sessions, bulk: true do |t|
+      t.change :date, :date
+      t.change :send_consent_at, :date
+      t.change :send_reminders_at, :date
+      t.change :close_consent_at, :date
+    end
+  end
+
+  def down
+    change_table :sessions, bulk: true do |t|
+      t.change :date, :datetime
+      t.change :send_consent_at, :datetime
+      t.change :send_reminders_at, :datetime
+      t.change :close_consent_at, :datetime
+    end
+  end
+end
