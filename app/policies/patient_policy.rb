bd70a3b6b09c6030fc4964ca462a7dce1412efbb
Misha Gorodnitzky <misaka@dmfg.io>
2024-02-02 16:31:31 +0000

    Update session cohort and validate

---
+++ app/policies/patient_policy.rb
@@ -0,0 +1,12 @@
+class PatientPolicy
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      @scope.joins(:location).where(location: { team_id: @user.teams.ids })
+    end
+  end
+end
