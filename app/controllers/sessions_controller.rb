3be5864bb63ada5ba9a2afc88ecb34fb2e4120a7
Jake Benilov <benilov@gmail.com>
2024-02-05 13:47:49 +0000

    Extract session stats calculations into a separate model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,28 +18,11 @@ class SessionsController < ApplicationController
     @patient_sessions =
       @session.patient_sessions.strict_loading.includes(:campaign, :consents)
 
-    @counts = {
-      with_consent_given: 0,
-      with_consent_refused: 0,
-      without_a_response: 0,
-      needing_triage: 0,
-      ready_to_vaccinate: 0,
-      unmatched_responses: @session.location.consent_forms.unmatched.count
-    }
-
-    @patient_sessions.each do |s|
-      @counts[:with_consent_given] += 1 if s.consent_given?
-      @counts[:with_consent_refused] += 1 if s.consent_refused?
-      @counts[:without_a_response] += 1 if s.no_consent?
-
-      if s.consent_given_triage_needed? || s.triaged_kept_in_triage?
-        @counts[:needing_triage] += 1
-        @counts[:ready_to_vaccinate] += 1
-      end
-
-      @counts[:ready_to_vaccinate] += 1 if s.triaged_ready_to_vaccinate? ||
-        s.added_to_session? || s.consent_given_triage_not_needed?
-    end
+    @counts =
+      SessionStats.new(
+        patient_sessions: @patient_sessions,
+        location: @session.location
+      ).call
   end
 
   def make_in_progress
