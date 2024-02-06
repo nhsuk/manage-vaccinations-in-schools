691f861de5e8798d32c85c455d589075245c3175
Theodor Vararu <theo@vararu.org>
2024-02-06 16:45:27 +0000

    Add URN to locations
    Schools in England can have a URN.

---
+++ db/migrate/20240206163847_add_urn_to_locations.rb
@@ -0,0 +1,5 @@
+class AddUrnToLocations < ActiveRecord::Migration[7.1]
+  def change
+    add_column :locations, :urn, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_160550) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_06_163847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -234,6 +234,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_160550) do
     t.datetime "updated_at", null: false
     t.integer "team_id", null: false
     t.boolean "registration_open", default: false
+    t.string "urn"
   end
 
   create_table "offline_passwords", force: :cascade do |t|
