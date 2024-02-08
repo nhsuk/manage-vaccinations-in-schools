0a9624b5110311d73f362ad4c3dafe0b7a5d71b5
Theodor Vararu <theo@vararu.org>
2024-02-08 11:08:01 +0000

    Make phone optional when uploading cohort
    This field is optional in the expression of interest registration, it
    should be optional here too.

--- app/models/cohort_list_row.rb
+++ app/models/cohort_list_row.rb
@@ -26,7 +26,6 @@ class CohortListRow
   validates :parent_relationship, presence: true
   validates :parent_email, presence: true
   validates :parent_email, email: true, if: -> { parent_email.present? }
-  validates :parent_phone, presence: true
   validates :parent_phone, phone: true, if: -> { parent_phone.present? }
   validates :child_first_name, presence: true
   validates :child_last_name, presence: true
