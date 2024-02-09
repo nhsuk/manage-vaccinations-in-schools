95ef61e5f9a07493c7cffa24d85cdd3c9fc2abb2
Misha Gorodnitzky <misaka@dmfg.io>
2024-02-09 12:31:52 +0000

    Validate school ids in cohort list belong to team
    Otherwise someone could add a child to a school that belongs to another
    school, which would make that child visible to the team that owns that
    school.

---
+++ spec/models/cohort_list_row_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe CohortListRow, type: :model do
+  describe "school_id validations" do
+    let(:user) { create(:user, team:) }
+    let(:location) { create(:location) }
+    let(:location2) { create(:location) }
+    let(:team) { create(:team, locations: [location]) }
+
+    subject(:cohort_list_row) { described_class.new(team:) }
+
+    it { should_not allow_value(location2.id).for(:school_id) }
+  end
+end
