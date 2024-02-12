b46bbad1235b3f210233e6fb51a50fb433e0362d
Misha Gorodnitzky <misaka@dmfg.io>
2024-02-12 13:04:27 +0000

    Add registration to the pilot journey test

--- tests/pilot_journey.spec.ts
+++ tests/pilot_journey.spec.ts
@@ -13,7 +13,12 @@ let csv;
 test("Pilot journey", async ({ page }) => {
   p = page;
   await given_the_app_is_setup();
-  await and_i_am_signed_in();
+  await and_registration_is_open();
+
+  await when_i_register_for_the_pilot_as_a_parent();
+  await then_i_see_that_i_have_registered();
+
+  await given_i_am_a_nurse_signed_in();
 
   await when_i_close_the_registration_for_the_pilot();
   await then_i_see_that_registrations_are_closed();
@@ -31,13 +36,72 @@ test("Pilot journey", async ({ page }) => {
 
   await when_i_look_at_children_that_need_consent_responses();
   await then_i_see_the_children_from_the_cohort();
+
+  await when_i_click_on_the_child_we_registered();
+  await then_i_see_the_childs_details();
 });
 
 async function given_the_app_is_setup() {
   await p.goto("/reset");
 }
 
-async function and_i_am_signed_in() {
+async function and_registration_is_open() {
+  await p.goto("/flipper/features/registration_open");
+  await expect(p.getByText("Home Features registration_open")).toBeVisible();
+  if (await p.getByText("Disabled").isVisible()) {
+    await p.getByRole("button", { name: "Fully Enable" }).click();
+    await expect(p.getByText("Fully enabled")).toBeVisible();
+  }
+}
+
+async function when_i_register_for_the_pilot_as_a_parent() {
+  await p.goto("/schools/3/registration/new");
+
+  // Parent's details
+  await p.getByLabel("Your name").fill("Big Daddy Tests");
+  await p.getByLabel("Dad").check();
+  await p.getByLabel("Email address").fill("daddy.tests@example.com");
+  await p.getByLabel("Phone number").fill("07123456789");
+
+  // Child's details
+  await p.getByLabel("First name").fill("Bobby");
+  await p.getByLabel("Last name").fill("Tables");
+  await p.getByLabel("Yes").check();
+  await p.getByLabel("Preferred name").fill("Drop Table");
+  await p.getByLabel("Day").fill("01");
+  await p.getByLabel("Month").fill("01");
+  await p.getByLabel("Year").fill("2020");
+  await p.getByLabel("Address line 1").fill("1 Test Street");
+  await p.getByLabel("Address line 1").fill("2nd Floor");
+  await p.getByLabel("Town or city").fill("Testville");
+  await p.getByLabel("Postcode").fill("TE1 1ST");
+  await p.getByLabel("NHS number").fill("999 888 7777");
+
+  // Checkvoxes
+  await p.getByLabel("I agree to take part in the pilot").check();
+  await p
+    .getByLabel(
+      "I agree to share my contact details with NHS England for the purpose of administering payments and electronic communications",
+    )
+    .check();
+  await p
+    .getByLabel(
+      "I confirm I’ve responded to the school’s regular request for consent for my child’s HPV vaccination",
+    )
+    .check();
+
+  await p.click("button[type='submit']");
+}
+
+async function then_i_see_that_i_have_registered() {
+  await expect(
+    p.getByRole("heading", {
+      name: "Thank you for registering your interest in the NHS school vaccinations pilot",
+    }),
+  ).toBeVisible();
+}
+
+async function given_i_am_a_nurse_signed_in() {
   await signInTestUser(
     p,
     "nurse.florence@example.com",
@@ -82,7 +146,7 @@ async function then_i_see_the_list_of_parents_who_have_registered() {
 
 async function when_i_edit_and_upload_the_cohort_list() {
   const lines = csv.split("\n");
-  const newCsv = lines.slice(0, 4).join("\n");
+  const newCsv = lines.slice(0, 4).concat([lines[6]]).join("\n");
   const tmpCohortFile = path.resolve(__dirname, "../tmp/edited_cohort.csv");
   await fs.writeFile(tmpCohortFile, newCsv);
 
@@ -94,7 +158,7 @@ async function when_i_edit_and_upload_the_cohort_list() {
 }
 
 async function then_i_see_that_the_cohort_has_been_uploaded() {
-  await expect(p.getByText("3 children have been added")).toBeVisible();
+  await expect(p.getByText("4 children have been added")).toBeVisible();
 }
 
 async function when_i_create_a_new_session() {
@@ -150,6 +214,14 @@ async function and_select_the_children_for_the_cohort() {
     })
     .locator("input[type=checkbox]")
     .click();
+  await p
+    .locator("tr", {
+      has: p.locator("td", {
+        hasText: "Bobby Tables",
+      }),
+    })
+    .locator("input[type=checkbox]")
+    .click();
 
   await p.getByRole("button", { name: "Continue" }).click();
 }
@@ -179,7 +251,7 @@ async function then_i_see_the_session_page() {
   await expect(p.getByText(formatDate(sessionDate))).toBeVisible();
   await expect(p.getByText("0 children with consent given")).toBeVisible();
   await expect(p.getByText("0 children with consent refused")).toBeVisible();
-  await expect(p.getByText("3 children without a response")).toBeVisible();
+  await expect(p.getByText("4 children without a response")).toBeVisible();
   await expect(
     p.getByText("0 responses need matching with a parent record"),
   ).toBeVisible();
@@ -208,7 +280,30 @@ async function then_i_see_the_children_from_the_cohort() {
       hasText: fixtures.registeredChildren[2].fullName,
     }),
   ).toBeVisible();
+  await expect(
+    p.locator(".nhsuk-table__body .nhsuk-table__row", {
+      hasText: "Bobby Tables",
+    }),
+  ).toBeVisible();
   await expect(p.locator(".nhsuk-table__body .nhsuk-table__row")).toHaveCount(
-    3,
+    4,
   );
+}
+
+async function when_i_click_on_the_child_we_registered() {
+  await p.click("text=Bobby Tables");
+}
+
+async function then_i_see_the_childs_details() {
+  await expect(
+    p.locator(".nhsuk-summary-list__row", { hasText: "Name" }),
+  ).toHaveText(/Bobby Tables/);
+
+  await expect(
+    p.locator(".nhsuk-summary-list__row", { hasText: "Date of birth" }),
+  ).toHaveText(/1 January 2020/);
+
+  await expect(
+    p.locator(".nhsuk-summary-list__row", { hasText: "NHS Number" }),
+  ).toHaveText(/999 888 7777/);
 }
