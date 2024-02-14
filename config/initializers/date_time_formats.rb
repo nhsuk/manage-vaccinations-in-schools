565940603b773b0d4da12a5405dad83b61c399d4
Theodor Vararu <theo@vararu.org>
2024-02-14 10:05:03 +0000

    Add comma to day_of_week date format

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 Date::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y"
-Date::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A %-d %B %Y"
+Date::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A, %-d %B %Y"
 Date::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y"
 
 Time::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y"
-Time::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A %-d %B %Y"
+Time::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A, %-d %B %Y"
 Time::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y"
 
 Time::DATE_FORMATS[:long_ordinal_uk] = "%-d %B %Y at %l:%M%P"
