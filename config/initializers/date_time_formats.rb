719db00509795b54c680b3c3e7a473895249177c
Theodor Vararu <theo@vararu.org>
2024-02-14 10:03:47 +0000

    Consolidate date and time formats
    Keeping them in the same file is easier to maintain.

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
-Time::DATE_FORMATS[:long_ordinal_uk] = "%-d %B %Y at %l:%M%P"
+Date::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y"
+Date::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A %-d %B %Y"
+Date::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y"
+
 Time::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y"
 Time::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A %-d %B %Y"
 Time::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y"
+
+Time::DATE_FORMATS[:long_ordinal_uk] = "%-d %B %Y at %l:%M%P"
 Time::DATE_FORMATS[:time] = "%-l:%M%P"
 Time::DATE_FORMATS[:app_date_time] = "%-d %b %Y at %-l:%M%P"
 Time::DATE_FORMATS[:app_date_time_long] = "%A %-d %B %Y at %-l:%M%P"
