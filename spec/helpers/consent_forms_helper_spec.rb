b94e7585f1fb46372a576ee47deca063370c08bc
Misha Gorodnitzky <misaka@dmfg.io>
2024-02-14 16:59:04 +0100

    Include Wicked conserns in more tests
    Without this tests are flakey... we could pick out which tests need them
    and only include it before those, but with the flakiness caused, it's
    safer to just always include it for this spec file.

--- spec/helpers/consent_forms_helper_spec.rb
+++ spec/helpers/consent_forms_helper_spec.rb
@@ -3,6 +3,12 @@ require "rails_helper"
 RSpec.describe ConsentFormsHelper, type: :helper do
   include ActionView::Helpers
 
+  before do
+    # For some reason the helper doesn't include this in the test environment.
+    # There may be some other way to achieve this, but this works for now.
+    ConsentFormsHelper.include Wicked::Controller::Concerns::Path
+  end
+
   describe "#health_question_backlink_path" do
     let(:consent_form) do
       create(:consent_form, :with_health_answers_asthma_branching)
@@ -15,10 +21,6 @@ RSpec.describe ConsentFormsHelper, type: :helper do
     end
 
     before do
-      # For some reason the helper doesn't include this in the test environment.
-      # There may be some other way to achieve this, but this works for now.
-      ConsentFormsHelper.include Wicked::Controller::Concerns::Path
-
       allow(helper).to receive(:wizard_path).with(
         Wicked::FINISH_STEP
       ).and_return(final_wizard_path)
