28af39f0be876d64f88f1e9d58db1e42eb66bedb
Misha Gorodnitzky <misaka@dmfg.io>
2024-02-14 13:56:49 +0100

    Add reply_to_ids when sending consent form emails
    We can (optionally) setup SAIS team emails in GOVUK Notify and use them
    when sending out emails for the sessions they are running. This change
    allows us to put the reply-to IDs into the DB where they'll be pulled
    out and used when seding emails via GOVUK Notify.

--- app/mailers/consent_form_mailer.rb
+++ app/mailers/consent_form_mailer.rb
@@ -1,4 +1,8 @@
 class ConsentFormMailer < ApplicationMailer
+  def reply_to_id(consent_form:)
+    consent_form.session.campaign.team.reply_to_id
+  end
+
   def confirmation(consent_form)
     if consent_form.common_name.present?
       short_patient_name = consent_form.common_name
@@ -15,6 +19,7 @@ class ConsentFormMailer < ApplicationMailer
     template_mail(
       "7cda7ae5-99a2-4c40-9a3e-1863e23f7a73",
       to: consent_form.parent_email,
+      reply_to_id: reply_to_id(consent_form:),
       personalisation: {
         short_date: consent_form.session.date.strftime("%-d %B"),
         parent_name: consent_form.parent_name,
@@ -42,6 +47,7 @@ class ConsentFormMailer < ApplicationMailer
     template_mail(
       "604ee667-c996-471e-b986-79ab98d0767c",
       to: consent_form.parent_email,
+      reply_to_id: reply_to_id(consent_form:),
       personalisation: {
         short_date: consent_form.session.date.strftime("%-d %B"),
         parent_name: consent_form.parent_name,
@@ -64,6 +70,7 @@ class ConsentFormMailer < ApplicationMailer
     template_mail(
       "4d09483a-8181-4acb-8ba3-7abd6c8644cd",
       to: consent_form.parent_email,
+      reply_to_id: reply_to_id(consent_form:),
       personalisation: {
         short_date: consent_form.session.date.strftime("%-d %B"),
         parent_name: consent_form.parent_name,
@@ -91,6 +98,7 @@ class ConsentFormMailer < ApplicationMailer
     template_mail(
       "5a676dac-3385-49e4-98c2-fc6b45b5a851",
       to: consent_form.parent_email,
+      reply_to_id: reply_to_id(consent_form:),
       personalisation: {
         short_date: consent_form.session.date.strftime("%-d %B"),
         parent_name: consent_form.parent_name,
