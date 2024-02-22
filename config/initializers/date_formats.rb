7b7881c34c29f12cc0d739f782ea0d70b6bb3cb5
Misha Gorodnitzky <misaka@dmfg.io>
2024-02-22 00:08:32 +0000

    Add app_date and app_short_date date formats
    This appears to be a commonly used date format, so it makes sense to set
    it up as a standard format.

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -2,3 +2,5 @@
 Date::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y"
 Date::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A %-d %B %Y"
 Date::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y"
+Date::DATE_FORMATS[:app_date] = "%A %-d %B"
+Date::DATE_FORMATS[:app_short_date] = "%-d %B"
