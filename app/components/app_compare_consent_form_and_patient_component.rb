d1fc9bbcd8191a9b5b2f5f0a698be1e2fe7bc26c
Jake Benilov <benilov@gmail.com>
2024-02-23 17:02:07 +0000

    Add a component for comparing a consent form and a patient record

---
+++ app/components/app_compare_consent_form_and_patient_component.rb
@@ -0,0 +1,32 @@
+class AppCompareConsentFormAndPatientComponent < ViewComponent::Base
+  attr_reader :heading, :consent_form, :patient
+
+  def initialize(heading:, consent_form:, patient:)
+    super
+
+    @heading = heading
+    @consent_form = consent_form
+    @patient = patient
+  end
+
+  def name_match?
+    consent_form.full_name == patient.full_name
+  end
+
+  def date_of_birth_match?
+    consent_form.date_of_birth == patient.date_of_birth
+  end
+
+  def address_match?
+    consent_form.address_fields == patient.address_fields
+  end
+
+  def mark(text, opts)
+    if !opts[:unless] && !opts[:if]
+      tag.span(class: "nhsuk-u-visually-hidden") { "Inconsistent: " } +
+        tag.mark(text)
+    else
+      text
+    end
+  end
+end
