ce57c535f9d9956a447624aafa2277997cc5fe5e
Jake Benilov <benilov@gmail.com>
2024-02-25 00:02:10 +0000

    Run a job to try to match a consent form to a patient on submission

---
+++ app/jobs/consent_form_matching_job.rb
@@ -0,0 +1,15 @@
+class ConsentFormMatchingJob < ApplicationJob
+  queue_as :default
+
+  def perform(consent_form_id)
+    consent_form = ConsentForm.find(consent_form_id)
+
+    matching_patient = consent_form.find_matching_patient
+
+    if matching_patient
+      patient_session =
+        consent_form.session.patient_sessions.find_by(patient: matching_patient)
+      Consent.from_consent_form!(consent_form, patient_session)
+    end
+  end
+end
