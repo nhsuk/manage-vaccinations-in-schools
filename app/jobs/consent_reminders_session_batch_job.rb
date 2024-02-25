7cf411264dfec8a03200d91f82b732b40654c8f2
Misha Gorodnitzky <misaka@dmfg.io>
2024-02-25 01:42:16 +0000

    Add ConsentRemindersJob

---
+++ app/jobs/consent_reminders_session_batch_job.rb
@@ -0,0 +1,20 @@
+# This job sends consent reminders for a session.
+#
+# Each patient that hasn't been sent a consent reminder yet will be sent one.
+# Typically this should happen on the day that the session has set as the date
+# for sending consent reminders.
+#
+# It is safe to re-run this job as it marks each patient as having been sent a
+# consent reminder, however only one of these jobs should be run at a time as
+# once started this job is not concurrency-safe.
+
+class ConsentRemindersSessionBatchJob < ApplicationJob
+  queue_as :default
+
+  def perform(session)
+    session.patients.consent_not_sent.each do |patient|
+      ConsentRequestMailer.consent_reminder(session, patient).deliver_now
+      patient.update!(sent_consent_at: Time.zone.now)
+    end
+  end
+end
