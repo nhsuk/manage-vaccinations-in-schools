19c15b67d590a64a107282acb2730a94cfc27619
Jake Benilov <benilov@gmail.com>
2024-02-25 10:54:54 +0000

    Add AppCompareConsentFormAndPatientComponent spec
    This was omitted when the component was first added

---
+++ spec/components/app_compare_consent_form_and_patient_component_spec.rb
@@ -0,0 +1,99 @@
+require "rails_helper"
+
+RSpec.describe AppCompareConsentFormAndPatientComponent, type: :component do
+  let(:consent_form) do
+    create(
+      :consent_form,
+      first_name: "John",
+      last_name: "Doe",
+      date_of_birth: "2000-01-01",
+      address_line_1: "1 Main Street",
+      address_line_2: "Area",
+      address_town: "Some Town",
+      address_postcode: "AB1 2CD"
+    )
+  end
+  let(:patient) { create(:patient) }
+  let(:component) { described_class.new(heading: "", consent_form:, patient:) }
+  subject { page }
+
+  before { render_inline(component) }
+
+  describe "when the consent form matches the patient" do
+    let(:patient) do
+      create(
+        :patient,
+        first_name: consent_form.first_name,
+        last_name: consent_form.last_name,
+        date_of_birth: consent_form.date_of_birth,
+        address_line_1: consent_form.address_line_1,
+        address_line_2: consent_form.address_line_2,
+        address_town: consent_form.address_town,
+        address_postcode: consent_form.address_postcode
+      )
+    end
+
+    it "displays the key consent form details without anything being highlighted as unmatched" do
+      expect(page).to have_text(["Child’s name", "John Doe", "John Doe"].join)
+      expect(page).to have_text(
+        ["Date of birth", "1 January 2000", "1 January 2000"].join
+      )
+      expect(page).to have_text(
+        [
+          "Address",
+          "1 Main Street",
+          "Area",
+          "Some Town",
+          "AB1 2CD",
+          "1 Main Street",
+          "Area",
+          "Some Town",
+          "AB1 2CD"
+        ].join
+      )
+    end
+  end
+
+  describe "when the consent form does not match the patient" do
+    let(:patient) do
+      create(
+        :patient,
+        first_name: "Jane", # different
+        last_name: consent_form.last_name,
+        date_of_birth: "2000-01-02", #different
+        address_line_1: "2 Main Street", # different
+        address_line_2: consent_form.address_line_2,
+        address_town: consent_form.address_town,
+        address_postcode: consent_form.address_postcode
+      )
+    end
+
+    it "displays the key consent form details with the unmatched details highlighted" do
+      expect(page).to have_text(
+        ["Child’s name", "Inconsistent: ", "John Doe", "Jane Doe"].join
+      )
+      expect(page).to have_text(
+        [
+          "Date of birth",
+          "Inconsistent: ",
+          "1 January 2000",
+          "2 January 2000"
+        ].join
+      )
+      expect(page).to have_text(
+        [
+          "Address",
+          "Inconsistent: ",
+          "1 Main Street",
+          "Area",
+          "Some Town",
+          "AB1 2CD",
+          "2 Main Street",
+          "Area",
+          "Some Town",
+          "AB1 2CD"
+        ].join
+      )
+    end
+  end
+end
