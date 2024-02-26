e16ff47d33f2256142c2592dca781857e4c9597c
Theodor Vararu <theo@vararu.org>
2024-02-26 17:12:05 +0000

    Add give_feedback consent form mailer template
    This will be sent to parents 1 hour after they submit a consent form.

--- spec/mailers/consent_form_mailer_spec.rb
+++ spec/mailers/consent_form_mailer_spec.rb
@@ -2,18 +2,35 @@ require "rails_helper"
 
 RSpec.describe ConsentFormMailer, type: :mailer do
   describe "#confirmation_injection" do
-    it "calls template_mail with correct personalisation" do
+    it "calls template_mail with correct reason_for_refusal" do
       mail =
         described_class.confirmation_injection(
           build(
             :consent_form,
             reason: :contains_gelatine,
+            recorded_at: Date.new(2021, 1, 1),
             session: create(:session, campaign: create(:campaign, :flu))
           )
         )
 
       expect(mail.message.header["personalisation"].unparsed_value).to include(
         reason_for_refusal: "of the gelatine in the nasal spray"
+      )
+    end
+  end
+
+  describe "#give_feedback" do
+    it "calls template_mail with correct survey_deadline_date" do
+      mail =
+        described_class.give_feedback(
+          build(
+            :consent_form,
+            recorded_at: Date.new(2021, 1, 1),
+          )
+        )
+
+      expect(mail.message.header["personalisation"].unparsed_value).to include(
+        survey_deadline_date: "8 January 2021"
       )
     end
   end
