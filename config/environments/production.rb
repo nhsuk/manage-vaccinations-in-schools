1d87c356547f7608c5327ca411a95dc266e65fcb
Theodor Vararu <theo@vararu.org>
2024-02-27 15:19:55 +0000

    Revert "Run good_job in external mode using supervisor"
    This reverts commit 5ab873112024b5a0493ae5cb28a8097f5288aac4.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,6 @@ Rails.application.configure do
   }
 
   config.good_job.enable_cron = true
-  config.good_job.execution_mode = :external
   config.good_job.cron = {
     consent_request: {
       cron: "every day at 9am",
