aca568d45b59d81bbb6b59d0c7c45fe4512dcf95
Theodor Vararu <theo@vararu.org>
2024-02-27 13:29:24 +0000

    Disable ConsentFormMatchingJob scheduler
    This also fails down the line and is possibly blocking users from
    finishing.

--- app/controllers/concerns/consent_form_mailer_concern.rb
+++ app/controllers/concerns/consent_form_mailer_concern.rb
@@ -2,16 +2,16 @@ module ConsentFormMailerConcern
   extend ActiveSupport::Concern
 
   def send_record_mail(consent_form)
-    # if consent_form.contact_injection?
-    #   ConsentFormMailer.confirmation_injection(consent_form).deliver_later
-    # elsif consent_form.consent_refused?
-    #   ConsentFormMailer.confirmation_refused(consent_form).deliver_later
-    # elsif consent_form.needs_triage?
-    #   ConsentFormMailer.confirmation_needs_triage(consent_form).deliver_later
-    # else
-    #   ConsentFormMailer.confirmation(consent_form).deliver_later
-    # end
-    #
-    # ConsentFormMailer.give_feedback(consent_form).deliver_later(wait: 1.hour)
+    if consent_form.contact_injection?
+      ConsentFormMailer.confirmation_injection(consent_form).deliver_later
+    elsif consent_form.consent_refused?
+      ConsentFormMailer.confirmation_refused(consent_form).deliver_later
+    elsif consent_form.needs_triage?
+      ConsentFormMailer.confirmation_needs_triage(consent_form).deliver_later
+    else
+      ConsentFormMailer.confirmation(consent_form).deliver_later
+    end
+
+    ConsentFormMailer.give_feedback(consent_form).deliver_later(wait: 1.hour)
   end
 end
