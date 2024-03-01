3e6d2634f31d400d65b31298309c2883c5f20842
Theodor Vararu <theo@vararu.org>
2024-03-01 12:30:23 +0000

    Add recorded_by field to Consent
    This will be used to track which nurse recorded verbal consent.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_150456) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_122902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -110,8 +110,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_150456) do
     t.datetime "updated_at", null: false
     t.datetime "recorded_at"
     t.jsonb "health_answers", default: []
+    t.bigint "recorded_by_user_id"
     t.index ["campaign_id"], name: "index_consents_on_campaign_id"
     t.index ["patient_id"], name: "index_consents_on_patient_id"
+    t.index ["recorded_by_user_id"], name: "index_consents_on_recorded_by_user_id"
   end
 
   create_table "flipper_features", force: :cascade do |t|
@@ -408,6 +410,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_150456) do
   add_foreign_key "consent_forms", "sessions"
   add_foreign_key "consents", "campaigns"
   add_foreign_key "consents", "patients"
+  add_foreign_key "consents", "users", column: "recorded_by_user_id"
   add_foreign_key "health_questions", "health_questions", column: "follow_up_question_id"
   add_foreign_key "health_questions", "health_questions", column: "next_question_id"
   add_foreign_key "health_questions", "vaccines"
