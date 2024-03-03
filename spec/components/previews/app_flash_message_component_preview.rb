1c2948d404ca9fe01d5541d7741c9ab94c991c44
Jake Benilov <benilov@gmail.com>
2024-03-03 14:50:13 +0000

    Add the first (basic) preview for AppFlashMessageComponent

---
+++ spec/components/previews/app_flash_message_component_preview.rb
@@ -0,0 +1,16 @@
+class AppFlashMessageComponentPreview < ViewComponent::Preview
+  def success
+    render AppFlashMessageComponent.new(
+             flash: {
+               success: {
+                 heading: "Record saved for John Smith",
+                 body:
+                   ActionController::Base.helpers.link_to(
+                     "View child record",
+                     "https://example.com"
+                   )
+               }
+             }
+           )
+  end
+end
