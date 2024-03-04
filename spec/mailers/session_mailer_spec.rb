cedeb8c37006874f1dce4135646daee1603df539
Misha Gorodnitzky <misaka@dmfg.io>
2024-03-04 14:49:13 +0000

    Add SessionMailer
    This will be used to send out reminders to parents about upcoming
    vaccinations

---
+++ spec/mailers/session_mailer_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe SessionMailer, type: :mailer do
+  describe "#session_reminder" do
+    let(:patient) { create(:patient, common_name: "Joey") }
+    let(:session) { create(:session, patients: [patient]) }
+    subject(:mail) { SessionMailer.session_reminder(session:, patient:) }
+
+    it { should have_attributes(to: [patient.parent_email]) }
+
+    describe "personalisation" do
+      subject { mail.message.header["personalisation"].unparsed_value }
+
+      it "sets the personalisation" do
+        expect(subject.keys).to include(
+          :full_and_preferred_patient_name,
+          :location_name,
+          :long_date,
+          :parent_name,
+          :short_patient_name,
+          :short_patient_name_apos,
+          :team_email,
+          :team_name,
+          :team_phone,
+          :vaccination
+        )
+      end
+    end
+  end
+end
