f28759ce51f25f84335aec41d46c101d47427494
Misha Gorodnitzky <misaka@dmfg.io>
2024-03-04 00:30:43 +0000

    Update ConsentFormMailer to accept consent object
    We will be sending consent emails during the verbal consent journey,
    where we won't have a consent_form available, so we need this mailer to
    work with both ConsentForm and Consent objects.

--- app/controllers/concerns/consent_form_mailer_concern.rb
+++ app/controllers/concerns/consent_form_mailer_concern.rb
@@ -3,15 +3,15 @@ module ConsentFormMailerConcern
 
   def send_record_mail(consent_form)
     if consent_form.contact_injection?
-      ConsentFormMailer.confirmation_injection(consent_form).deliver_later
+      ConsentFormMailer.confirmation_injection(consent_form:).deliver_later
     elsif consent_form.consent_refused?
-      ConsentFormMailer.confirmation_refused(consent_form).deliver_later
+      ConsentFormMailer.confirmation_refused(consent_form:).deliver_later
     elsif consent_form.needs_triage?
-      ConsentFormMailer.confirmation_needs_triage(consent_form).deliver_later
+      ConsentFormMailer.confirmation_needs_triage(consent_form:).deliver_later
     else
-      ConsentFormMailer.confirmation(consent_form).deliver_later
+      ConsentFormMailer.confirmation(consent_form:).deliver_later
     end
 
-    ConsentFormMailer.give_feedback(consent_form).deliver_later(wait: 1.hour)
+    ConsentFormMailer.give_feedback(consent_form:).deliver_later(wait: 1.hour)
   end
 end
--- app/mailers/consent_form_mailer.rb
+++ app/mailers/consent_form_mailer.rb
@@ -1,30 +1,46 @@
 class ConsentFormMailer < ApplicationMailer
-  def confirmation(consent_form)
-    template_mail("7cda7ae5-99a2-4c40-9a3e-1863e23f7a73", **opts(consent_form))
+  def confirmation(consent_form: nil, consent: nil, session: nil)
+    template_mail(
+      "7cda7ae5-99a2-4c40-9a3e-1863e23f7a73",
+      **opts(consent_form:, consent:, session:)
+    )
   end
 
-  def confirmation_needs_triage(consent_form)
-    template_mail("604ee667-c996-471e-b986-79ab98d0767c", **opts(consent_form))
+  def confirmation_needs_triage(consent_form: nil, consent: nil, session: nil)
+    template_mail(
+      "604ee667-c996-471e-b986-79ab98d0767c",
+      **opts(consent_form:, consent:, session:)
+    )
   end
 
-  def confirmation_injection(consent_form)
-    template_mail("4d09483a-8181-4acb-8ba3-7abd6c8644cd", **opts(consent_form))
+  def confirmation_injection(consent_form: nil, consent: nil, session: nil)
+    template_mail(
+      "4d09483a-8181-4acb-8ba3-7abd6c8644cd",
+      **opts(consent_form:, consent:, session:)
+    )
   end
 
-  def confirmation_refused(consent_form)
-    template_mail("5a676dac-3385-49e4-98c2-fc6b45b5a851", **opts(consent_form))
+  def confirmation_refused(consent_form: nil, consent: nil, session: nil)
+    template_mail(
+      "5a676dac-3385-49e4-98c2-fc6b45b5a851",
+      **opts(consent_form:, consent:, session:)
+    )
   end
 
-  def give_feedback(consent_form)
-    template_mail("1250c83b-2a5a-4456-8922-657946eba1fd", **opts(consent_form))
+  def give_feedback(consent_form: nil, consent: nil, session: nil)
+    template_mail(
+      "1250c83b-2a5a-4456-8922-657946eba1fd",
+      **opts(consent_form:, consent:, session:)
+    )
   end
 
   private
 
-  def opts(consent_form)
+  def opts(consent_form:, consent:, session: nil)
     @consent_form = consent_form
-    @patient = consent_form
-    @session = consent_form.session
+    @consent = consent
+    @patient = consent_form || consent.patient
+    @session = session || consent_form.session
 
     { to:, reply_to_id:, personalisation: consent_form_personalisation }
   end
@@ -34,10 +50,12 @@ class ConsentFormMailer < ApplicationMailer
   end
 
   def reason_for_refusal
-    I18n.t("consent_form_mailer.reasons_for_refusal.#{@consent_form.reason}")
+    reason = @consent_form&.reason || @consent&.reason_for_refusal
+    I18n.t("consent_form_mailer.reasons_for_refusal.#{reason}")
   end
 
   def survey_deadline_date
+    return unless @consent_form
     (@consent_form.recorded_at + 7.days).to_fs(:nhsuk_date)
   end
 end
