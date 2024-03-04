79bc516b6cad0e114a9df77263fcec08e9e27a3b
Misha Gorodnitzky <misaka@dmfg.io>
2024-03-04 16:51:35 +0000

    Only send session reminder if not already sent

--- app/jobs/session_reminders_batch_job.rb
+++ app/jobs/session_reminders_batch_job.rb
@@ -2,8 +2,9 @@ class SessionRemindersBatchJob < ApplicationJob
   queue_as :default
 
   def perform(session)
-    session.patients.each do |patient|
+    session.patients.not_reminded_about_session.each do |patient|
       SessionMailer.session_reminder(session:, patient:).deliver_now
+      patient.update!(session_reminder_sent_at: Time.zone.now)
     end
   end
 end
--- spec/jobs/session_reminders_batch_job_spec.rb
+++ spec/jobs/session_reminders_batch_job_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe SessionRemindersBatchJob, type: :job do
 
     expect(ActionMailer::Base.deliveries.count).to eq(1)
   end
+
+  it "does not send a reminder if one has already been sent" do
+    patient = create(:patient, session_reminder_sent_at: Time.zone.now)
+    session = create(:session, patients: [patient], date: 1.day.from_now)
+
+    expect { described_class.perform_now(session) }.not_to send_email(
+      to: patient.parent_email
+    )
+  end
+
+  it "updates the session_reminder_sent_at attribute for patients" do
+    patient = create(:patient)
+    session = create(:session, patients: [patient], date: 1.day.from_now)
+
+    expect { described_class.perform_now(session) }.to(
+      change { patient.reload.session_reminder_sent_at }
+    )
+  end
 end
