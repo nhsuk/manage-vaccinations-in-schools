1bc3f4b0ecb2ab97a94d6d9b09c8c24446078257
Misha Gorodnitzky <misaka@dmfg.io>
2024-03-04 16:08:57 +0000

    Add SessionRemindersJob
    This starts up a job to send out reminder emails to parents for all
    sessions happening tomorrow.

---
+++ app/jobs/session_reminders_job.rb
@@ -0,0 +1,16 @@
+# This job triggers a job to send a batch of consent reminders for each sessions
+# that needs them sent today.
+
+class SessionRemindersJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    return unless Flipper.enabled?(:scheduled_emails)
+
+    Session.active.each do |session|
+      if session.date == Time.zone.tomorrow
+        SessionRemindersBatchJob.perform_later(session)
+      end
+    end
+  end
+end
---
+++ spec/jobs/session_reminders_job_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe SessionRemindersJob do
+  before do
+    Flipper.enable(:scheduled_emails)
+    ActiveJob::Base.queue_adapter.enqueued_jobs.clear
+  end
+
+  it "enqueues SessionRemdindersJob for each session happening tomorrow" do
+    tomorrow_session = create(:session, date: Date.tomorrow)
+    _todays_session = create(:session, date: Time.zone.today)
+    _yesterdays_session = create(:session, date: 2.days.from_now)
+
+    described_class.perform_now
+    expect(SessionRemindersBatchJob).to have_been_enqueued.once
+    expect(SessionRemindersBatchJob).to have_been_enqueued.with(
+      tomorrow_session
+    )
+  end
+
+  context "with draft and active sessions" do
+    it "enqueues ConsentRemindersSessionBatchJob for each active sessions" do
+      active_session = create(:session, draft: false, date: Date.tomorrow)
+      _draft_session = create(:session, draft: true, date: Date.tomorrow)
+
+      described_class.perform_now
+      expect(SessionRemindersBatchJob).to have_been_enqueued.once
+      expect(SessionRemindersBatchJob).to have_been_enqueued.with(
+        active_session
+      )
+    end
+  end
+end
