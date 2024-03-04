8b817454d6eca071cbdff2f6de9dbc15013c2c61
Theodor Vararu <theo@vararu.org>
2024-03-04 12:18:34 +0000

    Remove production checks
    We're running in production now and it feels unlikely that we'd ship an
    environment without Sentry setup given that we use the same IAAS
    templates for everything.

--- config/initializers/check_production.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.production? && ENV["SKIP_PRODUCTION_CHECKS"].blank? &&
-     ENV.fetch("SENTRY_DSN", "").blank?
-  raise "Application is running in production mode but SENTRY_DSN is not set
-    up. Please set up Sentry."
-end
