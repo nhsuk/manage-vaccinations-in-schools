1c90a703cfb85c0cfe9fa64a6228aa8d9b93beba
Jake Benilov <benilov@gmail.com>
2024-03-04 16:36:09 +0000

    Set the Faker locale for ViewComponent previews
    This is likely to be needed in the majority of previews, due to their reliance on fake data and factories

--- app/controllers/component_previews_controller.rb
+++ app/controllers/component_previews_controller.rb
@@ -4,11 +4,16 @@ class ComponentPreviewsController < ApplicationController
   skip_after_action :verify_policy_scoped
 
   around_action :wrap_in_rollbackable_transaction
+  before_action :set_up_faker_locale
 
   def wrap_in_rollbackable_transaction
     ActiveRecord::Base.transaction do
       yield
       raise ActiveRecord::Rollback
     end
+  end
+
+  def set_up_faker_locale
+    Faker::Config.locale = "en-GB"
   end
 end
