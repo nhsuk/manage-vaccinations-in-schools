2419995ae738dfdf9e65dc4f8e6cf24c17762d8f
Jake Benilov <benilov@gmail.com>
2024-03-04 19:32:17 +0000

    Convert AppConsentResponseComponent to use ERB for clarity
    The declarative Ruby-based generation of HTML was a bit hard to scan. It was also not immediately obvious that the component dealt with multiple consents (although the initize parameter should have probably made it clear to me.
    
    Minor spec changes due to different whitespace.

--- app/components/app_consent_response_component.rb
+++ app/components/app_consent_response_component.rb
@@ -1,68 +1,62 @@
 class AppConsentResponseComponent < ViewComponent::Base
-  def call
-    response_contents =
-      @consents.map do |consent|
-        [
-          tag.p(class: "nhsuk-body") do
-            "#{response(consent:)} (#{route(consent:)})"
-          end,
-          tag.p(class: date_and_time_p_classes) do
-            [
-              recorded_by_link(consent:),
-              date_and_time(consent.recorded_at)
-            ].join("<br />").html_safe
-          end
-        ].join("\n").html_safe
+  class AppSingleConsentResponseComponent < ViewComponent::Base
+    erb_template <<-ERB
+      <p class="nhsuk-body">
+        <%= response %> (<%= route %>)
+      </p>
+      <p class="nhsuk-u-margin-bottom-2 nhsuk-u-secondary-text-color nhsuk-u-font-size-16 nhsuk-u-margin-bottom-0">
+        <%= recorded_by_link %><br />
+        <%= consent.recorded_at.to_fs(:nhsuk_date) %> at <%= consent.recorded_at.to_fs(:time) %>
+      </p>
+    ERB
+
+    attr_reader :consent
+
+    def initialize(consent:)
+      super
+      @consent = consent
+    end
+
+    def response
+      consent.human_enum_name(:response).capitalize
+    end
+
+    def route
+      if consent.respond_to?(:route)
+        consent.human_enum_name(:route).downcase
+      else
+        "online"
       end
+    end
 
-    return response_contents.first if response_contents.size == 1
-
-    tag.ul(class: "nhsuk-list nhsuk-list--bullet app-list--events") do
-      safe_join(response_contents.map { |content| tag.li(content) }, "\n")
+    def recorded_by_link
+      if consent.respond_to?(:via_phone?) && consent.via_phone?
+        link_to(
+          consent.recorded_by.full_name,
+          "mailto:#{consent.recorded_by.email}"
+        )
+      else
+        link_to(consent.parent_name, "mailto:#{consent.parent_email}")
+      end
     end
   end
+
+  erb_template <<-ERB
+    <% if @consents.size == 1 %>
+      <%= render(AppSingleConsentResponseComponent.new(consent: @consents.first)) %>
+    <% elsif @consents.size > 1 %>
+      <ul class="nhsuk-list nhsuk-list--bullet app-list--events">
+        <% @consents.each do |consent| %>
+          <li>
+            <%= render(AppSingleConsentResponseComponent.new(consent: consent)) %>
+          </li>
+        <% end %>
+      </ul>
+    <% end %>
+  ERB
 
   def initialize(consents:)
     super
-
     @consents = consents
-  end
-
-  private
-
-  def date_and_time_p_classes
-    %w[
-      nhsuk-u-margin-bottom-2
-      nhsuk-u-secondary-text-color
-      nhsuk-u-font-size-16
-      nhsuk-u-margin-bottom-0
-    ].join(" ")
-  end
-
-  def date_and_time(date)
-    %(#{date.to_fs(:nhsuk_date)} at #{date.to_fs(:time)}).html_safe
-  end
-
-  def response(consent:)
-    consent.human_enum_name(:response).capitalize
-  end
-
-  def route(consent:)
-    if consent.respond_to?(:route)
-      consent.human_enum_name(:route).downcase
-    else
-      "online"
-    end
-  end
-
-  def recorded_by_link(consent:)
-    if consent.respond_to?(:via_phone?) && consent.via_phone?
-      link_to(
-        consent.recorded_by.full_name,
-        "mailto:#{consent.recorded_by.email}"
-      )
-    else
-      link_to(consent.parent_name, "mailto:#{consent.parent_email}")
-    end
   end
 end
