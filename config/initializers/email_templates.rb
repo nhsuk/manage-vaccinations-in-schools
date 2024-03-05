20a7d7e5dbf29219ae43df5c45c3f992b030237c
Misha Gorodnitzky <misaka@dmfg.io>
2024-03-05 17:17:11 +0000

    Add mailer for when hpv vaccination didn't happen

--- config/initializers/email_templates.rb
+++ config/initializers/email_templates.rb
@@ -1,4 +1,6 @@
 EMAIL_TEMPLATES = {
   confirmation_the_hpv_vaccination_has_taken_place:
-    "8a65d7b5-045c-4f26-8f76-6e593c14cb6d"
+    "8a65d7b5-045c-4f26-8f76-6e593c14cb6d",
+  confirming_the_hpv_vaccination_didnt_happen:
+    "130fe52a-014a-45dd-9f53-8e65c1b8bb79"
 }.freeze
