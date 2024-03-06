197b27cb51f9dffd632fb63c9c8fb755f7ee1099
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-03-06 11:25:44 +0000

    Remove unused GOV.UK Frontend controllers

--- app/javascript/controllers/govuk_header_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-import { Header } from "govuk-frontend";
-
-// Connects to data-module="govuk-header"
-export default class extends Controller {
-  connect() {
-    new Header(this.element).init();
-  }
-}
