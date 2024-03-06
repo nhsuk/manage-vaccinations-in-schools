7fc7195d7d05c673cb83e3aa95c8ead7a0c36791
Jake Benilov <benilov@gmail.com>
2024-03-06 18:47:45 +0000

    Add patient banner preview for delayed vaccination
    Add missing patient_session factory trait

--- spec/components/previews/app_status_banner_component_preview.rb
+++ spec/components/previews/app_status_banner_component_preview.rb
@@ -1,4 +1,10 @@
 class AppStatusBannerComponentPreview < ViewComponent::Preview
+  def triaged_out_delay_vaccination
+    patient_session = FactoryBot.create(:patient_session, :delay_vaccination)
+
+    render AppStatusBannerComponent.new(patient_session:)
+  end
+
   def unable_to_vaccinate_with_contradications
     patient_session = FactoryBot.create(:patient_session, :unable_to_vaccinate)
 
