f0d008995dc0caeaa61c5ee0f63eb1538360e034
Jake Benilov <benilov@gmail.com>
2024-03-06 22:55:49 +0000

    Split up AppStatusBannerComponent
    The outcome banner logic goes into AppOutcomeBannerComponent,
    the simple banner logic goes into AppSimpleOutcomeBannerComponent

---
+++ app/components/app_simple_status_banner_component.rb
@@ -0,0 +1,68 @@
+class AppSimpleStatusBannerComponent < ViewComponent::Base
+  delegate :state, to: :@patient_session
+
+  def initialize(patient_session:)
+    super
+
+    @patient_session = patient_session
+  end
+
+  def call
+    render AppCardComponent.new(heading:, feature: true, colour:) do
+      tag.p do
+        I18n.t(
+          "patient_session_statuses.#{state}.banner_explanation",
+          default: "",
+          full_name:,
+          triage_nurse:,
+          who_responded:,
+          who_refused:
+        )
+      end
+    end
+  end
+
+  private
+
+  def consent
+    # HACK: Component needs to be updated to work with multiple consents.
+    @consent ||= @patient_session.consents.first
+  end
+
+  def most_recent_triage
+    @most_recent_triage ||= @patient_session.triage.order(:created_at).last
+  end
+
+  def who_responded
+    consent&.who_responded&.downcase
+  end
+
+  def who_refused
+    @patient_session
+      .consents
+      .response_refused
+      .map(&:who_responded)
+      .last
+      &.capitalize
+  end
+
+  def full_name
+    @patient_session.patient.full_name
+  end
+
+  def triage_nurse
+    most_recent_triage&.user&.full_name
+  end
+
+  def state
+    @patient_session.state
+  end
+
+  def heading
+    I18n.t("patient_session_statuses.#{state}.banner_title")
+  end
+
+  def colour
+    I18n.t("patient_session_statuses.#{state}.colour")
+  end
+end
---
+++ spec/components/app_simple_status_banner_component_spec.rb
@@ -0,0 +1,72 @@
+require "rails_helper"
+
+RSpec.describe AppSimpleStatusBannerComponent, type: :component do
+  let(:user) { create :user }
+  let(:patient_session) { create :patient_session, user: }
+  let(:component) { described_class.new(patient_session:) }
+  let!(:rendered) { render_inline(component) }
+  let(:triage_nurse_name) { patient_session.triage.last.user.full_name }
+  let(:patient_name) { patient_session.patient.full_name }
+
+  subject { page }
+
+  prepend_before do
+    patient_session.patient.update!(first_name: "Alya", last_name: "Merton")
+  end
+
+  context "state is added_to_session" do
+    let(:patient_session) { create :patient_session, :added_to_session }
+
+    it { should have_css(".app-card--blue") }
+  end
+
+  context "state is consent_given_triage_not_needed" do
+    let(:patient_session) do
+      create :patient_session, :consent_given_triage_not_needed
+    end
+
+    it { should have_css(".app-card--purple") }
+    it { should have_css(".nhsuk-card__heading", text: "Consent given") }
+    it { should have_text("#{patient_name} is ready to vaccinate") }
+  end
+
+  context "state is consent_given_triage_needed" do
+    let(:patient_session) do
+      create :patient_session, :consent_given_triage_needed
+    end
+
+    it { should have_css(".app-card--blue") }
+    it { should have_css(".nhsuk-card__heading", text: "Needs triage") }
+    it { should have_text("Responses to health questions need triage") }
+  end
+
+  context "state is consent_refused" do
+    let(:patient_session) { create :patient_session, :consent_refused }
+
+    it { should have_css(".app-card--orange") }
+    it { should have_css(".nhsuk-card__heading", text: "Consent refused") }
+    it { should have_text("Mum refused to give consent") }
+  end
+
+  context "state is triaged_kept_in_triage" do
+    let(:patient_session) { create :patient_session, :triaged_kept_in_triage }
+
+    it { should have_css(".app-card--blue") }
+    it { should have_css(".nhsuk-card__heading", text: "Needs triage") }
+    it { should have_text("Responses to health questions need triage") }
+  end
+
+  context "state is triaged_ready_to_vaccinate" do
+    let(:patient_session) do
+      create :patient_session, :triaged_ready_to_vaccinate
+    end
+
+    it { should have_css(".app-card--purple") }
+    it { should have_css(".nhsuk-card__heading", text: "Safe to vaccinate") }
+    it do
+      should have_text(
+               "#{triage_nurse_name} decided that #{patient_name} is safe to vaccinate"
+             )
+    end
+  end
+end
