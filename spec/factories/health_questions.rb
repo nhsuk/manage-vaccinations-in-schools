16717ba6cdf69c0bfe2f8d1286fafb4f228c4612
Theodor Vararu <theo@vararu.org>
2024-03-07 12:30:22 +0000

    Improve health question and vaccine factories
    Make the different health questions for the pilot traits on the health
    question factory.
    
    Then, use these traited health questions in the vaccine factories.

--- spec/factories/health_questions.rb
+++ spec/factories/health_questions.rb
@@ -26,6 +26,77 @@
 #
 FactoryBot.define do
   factory :health_question do
-    vaccine { create :vaccine }
+    vaccine { create :vaccine, :hpv }
+
+    # HPV vaccine questions
+    trait :severe_allergies do
+      question { "Does your child have any severe allergies?" }
+    end
+
+    trait :medical_conditions do
+      question do
+        "Does your child have any medical conditions for which they receive treatment?"
+      end
+    end
+
+    trait :severe_reaction do
+      question do
+        "Has your child ever had a severe reaction to any medicines, including vaccines?"
+      end
+    end
+
+    # Flu vaccine questions
+    trait :asthma do
+      question { "Has your child been diagnosed with asthma?" }
+    end
+
+    trait :steroids do
+      question { "Have they taken oral steroids in the last 2 weeks?" }
+    end
+
+    trait :intensive_care do
+      question { "Have they been admitted to intensive care for their asthma?" }
+    end
+
+    trait :flu_vaccination do
+      question { "Has your child had a flu vaccination in the last 5 months?" }
+    end
+
+    trait :immune_system do
+      question do
+        "Does your child have a disease or treatment that severely affects their immune system?"
+      end
+      hint do
+        "For example, treatment for leukaemia or taking immunosuppressant medication"
+      end
+    end
+
+    trait :household_immune_system do
+      question do
+        "Is anyone in your household currently having treatment that severely affects their immune system?"
+      end
+      hint { "For example, they need to be kept in isolation" }
+    end
+
+    trait :egg_allergy do
+      question do
+        "Has your child ever been admitted to intensive care due to an allergic reaction to egg?"
+      end
+    end
+
+    trait :allergies do
+      question { "Does your child have any allergies to medication?" }
+    end
+
+    trait :reaction do
+      question do
+        "Has your child ever had a reaction to previous vaccinations?"
+      end
+    end
+
+    trait :aspirin do
+      question { "Does you child take regular aspirin?" }
+      hint { "Also known as Salicylate therapy" }
+    end
   end
 end
