5fece153e9e9569f23c97cab09e71f9e1962f1e5
Misha Gorodnitzky <misaka@dmfg.io>
2024-03-12 17:24:39 +0000

    Take user to consent section after doing triage
    ... when they started from the consent section.

---
+++ spec/features/triage_during_consent_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+
+RSpec.describe "Triage" do
+  include EmailExpectations
+
+  scenario "during consent" do
+    given_a_campaign_with_a_running_session
+    and_a_patient_needing_triage
+    and_i_am_signed_in
+
+    when_i_go_to_the_given_tab_of_the_consents_page
+    and_i_go_to_the_patient_that_needs_triage
+    and_i_record_that_they_are_safe_to_vaccinate
+    then_i_see_the_consents_page
+  end
+
+  def given_a_campaign_with_a_running_session
+    @team = create(:team, :with_one_nurse, :with_one_location)
+    @campaign = create(:campaign, :hpv, team: @team)
+    @batch = @campaign.batches.first
+    @session =
+      create(:session, campaign: @campaign, location: @team.locations.first)
+  end
+
+  def and_a_patient_needing_triage
+    @patient =
+      create(
+        :patient_with_consent_given_triage_needed,
+        session: @session,
+        location: @session.location
+      )
+  end
+
+  def and_i_am_signed_in
+    sign_in @team.users.first
+  end
+
+  def when_i_go_to_the_given_tab_of_the_consents_page
+    visit consents_session_path(@session, anchor: "given")
+  end
+
+  def and_i_go_to_the_patient_that_needs_triage
+    click_link @patient.full_name
+  end
+
+  def and_i_record_that_they_are_safe_to_vaccinate
+    choose "Yes, it’s safe to vaccinate"
+    click_button "Save triage"
+  end
+
+  def then_i_see_the_consents_page
+    expect(page).to have_content("Check consent responses")
+  end
+end
