2c83d294298d07815b37d0573825b503158117a7
Jake Benilov <benilov@gmail.com>
2024-03-13 15:12:02 +0000

    For patient tables, take the status badge colour from the patient_session_statuses.en.yml
    The helper matches the values exactly:
    
    [12] pry(main)> I18n.t("patient_session_statuses").each { |status, opts| puts "#{status} | #{I18n.t("patient_session_statuses.#{status}.colour")} | #{PatientSessionHelper::STATUS_COLOURS_AND_TEXT[status][:colour]}" }
    added_to_session | blue | blue
    consent_given_triage_needed | blue | blue
    consent_given_triage_not_needed | purple | purple
    consent_refused | orange | orange
    consent_conflicts | orange | orange
    delay_vaccination | red | red
    triaged_do_not_vaccinate | red | red
    triaged_kept_in_triage | blue | blue
    triaged_ready_to_vaccinate | purple | purple
    unable_to_vaccinate | red | red
    unable_to_vaccinate_not_assessed | red | red
    unable_to_vaccinate_not_gillick_competent | red | red
    vaccinated | green | green

--- app/helpers/patient_session_helper.rb
+++ app/helpers/patient_session_helper.rb
@@ -1,72 +1,55 @@
 module PatientSessionHelper
   STATUS_COLOURS_AND_TEXT = {
     added_to_session: {
-      colour: "blue",
       text: "Get consent",
       banner_title: "No-one responded to our requests for consent"
     },
     consent_given_triage_needed: {
-      colour: "blue",
       text: "Triage",
       banner_title: "Triage needed"
     },
     consent_given_triage_not_needed: {
-      colour: "purple",
       text: "Vaccinate"
     },
     consent_refused: {
-      colour: "orange",
       text: "Check refusal",
       banner_title: "Their %{who_responded} has refused to give consent"
     },
     consent_conflicts: {
-      colour: "orange",
       text: "Check conflicting consent"
     },
     delay_vaccination: {
-      colour: "red",
       text: "Delay vaccination to a later date",
       banner_title: "Delay vaccination to a later date"
     },
     triaged_do_not_vaccinate: {
-      colour: "red",
       text: "Do not vaccinate",
       banner_title: "Do not vaccinate"
     },
     triaged_kept_in_triage: {
-      colour: "blue",
       text: "Triage started",
       banner_title: "Triage started"
     },
     triaged_ready_to_vaccinate: {
-      colour: "purple",
       text: "Vaccinate"
     },
     unable_to_vaccinate: {
-      colour: "red",
       text: "Unable to vaccinate",
       banner_title: "Could not vaccinate"
     },
     unable_to_vaccinate_not_assessed: {
-      colour: "red",
       text: "Not vaccinated",
       banner_title: "Not vaccinated"
     },
     unable_to_vaccinate_not_gillick_competent: {
-      colour: "red",
       text: "Not vaccinated",
       banner_title: "Not vaccinated"
     },
     vaccinated: {
-      colour: "green",
       text: "Vaccinated",
       banner_title: "Vaccinated"
     }
   }.with_indifferent_access.freeze
-
-  def status_colour_for_state(state)
-    STATUS_COLOURS_AND_TEXT[state][:colour]
-  end
 
   def status_text_for_state(state)
     STATUS_COLOURS_AND_TEXT[state][:text]
