f1f75b3a8a0c5bb9e050441a58947d9cb9e30d97
Misha Gorodnitzky <misaka@dmfg.io>
2024-03-14 15:32:19 +0000

    Add be_sent_with_govuk_notify_matcher
    A bit of added sugar to matching sent emails

--- spec/support/email_expectations.rb
+++ spec/support/email_expectations.rb
@@ -1,9 +1,17 @@
 module EmailExpectations
   def expect_email_to(to, template, nth = :first)
-    perform_enqueued_jobs
-
-    email = ActionMailer::Base.deliveries.send(nth)
+    email = sent_emails.send(nth)
+    expect(email).not_to be_nil
     expect(email.to).to eq [to]
     expect(email[:template_id].value).to eq template
+  end
+
+  def sent_emails
+    @sent_emails ||=
+      begin
+        perform_enqueued_jobs
+
+        ActionMailer::Base.deliveries
+      end
   end
 end
