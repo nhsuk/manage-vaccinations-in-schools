75a5b406d8fb76b91262c528023b84fd1f334a2e
Jake Benilov <benilov@gmail.com>
2024-03-15 22:49:40 +0000

    Remove unused remnants of the vaccs history code

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -10,7 +10,6 @@ Time::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y"
 Time::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A, %-d %B %Y"
 Time::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y"
 
-Time::DATE_FORMATS[:long_ordinal_uk] = "%-d %B %Y at %l:%M%P"
 Time::DATE_FORMATS[:time] = "%-l:%M%P"
 Time::DATE_FORMATS[:app_date_time] = "%-d %b %Y at %-l:%M%P"
 Time::DATE_FORMATS[:app_date_time_long] = "%A %-d %B %Y at %-l:%M%P"
