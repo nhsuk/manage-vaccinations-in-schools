4afb4ac5c28aa3a7b2ee476ede7a5059516dea87
Jake Benilov <benilov@gmail.com>
2024-03-17 22:06:12 +0000

    Update patient page and 'review manual match' page with timestamped entry component

--- app/components/app_consent_response_component.rb
+++ app/components/app_consent_response_component.rb
@@ -1,67 +1,47 @@
 class AppConsentResponseComponent < ViewComponent::Base
-  class AppSingleConsentResponseComponent < ViewComponent::Base
-    erb_template <<-ERB
-      <p class="nhsuk-body">
-        <%= response %> (<%= route %>)
-      </p>
-      <p class="nhsuk-u-margin-bottom-2 nhsuk-u-secondary-text-color nhsuk-u-font-size-16 nhsuk-u-margin-bottom-0">
-        <%= recorded_by_link %><br />
-        <%= consent.recorded_at.to_fs(:nhsuk_date) %> at <%= consent.recorded_at.to_fs(:time) %>
-      </p>
-    ERB
+  def initialize(consents:)
+    super
+    @consents = consents
+  end
 
-    attr_reader :consent
-
-    def initialize(consent:)
-      super
-      @consent = consent
-    end
-
-    def response
-      if consent.respond_to?(:response_not_provided?) &&
-           consent.response_not_provided?
-        "No response when contacted"
-      else
-        consent.human_enum_name(:response).capitalize
-      end
-    end
-
-    def route
-      if consent.respond_to?(:route)
-        consent.human_enum_name(:route).downcase
-      else
-        "online"
-      end
-    end
-
-    def recorded_by_link
-      if consent.respond_to?(:via_phone?) && consent.via_phone?
-        link_to(
-          consent.recorded_by.full_name,
-          "mailto:#{consent.recorded_by.email}"
+  def call
+    if @consents.size == 1
+      consent = @consents.first
+      render AppTimestampedEntryComponent.new(
+               text: summary_with_route(consent),
+               timestamp: consent.recorded_at,
+               recorded_by: staff_who_recorded(consent)
+             )
+    elsif @consents.size > 1
+      tag.ul(class: "nhsuk-list nhsuk-list--bullet app-list--events") do
+        safe_join(
+          @consents.map do |entry|
+            tag.li do
+              render AppTimestampedEntryComponent.new(
+                       text: summary_with_route(entry),
+                       timestamp: entry.recorded_at,
+                       recorded_by: staff_who_recorded(entry)
+                     )
+            end
+          end
         )
-      else
-        link_to(consent.parent_name, "mailto:#{consent.parent_email}")
       end
     end
   end
 
-  erb_template <<-ERB
-    <% if @consents.size == 1 %>
-      <%= render(AppSingleConsentResponseComponent.new(consent: @consents.first)) %>
-    <% elsif @consents.size > 1 %>
-      <ul class="nhsuk-list nhsuk-list--bullet app-list--events">
-        <% @consents.each do |consent| %>
-          <li>
-            <%= render(AppSingleConsentResponseComponent.new(consent: consent)) %>
-          </li>
-        <% end %>
-      </ul>
-    <% end %>
-  ERB
+  private
 
-  def initialize(consents:)
-    super
-    @consents = consents
+  def summary_with_route(consent)
+    route_string = consent.human_enum_name(:route).downcase.presence || "online"
+    if consent.respond_to?(:response_not_provided?) &&
+         consent.response_not_provided?
+      "No response when contacted (#{route_string})"
+    else
+      "#{consent.human_enum_name(:response).capitalize} (#{route_string})"
+    end
+  end
+
+  def staff_who_recorded(consent)
+    consent.recorded_by if consent.respond_to?(:recorded_by)
   end
 end
