a8750828072ef7690f86485f2ba3145c002b238e
Jake Benilov <benilov@gmail.com>
2024-03-17 22:06:12 +0000

    Update patient page and 'review manual match' page with timestamped entry component

--- spec/components/app_consent_response_component_spec.rb
+++ spec/components/app_consent_response_component_spec.rb
@@ -3,29 +3,27 @@ require "rails_helper"
 RSpec.describe AppConsentResponseComponent, type: :component do
   let(:consents) { [consent] }
   let(:component) { described_class.new(consents:) }
+  let(:nurse) do
+    create(:user, full_name: "Test User", email: "test@example.com")
+  end
 
   subject { page }
 
   before { render_inline(component) }
 
   context "with a single consent" do
-    let(:consent) { create(:consent, :given) }
+    let(:consent) do
+      create(
+        :consent,
+        :given,
+        recorded_at: Time.zone.local(2024, 2, 29, 12, 0, 0)
+      )
+    end
     let(:consents) { [consent] }
 
     it { should have_text("Consent given (online)") }
-    it { should have_text(consent.parent_name) }
-    it do
-      should have_text(
-               "#{consent.created_at.to_fs(:nhsuk_date)} at #{consent.created_at.to_fs(:time)}"
-             )
-    end
-    it do
-      should have_link(
-               consent.parent_name,
-               href: "mailto:#{consent.parent_email}"
-             )
-    end
-
+    it { should_not have_text(consent.parent_name) }
+    it { should have_text("29 Feb 2024 at 12:00pm") }
     it { should_not have_css("ul") }
 
     context "with consent refused" do
@@ -35,43 +33,36 @@ RSpec.describe AppConsentResponseComponent, type: :component do
     end
 
     context "with consent taken over the phone" do
-      let(:consent) { create(:consent, :given_verbally) }
+      let(:consent) { create(:consent, :given_verbally, recorded_by: nurse) }
 
       it { should have_text("Consent given (phone)") }
       it { should have_text("Test User") }
     end
 
     context "with no response to attempt to get verbal consent" do
-      let(:consent) { create(:consent, :not_provided) }
+      let(:consent) { create(:consent, :not_provided, recorded_by: nurse) }
 
       it { should have_text("No response when contacted") }
     end
   end
 
   context "with consent_form" do
-    let(:consent_form) { create(:consent_form, :recorded) }
+    let(:consent_form) do
+      create(
+        :consent_form,
+        :recorded,
+        recorded_at: Time.zone.local(2024, 2, 29, 12, 0, 0)
+      )
+    end
     let(:consents) { [consent_form] }
 
     it { should have_text("Consent given (online)") }
-    it { should have_text(consent_form.parent_name) }
-    it do
-      should have_text(
-               "#{consent_form.created_at.to_fs(:nhsuk_date)} at #{consent_form.created_at.to_fs(:time)}"
-             )
-    end
-
-    it do
-      should have_link(
-               consent_form.parent_name,
-               href: "mailto:#{consent_form.parent_email}"
-             )
-    end
+    it { should have_text("29 Feb 2024 at 12:00pm") }
+    it { should_not have_text(consent_form.parent_name) }
   end
 
   context "with multiple consents" do
-    let(:consent1) { create(:consent, :given) }
-    let(:consent2) { create(:consent, :given) }
-    let(:consents) { [consent1, consent2] }
+    let(:consents) { create_list(:consent, 2, :given) }
 
     it { should have_css("ul li p", text: "Consent given (online)", count: 2) }
   end
