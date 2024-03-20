48e815aa1f3feb09a6666c9b1c1e8ac4624fbaf0
Jake Benilov <benilov@gmail.com>
2024-03-20 15:29:19 +0000

    Document an existing bug in AppConsentCardComponent

--- app/components/app_consent_card_component.rb
+++ app/components/app_consent_card_component.rb
@@ -22,6 +22,17 @@ class AppConsentCardComponent < ViewComponent::Base
   end
 
   def call
+    # BUG: this code was originally written with the assumption that it is called
+    # when the user is contacting a refusing parent (that assumption is baked into the microcopy)
+    # However, this code is also invoked when a user is recording the first verbal consent,
+    # and in that case, it doesn't quite display the right thing:
+    #
+    #   Consent updated to given...
+    #
+    # (it's not actually being updated as there wasn't consent there before)
+    # The code really needs awareness of whether there was a previous consent or not, and
+    # if there was one, what the response was. i.e. the previous_response needs to be dynamic.
+
     render AppCardComponent.new(heading:) do
       render AppConsentSummaryComponent.new(
                name: @consent.parent_name,
