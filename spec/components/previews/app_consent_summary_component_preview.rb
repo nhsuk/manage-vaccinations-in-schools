bafba7d051c5f349ae7ad673f4394d47b8e446e2
Jake Benilov <benilov@gmail.com>
2024-03-20 12:13:09 +0000

    Add preview for AppConsentSummaryComponent

---
+++ spec/components/previews/app_consent_summary_component_preview.rb
@@ -0,0 +1,69 @@
+class AppConsentSummaryComponentPreview < ViewComponent::Preview
+  def self_consent
+    render AppConsentSummaryComponent.new(
+             name: "Mary Smith",
+             response: {
+               text: "Consent given (self-consent)",
+               timestamp: Time.zone.local(2024, 3, 2, 14, 23, 0)
+             }
+           )
+  end
+
+  def mum_refuses_consent_for_personal_reasons
+    render AppConsentSummaryComponent.new(
+             name: "Jane Smith",
+             relationship: "mum",
+             contact: {
+               email: "js@example.com",
+               phone: "079876554321"
+             },
+             response: {
+               text: "Consent refused (online)",
+               timestamp: Time.zone.local(2024, 3, 2, 14, 23, 0)
+             },
+             refusal_reason: {
+               reason: "Personal choice"
+             }
+           )
+  end
+
+  def consent_refused_with_notes
+    render AppConsentSummaryComponent.new(
+             name: "Jane Smith",
+             response: {
+               text: "Consent refused (online)",
+               timestamp: Time.zone.local(2024, 3, 2, 14, 23, 0)
+             },
+             refusal_reason: {
+               reason: "Already had the vaccine",
+               notes: "Had it at the GP"
+             }
+           )
+  end
+
+  def multiple_responses
+    render AppConsentSummaryComponent.new(
+             name: "Jane Smith",
+             relationship: "mum",
+             contact: {
+               email: "js@example.com"
+             },
+             response: [
+               {
+                 text: "Consent refused (online)",
+                 timestamp: Time.zone.local(2024, 3, 2, 14, 23, 0)
+               },
+               {
+                 text: "Consent given (phone)",
+                 timestamp: Time.zone.local(2024, 3, 5, 12, 48, 0),
+                 recorded_by:
+                   FactoryBot.build(
+                     :user,
+                     full_name: "Nurse Joy",
+                     email: "nurse.joy@example.com"
+                   )
+               }
+             ]
+           )
+  end
+end
