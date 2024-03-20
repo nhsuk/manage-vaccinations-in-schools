25b91084a82a8ea41fc465d1856bd756ae2bdebf
Jake Benilov <benilov@gmail.com>
2024-03-20 10:15:32 +0000

    Simplify AppConsentCardComponent

--- spec/components/app_consent_card_component_spec.rb
+++ spec/components/app_consent_card_component_spec.rb
@@ -11,24 +11,34 @@ RSpec.describe AppConsentCardComponent, type: :component do
   let(:recorded_by) { create(:user, full_name: "Nurse Joy") }
 
   context "when consent is given" do
-    let(:consent) { create(:consent, :given, :from_mum, recorded_by:) }
+    let(:consent) do
+      create(:consent, :given, :from_mum, route: :phone, recorded_by:)
+    end
 
     it { should have_css("h2", text: /Consent given by.*Mum/) }
-    it { should have_css("p", text: /Consent updated.*by phone/) }
+    it { should have_css("p", text: /Consent updated to given \(by phone\)/) }
     it { should have_css("p", text: recorded_by.full_name) }
   end
 
   context "when consent is refused" do
-    let(:consent) { create(:consent, :refused, :from_dad, recorded_by:) }
+    let(:consent) do
+      create(:consent, :refused, :from_dad, route: :phone, recorded_by:)
+    end
 
     it { should have_css("h2", text: /Refusal confirmed by.*Dad/) }
-    it { should have_css("p", text: /Refusal.*by phone/) }
+    it { should have_css("p", text: /Refusal confirmed \(by phone\)/) }
     it { should have_css("p", text: recorded_by.full_name) }
   end
 
   context "when consent is not provided" do
     let(:consent) do
-      create(:consent, :not_provided, :from_granddad, recorded_by:)
+      create(
+        :consent,
+        :not_provided,
+        :from_granddad,
+        route: :phone,
+        recorded_by:
+      )
     end
 
     it { should have_css("h2", text: /Granddad/) }
