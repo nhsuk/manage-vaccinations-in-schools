8200353266453e7728cbdc76cdb2e6a35c7bc05f
Misha Gorodnitzky <misaka@dmfg.io>
2024-03-25 11:37:26 +0000

    Add state concern to replace state machine
    Now that we understand how states work better, and put all the states we
    need into the various objects connected to PatientSessions, it seems
    clear that the state machine isn't actually adding anything beyond
    additional hurdles and a false sense of comfort.
    
    The new approach in this concern is something we discussed in the
    beginning; we "calculate" the PatientSession state by looking at the
    state of the various connected objects: consent, triage and
    vaccination_record. This was all being done in the state machine as it
    stands, anyway, the only additional complexity is the order in which we
    look at these objects. What's removed, really, are the transitions, and
    all the code to define, manage and test them.

---
+++ spec/models/concerns/patient_session_state_concern_spec.rb
@@ -0,0 +1,99 @@
+require "rails_helper"
+
+RSpec.describe PatientSessionStateConcern do
+  let(:fake_state_machine_class) do
+    Class.new do
+      attr_accessor :state
+
+      include PatientSessionStateConcern
+    end
+  end
+
+  subject(:fsm) { fake_state_machine_class.new }
+
+  describe "#state" do
+    subject { fsm.state }
+
+    RSpec.shared_examples "it supports the state" do |state:, conditions:|
+      conditions_list = conditions.to_sentence
+
+      conditions_hash =
+        conditions.map { |condition| [:"#{condition}?", true] }.to_h
+
+      messages = {
+        consent_given?: false,
+        consent_refused?: false,
+        consent_conflicts?: false,
+        no_consent?: false,
+        not_gillick_competent?: false,
+        triage_needed?: false,
+        triage_not_needed?: false,
+        triage_ready_to_vaccinate?: false,
+        triage_do_not_vaccinate?: false,
+        triage_keep_in_triage?: false,
+        triage_delay_vaccination?: false,
+        vaccination_administered?: false,
+        vaccination_not_administered?: false,
+        vaccination_can_be_delayed?: false
+      }.merge(conditions_hash)
+
+      context "with conditions #{conditions_list}" do
+        before { allow(fsm).to receive_messages(**messages) }
+
+        it { should eq state.to_s }
+      end
+    end
+
+    it_behaves_like "it supports the state",
+                    state: :added_to_session,
+                    conditions: []
+
+    it_behaves_like "it supports the state",
+                    state: :consent_given_triage_needed,
+                    conditions: %i[consent_given triage_needed]
+
+    it_behaves_like "it supports the state",
+                    state: :consent_given_triage_not_needed,
+                    conditions: %i[consent_given triage_not_needed]
+
+    it_behaves_like "it supports the state",
+                    state: :consent_refused,
+                    conditions: [:consent_refused]
+
+    it_behaves_like "it supports the state",
+                    state: :consent_conflicts,
+                    conditions: [:consent_conflicts]
+
+    it_behaves_like "it supports the state",
+                    state: :unable_to_vaccinate_not_gillick_competent,
+                    conditions: [:not_gillick_competent]
+
+    it_behaves_like "it supports the state",
+                    state: :triaged_ready_to_vaccinate,
+                    conditions: [:triage_ready_to_vaccinate]
+
+    it_behaves_like "it supports the state",
+                    state: :triaged_do_not_vaccinate,
+                    conditions: [:triage_do_not_vaccinate]
+
+    it_behaves_like "it supports the state",
+                    state: :triaged_kept_in_triage,
+                    conditions: [:triage_keep_in_triage]
+
+    it_behaves_like "it supports the state",
+                    state: :delay_vaccination,
+                    conditions: [:triage_delay_vaccination]
+
+    it_behaves_like "it supports the state",
+                    state: :vaccinated,
+                    conditions: [:vaccination_administered]
+
+    it_behaves_like "it supports the state",
+                    state: :unable_to_vaccinate,
+                    conditions: [:vaccination_not_administered]
+
+    it_behaves_like "it supports the state",
+                    state: :delay_vaccination,
+                    conditions: [:vaccination_can_be_delayed]
+  end
+end
