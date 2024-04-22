de652c80f86b953117359b59a55f40ada728a3ac
Misha Gorodnitzky <misaka@dmfg.io>
2024-04-22 17:35:07 +0100

    Move triage pages tab conditions to concern
    Makes for simpler controllers and better test coverage.

--- app/controllers/concerns/patient_tabs_concern.rb
+++ app/controllers/concerns/patient_tabs_concern.rb
@@ -1,6 +1,23 @@
 module PatientTabsConcern
   extend ActiveSupport::Concern
 
+  TAB_STATES = {
+    triage: {
+      needs_triage: %w[consent_given_triage_needed triaged_kept_in_triage],
+      triage_complete: %w[
+        delay_vaccination
+        triaged_ready_to_vaccinate
+        triaged_do_not_vaccinate
+      ],
+      no_triage_needed: %w[
+        consent_refused
+        consent_given_triage_not_needed
+        vaccinated
+        unable_to_vaccinate
+      ]
+    }
+  }.with_indifferent_access.freeze
+
   TAB_CONDITIONS = {
     consents: {
       consent_given: %i[consent_given?],
@@ -23,12 +40,19 @@ module PatientTabsConcern
       .with_indifferent_access
   end
 
-  def group_patient_sessions_by_state(all_patient_sessions, tab_states)
+  def group_patient_sessions_by_state(
+    all_patient_sessions,
+    tab_states = nil,
+    section: nil
+  )
+    tab_states ||= TAB_STATES.fetch(section)
+
     all_patient_sessions
       .group_by do |patient_session| # rubocop:disable Style/BlockDelimiters
         tab_states.find { |_, states| patient_session.state.in? states }&.first
       end
       .tap { |groups| tab_states.each_key { groups[_1] ||= [] } }
+      .with_indifferent_access
   end
 
   def count_patient_sessions(tab_patient_sessions)
