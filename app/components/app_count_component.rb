ca609dbe06c98f264297f47037f39dab32abf38d
Theodor Vararu <theo@vararu.org>
2024-05-14 15:22:04 +0200

    Add AppCountComponent
    This will be used in the secondary navigation on the patients page.

---
+++ app/components/app_count_component.rb
@@ -0,0 +1,15 @@
+class AppCountComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <span class="app-count">
+      <span class="nhsuk-u-visually-hidden">(</span>
+      <%= @count %>
+      <span class="nhsuk-u-visually-hidden">)</span>
+    </span>
+  ERB
+
+  def initialize(count:)
+    super
+
+    @count = count
+  end
+end
