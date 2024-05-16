eb0be58f083225739a519d7bbdd3a4d67d09125d
Jake Benilov <benilov@gmail.com>
2024-05-16 15:29:07 +0100

    Improve readability in ConsolidatedHealthAnswers

--- app/models/consolidated_health_answers.rb
+++ app/models/consolidated_health_answers.rb
@@ -8,6 +8,16 @@ class ConsolidatedHealthAnswers
     @answers[question] << { responder:, answer:, notes: }
   end
 
+  # Produces responses in the format:
+  # {
+  #   "Question 1" => [
+  #     { responder: "All", answer: "No" }, # if both Person 1 and Person 2 answered No to Question 1
+  #   ],
+  #   "Question 2" => [
+  #     { responder: "Person 1", answer: "No" },
+  #     { responder: "Person 2", answer: "Yes", notes: "Some notes" }
+  #   ]
+  # }
   def to_h
     consolidated_answers
   end
@@ -19,12 +29,14 @@ class ConsolidatedHealthAnswers
   end
 
   def consolidate_answers(answers)
-    if answers.length == 1
-      answers
-    elsif answers.uniq { |a| [a[:answer], a[:notes]] }.length == 1
+    if answers.length > 1 && same_answer_and_notes_for_all?(answers)
       [answers.first.merge(responder: "All")]
     else
       answers
     end
+  end
+
+  def same_answer_and_notes_for_all?(answers)
+    answers.uniq { |a| [a[:answer], a[:notes]] }.length == 1
   end
 end
