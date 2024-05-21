5ce9dbd1281bfe11a43d42edf4c253a33901c772
Misha Gorodnitzky <misaka@dmfg.io>
2024-05-21 16:17:44 +0100

    Add session in the future to example campaigns

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -34,7 +34,14 @@ class DevController < ApplicationController
             email: "nurse.joy@example.com",
             password: "nurse.joy@example.com"
           )
-      FactoryBot.create(:example_campaign, :in_progress, :in_past, :hpv, user:)
+      FactoryBot.create(
+        :example_campaign,
+        :in_progress,
+        :in_past,
+        :in_future,
+        :hpv,
+        user:
+      )
     end
 
     redirect_to root_path
