2f42099a9d3724786db943d5cb98e5578c6bfad9
Theodor Vararu <theo@vararu.org>
2024-05-23 16:29:28 +0200

    Add example campaign to seeds
    Allows us to provision a test database with some dummy data on Heroku
    review apps.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,22 @@
 FEATURE_FLAGS = %i[make_session_in_progress_button].freeze
 
 FEATURE_FLAGS.each { |flag| Flipper.add(flag) unless Flipper.exist?(flag) }
+
+Faker::Config.locale = "en-GB"
+
+user =
+  User.find_by(email: "nurse.joy@example.com") ||
+    FactoryBot.create(
+      :user,
+      full_name: "Nurse Joy",
+      email: "nurse.joy@example.com",
+      password: "nurse.joy@example.com"
+    )
+FactoryBot.create(
+  :example_campaign,
+  :in_progress,
+  :in_past,
+  :in_future,
+  :hpv,
+  user:
+)
