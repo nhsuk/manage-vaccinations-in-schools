d2e2234c5a2f0f83ee811de86511191ce4296c16
Theodor Vararu <theo@vararu.org>
2024-05-24 10:14:35 +0200

    Add sort_and_filter_patients! to sorting concern
    The filtering will be used across multiple controllers so it should be a
    part of the concern.

--- app/controllers/concerns/patient_sorting_concern.rb
+++ app/controllers/concerns/patient_sorting_concern.rb
@@ -7,12 +7,31 @@ module PatientSortingConcern
     "outcome" => "state"
   }.freeze
 
+  def sort_and_filter_patients!(patient_sessions)
+    sort_patients!(patient_sessions)
+    filter_patients!(patient_sessions)
+  end
+
   def sort_patients!(patient_sessions)
     return if params[:sort].blank?
 
     method_path = SORT_MAPPING[params[:sort]]
     patient_sessions.sort_by! { deep_send(_1, method_path) }
     patient_sessions.reverse! if params[:direction] == "desc"
+  end
+
+  def filter_patients!(patient_sessions)
+    if params[:name].present?
+      patient_sessions.select! do
+        _1.patient.full_name.downcase.include?(params[:name].downcase)
+      end
+    end
+
+    if params[:dob].present?
+      patient_sessions.select! do
+        _1.patient.date_of_birth.strftime("%d/%m/%Y").include?(params[:dob])
+      end
+    end
   end
 
   private
