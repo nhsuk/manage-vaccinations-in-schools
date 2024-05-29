24c7989022d0ffc9e3f7e0a3a47eb3e3d5d6751a
Theodor Vararu <theo@vararu.org>
2024-05-29 18:50:15 +0200

    Add example comments to date time formats
    Might be useful for future readers.

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
-Date::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y"
-Date::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A, %-d %B %Y"
-Date::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y"
-Date::DATE_FORMATS[:sunday_1_may] = "%A %-d %B"
-Date::DATE_FORMATS[:"1_may"] = "%-d %B"
-Date::DATE_FORMATS[:YYYYMMDD] = "%Y%m%d"
+Date::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y" # 5 January 2023
+Date::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A, %-d %B %Y" # Wednesday, 5 January 2023
+Date::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y" # 5 Jan 2023
+Date::DATE_FORMATS[:sunday_1_may] = "%A %-d %B" # Sunday 1 May
+Date::DATE_FORMATS[:"1_may"] = "%-d %B" # 1 May
+Date::DATE_FORMATS[:YYYYMMDD] = "%Y%m%d" # 20230105
 
-Time::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y"
-Time::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A, %-d %B %Y"
-Time::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y"
+Time::DATE_FORMATS[:nhsuk_date] = "%-d %B %Y" # 5 January 2023
+Time::DATE_FORMATS[:nhsuk_date_day_of_week] = "%A, %-d %B %Y" # Wednesday, 5 January 2023
+Time::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y" # 5 Jan 2023
 
-Time::DATE_FORMATS[:time] = "%-l:%M%P"
-Time::DATE_FORMATS[:app_date_time] = "%-d %b %Y at %-l:%M%P"
-Time::DATE_FORMATS[:app_date_time_long] = "%A %-d %B %Y at %-l:%M%P"
+Time::DATE_FORMATS[:time] = "%-l:%M%P" # 3:45pm
+Time::DATE_FORMATS[:app_date_time] = "%-d %b %Y at %-l:%M%P" # 5 Jan 2023 at 3:45pm
+Time::DATE_FORMATS[:app_date_time_long] = "%A %-d %B %Y at %-l:%M%P" # Wednesday 5 January 2023 at 3:45pm
