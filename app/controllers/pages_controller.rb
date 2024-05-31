61d65333e5e4f11e503877472d6d23956c4d46de
Theodor Vararu <theo@vararu.org>
2024-05-31 12:59:05 +0200

    Align start page to prototype
    Update the bullets and use `two_thirds` in the controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   skip_after_action :verify_policy_scoped, only: :start
 
+  layout "two_thirds"
+
   def start
   end
 end
