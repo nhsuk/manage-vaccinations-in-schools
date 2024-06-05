f65acb4c2e67888ed22d975ef4d8bb9cd2882039
Theodor Vararu <theo@vararu.org>
2024-06-05 10:49:33 +0200

    Ensure patient_sessions are created_by Nurse Joy
    Assigns `user` and sets `created_by` to specified `user` when running
    the example campaign factory.

--- spec/factories/example_campaigns.rb
+++ spec/factories/example_campaigns.rb
@@ -14,9 +14,11 @@ FactoryBot.define do
   trait :in_progress do
     after(:create) do |campaign, context|
       location = context.location
+      user = context.user
 
       create(:session, :in_progress, campaign:, location:).tap do |session|
-        patients_without_consent = create_list(:patient_session, 4, session:)
+        patients_without_consent =
+          create_list(:patient_session, 4, session:, user:)
         unmatched_patients = patients_without_consent.sample(2).map(&:patient)
         unmatched_patients.each do |patient|
           create(
@@ -32,18 +34,25 @@ FactoryBot.define do
           :patient_session,
           4,
           :consent_given_triage_not_needed,
-          session:
+          session:,
+          user:
         )
-        create_list(:patient_session, 4, :consent_given_triage_needed, session:)
+        create_list(
+          :patient_session,
+          4,
+          :consent_given_triage_needed,
+          session:,
+          user:
+        )
         create_list(
           :patient_session,
           4,
           :triaged_ready_to_vaccinate,
           session:,
-          user: context.user
+          user:
         )
-        create_list(:patient_session, 4, :consent_refused, session:)
-        create_list(:patient_session, 2, :consent_conflicting, session:)
+        create_list(:patient_session, 4, :consent_refused, session:, user:)
+        create_list(:patient_session, 2, :consent_conflicting, session:, user:)
       end
     end
   end
@@ -51,29 +60,12 @@ FactoryBot.define do
   trait :in_past do
     after(:create) do |campaign, context|
       location = context.location
+      user = context.user
 
       create(:session, :in_past, campaign:, location:).tap do |session|
-        create_list(
-          :patient_session,
-          4,
-          :vaccinated,
-          session:,
-          user: context.user
-        )
-        create_list(
-          :patient_session,
-          4,
-          :delay_vaccination,
-          session:,
-          user: context.user
-        )
-        create_list(
-          :patient_session,
-          4,
-          :unable_to_vaccinate,
-          session:,
-          user: context.user
-        )
+        create_list(:patient_session, 4, :vaccinated, session:, user:)
+        create_list(:patient_session, 4, :delay_vaccination, session:, user:)
+        create_list(:patient_session, 4, :unable_to_vaccinate, session:, user:)
       end
     end
   end
@@ -81,9 +73,11 @@ FactoryBot.define do
   trait :in_future do
     after(:create) do |campaign, context|
       location = context.location
+      user = context.user
 
       create(:session, :in_future, campaign:, location:).tap do |session|
-        patients_without_consent = create_list(:patient_session, 16, session:)
+        patients_without_consent =
+          create_list(:patient_session, 16, session:, user:)
         unmatched_patients = patients_without_consent.sample(8).map(&:patient)
         unmatched_patients.each do |patient|
           create(
@@ -98,11 +92,18 @@ FactoryBot.define do
           :patient_session,
           8,
           :consent_given_triage_not_needed,
-          session:
+          session:,
+          user:
         )
-        create_list(:patient_session, 8, :consent_given_triage_needed, session:)
-        create_list(:patient_session, 8, :consent_refused, session:)
-        create_list(:patient_session, 4, :consent_conflicting, session:)
+        create_list(
+          :patient_session,
+          8,
+          :consent_given_triage_needed,
+          session:,
+          user:
+        )
+        create_list(:patient_session, 8, :consent_refused, session:, user:)
+        create_list(:patient_session, 4, :consent_conflicting, session:, user:)
       end
     end
   end
