a16da3e67f9af521efaeae8acb4b320575758c6c
Misha Gorodnitzky <misaka@dmfg.io>
2024-06-06 16:39:25 +0100

    Rename openid_connect callback to cis2

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :authenticate_user!
   skip_after_action :verify_policy_scoped
 
-  def openid_connect
+  def cis2
   end
+  alias_method :openid_connect, :cis2
 end
