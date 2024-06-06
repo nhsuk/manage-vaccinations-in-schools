387e249fef01579de73c22f7d9fba19067ad66e2
Misha Gorodnitzky <misaka@dmfg.io>
2024-06-06 16:40:03 +0100

    Find or create user on cis2 login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_after_action :verify_policy_scoped
 
   def cis2
+    @user =
+      User.find_or_create_user_from_cis2_oidc(request.env["omniauth.auth"])
+    sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
   end
   alias_method :openid_connect, :cis2
 end
