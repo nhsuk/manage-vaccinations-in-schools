3741d42899fd605f126642dbfa39916658d8013a
Misha Gorodnitzky <misaka@dmfg.io>
2024-06-06 16:37:06 +0100

    Add provider,uid index to user

--- db/schema.rb
+++ db/schema.rb
@@ -357,6 +357,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_16_121453) do
     t.string "provider"
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
