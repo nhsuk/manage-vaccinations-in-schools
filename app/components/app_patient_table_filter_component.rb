3720adeac6491af5555beb6aa11c9a86255739a8
Theodor Vararu <theo@vararu.org>
2024-06-07 11:32:19 +0200

    Remove AppPatientTableFilterComponent
    We don't use it anymore.

--- app/components/app_patient_table_filter_component.rb
+++
@@ -1,8 +0,0 @@
-class AppPatientTableFilterComponent < ViewComponent::Base
-  def initialize(tab_id:, filter_actions: false)
-    super
-
-    @tab_id = tab_id
-    @filter_actions = filter_actions
-  end
-end
