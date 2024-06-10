7c35e9001a4541b2eae7e571b37560421284446a
Jake Benilov <benilov@gmail.com>
2024-06-10 18:11:36 +0100

    Drop Consent.parent_* fields entirely from DB

--- spec/models/parent_spec.rb
+++ spec/models/parent_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: parents
+#
+#  id                   :bigint           not null, primary key
+#  contact_method       :integer
+#  contact_method_other :text
+#  email                :string
+#  name                 :string
+#  phone                :string
+#  relationship         :integer
+#  relationship_other   :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
 require "rails_helper"
 
 RSpec.describe Parent do
