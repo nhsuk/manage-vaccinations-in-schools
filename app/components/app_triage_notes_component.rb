e99b4426795bc270f2a7058be609cc91f3f521e2
Thomas Leese <thomas@leese.io>
2024-06-27 14:33:08 +0200

    Show all triage entries
    We don't want to only show triage entries which have notes now as we
    also want to include the triage decision so there will always be some
    content to show.

--- app/components/app_triage_notes_component.rb
+++ app/components/app_triage_notes_component.rb
@@ -14,36 +14,6 @@ class AppTriageNotesComponent < ViewComponent::Base
   private
 
   def entries
-    @entries ||=
-      @patient_session.triage.where.not(notes: nil).order(created_at: :desc)
+    @entries ||= @patient_session.triage.order(created_at: :desc)
   end
-<<<<<<< HEAD
-||||||| parent of 41a4b57a (Show decision on triage notes)
-
-  def triage_notes(triage)
-    [
-      tag.p(class: "nhsuk-u-margin-bottom-1") { triage.notes },
-      tag.p(class: "nhsuk-u-secondary-text-color nhsuk-u-font-size-16") do
-        "#{triage.user.full_name}, #{triage.created_at.to_fs(:app_date_time)}"
-      end
-    ].join("\n").html_safe
-  end
-=======
-
-  def triage_notes(triage)
-    [
-      tag.h3(
-        "Triaged decision: #{triage.human_enum_name(:status)}",
-        class: "nhsuk-heading-s nhsuk-u-margin-bottom-2"
-      ),
-      if (notes = triage.notes).present?
-        tag.p(notes, class: "nhsuk-u-margin-bottom-1")
-      end,
-      tag.p(
-        "#{triage.user.full_name}, #{triage.created_at.to_fs(:app_date_time)}",
-        class: "nhsuk-u-secondary-text-color nhsuk-u-font-size-16"
-      )
-    ].compact.join("\n").html_safe
-  end
->>>>>>> 41a4b57a (Show decision on triage notes)
 end
