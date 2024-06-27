0d8899603faec5b456004a8c92b19a50774f095c
Misha Gorodnitzky <misaka@dmfg.io>
2024-06-27 11:44:10 +0100

    Add any option to expect_email_to
    There are scenarios where the order that emails are sent isn't strictly
    deterministic, or relevant, in a spec. These specs can fail when emails
    aren't sent in the expected order, this option allows specs to disregard
    the order the emails are sent in.

--- spec/support/email_expectations.rb
+++ spec/support/email_expectations.rb
@@ -2,7 +2,12 @@
 
 module EmailExpectations
   def expect_email_to(to, template, nth = :first)
-    email = sent_emails.send(nth)
+    email =
+      if nth == :any
+        sent_emails.find { |e| e.to.include?(to) }
+      else
+        sent_emails.send(nth)
+      end
     expect(email).not_to be_nil
     expect(email).to be_sent_with_govuk_notify.using_template(template).to(to)
   end
