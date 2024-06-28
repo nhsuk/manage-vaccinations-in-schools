773560a11e18163f903647023873ec9a9b01b5b0
Thomas Leese <thomas@leese.io>
2024-06-28 08:55:07 +0200

    Remove unused date format
    This removes the `app_date_time_long` date format which isn't being used
    anywhere.

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -12,4 +12,3 @@ Time::DATE_FORMATS[:nhsuk_date_short_month] = "%-d %b %Y" # 5 Jan 2023
 
 Time::DATE_FORMATS[:time] = "%-l:%M%P" # 3:45pm
 Time::DATE_FORMATS[:app_date_time] = "%-d %b %Y at %-l:%M%P" # 5 Jan 2023 at 3:45pm
-Time::DATE_FORMATS[:app_date_time_long] = "%A %-d %B %Y at %-l:%M%P" # Wednesday 5 January 2023 at 3:45pm
