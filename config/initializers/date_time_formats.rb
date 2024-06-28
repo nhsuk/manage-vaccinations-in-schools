b855a33701c8b2e4a83a599d6385a34200414ce2
Thomas Leese <thomas@leese.io>
2024-06-28 10:01:16 +0200

    Add comment about date time formats
    This explains where our customised date and time formats come from
    and how they differ from the standard Rails date and time formats.

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Here we replace the standard Rails date and time formats with slightly
+# different versions based on the guidance in the NHS service manual.
+# See https://railsdatetimeformats.com/ for the default values.
+
+# Dates - https://service-manual.nhs.uk/content/numbers-measurements-dates-time#dates
+
 Date::DATE_FORMATS[:short] = "%-d %B" # 1 January
 Date::DATE_FORMATS[:short_day_of_week] = "%A %-d %B" # Monday 1 January
 
@@ -7,6 +13,8 @@ Date::DATE_FORMATS[:long] = "%-d %B %Y" # 5 January 2023
 Date::DATE_FORMATS[:long_day_of_week] = "%A %-d %B %Y" # Wednesday 5 January 2023
 
 Date::DATE_FORMATS[:uk_short] = "%d/%m/%Y" # 01/01/2020
+
+# Time - https://service-manual.nhs.uk/content/numbers-measurements-dates-time#time
 
 Time::DATE_FORMATS[:time] = "%-l:%M%P" # 3:45pm
 
