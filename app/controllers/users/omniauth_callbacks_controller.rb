53624dd007eee3034be6e0f6bb93b7ff3c4f63e0
Misha Gorodnitzky <misaka@dmfg.io>
2024-07-03 17:16:48 +0100

    Check user's selected position maps to a team
    We need to ensure the user belongs to a team that exists in Mavis. A
    button to change their position via CIS2 will be added next.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,8 +5,28 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_after_action :verify_policy_scoped
 
   def cis2
-    @user =
-      User.find_or_create_user_from_cis2_oidc(request.env["omniauth.auth"])
-    sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+    user_info = request.env["omniauth.auth"]
+    raw_info = user_info["extra"]["raw_info"]
+    selected_role = raw_info["selected_roleid"]
+    nrbac_role =
+      raw_info["nhsid_nrbac_roles"].find do
+        _1["person_roleid"] == selected_role
+      end
+
+    team = Team.find_by(ods_code: nrbac_role["org_code"])
+    if team.nil?
+      @selected_org =
+        raw_info["nhsid_user_orgs"].find do
+          _1["org_code"] == nrbac_role["org_code"]
+        end
+
+      flash[:org_name] = @selected_org["org_name"]
+      flash[:org_code] = @selected_org["org_code"]
+      redirect_to team_not_found_path
+      false
+    else
+      @user = User.find_or_create_user_from_cis2_oidc(user_info)
+      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+    end
   end
 end
