f5998f0c27ecace65d34db54ed8138b19ee164ce
Theodor Vararu <theo@vararu.org>
2024-07-04 18:18:02 +0300

    Add ImmunisationImport model
    This will be used to validate and process NIVS uploads by parsing each
    row and creating necessary vaccination, patient, location, and other
    records.

---
+++ app/models/immunisation_import.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: immunisation_imports
+#
+#  id         :bigint           not null, primary key
+#  csv        :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_immunisation_imports_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class ImmunisationImport < ApplicationRecord
+  belongs_to :user
+
+  validates :csv, presence: true
+end
