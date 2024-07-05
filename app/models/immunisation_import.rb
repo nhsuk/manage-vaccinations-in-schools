cfb206743fd8abc3d349e4047b1920342620ae3a
Theodor Vararu <theo@vararu.org>
2024-07-05 15:51:47 +0300

    Add process! method to ImmunisationImport
    This will be used to generate VaccinationRecords and other necessary
    records such as locations or sessions.

--- app/models/immunisation_import.rb
+++ app/models/immunisation_import.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "csv"
+
 # == Schema Information
 #
 # Table name: immunisation_imports
@@ -22,4 +24,59 @@ class ImmunisationImport < ApplicationRecord
   belongs_to :user
 
   validates :csv, presence: true
+
+  def csv=(value)
+    super(value.respond_to?(:read) ? value.read : value)
+  end
+
+  def process!(patient_session:)
+    data = CSV.parse(csv, headers: true, skip_blanks: true)
+    data.each do |row|
+      record = Row.new(row).to_vaccination_record
+      record.user = user
+      record.patient_session = patient_session
+      record.save!
+    end
+  end
+
+  class Row
+    def initialize(row)
+      @row = row
+    end
+
+    def to_vaccination_record
+      VaccinationRecord.new(
+        administered: administered?,
+        delivery_site:,
+        delivery_method:,
+        recorded_at: Time.zone.now
+      )
+    end
+
+    def delivery_site
+      {
+        "left thigh" => :left_thigh,
+        "right thigh" => :right_thigh,
+        "left upper arm" => :left_arm_upper_position,
+        "right upper arm" => :right_arm_upper_position,
+        "left buttock" => :left_buttock,
+        "right buttock" => :right_buttock,
+        "nasal" => :nose
+      }[
+        @row["ANATOMICAL_SITE"]&.downcase
+      ]
+    end
+
+    def delivery_method
+      if @row["ANATOMICAL_SITE"]&.downcase == "nasal"
+        :nasal_spray
+      else
+        :intramuscular
+      end
+    end
+
+    def administered?
+      @row["VACCINATED"].downcase == "yes"
+    end
+  end
 end
--- spec/models/immunisation_import_spec.rb
+++ spec/models/immunisation_import_spec.rb
@@ -21,8 +21,22 @@
 require "rails_helper"
 
 describe ImmunisationImport do
+  subject { described_class.create!(csv:, user:) }
+
   let(:file) { "nivs.csv" }
   let(:csv) { fixture_file_upload("spec/fixtures/immunisation_import/#{file}") }
+  let(:user) { create(:user) }
+  let(:patient_session) { create(:patient_session, user:) }
 
   it { should validate_presence_of(:csv) }
+
+  describe "#process!" do
+    it "creates vaccination records" do
+      # TEMPORARY: Pass in a dummy patient session. We will iterate this out.
+      expect { subject.process!(patient_session:) }.to change(
+        VaccinationRecord,
+        :count
+      ).by(11)
+    end
+  end
 end
