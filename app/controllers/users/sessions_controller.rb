0d905381f9cf68bc90b7f8670e85874624761e63
Misha Gorodnitzky <misaka@dmfg.io>
2024-07-05 17:11:01 +0100

    Check user org when authenticating requests
    Ensures that no matter what, the user goes to the "team-not-found" page
    if they've selected a team that hasn't been registered in the system.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  skip_before_action :authenticate_user!, only: %i[new destroy]
+
   skip_after_action :verify_policy_scoped
 
   layout "one_half"
