26585f7c77b1bc083c401bf650b00480bd96aeff
Misha Gorodnitzky <misaka@dmfg.io>
2024-07-22 17:07:58 +0100

    Move vaccination_records queries into methods ...
    ... in ReportsController

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,15 +4,7 @@ class ReportsController < ApplicationController
   before_action :set_campaign
 
   def download
-    vaccinations =
-      policy_scope(VaccinationRecord)
-        .administered
-        .recorded
-        .where(campaign: @campaign)
-        .includes(:session, :patient, :campaign, batch: :vaccine)
-        .order("vaccination_records.recorded_at")
-
-    csv = NivsReport.new(vaccinations).to_csv
+    csv = NivsReport.new(vaccination_records).to_csv
     filename =
       "NIVS-#{@campaign.name.parameterize(preserve_case: true)}-report-MAVIS.csv"
 
@@ -20,20 +12,11 @@ class ReportsController < ApplicationController
   end
 
   def dps_export
-    vaccinations =
-      policy_scope(VaccinationRecord)
-        .administered
-        .recorded
-        .where(campaign: @campaign)
-        .where(exported_to_dps_at: nil)
-        .includes(:session, :patient, :campaign, batch: :vaccine)
-        .order("vaccination_records.recorded_at")
-
     date = Time.zone.today.strftime("%Y-%m-%d")
     filename =
       "DPS-export-#{@campaign.name.parameterize(preserve_case: true)}-#{date}.csv"
 
-    csv = DPSExport.new(vaccinations).export_csv
+    csv = DPSExport.new(vaccination_records_for_dps_export).export_csv
     send_data(csv, filename:)
   end
 
@@ -41,5 +24,20 @@ class ReportsController < ApplicationController
 
   def set_campaign
     @campaign = policy_scope(Campaign).find(params[:campaign_id])
+  end
+
+  def vaccination_records
+    @vaccination_records ||=
+      policy_scope(VaccinationRecord)
+        .administered
+        .recorded
+        .where(campaign: @campaign)
+        .includes(:session, :patient, :campaign, batch: :vaccine)
+        .order("vaccination_records.recorded_at")
+  end
+
+  def vaccination_records_for_dps_export
+    @vaccination_records_for_dps_export ||=
+      vaccination_records.where(exported_to_dps_at: nil)
   end
 end
