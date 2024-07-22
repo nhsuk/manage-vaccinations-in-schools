8f7b2bf52462b81748afac4abf34625092f17102
Misha Gorodnitzky <misaka@dmfg.io>
2024-07-22 23:16:14 +0100

    Add more fields to dps export

--- app/models/dps_export_row.rb
+++ app/models/dps_export_row.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class DPSExportRow
-  FIELDS = %i[date_and_time].freeze
+  FIELDS = %i[
+    nhs_number
+    person_forename
+    person_surname
+    person_dob
+    person_gender_code
+    person_postcode
+    date_and_time
+  ].freeze
 
   attr_reader :vaccination
 
@@ -11,6 +19,32 @@ class DPSExportRow
 
   def to_a
     FIELDS.map { send _1 }
+  end
+
+  private
+
+  def nhs_number
+    vaccination.patient.nhs_number
+  end
+
+  def person_forename
+    vaccination.patient.first_name
+  end
+
+  def person_surname
+    vaccination.patient.last_name
+  end
+
+  def person_dob
+    vaccination.patient.date_of_birth
+  end
+
+  def person_gender_code
+    vaccination.patient.gender_code_before_type_cast
+  end
+
+  def person_postcode
+    vaccination.patient.address_postcode
   end
 
   def date_and_time
