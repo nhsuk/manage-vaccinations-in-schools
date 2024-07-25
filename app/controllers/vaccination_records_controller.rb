ed19e0f6ffd743be14e2f2fd5f3662ef27c0e639
Thomas Leese <thomas@leese.io>
2024-07-25 09:40:44 +0100

    Make vaccination records part of campaign
    This moves the controller to be within the campaign routing in
    prepartion for a new section on each campaign listing the vaccination
    records.
    
    It also adds breadcrumb navigation to the vaccination records show page
    as per the designs.

--- app/controllers/vaccination_records_controller.rb
+++ app/controllers/vaccination_records_controller.rb
@@ -3,15 +3,19 @@
 class VaccinationRecordsController < ApplicationController
   def show
     @vaccination_record =
-      policy_scope(VaccinationRecord).includes(
-        :vaccine,
-        :batch,
-        patient: :location,
-        session: :location
-      ).find(params[:id])
+      policy_scope(VaccinationRecord)
+        .includes(:vaccine, :batch, patient: :location, session: :location)
+        .where(campaign:)
+        .find(params[:id])
 
     @patient = @vaccination_record.patient
     @session = @vaccination_record.session
     @school = @patient.location
+  end
+
+  private
+
+  def campaign
+    @campaign ||= policy_scope(Campaign).find(params[:campaign_id])
   end
 end
