905777251d9565657a9e4b774016b970749165d4
Thomas Leese <thomas@leese.io>
2024-07-30 16:11:01 +0100

    Move where we determine vaccination record vaccine
    We don't currently have an interface for picking the vaccine, so we need
    to assume the vaccine by picking one in the campaign. For the
    immunisation import process, we will always know the vaccine so we don't
    want that to conflict with this assumption.

--- app/models/patient_session.rb
+++ app/models/patient_session.rb
@@ -56,7 +56,14 @@ class PatientSession < ApplicationRecord
   end
 
   def draft_vaccination_record
-    vaccination_records.draft.find_or_initialize_by(recorded_at: nil)
+    # HACK: this code will need to be revisited in future as it only really works for HPV, where we only have one
+    # vaccine. It is likely to fail for the Doubles programme as that has 2 vaccines. It is also likely to fail for
+    # the flu programme for the SAIS teams that offer both nasal and injectable vaccines.
+    vaccine = campaign&.vaccines&.first
+    vaccination_records
+      .draft
+      .create_with(vaccine:)
+      .find_or_initialize_by(recorded_at: nil)
   end
 
   def gillick_competent?
