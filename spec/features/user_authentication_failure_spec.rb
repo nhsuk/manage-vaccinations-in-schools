5e3d593f3933ce6dece805fa98c7172741ed420f
Misha Gorodnitzky <misaka@dmfg.io>
2024-08-02 17:15:32 +0100

    Keep user on login page when they fail auth
    This was sending users back to the start page which can be confusing.
    Worth noting, users will probably not see this journey, but it's still a
    qol issue in development.

---
+++ spec/features/user_authentication_failure_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "User authentication failure" do
+  scenario "user enters wrong username or password" do
+    given_that_i_have_an_account
+    when_i_go_to_the_start_page
+    and_i_click_on_start_now
+    and_i_click_on_log_in
+    then_i_see_the_sign_in_form
+  end
+
+  def given_that_i_have_an_account
+    @user = create :user, password: "rosebud123"
+  end
+
+  def when_i_go_to_the_start_page
+    visit start_path
+  end
+
+  def and_i_click_on_start_now
+    click_link "Start now"
+  end
+
+  def and_i_click_on_log_in
+    click_button "Log in"
+  end
+
+  def then_i_see_the_sign_in_form
+    expect(page).to have_selector :heading, "Log in"
+  end
+end
