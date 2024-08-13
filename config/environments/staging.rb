cdf05fd39fa0bd0cf82f5e1e3deefd9ad4f8bfa5
Misha Gorodnitzky <misaka@dmfg.io>
2024-08-13 15:36:49 +0100

    Enable the MESH validate mailbox job in staging
    This validates the MESH mailbox in the INT env and will be required for
    proving that our integration works.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,14 @@
 # frozen_string_literal: true
 
 require Rails.root.join("config/environments/production")
+
+Rails.application.configure do
+  config.good_job.enable_cron = true
+  config.good_job.cron = {
+    mesh_validate_mailbox: {
+      cron: "every day at 1am",
+      class: "MESHValidateMailboxJob",
+      description: "Validate MESH mailbox"
+    }
+  }
+end
