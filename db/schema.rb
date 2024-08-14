081dfa95f5d250767dd85626ed0f17227dea7b05
Thomas Leese <thomas@leese.io>
2024-08-14 18:32:42 +0100

    Add academic year column to campaigns
    This adds a column to campaigns which allows us to select the academic
    year for a particular campaign.
    
    Academic years span two real years, this field will store the first real
    year, for example the value 2024 means the academic year 2024/25.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_14_151845) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_14_172542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_14_151845) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "team_id"
+    t.integer "academic_year", null: false
   end
 
   create_table "campaigns_vaccines", id: false, force: :cascade do |t|
