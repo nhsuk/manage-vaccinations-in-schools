9820bc977596b16672d909f5c8fc52d004236928
Thomas Leese <thomas@leese.io>
2024-08-15 11:14:59 +0100

    Send active campaigns to DPS
    This updates the export job to only send active campaigns to DPS. This
    code is temporary, but it serves as a reminder that when we update the
    code for this job we only fetch records from active campaigns.

--- app/jobs/dps_export_job.rb
+++ app/jobs/dps_export_job.rb
@@ -4,7 +4,7 @@ class DPSExportJob < ApplicationJob
   queue_as :default
 
   def perform
-    campaign = Campaign.first # TODO: Not .first
+    campaign = Campaign.active.first # TODO: Not .first
     data = DPSExport.create!(campaign:).export!
     MESH.send_file(data:, to: Settings.mesh.dps_mailbox)
   end
