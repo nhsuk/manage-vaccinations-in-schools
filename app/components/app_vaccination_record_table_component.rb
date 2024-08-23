880d154eea02ae0ed3e7c3a007c841251a2008f9
Thomas Leese <thomas@leese.io>
2024-08-23 13:21:36 +0100

    Update vaccination record table caption
    If we're showing new records we want the caption to include the word
    "new". Also made a number of small changes to match the designs.

--- app/components/app_vaccination_record_table_component.rb
+++ app/components/app_vaccination_record_table_component.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 class AppVaccinationRecordTableComponent < ViewComponent::Base
-  def initialize(vaccination_records)
+  def initialize(vaccination_records, new_records: false)
     super
 
     @vaccination_records = vaccination_records
+    @new_records = new_records
   end
 
   private
 
-  attr_reader :vaccination_records
+  attr_reader :vaccination_records, :new_records
+
+  def heading
+    [
+      vaccination_records.count.to_s,
+      new_records ? "new" : nil,
+      "vaccination records"
+    ].compact.join(" ")
+  end
 end
