7a9eb8d656d29e7bf319605d4563d8a93ea4f8c0
Misha Gorodnitzky <misaka@dmfg.io>
2024-08-23 18:20:55 +0200

    Schedule MESHTrackDPSExportsJob in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -14,6 +14,11 @@ Rails.application.configure do
       cron: "every day at 2am",
       class: "MESHDPSExportJob",
       description: "Export DPS data via MESH"
+    },
+    mesh_track_dps_exports: {
+      cron: "every day at 3am",
+      class: "MESHTrackDPSExportsJob",
+      description: "Track the status of DPS exports"
     }
   }
 end
