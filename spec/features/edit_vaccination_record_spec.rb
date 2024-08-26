a292ed5b53cd4f4d05a7a49c1ad31ba9f6c9a081
Thomas Leese <thomas@leese.io>
2024-08-26 17:36:43 +0100

    Add feature test for editing date/time
    This adds a new feature test which represents a user editing the
    date/time of a vaccination record.
    
    I also spotted an issue with the pluralisation of the vaccination record
    table header which has been fixed.

---
+++ spec/features/edit_vaccination_record_spec.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+describe "Edit vaccination record" do
+  scenario "User edits the date/time" do
+    given_i_am_signed_in
+    and_an_hpv_campaign_is_underway
+    and_a_vaccination_record_exists
+
+    when_i_go_to_the_vaccination_records_page
+    then_i_should_see_the_vaccination_records
+
+    when_i_click_on_the_vaccination_record
+    then_i_should_see_the_vaccination_record
+
+    when_i_click_on_the_change_link
+    then_i_should_see_the_date_time_form
+
+    when_i_fill_in_the_date
+    and_i_fill_in_the_time
+    and_i_click_continue
+    then_i_should_see_the_vaccination_record
+    and_i_should_see_the_updated_date_time
+  end
+
+  def given_i_am_signed_in
+    @team = create(:team, :with_one_nurse, ods_code: "R1L")
+    sign_in @team.users.first
+  end
+
+  def and_an_hpv_campaign_is_underway
+    campaign = create(:campaign, :hpv, academic_year: 2023, team: @team)
+    location = create(:location, :school)
+    @session = create(:session, campaign:, location:)
+  end
+
+  def and_a_vaccination_record_exists
+    patient = create(:patient, first_name: "John", last_name: "Smith")
+
+    create(
+      :vaccination_record,
+      patient_session: create(:patient_session, patient:, session: @session)
+    )
+  end
+
+  def when_i_go_to_the_vaccination_records_page
+    visit "/dashboard"
+
+    click_on "Vaccination programmes", match: :first
+    click_on "HPV"
+    click_on "Vaccination records"
+  end
+
+  def then_i_should_see_the_vaccination_records
+    expect(page).to have_content("1 vaccination record")
+    expect(page).to have_content("John Smith")
+  end
+
+  def when_i_click_on_the_vaccination_record
+    click_on "John Smith"
+  end
+
+  def then_i_should_see_the_vaccination_record
+    expect(page).to have_content("NameJohn Smith")
+  end
+
+  def when_i_click_on_the_change_link
+    click_on "Change"
+  end
+
+  def then_i_should_see_the_date_time_form
+    expect(page).to have_content("Date")
+    expect(page).to have_content("Time")
+  end
+
+  def when_i_fill_in_the_date
+    fill_in "Year", with: "2023"
+    fill_in "Month", with: "9"
+    fill_in "Day", with: "1"
+  end
+
+  def and_i_fill_in_the_time
+    fill_in "Hour", with: "12"
+    fill_in "Minute", with: "00"
+  end
+
+  def and_i_click_continue
+    click_on "Continue"
+  end
+
+  def and_i_should_see_the_updated_date_time
+    expect(page).to have_content("Vaccination date1 September 2023 at 12:00pm")
+  end
+end
