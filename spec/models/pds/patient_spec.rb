7c269a395f3ea413c7e28a355c344103a46ad7ff
Misha Gorodnitzky <misaka@dmfg.io>
2024-09-02 17:35:48 +0100

    Update model PDS::Patient to use new pds library

--- spec/models/pds/patient_spec.rb
+++ spec/models/pds/patient_spec.rb
@@ -6,25 +6,20 @@ describe PDS::Patient do
       File.read("spec/support/pds-get-patient-response.json")
     end
     let(:request_id) { "123e4567-e89b-12d3-a456-426614174000" }
+    let(:patient_json) do
+      File.read(Rails.root.join("spec/fixtures/patient_record.json"))
+    end
 
     before do
-      allow(SecureRandom).to receive(:uuid).and_return(request_id)
-      stub_request(
-        :get,
-        "https://sandbox.api.service.nhs.uk/personal-demographics/FHIR/R4/Patient/9000000009"
-      ).with(headers: { "X-Request-ID" => request_id }).to_return(
-        status: 200,
-        body: json_response
+      allow(NHS::PDS::Patient).to receive(:find).and_return(
+        instance_double(Faraday::Response, status: 200, body: json_response)
       )
     end
 
-    it "returns a patient with the correct attributes" do
-      patient = described_class.find("9000000009")
+    it "calls find_patient on PDS library" do
+      described_class.find("9449306168")
 
-      expect(patient.nhs_number).to eq("9000000009")
-      expect(patient.given_name).to eq("Jane")
-      expect(patient.family_name).to eq("Smith")
-      expect(patient.date_of_birth).to eq("2010-10-22")
+      expect(NHS::PDS::Patient).to have_received(:find).with("9449306168")
     end
   end
 end
