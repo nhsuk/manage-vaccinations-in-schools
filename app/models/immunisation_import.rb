nhsuk/manage-vaccinations-in-schools

commit ae6f10d38ada480af17e7ec44113e546e6c10229

Rename csv column to csv_data
As we're going to add more CSV columns to the immunisation import model
this renames the column to be clearer.

app/models/immunisation_import.rb
@@ -7,7 +7,7 @@ require "csv"
 # Table name: immunisation_imports
 #
 #  id                            :bigint           not null, primary key
-#  csv                           :text             not null
+#  csv_data                      :text             not null
 #  csv_filename                  :text             not null
 #  exact_duplicate_record_count  :integer
 #  new_record_count              :integer
@@ -64,9 +64,9 @@ class ImmunisationImport < ApplicationRecord
     ANATOMICAL_SITE
   ].freeze
 
+  validates :csv_data, presence: true
   validates :csv_filename, presence: true
 
-  validates :csv, presence: true
   validate :csv_is_valid
   validate :csv_has_records
   validate :headers_are_valid
@@ -78,9 +78,9 @@ class ImmunisationImport < ApplicationRecord
     not_administered_record_count
   ].freeze
 
-  def csv=(value)
-    super(value.respond_to?(:read) ? value.read : value)
-    self.csv_filename = value.original_filename if value.respond_to?(:read)
+  def csv=(file)
+    self.csv_data = file.read
+    self.csv_filename = file.original_filename
   end
 
   def processed?
@@ -90,11 +90,9 @@ class ImmunisationImport < ApplicationRecord
   def load_data!
     return if invalid?
 
-    self.data ||= CSV.parse(csv, headers: true, skip_blanks: true)
+    self.data ||= CSV.parse(csv_data, headers: true, skip_blanks: true)
   rescue CSV::MalformedCSVError
     self.csv_is_malformed = true
-  ensure
-    csv.close if csv.respond_to?(:close)
   end
 
   def parse_rows!

db/migrate/20240827080655_rename_immunisation_import_csv_to_csv_data.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+class RenameImmunisationImportCSVToCSVData < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :immunisation_imports, :csv, :csv_data
+  end
+end

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_27_075248) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_27_080655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -261,7 +261,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_27_075248) do
   end
 
   create_table "immunisation_imports", force: :cascade do |t|
-    t.text "csv", null: false
+    t.text "csv_data", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

spec/factories/immunisation_imports.rb
@@ -5,7 +5,7 @@
 # Table name: immunisation_imports
 #
 #  id                            :bigint           not null, primary key
-#  csv                           :text             not null
+#  csv_data                      :text             not null
 #  csv_filename                  :text             not null
 #  exact_duplicate_record_count  :integer
 #  new_record_count              :integer
@@ -32,7 +32,7 @@ FactoryBot.define do
     association :campaign
     association :user
 
-    csv { "my,csv\n" }
+    csv_data { "my,csv\n" }
     csv_filename { Faker::File.file_name(ext: "csv") }
   end
 end

spec/models/immunisation_import_spec.rb
@@ -5,7 +5,7 @@
 # Table name: immunisation_imports
 #
 #  id                            :bigint           not null, primary key
-#  csv                           :text             not null
+#  csv_data                      :text             not null
 #  csv_filename                  :text             not null
 #  exact_duplicate_record_count  :integer
 #  new_record_count              :integer
@@ -46,7 +46,7 @@ describe ImmunisationImport, type: :model do
   let(:team) { create(:team, ods_code: "R1L") }
   let(:user) { create(:user, teams: [team]) }
 
-  it { should validate_presence_of(:csv) }
+  it { should validate_presence_of(:csv_data) }
   it { should validate_presence_of(:csv_filename) }
 
   it "raises if processed without updating the statistics" do
@@ -56,7 +56,9 @@ describe ImmunisationImport, type: :model do
   end
 
   describe "#csv=" do
-    before { immunisation_import.csv = csv }
+    it "sets the data" do
+      expect(immunisation_import.csv_data).not_to be_empty
+    end
 
     it "sets the filename" do
       expect(immunisation_import.csv_filename).to eq("valid_flu.csv")

commit f23fcdcabb1b1511fee40c47fd5369805548ef9e

Ensure immunisation import CSV is encrypted
This should have been working but was accidentally broken in ae6f10d.

app/models/immunisation_import.rb
@@ -35,7 +35,7 @@ class ImmunisationImport < ApplicationRecord
 
   attr_accessor :csv_is_malformed, :data, :rows
 
-  encrypts :csv
+  encrypts :csv_data
 
   belongs_to :user
   belongs_to :campaign