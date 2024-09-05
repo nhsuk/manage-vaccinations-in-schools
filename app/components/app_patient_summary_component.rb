e017a7d0173bc687c5a952ff35db7bea0dba4302
Theodor Vararu <theo@vararu.org>
2024-09-05 11:32:14 +0300

    Refactor duplicate patient summary to component
    Also make it display highlighted attributes if they are dirty.

---
+++ app/components/app_patient_summary_component.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+class AppPatientSummaryComponent < ViewComponent::Base
+  def initialize(patient:)
+    super
+
+    @patient = patient
+  end
+
+  def call
+    govuk_summary_list do |summary_list|
+      summary_list.with_row do |row|
+        row.with_key { "NHS number" }
+        row.with_value { format_nhs_number }
+      end
+      summary_list.with_row do |row|
+        row.with_key { "Full name" }
+        row.with_value { format_full_name }
+      end
+      summary_list.with_row do |row|
+        row.with_key { "Date of birth" }
+        row.with_value { format_date_of_birth }
+      end
+      summary_list.with_row do |row|
+        row.with_key { "Sex" }
+        row.with_value { format_gender_code }
+      end
+      summary_list.with_row do |row|
+        row.with_key { "Postcode" }
+        row.with_value { format_postcode }
+      end
+      summary_list.with_row do |row|
+        row.with_key { "School" }
+        row.with_value { format_school }
+      end
+    end
+  end
+
+  private
+
+  def format_nhs_number
+    highlight_if(
+      helpers.format_nhs_number(@patient.nhs_number),
+      @patient.nhs_number_changed?
+    )
+  end
+
+  def format_full_name
+    highlight_if(
+      @patient.full_name,
+      @patient.first_name_changed? || @patient.last_name_changed?
+    )
+  end
+
+  def format_date_of_birth
+    highlight_if(
+      @patient.date_of_birth.to_date.to_fs(:long),
+      @patient.date_of_birth_changed?
+    )
+  end
+
+  def format_gender_code
+    highlight_if(
+      @patient.gender_code.to_s.humanize,
+      @patient.gender_code_changed?
+    )
+  end
+
+  def format_postcode
+    highlight_if(@patient.address_postcode, @patient.address_postcode_changed?)
+  end
+
+  def format_school
+    highlight_if(@patient.school.name, @patient.school_id_changed?)
+  end
+
+  def highlight_if(value, condition)
+    condition ? tag.span(value, class: "app-highlight") : value
+  end
+end
---
+++ spec/components/app_patient_summary_component_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+describe AppPatientSummaryComponent, type: :component do
+  subject { page }
+
+  before { render_inline(component) }
+
+  let(:component) { described_class.new(patient:) }
+  let(:school) { create(:location, :school, name: "Test School") }
+  let(:other_school) { create(:location, :school, name: "Other School") }
+  let(:patient) do
+    create(
+      :patient,
+      nhs_number: "1234567890",
+      first_name: "John",
+      last_name: "Doe",
+      date_of_birth: Date.new(2000, 1, 1),
+      gender_code: "male",
+      address_postcode: "SW1A 1AA",
+      school:,
+      pending_changes: {
+        first_name: "Jane",
+        date_of_birth: Date.new(2001, 1, 1),
+        address_postcode: "SW1A 2AA",
+        school_id: other_school.id
+      }
+    )
+  end
+
+  it { should have_content("NHS number") }
+  it { should have_content("123\u00A0\u200D456\u00A0\u200D7890") }
+
+  it { should have_content("Full name") }
+  it { should have_content("John Doe") }
+
+  it { should have_content("Date of birth") }
+  it { should have_content("1 January 2000") }
+
+  it { should have_content("Sex") }
+  it { should have_content("Male") }
+
+  it { should have_content("Postcode") }
+  it { should have_content("SW1A 1AA") }
+
+  it { should have_content("School") }
+  it { should have_content("Test School") }
+
+  it { should_not have_css(".app-highlight") }
+
+  context "with pending changes" do
+    let(:component) do
+      described_class.new(patient: patient.with_pending_changes)
+    end
+
+    it { should have_css(".app-highlight", text: "Jane Doe") }
+    it { should have_css(".app-highlight", text: "1 January 2001") }
+    it { should have_css(".app-highlight", text: "SW1A 2AA") }
+    it { should_not have_css(".app-highlight", text: "Male") }
+    it { should_not have_css(".app-highlight", text: "Test School") }
+    it { should have_css(".app-highlight", text: "Other School") }
+  end
+end
