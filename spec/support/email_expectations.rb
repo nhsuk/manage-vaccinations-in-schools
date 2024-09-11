fd16100b97028c69cd10fd73d46e952055e01f23
Thomas Leese <thomas@leese.io>
2024-09-11 21:50:40 +0200

    Remove be_sent_with_govuk_notify matcher
    It was only used in one place so we can combine the two in to a single
    expectation.

--- spec/support/email_expectations.rb
+++ spec/support/email_expectations.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 module EmailExpectations
-  def expect_email_to(to, template, nth = :first)
+  def expect_email_to(to, template_name, nth = :first)
     email =
       if nth == :any
         sent_emails.find { |e| e.to.include?(to) }
       else
         sent_emails.send(nth)
       end
+
     expect(email).not_to be_nil
-    expect(email).to be_sent_with_govuk_notify.using_template(template).to(to)
+
+    expect(email.to).to eq([to])
+
+    expect(email.template_id).to eq(GOVUK_NOTIFY_EMAIL_TEMPLATES[template_name])
   end
 
   def sent_emails
