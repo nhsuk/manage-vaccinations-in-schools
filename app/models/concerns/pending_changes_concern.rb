db342ddb24c09d30678f4ca91fbe08bffd63d6d5
Theodor Vararu <theo@vararu.org>
2024-09-13 16:34:07 +0300

    Extract pending_changes methods to concern
    This allows us to reuse the logic for the VaccinationRecords.

---
+++ app/models/concerns/pending_changes_concern.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module PendingChangesConcern
+  extend ActiveSupport::Concern
+
+  included { attribute :pending_changes, :jsonb, default: {} }
+
+  def stage_changes(attributes)
+    new_pending_changes =
+      attributes.each_with_object({}) do |(attr, new_value), staged_changes|
+        current_value = public_send(attr)
+        staged_changes[attr.to_s] = new_value if new_value.present? &&
+          new_value != current_value
+      end
+
+    if new_pending_changes.any?
+      update!(pending_changes: pending_changes.merge(new_pending_changes))
+    end
+  end
+
+  def with_pending_changes
+    return self if pending_changes.blank?
+
+    dup.tap do |record|
+      record.clear_changes_information
+      pending_changes.each do |attr, value|
+        record.public_send("#{attr}=", value)
+      end
+    end
+  end
+
+  def apply_pending_changes!
+    pending_changes.each { |attr, value| public_send("#{attr}=", value) }
+    discard_pending_changes!
+  end
+
+  def discard_pending_changes!
+    self.pending_changes = {}
+    save!
+  end
+end
---
+++ spec/models/concerns/pending_changes_concern_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+describe PendingChangesConcern do
+  let(:model_class) do
+    Class.new(ApplicationRecord) do
+      include PendingChangesConcern
+
+      self.table_name = "patients"
+    end
+  end
+
+  let(:model) { model_class.create(first_name: "John", last_name: "Doe") }
+
+  describe "#stage_changes" do
+    it "stages new changes in pending_changes" do
+      model.stage_changes(first_name: "Jane", address_line_1: "123 New St")
+
+      expect(model.pending_changes).to eq(
+        { "first_name" => "Jane", "address_line_1" => "123 New St" }
+      )
+    end
+
+    it "does not stage unchanged attributes" do
+      model.stage_changes(first_name: "John", last_name: "Smith")
+
+      expect(model.pending_changes).to eq({ "last_name" => "Smith" })
+    end
+
+    it "does not stage blank values" do
+      model.stage_changes(
+        first_name: "",
+        last_name: nil,
+        address_line_1: "123 New St"
+      )
+
+      expect(model.pending_changes).to eq({ "address_line_1" => "123 New St" })
+    end
+
+    it "updates the pending_changes attribute" do
+      expect { model.stage_changes(first_name: "Jane") }.to change {
+        model.reload.pending_changes
+      }.from({}).to({ "first_name" => "Jane" })
+    end
+
+    it "does not update other attributes directly" do
+      model.stage_changes(first_name: "Jane", last_name: "Smith")
+
+      expect(model.first_name).to eq("John")
+      expect(model.last_name).to eq("Doe")
+    end
+
+    it "does not save any changes if no valid changes are provided" do
+      expect { model.stage_changes(first_name: "John") }.not_to(
+        change { model.reload.pending_changes }
+      )
+    end
+  end
+
+  describe "#with_pending_changes" do
+    it "returns the model with pending changes applied" do
+      model.stage_changes(first_name: "Jane")
+      expect(model.first_name).to eq("John")
+
+      changed_model = model.with_pending_changes
+      expect(changed_model.first_name).to eq("Jane")
+      expect(changed_model.last_name).to eq("Doe")
+    end
+  end
+end
