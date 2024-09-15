fd9c0a7ba5cdbf785cbb341d046e48b513274eac
Thomas Leese <thomas@leese.io>
2024-09-15 16:56:35 +0200

    Refactor SessionRemindersJob
    This refactors the SessionRemindersJob to combine it with the
    SessionRemindersBatchJob to avoid needing to run a job for each session
    and instead we can run a single query to get all the patient sessions
    and consents that require emails to be sent for.

--- app/jobs/session_reminders_job.rb
+++ app/jobs/session_reminders_job.rb
@@ -1,16 +1,27 @@
 # frozen_string_literal: true
 
-# This job triggers a job to send a batch of consent reminders for each sessions
-# that needs them sent today.
-
 class SessionRemindersJob < ApplicationJob
   queue_as :default
 
-  def perform(*_args)
+  def perform
     return unless Flipper.enabled?(:scheduled_emails)
 
-    Session.active.tomorrow.each do |session|
-      SessionRemindersBatchJob.perform_later(session)
+    patient_sessions.each do |patient_session|
+      patient_session.consents_to_send_communication.each do |consent|
+        SessionMailer.with(consent:, patient_session:).reminder.deliver_later
+      end
+
+      patient_session.update!(reminder_sent_at: Time.zone.now)
     end
+  end
+
+  private
+
+  def patient_sessions
+    PatientSession
+      .includes(:consents)
+      .joins(:session)
+      .merge(Session.active.tomorrow)
+      .reminder_not_sent
   end
 end
--- spec/jobs/session_reminders_job_spec.rb
+++ spec/jobs/session_reminders_job_spec.rb
@@ -1,35 +1,83 @@
 # frozen_string_literal: true
 
 describe SessionRemindersJob do
-  before do
-    Flipper.enable(:scheduled_emails)
-    ActiveJob::Base.queue_adapter.enqueued_jobs.clear
-  end
+  subject(:perform_now) { described_class.perform_now }
+
+  before { Flipper.enable(:scheduled_emails) }
 
   let(:programme) { create(:programme, :active) }
+  let(:parents) { create_list(:parent, 2, :recorded) }
+  let(:patient) { create(:patient, parents:) }
 
-  it "enqueues SessionRemdindersJob for each session happening tomorrow" do
-    tomorrow_session = create(:session, date: Date.tomorrow, programme:)
-    _todays_session = create(:session, date: Time.zone.today, programme:)
-    _yesterdays_session = create(:session, date: 2.days.from_now, programme:)
-
-    described_class.perform_now
-    expect(SessionRemindersBatchJob).to have_been_enqueued.once
-    expect(SessionRemindersBatchJob).to have_been_enqueued.with(
-      tomorrow_session
+  let!(:consent) do
+    create(
+      :consent,
+      :given,
+      :recorded,
+      patient:,
+      parent: parents.first,
+      programme:
     )
   end
 
-  context "with draft and active sessions" do
-    it "enqueues ConsentRemindersSessionBatchJob for each active sessions" do
-      active_session = create(:session, date: Date.tomorrow, programme:)
-      _draft_session = create(:session, :draft, date: Date.tomorrow, programme:)
+  context "for an active session tomorrow" do
+    let!(:session) do
+      create(:session, programme:, date: Date.tomorrow, patients: [patient])
+    end
+    let(:patient_session) { PatientSession.find_by!(patient:, session:) }
 
-      described_class.perform_now
-      expect(SessionRemindersBatchJob).to have_been_enqueued.once
-      expect(SessionRemindersBatchJob).to have_been_enqueued.with(
-        active_session
+    it "sends an email to the parent who consented" do
+      expect { perform_now }.to have_enqueued_mail(
+        SessionMailer,
+        :reminder
+      ).with(params: { consent:, patient_session: }, args: [])
+    end
+
+    it "does not send a reminder if one has already been sent" do
+      patient_session.update!(reminder_sent_at: Time.zone.now)
+      expect { perform_now }.not_to have_enqueued_mail(SessionMailer, :reminder)
+    end
+
+    it "updates the reminder_sent_at attribute for patient sessions" do
+      expect { perform_now }.to(
+        change { patient_session.reload.reminder_sent_at }
       )
+    end
+  end
+
+  context "for a session today" do
+    before do
+      create(:session, programme:, date: Time.zone.today, patients: [patient])
+    end
+
+    it "doesn't send an email" do
+      expect { perform_now }.not_to have_enqueued_mail(SessionMailer, :reminder)
+    end
+  end
+
+  context "for a session yesterday" do
+    before do
+      create(:session, programme:, date: Date.yesterday, patients: [patient])
+    end
+
+    it "doesn't send an email" do
+      expect { perform_now }.not_to have_enqueued_mail(SessionMailer, :reminder)
+    end
+  end
+
+  context "for a draft session tomorrow" do
+    before do
+      create(
+        :session,
+        :draft,
+        programme:,
+        date: Date.tomorrow,
+        patients: [patient]
+      )
+    end
+
+    it "doesn't send an email" do
+      expect { perform_now }.not_to have_enqueued_mail(SessionMailer, :reminder)
     end
   end
 end
