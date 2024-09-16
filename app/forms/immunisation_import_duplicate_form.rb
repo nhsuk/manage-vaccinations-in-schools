276755b7b6a3df861f5c5450886b0933f98a942b
Theodor Vararu <theo@vararu.org>
2024-09-16 12:19:31 +0300

    Apply pending changes on vaccinations
    When users submit the immunisation import duplicate form, we should
    update both the patient record as well as the vaccination record.

--- app/forms/immunisation_import_duplicate_form.rb
+++ app/forms/immunisation_import_duplicate_form.rb
@@ -13,7 +13,9 @@ class ImmunisationImportDuplicateForm
     ActiveRecord::Base.transaction do
       if apply_changes == "apply"
         vaccination_record.patient.apply_pending_changes!
+        vaccination_record.apply_pending_changes!
       elsif apply_changes == "discard"
+        vaccination_record.patient.discard_pending_changes!
         vaccination_record.discard_pending_changes!
       end
     end
