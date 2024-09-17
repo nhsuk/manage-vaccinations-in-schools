021121299406428eb7531b78b6d9ceaa6699e9fc
Thomas Leese <thomas@leese.io>
2024-09-17 16:45:54 +0200

    Add Cohort#year_group
    This is used to determine the year group this cohort represents,
    dynamically based on today's date.

--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -22,5 +22,33 @@
 describe Cohort do
   subject(:cohort) { build(:cohort) }
 
-  it { should be_valid }
+  describe "validations" do
+    it { should be_valid }
+  end
+
+  describe "#year_group" do
+    subject(:year_group) do
+      build(:cohort, reception_starting_year: 2000).year_group
+    end
+
+    around { |example| travel_to(today) { example.run } }
+
+    context "when the date is the first day of reception" do
+      let(:today) { Date.new(2000, 9, 1) }
+
+      it { should eq(0) }
+    end
+
+    context "when the date is the last day of reception" do
+      let(:today) { Date.new(2001, 8, 31) }
+
+      it { should eq(0) }
+    end
+
+    context "when the date is the first day of year 1" do
+      let(:today) { Date.new(2001, 9, 1) }
+
+      it { should eq(1) }
+    end
+  end
 end
