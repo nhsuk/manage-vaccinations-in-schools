2d3384bffc7a5f6dc374e53460a3a0dcecd2d47d
Thomas Leese <thomas@leese.io>
2024-09-18 10:40:05 +0200

    Simplify AppPatientTableComponent
    This simplifies the patient table to match the latest designs in the
    prototype, reducing content in the heading, and replacing the outcome
    column with postcodes.

--- app/components/app_patient_table_component.rb
+++ app/components/app_patient_table_component.rb
@@ -1,52 +1,17 @@
 # frozen_string_literal: true
 
 class AppPatientTableComponent < ViewComponent::Base
-  def initialize(patients, programme:)
+  def initialize(patients)
     super
 
     @patients = patients
-    @programme = programme
   end
 
   private
 
-  attr_reader :patients, :programme
+  attr_reader :patients
 
   def heading
-    "#{pluralize(patients.count, "child")} in this programme’s cohort"
-  end
-
-  def outcome_for(patient:)
-    patient_session = patient_session_for(patient:)
-    if patient_session&.vaccination_administered?
-      govuk_tag(text: "Vaccinated", colour: "green")
-    elsif patient_session&.vaccination_not_administered?
-      govuk_tag(text: "Could not vaccinate", colour: "red")
-    else
-      govuk_tag(text: "No outcome yet", colour: "grey")
-    end
-  end
-
-  def href_for(patient:)
-    patient_session = patient_session_for(patient:)
-    return nil if patient_session.nil?
-
-    session = patient_session.session
-
-    tab =
-      if patient_session.vaccination_administered?
-        "vaccinated"
-      elsif patient_session.vaccination_not_administered?
-        "could-not"
-      else
-        "vaccinate"
-      end
-
-    session_patient_path(session, patient, section: "vaccinations", tab:)
-  end
-
-  def patient_session_for(patient:)
-    # TODO: handle multiple patient sessions
-    programme.patient_sessions.active.order(:created_at).find_by(patient:)
+    pluralize(patients.count, "child")
   end
 end
