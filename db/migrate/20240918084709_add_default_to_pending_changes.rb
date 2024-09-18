22a979cf274be59e8ba9539983e8eb3ccb611c33
Theodor Vararu <theo@vararu.org>
2024-09-18 14:59:00 +0300

    Update pending changes migration
    We need to first set all the nil `pending_changes` fields to `{}` before
    we set the field to be non-nullable.

--- db/migrate/20240918084709_add_default_to_pending_changes.rb
+++ db/migrate/20240918084709_add_default_to_pending_changes.rb
@@ -2,18 +2,21 @@
 
 class AddDefaultToPendingChanges < ActiveRecord::Migration[7.2]
   def up
-    change_table :vaccination_records, bulk: true do |t|
-      t.change_default :pending_changes, {}
-      t.change_null :pending_changes, false
-    end
-
     VaccinationRecord.where(pending_changes: nil).update_all(
       pending_changes: {
       }
     )
+
+    change_table :vaccination_records, bulk: true do |t|
+      t.change_default :pending_changes, {}
+      t.change_null :pending_changes, false
+    end
   end
 
   def down
-    change_column_default :vaccination_records, :pending_changes, nil
+    change_table :vaccination_records, bulk: true do |t|
+      t.change_default :pending_changes, nil
+      t.change_null :pending_changes, true
+    end
   end
 end
