d0a8a07508292a856b77ae11465a84bf16994eef
Thomas Leese <thomas@leese.io>
2024-09-19 12:23:42 +0200

    Remove SCHOOL_NAME from cohort import
    This removes the SCHOOL_NAME column from the list of required columns
    when importing cohorts since we're not using it. Instead, the schools
    are already imported and we match each row with a school using the URN.

--- spec/models/cohort_import_row_spec.rb
+++ spec/models/cohort_import_row_spec.rb
@@ -7,21 +7,20 @@ describe CohortImportRow do
 
   let(:valid_data) do
     {
-      "SCHOOL_URN" => "123456",
-      "SCHOOL_NAME" => "Surrey Primary",
-      "PARENT_NAME" => "John Smith",
-      "PARENT_RELATIONSHIP" => "Father",
-      "PARENT_EMAIL" => "john@example.com",
-      "PARENT_PHONE" => "07412345678",
-      "CHILD_FIRST_NAME" => "Jimmy",
-      "CHILD_LAST_NAME" => "Smith",
-      "CHILD_COMMON_NAME" => "Jim",
-      "CHILD_DATE_OF_BIRTH" => "2010-01-01",
       "CHILD_ADDRESS_LINE_1" => "10 Downing Street",
       "CHILD_ADDRESS_LINE_2" => "",
-      "CHILD_ADDRESS_TOWN" => "London",
       "CHILD_ADDRESS_POSTCODE" => "SW1A 1AA",
-      "CHILD_NHS_NUMBER" => "1234567890"
+      "CHILD_ADDRESS_TOWN" => "London",
+      "CHILD_COMMON_NAME" => "Jim",
+      "CHILD_DATE_OF_BIRTH" => "2010-01-01",
+      "CHILD_FIRST_NAME" => "Jimmy",
+      "CHILD_LAST_NAME" => "Smith",
+      "CHILD_NHS_NUMBER" => "1234567890",
+      "PARENT_EMAIL" => "john@example.com",
+      "PARENT_NAME" => "John Smith",
+      "PARENT_PHONE" => "07412345678",
+      "PARENT_RELATIONSHIP" => "Father",
+      "SCHOOL_URN" => "123456"
     }
   end
 
