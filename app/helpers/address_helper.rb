6002b092183e7ed38102092e05dd5e4fdc2ff3d5
Thomas Leese <thomas@leese.io>
2024-09-20 16:59:20 +0200

    Add format_address_single_line
    This adds a helper method for formatting an address on a single line in
    a standard way across the service.

--- app/helpers/address_helper.rb
+++ app/helpers/address_helper.rb
@@ -4,4 +4,15 @@ module AddressHelper
   def format_address_multi_line(addressable)
     safe_join(addressable.address_parts, tag.br)
   end
+
+  def format_address_single_line(addressable)
+    [
+      [
+        addressable.address_line_1,
+        addressable.address_line_2,
+        addressable.address_town
+      ].compact_blank.join(", "),
+      addressable.address_postcode
+    ].compact_blank.join(". ")
+  end
 end
