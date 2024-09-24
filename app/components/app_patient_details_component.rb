4e6c0aee626fcab8d6303918c47734238edbb431
Thomas Leese <thomas@leese.io>
2024-09-24 09:57:44 +0100

    Don't pass consent_form to AppPatientDetailsComponent
    We don't pass this value in at any point where we use this component so
    we can safely remove it and simplify the component.

--- app/components/app_patient_details_component.rb
+++ app/components/app_patient_details_component.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
 
 class AppPatientDetailsComponent < ViewComponent::Base
-  def initialize(patient: nil, consent_form: nil)
+  def initialize(patient)
     super
 
-    unless patient || consent_form
-      raise ArgumentError, "patient or consent_form must be provided"
-    end
-
-    @object = patient || consent_form
-    @school = patient&.school
+    @patient = patient
   end
 
   def call
@@ -18,57 +13,47 @@ class AppPatientDetailsComponent < ViewComponent::Base
     ) do |summary_list|
       summary_list.with_row do |row|
         row.with_key { "Name" }
-        row.with_value { @object.full_name }
+        row.with_value { @patient.full_name }
       end
 
-      if @object.common_name.present?
+      if @patient.common_name.present?
         summary_list.with_row do |row|
           row.with_key { "Known as" }
-          row.with_value { @object.common_name }
+          row.with_value { @patient.common_name }
         end
       end
 
-      if @object.date_of_birth.present?
+      if @patient.date_of_birth.present?
         summary_list.with_row do |row|
           row.with_key { "Date of birth" }
-          row.with_value { "#{@object.date_of_birth.to_fs(:long)} (#{aged})" }
+          row.with_value do
+            "#{@patient.date_of_birth.to_fs(:long)} (aged #{@patient.age})"
+          end
         end
       end
 
-      if address_present?
+      if @patient.has_address?
         summary_list.with_row do |row|
           row.with_key { "Address" }
-          row.with_value { helpers.format_address_multi_line(@object) }
+          row.with_value { helpers.format_address_multi_line(@patient) }
         end
       end
 
-      if @school.present?
+      if (school = @patient.school).present?
         summary_list.with_row do |row|
           row.with_key { "School" }
-          row.with_value { @school.name }
+          row.with_value { school.name }
         end
       end
 
-      if gp_response_present?
-        if @object.gp_response_yes?
-          summary_list.with_row do |row|
-            row.with_key { "GP" }
-            row.with_value { @object.gp_name }
-          end
-        else
-          summary_list.with_row do |row|
-            row.with_key { "Registered with a GP" }
-            row.with_value { @object.gp_response_no? ? "No" : "I don’t know" }
-          end
+      if (nhs_number = @patient.nhs_number).present?
+        summary_list.with_row do |row|
+          row.with_key { "NHS number" }
+          row.with_value { helpers.format_nhs_number(nhs_number) }
         end
       end
 
-      summary_list.with_row do |row|
-        row.with_key { "NHS number" }
-        row.with_value { helpers.format_nhs_number(nhs_number) }
-      end
-
-      if parent_or_guardians.present?
+      if @patient.parent_relationships.present?
         summary_list.with_row do |row|
           row.with_key { "Parent or guardian" }
           row.with_value { parent_or_guardians_formatted }
@@ -79,58 +64,14 @@ class AppPatientDetailsComponent < ViewComponent::Base
 
   private
 
-  def aged
-    "aged #{@object.date_of_birth ? @object.age : ""}"
-  end
-
-  def parent_guardian_or_other
-    @object&.parent&.relationship_label
-  end
-
-  def address_present?
-    @object.try(:address_line_1).present? ||
-      @object.try(:address_line_2).present? ||
-      @object.try(:address_town).present? ||
-      @object.try(:address_postcode).present?
-  end
-
-  def gp_response_present?
-    @object.try(:gp_response).present?
-  end
-
-  def nhs_number
-    @object.try(:nhs_number)
-  end
-
-  def parent_or_guardians
-    @parent_or_guardians ||=
-      if @object.is_a?(ConsentForm)
-        [
-          {
-            name: @object.parent_name,
-            relationship: @object.parent_relationship_label,
-            phone: @object.parent_phone
-          }
-        ]
-      else
-        @object.parent_relationships.map do |parent_relationship|
-          {
-            name: parent_relationship.parent.name,
-            relationship: parent_relationship.label,
-            phone: parent_relationship.parent.phone
-          }
-        end
-      end
-  end
-
   def parent_or_guardians_formatted
     tag.ul(class: "nhsuk-list") do
       safe_join(
-        parent_or_guardians.map do |parent_or_guardian|
+        @patient.parent_relationships.map do |parent_relationship|
           tag.li do
             [
-              "#{parent_or_guardian[:name]} (#{parent_or_guardian[:relationship]})",
-              if (phone = parent_or_guardian[:phone]).present?
+              "#{parent_relationship.parent.name} (#{parent_relationship.label})",
+              if (phone = parent_relationship.parent.phone).present?
                 tag.span(phone, class: "nhsuk-u-secondary-text-color")
               end
             ].compact.join(tag.br).html_safe
--- spec/components/app_patient_details_component_spec.rb
+++ spec/components/app_patient_details_component_spec.rb
@@ -1,227 +1,79 @@
 # frozen_string_literal: true
 
 describe AppPatientDetailsComponent do
-  subject { page }
+  subject(:rendered) { render_inline(component) }
 
-  before { render_inline(component) }
+  let(:component) { described_class.new(patient) }
 
-  context "with a patient object" do
-    let(:parent) { create(:parent) }
-    let(:school) { create(:location, :school) }
-    let(:patient) do
-      create(
-        :patient,
-        nhs_number: 1_234_567_890,
-        common_name: "Homer",
-        parents: [parent],
-        school:,
-      )
-    end
-
-    let(:component) { described_class.new(patient:) }
-
-    it "renders the patient's full name" do
-      expect(page).to(
-        have_css(".nhsuk-summary-list__row", text: "Name#{patient.full_name}")
-      )
-    end
-
-    it "renders the patient's preferred name" do
-      expect(page).to(
-        have_css(".nhsuk-summary-list__row", text: "Known asHomer")
-      )
-    end
-
-    it "renders the patient's date of birth" do
-      expected_dob =
-        "#{patient.date_of_birth.to_fs(:long)} (aged #{patient.age})"
-      expect(page).to(
-        have_css(
-          ".nhsuk-summary-list__row",
-          text: "Date of birth#{expected_dob}"
-        )
-      )
-    end
-
-    it "renders the school name" do
-      expect(page).to(
-        have_css(".nhsuk-summary-list__row", text: "School#{school.name}")
-      )
-    end
-
-    it "does not render a GP name" do
-      expect(page).not_to have_css(".nhsuk-summary-list__row", text: /^GP/)
-    end
-
-    it "does not render a GP response" do
-      expect(page).not_to(
-        have_css(".nhsuk-summary-list__row", text: "Registered with a GP")
-      )
-    end
-
-    it "renders the patient's NHS number" do
-      expect(page).to(have_css(".nhsuk-summary-list__key", text: "NHS number"))
-      expect(page).to(
-        have_css(
-          ".nhsuk-summary-list__row .app-u-monospace",
-          text: "123\u00A0\u200D456\u00A0\u200D7890"
-        )
-      )
-    end
-
-    it "renders the patient's parents" do
-      expect(page).to(
-        have_css(".nhsuk-summary-list__key", text: "Parent or guardian")
-      )
-      expect(page).to(have_css(".nhsuk-summary-list__row", text: parent.name))
-      expect(page).to(have_css(".nhsuk-summary-list__row", text: parent.phone))
-    end
-
-    context "without a preferred name" do
-      let(:patient) { create(:patient, common_name: nil) }
-
-      it "does not render known as" do
-        expect(page).not_to(
-          have_css(".nhsuk-summary-list__row", text: "Known as")
-        )
-      end
-    end
+  let(:parent) { create(:parent) }
+  let(:school) { create(:location, :school) }
+  let(:patient) do
+    create(
+      :patient,
+      nhs_number: 1_234_567_890,
+      common_name: "Homer",
+      parents: [parent],
+      school:
+    )
   end
 
-  context "with a consent_form object" do
-    let(:consent_form) do
-      create(:consent_form, common_name: "Homer", use_common_name: true)
-    end
-    let(:component) { described_class.new(consent_form:) }
+  it "renders the patient's full name" do
+    expect(rendered).to(
+      have_css(".nhsuk-summary-list__row", text: "Name\n#{patient.full_name}")
+    )
+  end
 
-    it "renders the child's full name" do
-      expect(page).to(
-        have_css(
-          ".nhsuk-summary-list__row",
-          text: "Name#{consent_form.full_name}"
-        )
-      )
-    end
+  it "renders the patient's preferred name" do
+    expect(rendered).to(
+      have_css(".nhsuk-summary-list__row", text: "Known as\nHomer")
+    )
+  end
 
-    it "renders the child's common name" do
-      expect(page).to have_css(
+  it "renders the patient's date of birth" do
+    expected_dob = "#{patient.date_of_birth.to_fs(:long)} (aged #{patient.age})"
+    expect(rendered).to(
+      have_css(
         ".nhsuk-summary-list__row",
-        text: "Known asHomer"
+        text: "Date of birth\n#{expected_dob}"
       )
-    end
+    )
+  end
 
-    it "renders the child's date of birth" do
-      formatted_date = consent_form.date_of_birth.to_fs(:long)
-      expected_dob = "#{formatted_date} (aged #{consent_form.age})"
-      expect(page).to(
-        have_css(
-          ".nhsuk-summary-list__row",
-          text: "Date of birth#{expected_dob}"
-        )
+  it "renders the school name" do
+    expect(rendered).to(
+      have_css(".nhsuk-summary-list__row", text: "School\n#{school.name}")
+    )
+  end
+
+  it "renders the patient's NHS number" do
+    expect(rendered).to(
+      have_css(".nhsuk-summary-list__key", text: "NHS number")
+    )
+    expect(rendered).to(
+      have_css(
+        ".nhsuk-summary-list__row .app-u-monospace",
+        text: "123\u00A0\u200D456\u00A0\u200D7890"
       )
-    end
+    )
+  end
 
-    it "renders the child's address" do
-      expect(page).to(
-        have_css(
-          ".nhsuk-summary-list__row",
-          text:
-            "Address" \
-              "#{consent_form.address_line_1}" \
-              "#{consent_form.address_line_2}" \
-              "#{consent_form.address_town}" \
-              "#{consent_form.address_postcode}"
-        )
+  it "renders the patient's parents" do
+    expect(rendered).to(
+      have_css(".nhsuk-summary-list__key", text: "Parent or guardian")
+    )
+    expect(rendered).to(have_css(".nhsuk-summary-list__row", text: parent.name))
+    expect(rendered).to(
+      have_css(".nhsuk-summary-list__row", text: parent.phone)
+    )
+  end
+
+  context "without a preferred name" do
+    let(:patient) { create(:patient, common_name: nil) }
+
+    it "does not render known as" do
+      expect(rendered).not_to(
+        have_css(".nhsuk-summary-list__row", text: "Known as")
       )
-    end
-
-    it "renders the GP name" do
-      expect(page).to(
-        have_css(".nhsuk-summary-list__row", text: "GP#{consent_form.gp_name}")
-      )
-    end
-
-    it "renders 'Not provided' for the NHS number" do
-      expect(page).to(
-        have_css(".nhsuk-summary-list__row", text: "NHS numberNot provided")
-      )
-    end
-
-    it "renders the parent details" do
-      expect(page).to(
-        have_css(".nhsuk-summary-list__key", text: "Parent or guardian")
-      )
-      expect(page).to(
-        have_css(".nhsuk-summary-list__row", text: consent_form.parent_name)
-      )
-      expect(page).to(
-        have_css(".nhsuk-summary-list__row", text: consent_form.parent_phone)
-      )
-    end
-
-    context "without a common name" do
-      let(:consent_form) { create(:consent_form, common_name: nil) }
-
-      it "does not render known as" do
-        expect(page).not_to(
-          have_css(".nhsuk-summary-list__row", text: "Known as")
-        )
-      end
-    end
-
-    context "when child does not have a date of birth on record" do
-      let(:consent_form) { create(:consent_form, date_of_birth: nil) }
-
-      it "does not render the child's date of birth" do
-        expect(page).not_to(
-          have_css(".nhsuk-summary-list__row", text: "Date of birth")
-        )
-      end
-    end
-
-    context "when child does not have a GP" do
-      let(:consent_form) do
-        create(
-          :consent_form,
-          common_name: "Homer",
-          gp_name: nil,
-          gp_response: "no"
-        )
-      end
-
-      it "does not render a GP name" do
-        expect(page).not_to(have_css(".nhsuk-summary-list__row", text: /^GP/))
-      end
-
-      it "renders GP response" do
-        expect(page).to(
-          have_css(".nhsuk-summary-list__row", text: "Registered with a GPNo")
-        )
-      end
-    end
-
-    context "when it's unknown whether the child has a GP" do
-      let(:consent_form) do
-        create(
-          :consent_form,
-          common_name: "Homer",
-          gp_name: nil,
-          gp_response: "dont_know"
-        )
-      end
-
-      it "does not render a GP name" do
-        expect(page).not_to(have_css(".nhsuk-summary-list__row", text: /^GP/))
-      end
-
-      it "renders GP response" do
-        expect(page).to(
-          have_css(
-            ".nhsuk-summary-list__row",
-            text: "Registered with a GPI don’t know"
-          )
-        )
-      end
     end
   end
 end
