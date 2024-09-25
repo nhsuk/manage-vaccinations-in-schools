88e8ee1907a6a2a8e0d7c2fdd5194a90b52a8bb8
Thomas Leese <thomas@leese.io>
2024-09-25 14:42:15 +0100

    Validate SessionDate#value
    This adds validate to ensure that the date of the session is within the
    academic year of the session.

--- spec/models/session_date_spec.rb
+++ spec/models/session_date_spec.rb
@@ -18,7 +18,17 @@
 #  fk_rails_...  (session_id => sessions.id)
 #
 describe SessionDate do
-  subject(:session_date) { build(:session_date) }
+  subject(:session_date) { build(:session_date, session:) }
 
-  it { should be_valid }
+  let(:session) { create(:session, academic_year: 2024) }
+
+  describe "validations" do
+    it "validates that the date is within the academic year" do
+      expect(session_date).to validate_comparison_of(
+        :value
+      ).is_greater_than_or_equal_to(
+        Date.new(2024, 9, 1)
+      ).is_less_than_or_equal_to(Date.new(2025, 8, 31))
+    end
+  end
 end
