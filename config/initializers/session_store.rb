bb8c4e1a756bc284d48f4961a8c84eefec353ef5
Misha Gorodnitzky <misaka@dmfg.io>
2024-09-26 17:22:11 +0100

    Set maximum session time to 12 hours
    Required by CIS2:
    - NHS CIA Ses Req 007

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 Rails.application.config.session_store :cookie_store,
                                        secure: Rails.env.production?,
-                                       expire_after: 2.hours
+                                       expire_after: 12.hours
