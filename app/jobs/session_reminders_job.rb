e86dc202f675eac4b26af740014f020ee71651c4
Thomas Leese <thomas@leese.io>
2024-10-01 10:07:50 +0100

    Send reminders per session date
    This updates the session reminders job to send a reminder email per
    session date that a patient may get vaccinated.

--- app/jobs/session_reminders_job.rb
+++ app/jobs/session_reminders_job.rb
@@ -8,33 +8,33 @@ class SessionRemindersJob < ApplicationJob
 
     date = Date.tomorrow
 
-    patient_sessions(date).each do |patient_session|
-      patient_session.consents_to_send_communication.each do |consent|
-        SessionMailer.with(consent:, patient_session:).reminder.deliver_later
-        TextDeliveryJob.perform_later(
-          :session_reminder,
-          consent:,
-          patient_session:
-        )
-      end
+    patient_sessions =
+      PatientSession
+        .includes(:consents, :patient)
+        .joins(:session)
+        .merge(Session.has_date(date))
+        .reminder_not_sent(date)
 
-      patient_session.update!(reminder_sent_at: Time.zone.now)
+    patient_sessions.each do |patient_session|
+      # We create a record in the database first to avoid sending duplicate emails/texts.
+      # If a problem occurs while the emails/texts are sent, they will be in the job
+      # queue and restarted at a later date.
 
       SessionNotification.create!(
         patient: patient_session.patient,
         session: patient_session.session,
         session_date: date
       )
+
+      patient_session.consents_to_send_communication.each do |consent|
+        SessionMailer.with(consent:, patient_session:).reminder.deliver_later
+
+        TextDeliveryJob.perform_later(
+          :session_reminder,
+          consent:,
+          patient_session:
+        )
+      end
     end
-  end
-
-  private
-
-  def patient_sessions(date)
-    PatientSession
-      .includes(:consents, :patient)
-      .joins(:session)
-      .merge(Session.has_date(date))
-      .reminder_not_sent
   end
 end
