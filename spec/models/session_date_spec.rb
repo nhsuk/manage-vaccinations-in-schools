26bd246a60d440a4af174135838e6e01b89ea6f9
Thomas Leese <thomas@leese.io>
2024-10-01 16:00:47 +0100

    Add SessionDate#today_or_future?
    This adds a convenience method for determining whether the session date
    is due to happen.

--- spec/models/session_date_spec.rb
+++ spec/models/session_date_spec.rb
@@ -18,9 +18,10 @@
 #  fk_rails_...  (session_id => sessions.id)
 #
 describe SessionDate do
-  subject(:session_date) { build(:session_date, session:) }
+  subject(:session_date) { build(:session_date, session:, value:) }
 
   let(:session) { create(:session, academic_year: 2024) }
+  let(:value) { Date.current }
 
   describe "validations" do
     it "validates that the date is within the academic year" do
@@ -29,6 +30,26 @@ describe SessionDate do
       ).is_greater_than_or_equal_to(
         Date.new(2024, 9, 1)
       ).is_less_than_or_equal_to(Date.new(2025, 8, 31))
+    end
+  end
+
+  describe "#today_or_future?" do
+    subject(:today_or_future?) { session_date.today_or_future? }
+
+    context "with a today's date" do
+      it { should be(true) }
+    end
+
+    context "with a date in the past" do
+      let(:value) { Date.yesterday }
+
+      it { should be(false) }
+    end
+
+    context "with a date in the future" do
+      let(:value) { Date.tomorrow }
+
+      it { should be(true) }
     end
   end
 end
