b198dde584eaa2bbb8ae17ca38a99ec0bb30b299
Jake Benilov <benilov@gmail.com>
2024-10-09 14:28:33 +0100

    Make the /reset/<team ODS code> endpoint work irrespective of ODS code case

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -17,7 +17,7 @@ class DevController < ApplicationController
   end
 
   def reset_team
-    team = Team.find_by!(ods_code: params[:team_ods_code])
+    team = Team.find_by!("LOWER(ods_code) = ?", params[:team_ods_code].downcase)
 
     cohort_imports = CohortImport.where(team:)
     cohort_imports.find_each do |cohort_import|
