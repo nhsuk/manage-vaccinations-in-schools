58b3ca6837b1676ccc6e4bb6e74260d57e37c926
Thomas Leese <thomas@leese.io>
2024-10-10 17:23:33 +0100

    Add a notifications queue
    This is for the reminders and requests jobs so they can be managed
    separately to the other jobs.

--- app/jobs/session_reminders_job.rb
+++ app/jobs/session_reminders_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SessionRemindersJob < ApplicationJob
-  queue_as :default
+  queue_as :notifications
 
   def perform
     return unless Flipper.enabled?(:scheduled_emails)
