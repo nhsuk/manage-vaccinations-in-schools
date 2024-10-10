a2fec43376fe8891814c84a0503238eaebc727e4
Theodor Vararu <theo@vararu.org>
2024-10-10 17:51:19 +0300

    Add data-qa attributes to import errors
    These will be used by our testing automation.

--- app/components/app_import_errors_component.rb
+++ app/components/app_import_errors_component.rb
@@ -2,25 +2,29 @@
 
 class AppImportErrorsComponent < ViewComponent::Base
   erb_template <<-ERB
-    <% @errors.each do |error| %>
-      <h2 class="nhsuk-heading-s nhsuk-u-margin-bottom-2">
-        <% if error.attribute == :csv %>
-          CSV
-        <% else %>
-          <%= error.attribute.to_s.humanize %>
-        <% end %>
-      </h2>
-    
-      <ul class="nhsuk-list nhsuk-list--bullet nhsuk-u-font-size-16">
-        <% if error.type.is_a?(Array) %>
-          <% error.type.each do |type| %>
-            <li><%= sanitize type %></li>
+    <div data-qa="import-errors">
+      <% @errors.each do |error| %>
+        <h2 class="nhsuk-heading-s nhsuk-u-margin-bottom-2"
+            data-qa="import-errors__heading">
+          <% if error.attribute == :csv %>
+            CSV
+          <% else %>
+            <%= error.attribute.to_s.humanize %>
           <% end %>
-        <% else %>
-          <li><%= sanitize error.type %></li>
-        <% end %>
-      </ul>
-    <% end %>
+        </h2>
+
+        <ul class="nhsuk-list nhsuk-list--bullet nhsuk-u-font-size-16"
+            data-qa="import-errors__list">
+          <% if error.type.is_a?(Array) %>
+            <% error.type.each do |type| %>
+              <li><%= sanitize type %></li>
+            <% end %>
+          <% else %>
+            <li><%= sanitize error.type %></li>
+          <% end %>
+        </ul>
+      <% end %>
+    </div>
   ERB
 
   def initialize(errors)
