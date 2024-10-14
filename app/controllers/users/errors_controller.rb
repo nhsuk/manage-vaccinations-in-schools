b356eeb59fd74c9cdb315db3b13ef640d08e5feb
Thomas Leese <thomas@leese.io>
2024-10-14 09:22:53 +0100

    Remove "Your account" section
    This removes a section from the app that currently allows users to
    change the details of their account, but users will be signing in via
    CIS2 in the future so we don't need this functionality.

---
+++ app/controllers/users/errors_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Users::ErrorsController < ::ApplicationController
+  skip_before_action :store_user_location!
+  skip_before_action :authenticate_user!
+  skip_after_action :verify_policy_scoped
+
+  def team_not_found
+    if session.key? :cis2_info
+      @cis2_info = session[:cis2_info].with_indifferent_access
+      render status: :not_found
+    else
+      redirect_to root_path
+    end
+  end
+
+  def role_not_found
+    if session.key? :cis2_info
+      @cis2_info = session[:cis2_info].with_indifferent_access
+      render status: :not_found
+    else
+      redirect_to root_path
+    end
+  end
+end
