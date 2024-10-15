52df15824dac5a45045f4d0eb06fb91e092f0c9c
Thomas Leese <thomas@leese.io>
2024-10-15 10:18:04 +0100

    Add team references to batches
    This adds a team references to batches to ensure that teams can't see
    the batches used by other teams.

---
+++ db/migrate/20241015090951_add_team_to_batches.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class AddTeamToBatches < ActiveRecord::Migration[7.2]
+  def up
+    add_reference :batches, :team, foreign_key: true
+    Batch.update_all(team_id: Team.first.id)
+    change_column_null :batches, :team_id, false
+    add_index :batches, %i[team_id name expiry vaccine_id], unique: true
+  end
+
+  def down
+    change_table :batches, bulk: true do |t|
+      t.remove_index %i[team_id name expiry vaccine_id]
+      t.remove_references :team
+    end
+  end
+end
