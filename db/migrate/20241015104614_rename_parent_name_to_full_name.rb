89686e9abbc74400db79d82d4d08c9a2d3915242
Thomas Leese <thomas@leese.io>
2024-10-15 13:31:18 +0100

    Fix column name in migration
    This column doesn't exist, it's what we're renaming the column to.

--- db/migrate/20241015104614_rename_parent_name_to_full_name.rb
+++ db/migrate/20241015104614_rename_parent_name_to_full_name.rb
@@ -3,6 +3,6 @@
 class RenameParentNameToFullName < ActiveRecord::Migration[7.2]
   def change
     rename_column :parents, :name, :full_name
-    rename_column :consent_forms, :parent_full_name, :parent_full_name
+    rename_column :consent_forms, :parent_name, :parent_full_name
   end
 end
