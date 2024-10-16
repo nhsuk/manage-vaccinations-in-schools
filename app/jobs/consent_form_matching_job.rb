1cd25020d1f3ca2dea3c3dd654b5d69f0f8b4c93
Thomas Leese <thomas@leese.io>
2024-10-16 19:00:12 +0100

    Look up NHS numbers when matching consent forms
    When matching a consent form with a patient, we can look up an NHS
    number using the patient details provided in the consent form, which
    makes it more likely that we'll successfully match a consent form with a
    patient and with increased accuracy.

--- app/jobs/consent_form_matching_job.rb
+++ app/jobs/consent_form_matching_job.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 class ConsentFormMatchingJob < ApplicationJob
-  include NHSAPIConcurrencyConcern
+  include NHSNumberLookupConcern
+
+  queue_as :consents
 
   def perform(consent_form)
     session = consent_form.scheduled_session
 
+    nhs_number = find_nhs_number(consent_form)
+
+    # TODO: What happens if we find an NHS number for a patient,
+    # TODO: but they're not in the session?
+
     patients =
       session.patients.match_existing(
-        nhs_number: nil,
+        nhs_number:,
         given_name: consent_form.given_name,
         family_name: consent_form.family_name,
         date_of_birth: consent_form.date_of_birth,
--- spec/jobs/consent_form_matching_job_spec.rb
+++ spec/jobs/consent_form_matching_job_spec.rb
@@ -10,17 +10,34 @@ describe ConsentFormMatchingJob do
       session:,
       given_name: "John",
       family_name: "Smith",
-      date_of_birth: Date.new(2010, 1, 1)
+      date_of_birth: Date.new(2010, 1, 1),
+      address_postcode: "SW11 1AA"
+    )
+  end
+
+  before do
+    stub_request(
+      :get,
+      "https://sandbox.api.service.nhs.uk/personal-demographics/FHIR/R4/Patient"
+    ).with(query: hash_including({})).to_return(
+      body: file_fixture(response_file),
+      headers: {
+        "Content-Type" => "application/fhir+json"
+      }
     )
   end
 
   context "with no matching patients" do
+    let(:response_file) { "pds/search-patients-no-results-response.json" }
+
     it "doesn't create a consent" do
       expect { perform }.not_to change(Consent, :count)
     end
   end
 
   context "with one matching patient" do
+    let(:response_file) { "pds/search-patients-no-results-response.json" }
+
     let!(:patient) do
       create(
         :patient,
@@ -64,9 +81,22 @@ describe ConsentFormMatchingJob do
         expect { perform }.to change(Consent, :count).by(1)
       end
     end
+
+    context "with a successful NHS number lookup" do
+      let(:response_file) { "pds/search-patients-response.json" }
+
+      let!(:patient) { create(:patient, nhs_number: "9449306168", session:) }
+
+      it "creates a consent" do
+        expect { perform }.to change(Consent, :count).by(1)
+        expect(Consent.first.patient).to eq(patient)
+      end
+    end
   end
 
   context "with multiple matching patients" do
+    let(:response_file) { "pds/search-patients-no-results-response.json" }
+
     before do
       create_list(
         :patient,
