3dba0cba57137f4cd2445f58dedb408832dd4af9
Thomas Leese <thomas@leese.io>
2024-10-16 15:11:33 +0100

    Add archived_at column to batches
    This allows us to record when a batch has been archived, meaning it
    can't be used to record vaccinations.

--- spec/factories/batches.rb
+++ spec/factories/batches.rb
@@ -4,13 +4,14 @@
 #
 # Table name: batches
 #
-#  id         :bigint           not null, primary key
-#  expiry     :date             not null
-#  name       :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  team_id    :bigint           not null
-#  vaccine_id :bigint           not null
+#  id          :bigint           not null, primary key
+#  archived_at :datetime
+#  expiry      :date             not null
+#  name        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  team_id     :bigint           not null
+#  vaccine_id  :bigint           not null
 #
 # Indexes
 #
@@ -33,5 +34,9 @@ FactoryBot.define do
 
     name { "#{prefix}#{Faker::Number.number(digits: 4)}" }
     expiry { Faker::Time.forward(days: 50) }
+
+    trait :archived do
+      archived_at { Time.current }
+    end
   end
 end
