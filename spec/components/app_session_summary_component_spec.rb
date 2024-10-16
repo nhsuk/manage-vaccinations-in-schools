d33a2dce74bc040aceeb64a27f06de56699f3f5f
Jake Benilov <benilov@gmail.com>
2024-10-16 17:56:58 +0100

    Fix duplicate text on link
    Calling `govuk_link_to` with `new_tab: true` already adds "(opens in new tab)" to the link text, so adding it explicitly creates duplicate copy

--- spec/components/app_session_summary_component_spec.rb
+++ spec/components/app_session_summary_component_spec.rb
@@ -40,6 +40,6 @@ describe AppSessionSummaryComponent do
     end
 
     it { should have_content("Consent link") }
-    it { should have_link("View parental consent form (opens in a new tab)") }
+    it { should have_link("View parental consent form (opens in new tab)") }
   end
 end
