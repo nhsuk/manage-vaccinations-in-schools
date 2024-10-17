2b2f5c42590f07c6f931353613b4bc4658f21132
Thomas Leese <thomas@leese.io>
2024-10-17 12:14:19 +0100

    Add a view for a single patient
    This adds a page that allows the user to see an individual child record
    based on the designs in the prototype.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,9 +3,13 @@
 class PatientsController < ApplicationController
   include Pagy::Backend
 
-  layout "full"
-
   def index
     @pagy, @patients = pagy(policy_scope(Patient).order_by_name)
+
+    render layout: "full"
+  end
+
+  def show
+    @patient = policy_scope(Patient).find(params[:id])
   end
 end
