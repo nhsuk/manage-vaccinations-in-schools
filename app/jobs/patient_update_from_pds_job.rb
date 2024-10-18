9c8b027939b2b04fc038983bc52cba82ac87e309
Thomas Leese <thomas@leese.io>
2024-10-18 13:28:55 +0100

    Handle patients where the NHS number has changed
    If a patient has been issued a new NHS number, we need to handle this
    situation by either removing the old patient record and merging it with
    one that already exists, or updating the NHS number on the existing
    record to reflect the fact that it has changed.

--- app/jobs/patient_update_from_pds_job.rb
+++ app/jobs/patient_update_from_pds_job.rb
@@ -2,6 +2,7 @@
 
 class PatientUpdateFromPDSJob < ApplicationJob
   include NHSAPIConcurrencyConcern
+  include MergePatientsConcern
 
   queue_as :imports
 
@@ -9,7 +10,26 @@ class PatientUpdateFromPDSJob < ApplicationJob
     raise MissingNHSNumber if patient.nhs_number.nil?
 
     pds_patient = PDS::Patient.find(patient.nhs_number)
-    patient.update_from_pds!(pds_patient)
+
+    if pds_patient.nhs_number != patient.nhs_number
+      if (
+           existing_patient =
+             Patient.includes(
+               :class_imports,
+               :cohort_imports,
+               :immunisation_imports,
+               :patient_sessions
+             ).find_by(nhs_number: pds_patient.nhs_number)
+         )
+        merge_patients!(existing_patient, patient)
+        existing_patient.update_from_pds!(pds_patient)
+      else
+        patient.nhs_number = pds_patient.nhs_number
+        patient.update_from_pds!(pds_patient)
+      end
+    else
+      patient.update_from_pds!(pds_patient)
+    end
   end
 
   class MissingNHSNumber < StandardError
--- spec/jobs/patient_update_from_pds_job_spec.rb
+++ spec/jobs/patient_update_from_pds_job_spec.rb
@@ -17,7 +17,9 @@ describe PatientUpdateFromPDSJob do
     before do
       stub_request(
         :get,
-        "https://sandbox.api.service.nhs.uk/personal-demographics/FHIR/R4/Patient/0123456789"
+        Addressable::Template.new(
+          "https://sandbox.api.service.nhs.uk/personal-demographics/FHIR/R4/Patient/{nhs_number}"
+        )
       ).to_return(
         body: file_fixture("pds/get-patient-response.json"),
         headers: {
@@ -26,11 +28,36 @@ describe PatientUpdateFromPDSJob do
       )
     end
 
-    let(:patient) { create(:patient, nhs_number: "0123456789") }
+    let!(:patient) { create(:patient, nhs_number: "9000000009") }
 
     it "updates the patient details from PDS" do
       expect(patient).to receive(:update_from_pds!)
       perform_now
+    end
+
+    it "doesn't change the NHS number" do
+      expect { perform_now }.not_to change(patient, :nhs_number)
+    end
+
+    it "doesn't delete the patient number" do
+      expect { perform_now }.not_to change(Patient, :count)
+    end
+
+    context "when the NHS number for the patient has changed" do
+      let!(:patient) { create(:patient, nhs_number: "0123456789") }
+
+      it "updates the NHS number" do
+        expect { perform_now }.to change(patient, :nhs_number).to("9000000009")
+      end
+
+      context "when a patient already exists for the new NHS number" do
+        before { create(:patient, nhs_number: "9000000009") }
+
+        it "deletes the patient without an NHS number" do
+          expect { perform_now }.to change(Patient, :count).by(-1)
+          expect { patient.reload }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+      end
     end
   end
 end
