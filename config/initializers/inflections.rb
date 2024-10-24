e251d738dd471a29041e07a459243222a93a1fcc
Misha Gorodnitzky <misaka@dmfg.io>
2024-10-24 18:45:36 +0100

    Add inflection for OAuth2
    In some envs the tests are unable to find RackOAuth2ClientPatch

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -23,6 +23,7 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym "NHS"
   inflect.acronym "ODS"
   inflect.acronym "PDS"
+  inflect.acronym "OAuth2"
 
   inflect.irregular "batch", "batches"
   inflect.irregular "child", "children"
