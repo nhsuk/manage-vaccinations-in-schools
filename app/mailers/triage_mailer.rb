2ac2bf2b7b9c87ded8cf44e11f49f38817be6be4
Thomas Leese <thomas@leese.io>
2024-10-25 15:07:43 +0100

    Add TriageMailer#vaccination_at_clinic
    This adds an email that we will send to parents if the triage outcome
    from the nurse recommends that the patient be vaccinated at a clinic
    instead of at school.

--- app/mailers/triage_mailer.rb
+++ app/mailers/triage_mailer.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class TriageMailer < ApplicationMailer
+  def vaccination_at_clinic
+    app_template_mail(:triage_vaccination_at_clinic)
+  end
+
   def vaccination_will_happen
     app_template_mail(:triage_vaccination_will_happen)
   end
