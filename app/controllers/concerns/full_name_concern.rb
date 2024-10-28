42977f8bee0152d9ce6d232aec540d5a9e4645a8
Thomas Leese <thomas@leese.io>
2024-10-28 10:58:23 +0000

    Allow specifying preferred given and family name
    During the consent form process for parents, rather than a single common
    name field, we can ask them for a preferred given and family name.

--- app/controllers/concerns/full_name_concern.rb
+++ app/controllers/concerns/full_name_concern.rb
@@ -6,7 +6,22 @@ module FullNameConcern
   included { scope :order_by_name, -> { order(:given_name, :family_name) } }
 
   def full_name
-    "#{given_name} #{family_name}"
+    [given_name, family_name].join(" ")
+  end
+
+  def has_preferred_name?
+    preferred_given_name.present? || preferred_family_name.present?
+  end
+
+  def preferred_full_name
+    [
+      preferred_given_name.presence || given_name,
+      preferred_family_name.presence || family_name
+    ].join(" ")
+  end
+
+  def preferred_full_name_changed?
+    preferred_given_name_changed? || preferred_family_name_changed?
   end
 
   def initials
