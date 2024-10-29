60506c4c19fdd6ffee69c4e1cb1598236479c813
Thomas Leese <thomas@leese.io>
2024-10-29 07:21:28 +0000

    Merge staging jobs with production
    This ensures that in staging environments we still get the same jobs as
    production (specifically the ones which schedule automated emails to be
    sent) plus the additional ones configured for staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,13 +3,7 @@
 require Rails.root.join("config/environments/production")
 
 Rails.application.configure do
-  config.good_job.enable_cron = true
-  config.good_job.cron = {
-    bulk_update_patients_from_pds: {
-      cron: "every day at 00:00 and 6:00 and 12:00 and 18:00",
-      class: "BulkUpdatePatientsFromPDSJob",
-      description: "Keep patient details up to date with PDS."
-    },
+  config.good_job.cron.merge!(
     mesh_validate_mailbox: {
       cron: "every day at 1am",
       class: "MESHValidateMailboxJob",
@@ -24,11 +18,6 @@ Rails.application.configure do
       cron: "every day at 3am",
       class: "MESHTrackDPSExportsJob",
       description: "Track the status of DPS exports"
-    },
-    remove_import_csv: {
-      cron: "every day at 1am",
-      class: "RemoveImportCSVJob",
-      description: "Remove CSV data from old cohort and immunisation imports"
     }
-  }
+  )
 end
