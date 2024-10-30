86ffecb6b820e537a6e70e8e58334bfbc0dbb54c
Thomas Leese <thomas@leese.io>
2024-10-30 12:27:58 +0000

    Show when invitations are going to be sent
    This adds a row on the edit session page allowing the user to see when
    the invitations are going to be sent out for the clinics, based on the
    same design as the consent requests.

--- spec/features/manage_clinic_sessions_spec.rb
+++ spec/features/manage_clinic_sessions_spec.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
-describe "Manage sessions" do
+describe "Manage clinic sessions" do
   around { |example| travel_to(Time.zone.local(2024, 2, 18)) { example.run } }
 
-  scenario "Adding a new session, closing consent, and closing the session" do
+  scenario "Adding dates to the session and closing consent" do
     given_my_team_is_running_an_hpv_vaccination_programme
 
     when_i_go_to_todays_sessions_as_a_nurse
     then_i_see_no_sessions
 
     when_i_go_to_unscheduled_sessions
-    then_i_see_the_school
+    then_i_see_the_community_clinic
 
-    when_i_click_on_the_school
-    then_i_see_the_school_session
-    and_i_see_a_child_in_the_cohort
+    when_i_click_on_the_community_clinic
+    then_i_see_the_clinic_session
 
     when_i_click_on_schedule_sessions
     then_i_see_the_dates_page
@@ -24,16 +23,6 @@ describe "Manage sessions" do
 
     when_i_choose_the_dates
     then_i_see_the_confirmation_page
-
-    when_i_click_on_change_consent_requests
-    then_i_see_the_change_consent_requests_page
-    and_i_change_consent_requests_date
-    and_i_confirm
-
-    when_i_click_on_change_consent_reminders
-    then_i_see_the_change_consent_reminders_page
-    and_i_change_consent_reminders_weeks
-    and_i_confirm
 
     when_i_confirm
     then_i_should_see_the_session_details
@@ -45,7 +34,7 @@ describe "Manage sessions" do
     then_i_see_no_sessions
 
     when_i_go_to_scheduled_sessions
-    then_i_see_the_school
+    then_i_see_the_community_clinic
 
     when_i_go_to_completed_sessions
     then_i_see_no_sessions
@@ -58,21 +47,7 @@ describe "Manage sessions" do
 
     when_i_go_to_todays_sessions_as_a_nurse
     and_i_go_to_completed_sessions
-    then_i_see_the_school
-    and_i_click_on_the_school
-
-    when_i_click_on_close_session
-    then_i_see_the_close_session_page
-
-    when_i_close_the_session
-    then_i_see_the_close_confirmation
-    and_i_go_back_to_sessions
-
-    when_i_go_to_unscheduled_sessions
-    then_i_see_the_team_clinic
-
-    when_i_click_on_the_team_clinic
-    then_i_see_a_child_in_the_cohort
+    then_i_see_the_community_clinic
   end
 
   def given_my_team_is_running_an_hpv_vaccination_programme
@@ -84,16 +59,8 @@ describe "Manage sessions" do
         :with_generic_clinic,
         programmes: [@programme]
       )
-    @location = create(:location, :secondary, team: @team)
-    session =
-      create(
-        :session,
-        :unscheduled,
-        location: @location,
-        team: @team,
-        programme: @programme
-      )
-    @patient = create(:patient, year_group: 8, session:)
+
+    @team.generic_clinic_session # ensure it exists
   end
 
   def when_i_go_to_todays_sessions_as_a_nurse
@@ -120,13 +87,12 @@ describe "Manage sessions" do
     expect(page).to have_content(/There are no (sessions|locations)/)
   end
 
-  def when_i_click_on_the_school
-    click_link @location.name
+  def when_i_click_on_the_community_clinic
+    click_link "Community clinics"
   end
 
-  def then_i_see_the_school_session
-    expect(page).to have_content(@location.name)
-    expect(page).to have_content(@location.urn)
+  def then_i_see_the_clinic_session
+    expect(page).to have_content("Community clinics")
     expect(page).to have_content("No sessions scheduled")
     expect(page).to have_content("Schedule sessions")
   end
@@ -182,36 +148,7 @@ describe "Manage sessions" do
 
   def then_i_see_the_confirmation_page
     expect(page).to have_content("Edit session")
-  end
-
-  def when_i_click_on_change_consent_requests
-    click_on "Change consent requests"
-  end
-
-  def then_i_see_the_change_consent_requests_page
-    expect(page).to have_content(
-      "When should parents get a request to give consent?"
-    )
-  end
-
-  def and_i_change_consent_requests_date
-    fill_in "Day", with: "1"
-    fill_in "Month", with: "3"
-    fill_in "Year", with: "2024"
-  end
-
-  def when_i_click_on_change_consent_reminders
-    click_on "Change consent reminders"
-  end
-
-  def then_i_see_the_change_consent_reminders_page
-    expect(page).to have_content(
-      "When should parents get a reminder to give consent?"
-    )
-  end
-
-  def and_i_change_consent_reminders_weeks
-    fill_in "When should parents get a reminder to give consent?", with: "1"
+    expect(page).to have_content("InvitationsSend on Sunday 18 February 2024")
   end
 
   def when_i_confirm
@@ -221,7 +158,6 @@ describe "Manage sessions" do
   alias_method :and_i_confirm, :when_i_confirm
 
   def then_i_should_see_the_session_details
-    expect(page).to have_content(@location.name.to_s)
     expect(page).to have_content("10 March 2024")
     expect(page).to have_content("11 March 2024")
   end
@@ -245,51 +181,7 @@ describe "Manage sessions" do
     expect(page).to have_content("The deadline for responding has passed")
   end
 
-  def then_i_see_the_school
-    expect(page).to have_content(@location.name)
-  end
-
-  def and_i_click_on_the_school
-    click_on @location.name
-  end
-
-  def when_i_click_on_close_session
-    click_on "Close session"
-  end
-
-  def then_i_see_the_close_session_page
-    expect(page).to have_content("Close session")
-    expect(page).to have_content("1 child who could not be vaccinated")
-  end
-
-  alias_method :when_i_close_the_session, :when_i_click_on_close_session
-
-  def then_i_see_the_close_confirmation
-    expect(page).to have_content("Session closed.")
-  end
-
-  def and_i_go_back_to_sessions
-    click_on "Sessions", match: :first
-  end
-
-  def then_i_see_the_team_clinic
+  def then_i_see_the_community_clinic
     expect(page).to have_content("Community clinics")
   end
-
-  alias_method :and_i_see_the_team_clinic, :then_i_see_the_team_clinic
-
-  def when_i_click_on_the_team_clinic
-    click_on "Community clinics"
-  end
-
-  def then_i_see_no_children_in_the_cohort
-    expect(page).to have_content("Children\n0")
-  end
-
-  def then_i_see_a_child_in_the_cohort
-    expect(page).to have_content("1 child in this session")
-  end
-
-  alias_method :and_i_see_a_child_in_the_cohort,
-               :then_i_see_a_child_in_the_cohort
 end
