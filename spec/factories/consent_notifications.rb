7f019cf6e214e623b5f07acc523691307dbf432e
Thomas Leese <thomas@leese.io>
2024-10-30 12:54:55 +0000

    Add session reference on consent notifications
    This allows us to know if a consent notification has already been sent
    out for a particular session, which is needed to know whether users are
    allowed to change the consent dates.
    
    I think we could possibly look at combining consent notifications and
    session notifications at some point in the future, but for now we can
    leave them separate.

--- spec/factories/consent_notifications.rb
+++ spec/factories/consent_notifications.rb
@@ -10,6 +10,7 @@
 #  patient_id      :bigint           not null
 #  programme_id    :bigint           not null
 #  sent_by_user_id :bigint
+#  session_id      :bigint           not null
 #
 # Indexes
 #
@@ -17,17 +18,20 @@
 #  index_consent_notifications_on_patient_id_and_programme_id  (patient_id,programme_id)
 #  index_consent_notifications_on_programme_id                 (programme_id)
 #  index_consent_notifications_on_sent_by_user_id              (sent_by_user_id)
+#  index_consent_notifications_on_session_id                   (session_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (patient_id => patients.id)
 #  fk_rails_...  (programme_id => programmes.id)
 #  fk_rails_...  (sent_by_user_id => users.id)
+#  fk_rails_...  (session_id => sessions.id)
 #
 FactoryBot.define do
   factory :consent_notification do
     patient
     programme
+    session { association :session, programme: }
 
     traits_for_enum :type
   end
