0b293304a816981a5528e1a685c65f58fe0325e6
Thomas Leese <thomas@leese.io>
2024-11-05 14:49:23 +0000

    Allow passing change links to vaccination check and confirm
    This makes it possible to pass change links to the vaccination check and
    confirm component, which we will use to show these links allowing users
    to make changes to the parts of the vaccination record.

--- app/components/app_vaccination_check_and_confirm_component.rb
+++ app/components/app_vaccination_check_and_confirm_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AppVaccinationCheckAndConfirmComponent < ViewComponent::Base
-  def initialize(vaccination_record, current_user:)
+  def initialize(vaccination_record, current_user:, change_links: {})
     super
 
     @vaccination_record = vaccination_record
@@ -11,11 +11,12 @@ class AppVaccinationCheckAndConfirmComponent < ViewComponent::Base
     @vaccine = vaccination_record.vaccine
 
     @current_user = current_user
+    @change_links = change_links
   end
 
   def call
     govuk_summary_list(
-      actions: false,
+      actions: @change_links.present?,
       classes: "app-summary-list--no-bottom-border"
     ) do |summary_list|
       summary_list.with_row do |row|
@@ -39,6 +40,13 @@ class AppVaccinationCheckAndConfirmComponent < ViewComponent::Base
           row.with_value do
             "#{@batch.name} (expires #{@batch.expiry.to_fs(:long)})"
           end
+          if (href = @change_links[:batch])
+            row.with_action(
+              text: "Change",
+              href:,
+              visually_hidden_text: "batch"
+            )
+          end
         end
 
         summary_list.with_row do |row|
@@ -46,21 +54,47 @@ class AppVaccinationCheckAndConfirmComponent < ViewComponent::Base
           row.with_value do
             @vaccination_record.human_enum_name(:delivery_method)
           end
+          if (href = @change_links[:delivery_method])
+            row.with_action(
+              text: "Change",
+              href:,
+              visually_hidden_text: "method"
+            )
+          end
         end
 
         summary_list.with_row do |row|
           row.with_key { "Site" }
           row.with_value { @vaccination_record.human_enum_name(:delivery_site) }
+          if (href = @change_links[:delivery_site])
+            row.with_action(text: "Change", href:, visually_hidden_text: "site")
+          end
         end
 
         summary_list.with_row do |row|
           row.with_key { "Outcome" }
           row.with_value { "Vaccinated" }
+
+          if (href = @change_links[:outcome])
+            row.with_action(
+              text: "Change",
+              href:,
+              visually_hidden_text: "outcome"
+            )
+          end
         end
       else
         summary_list.with_row do |row|
           row.with_key { "Outcome" }
           row.with_value { @vaccination_record.human_enum_name(:reason) }
+
+          if (href = @change_links[:outcome])
+            row.with_action(
+              text: "Change",
+              href:,
+              visually_hidden_text: "outcome"
+            )
+          end
         end
       end
 
@@ -77,6 +111,14 @@ class AppVaccinationCheckAndConfirmComponent < ViewComponent::Base
       summary_list.with_row do |row|
         row.with_key { "Location" }
         row.with_value { @session.location.name }
+
+        if (href = @change_links[:location])
+          row.with_action(
+            text: "Change",
+            href:,
+            visually_hidden_text: "location"
+          )
+        end
       end
 
       if vaccinator
