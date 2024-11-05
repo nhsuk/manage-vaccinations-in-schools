acda4689ae010576963678618d7762879b8d1caa
Theodor Vararu <theo@vararu.org>
2024-11-05 13:51:05 +0000

    Reference Sentry DSN from credentials
    Removes the reliance on the SENTRY_DSN environment variable.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,6 +3,8 @@
 require "active_support/parameter_filter"
 
 Sentry.init do |config|
+  config.dsn = Rails.application.credentials.sentry_dsn
+
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   config.traces_sample_rate = 1.0
