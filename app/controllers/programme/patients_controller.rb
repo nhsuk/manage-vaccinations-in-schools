01d0194e53fd802245b8b5b3e1527f38c4fafbb4
Thomas Leese <thomas@leese.io>
2024-11-05 09:41:06 +0000

    Use AppSessionPatientTableComponent for programmes
    This switches the children view on programmes to use this table
    component that includes filters on the right and allows the user to sort
    the columns.

--- app/controllers/programme/patients_controller.rb
+++ app/controllers/programme/patients_controller.rb
@@ -1,12 +1,32 @@
 # frozen_string_literal: true
 
+require "pagy/extras/array"
+
 class Programme::PatientsController < ApplicationController
   include Pagy::Backend
+  include PatientSortingConcern
 
   before_action :set_programme
 
   def index
-    @pagy, @patients = pagy(@programme.patients.not_deceased.order_by_name)
+    patients =
+      policy_scope(Patient).where(
+        cohort: policy_scope(Cohort).for_year_groups(@programme.year_groups)
+      ).not_deceased
+
+    sessions = policy_scope(Session).has_programme(@programme)
+
+    patient_sessions =
+      PatientSession
+        .where(patient: patients, session: sessions)
+        .preload_for_state
+        .eager_load(:session, patient: :cohort)
+        .order_by_name
+        .strict_loading
+        .to_a
+
+    sort_and_filter_patients!(patient_sessions)
+    @pagy, @patient_sessions = pagy_array(patient_sessions)
 
     render layout: "full"
   end
