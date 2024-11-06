e4470fa90797332e1930fbd9bb653157299c3aee
Thomas Leese <thomas@leese.io>
2024-11-06 19:01:26 +0000

    Update percentage of refused consents
    This changes the percentage to be a fraction of all the refused consent
    requests rather than all consents including given or no responded.

--- spec/components/app_consent_refused_table_component_spec.rb
+++ spec/components/app_consent_refused_table_component_spec.rb
@@ -29,19 +29,18 @@ describe AppConsentRefusedTableComponent do
     create(:consent, :refused, programme:, reason_for_refusal: :medical_reasons)
     create(:consent, :refused, programme:, reason_for_refusal: :personal_choice)
     create(:consent, :refused, programme:, reason_for_refusal: :other)
-    create_list(:consent, 4, :given, programme:)
   end
 
-  it { should have_content("Contains gelatine\n\n            10.0%") }
-  it { should have_content("Already vaccinated\n\n            10.0%") }
+  it { should have_content("Contains gelatine\n\n            16.7%") }
+  it { should have_content("Already vaccinated\n\n            16.7%") }
 
   it do
     expect(rendered).to have_content(
-      "Will be vaccinated elsewhere\n\n            10.0%"
+      "Will be vaccinated elsewhere\n\n            16.7%"
     )
   end
 
-  it { should have_content("Medical reasons\n\n            10.0%") }
-  it { should have_content("Personal choice\n\n            10.0%") }
-  it { should have_content("Other\n\n            10.0%") }
+  it { should have_content("Medical reasons\n\n            16.7%") }
+  it { should have_content("Personal choice\n\n            16.7%") }
+  it { should have_content("Other\n\n            16.7%") }
 end
