ddf1bd0fb3e44df7b3ede9e64ecdede5feee2a66
Thomas Leese <thomas@leese.io>
2024-11-09 16:25:48 +0000

    Update Gillick assessment tests
    This updates the tests to include editing an existing assessment before
    continuing to request consent, to ensure we're also testing the edit
    functionality.

--- spec/features/self_consent_spec.rb
+++ spec/features/self_consent_spec.rb
@@ -8,13 +8,25 @@ describe "Self-consent" do
     given_an_hpv_programme_is_underway
     and_there_is_a_child_without_parental_consent
 
-    when_the_nurse_assesses_the_child_as_gillick_competent
+    when_the_nurse_assesses_the_child_as_not_being_gillick_competent
+    and_the_details_of_the_gillick_non_competence_assessment_are_visible
+    then_the_nurse_assesses_the_child_as_gillick_competent
     then_the_child_can_give_their_own_consent_that_the_nurse_records
 
     when_the_nurse_views_the_childs_record
     then_they_see_that_the_child_has_consent
-    and_the_details_of_the_gillick_competence_assessment_are_visible
     and_the_child_should_be_safe_to_vaccinate
+  end
+
+  scenario "No consent from parent, the child is not Gillick competent" do
+    given_an_hpv_programme_is_underway
+    and_there_is_a_child_without_parental_consent
+
+    when_the_nurse_assesses_the_child_as_gillick_competent
+    and_the_details_of_the_gillick_competence_assessment_are_visible
+    then_the_nurse_assesses_the_child_as_not_being_gillick_competent
+    then_the_child_cannot_give_their_own_consent
+    and_the_childs_status_reflects_that_there_is_no_consent
   end
 
   def given_an_hpv_programme_is_underway
@@ -56,7 +68,17 @@ describe "Self-consent" do
   def when_the_nurse_assesses_the_child_as_gillick_competent
     click_on @patient.full_name
     click_on "Assess Gillick competence"
+    fill_in_as_competent
+    click_on "Complete your assessment"
+  end
 
+  def then_the_nurse_assesses_the_child_as_gillick_competent
+    click_on "Edit Gillick competence"
+    fill_in_as_competent
+    click_on "Update your assessment"
+  end
+
+  def fill_in_as_competent
     within(
       "fieldset",
       text: "The child knows which vaccination they will have"
@@ -84,8 +106,49 @@ describe "Self-consent" do
 
     fill_in "Assessment notes (optional)",
             with: "They understand the benefits and risks of the vaccine"
+  end
 
+  def when_the_nurse_assesses_the_child_as_not_being_gillick_competent
+    click_on @patient.full_name
+    click_on "Assess Gillick competence"
+    fill_in_as_non_competent
     click_on "Complete your assessment"
+  end
+
+  def then_the_nurse_assesses_the_child_as_not_being_gillick_competent
+    click_on "Edit Gillick competence"
+    fill_in_as_non_competent
+    click_on "Update your assessment"
+  end
+
+  def fill_in_as_non_competent
+    within(
+      "fieldset",
+      text: "The child knows which vaccination they will have"
+    ) { choose "No" }
+
+    within(
+      "fieldset",
+      text: "The child knows which disease the vaccination protects against"
+    ) { choose "No" }
+
+    within(
+      "fieldset",
+      text: "The child knows what could happen if they got the disease"
+    ) { choose "No" }
+
+    within(
+      "fieldset",
+      text: "The child knows how the injection will be given"
+    ) { choose "No" }
+
+    within(
+      "fieldset",
+      text: "The child knows which side effects they might experience"
+    ) { choose "No" }
+
+    fill_in "Assessment notes (optional)",
+            with: "They didn't understand the benefits and risks of the vaccine"
   end
 
   def then_the_child_can_give_their_own_consent_that_the_nurse_records
@@ -125,13 +188,29 @@ describe "Self-consent" do
     expect(page).to have_content(
       "They understand the benefits and risks of the vaccine"
     )
+  end
 
-    click_on @patient.full_name
-    expect(page).not_to have_content("Parent or guardian")
-    click_on "Back to patient page"
+  def and_the_details_of_the_gillick_non_competence_assessment_are_visible
+    expect(page).to have_content("Child assessed as not Gillick competent")
+    expect(page).to have_content(
+      "They didn't understand the benefits and risks of the vaccine"
+    )
   end
 
   def and_the_child_should_be_safe_to_vaccinate
     expect(page).to have_content("Safe to vaccinate")
+  end
+
+  def then_the_child_cannot_give_their_own_consent
+    click_on "Get consent"
+    expect(page).to have_content("Who are you trying to get consent from?")
+    expect(page).not_to have_content(
+      "Do they agree to them having the HPV vaccination?"
+    )
+    click_on "Back"
+  end
+
+  def and_the_childs_status_reflects_that_there_is_no_consent
+    expect(page).to have_content("No response")
   end
 end
