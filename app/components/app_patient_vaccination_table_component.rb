556657be035ee1ac2c4e9e39e50ee14beade08ed
Thomas Leese <thomas@leese.io>
2024-11-12 09:34:22 +0000

    Don't try to show patient's unadministered vaccination records
    This currently breaks as we try and show the administered at date and
    vaccine on a record that won't have that information. Perhaps we might
    want to improve this component to show details about unadministered
    vaccinations in the future.

--- app/components/app_patient_vaccination_table_component.rb
+++ app/components/app_patient_vaccination_table_component.rb
@@ -12,6 +12,6 @@ class AppPatientVaccinationTableComponent < ViewComponent::Base
   attr_reader :patient
 
   def vaccination_records
-    patient.vaccination_records.order(administered_at: :desc)
+    patient.vaccination_records.administered.order(administered_at: :desc)
   end
 end
