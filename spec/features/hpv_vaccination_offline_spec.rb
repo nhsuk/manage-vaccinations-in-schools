54fe50a50c32c1dd17915a2f16f559a798cfa3b1
Jake Benilov <benilov@gmail.com>
2024-11-12 22:14:32 +0000

    Extend the HPV offline recording feature spec

--- spec/features/hpv_vaccination_offline_spec.rb
+++ spec/features/hpv_vaccination_offline_spec.rb
@@ -4,6 +4,10 @@ describe "HPV Vaccination" do
   before { Flipper.enable(:release_1b) }
   after { Flipper.disable(:release_1b) }
 
+  around do |example|
+    travel_to(Time.zone.local(2024, 2, 1, 12, 0, 0)) { example.run }
+  end
+
   scenario "Download spreadsheet" do
     given_an_hpv_programme_is_underway
     and_i_am_signed_in
@@ -12,9 +16,9 @@ describe "HPV Vaccination" do
     and_i_click_record_offline
     then_i_see_a_csv_file
 
-    # TODO: modify the file to add a vaccination
-    # TODO: upload the file
-    # TODO: check the vaccination appears
+    when_i_modify_the_csv_file_to_add_a_vaccination
+    and_i_upload_the_modified_csv_file
+    then_i_see_the_vaccination_in_the_session
   end
 
   def given_an_hpv_programme_is_underway
@@ -27,9 +31,20 @@ describe "HPV Vaccination" do
     @batch = create(:batch, organisation: @organisation, vaccine:)
 
     @session =
-      create(:session, organisation: @organisation, programme:, location:)
+      create(
+        :session,
+        :today,
+        organisation: @organisation,
+        programme:,
+        location:
+      )
     @patient =
-      create(:patient, :consent_given_triage_not_needed, session: @session)
+      create(
+        :patient,
+        :consent_given_triage_not_needed,
+        session: @session,
+        year_group: 8
+      )
   end
 
   def and_i_am_signed_in
@@ -46,8 +61,52 @@ describe "HPV Vaccination" do
 
   def then_i_see_a_csv_file
     expect(page.status_code).to eq(200)
-
-    # check headers look right
     expect(page).to have_content("ORGANISATION_CODE")
+  end
+
+  def when_i_modify_the_csv_file_to_add_a_vaccination
+    csv_contents = page.body
+    spreadsheet = CSV.parse(csv_contents, headers: true)
+
+    row = spreadsheet.first
+    row["DATE_OF_VACCINATION"] = Date.current.strftime("%Y%m%d")
+    row["TIME_OF_VACCINATION"] = "10:00:00"
+    row["VACCINATED"] = "Y"
+    row["VACCINE_GIVEN"] = "Gardasil9"
+    row["BATCH_NUMBER"] = @batch.name
+    row["BATCH_EXPIRY_DATE"] = @batch.expiry.strftime("%Y%m%d")
+    row["ANATOMICAL_SITE"] = "Left Upper Arm"
+    row["PERFORMING_PROFESSIONAL_EMAIL"] = @organisation.users.first.email
+
+    File.write("tmp/modified.csv", spreadsheet.to_csv)
+  end
+
+  def and_i_upload_the_modified_csv_file
+    visit "/"
+    click_on "Programmes", match: :first
+    click_on "HPV"
+    click_on "Vaccinations", match: :first
+    click_on "Import vaccination records"
+
+    attach_file("immunisation_import[csv]", "tmp/modified.csv")
+    click_on "Continue"
+  end
+
+  def then_i_see_the_vaccination_in_the_session
+    visit session_path(@session)
+
+    click_on "Record vaccinations"
+    click_on "Vaccinated"
+
+    click_on @patient.full_name
+
+    expect(page).to have_content("Vaccinated")
+    expect(page).to have_content("HPV (Gardasil 9, #{@batch.name})")
+    expect(page).to have_content("DateToday (1 February 2024)")
+    expect(page).to have_content("Time10:00am")
+    expect(page).to have_content(
+      "VaccinatorYou (#{@organisation.users.first.full_name})"
+    )
+    expect(page).to have_content("SiteLeft arm (upper position)")
   end
 end
