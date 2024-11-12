eec1b03c757150127f8da01b68b763aa016a94a0
Misha Gorodnitzky <misaka@dmfg.io>
2024-11-12 16:48:11 +0000

    Set Sentry sampling rate to 0.2
    We're getting closer to our allowance of traces and this will only go up
    as we go into private beta. 0.2 should give us enough samples to be able
    to trace most issues.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,7 +7,7 @@ Sentry.init do |config|
 
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
-  config.traces_sample_rate = 1.0
+  config.traces_sample_rate = 0.2
 
   rails_filter_parameters =
     Rails.application.config.filter_parameters.map(&:to_s)
