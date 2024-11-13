68ff9db339736520acddc798011f3fa501afc262
Thomas Leese <thomas@leese.io>
2024-11-13 16:28:01 +0000

    Add VaccinationReport
    This adds a session-stored model which stores a number of fields
    allowing the user to choose information they want to export before
    running the export.

---
+++ app/models/vaccination_report.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class VaccinationReport
+  include RequestSessionPersistable
+  include WizardStepConcern
+
+  FILE_FORMATS = %w[careplus mavis].freeze
+
+  def self.request_session_key
+    "vaccination_report"
+  end
+
+  attribute :date_from, :datetime
+  attribute :date_to, :integer
+  attribute :file_format, :string
+  attribute :programme_id, :integer
+
+  def wizard_steps
+    %i[dates file_format]
+  end
+
+  on_wizard_step :file_format, exact: true do
+    validates :file_format, inclusion: { in: FILE_FORMATS }
+  end
+
+  def programme
+    ProgrammePolicy::Scope
+      .new(@current_user, Programme)
+      .resolve
+      .find_by(id: programme_id)
+  end
+
+  def programme=(value)
+    self.programme_id = value.id
+  end
+
+  def filename
+    return nil if invalid?
+
+    from_str = date_from&.to_fs(:long) || "earliest"
+    to_str = date_to&.to_fs(:long) || "latest"
+
+    "#{programme.name} - #{file_format} - #{from_str} - #{to_str}.csv"
+  end
+
+  private
+
+  def reset_unused_fields
+  end
+end
