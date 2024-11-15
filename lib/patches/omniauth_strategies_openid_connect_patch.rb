2d20601df86a9f8fcbdc21cf2877dd3f6194d3e7
Misha Gorodnitzky <misaka@dmfg.io>
2024-11-15 15:44:34 +0000

    Move client assertion to private method

--- lib/patches/omniauth_strategies_openid_connect_patch.rb
+++ lib/patches/omniauth_strategies_openid_connect_patch.rb
@@ -7,34 +7,11 @@ module OmniAuthStrategiesOpenIDConnectPatch
     token_request_params = {
       scope: (options.scope if options.send_scope_to_token_endpoint),
       client_auth_method: options.client_auth_method,
+      client_assertion: generate_client_assertion
     }
 
     token_request_params[:code_verifier] = params["code_verifier"] ||
       session.delete("omniauth.pkce.verifier") if options.pkce
-
-    payload = {
-      iss: client_options.identifier,
-      sub: client_options.identifier,
-      aud: client_options.token_endpoint,
-      jti: SecureRandom.hex(16),
-      iat: Time.zone.now.to_i,
-      exp: 3.minutes.from_now.to_i,
-    }
-    jwk =
-      ::JWT::JWK.new(
-        client_options.private_key,
-        { alg: "RS256" },
-        kid_generator: ::JWT::JWK::Thumbprint,
-      )
-
-    headers = { kid: jwk.kid, typ: "JWT" }
-
-    token_request_params[:client_assertion] = ::JWT.encode(
-      payload,
-      client_options.private_key,
-      "RS256",
-      headers
-    )
 
     @access_token = client.access_token!(token_request_params)
     if configured_response_type == "code"
@@ -42,5 +19,28 @@ module OmniAuthStrategiesOpenIDConnectPatch
     end
 
     @access_token
+  end
+
+  private
+
+  def generate_client_assertion
+    payload = {
+      iss: client_options.identifier,
+      sub: client_options.identifier,
+      aud: client_options.token_endpoint,
+      jti: SecureRandom.hex(16),
+      iat: Time.zone.now.to_i,
+      exp: 3.minutes.from_now.to_i
+    }
+    jwk =
+      ::JWT::JWK.new(
+        client_options.private_key,
+        { alg: "RS256" },
+        kid_generator: ::JWT::JWK::Thumbprint
+      )
+
+    headers = { kid: jwk.kid, typ: "JWT" }
+
+    ::JWT.encode(payload, client_options.private_key, "RS256", headers)
   end
 end
