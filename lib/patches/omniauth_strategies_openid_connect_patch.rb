2c221c86012a72c3d1da2f19b17371707783802d
Misha Gorodnitzky <misaka@dmfg.io>
2024-11-15 15:26:16 +0000

    Add OmniAuth OpenIDConnect patch
    The rack-oauth2 gem seems to struggle to add a kid to the JWT it
    generates when used the way it is by omniauth-openidconnect. This
    monkey-patch circumvents that code-path by providing our own client
    assertion (AKA JWT) generated with the ruby-jwt gem.

---
+++ lib/patches/omniauth_strategies_openid_connect_patch.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module OmniAuthStrategiesOpenIDConnectPatch
+  def access_token
+    return @access_token if @access_token
+
+    token_request_params = {
+      scope: (options.scope if options.send_scope_to_token_endpoint),
+      client_auth_method: options.client_auth_method,
+    }
+
+    token_request_params[:code_verifier] = params["code_verifier"] ||
+      session.delete("omniauth.pkce.verifier") if options.pkce
+
+    payload = {
+      iss: client_options.identifier,
+      sub: client_options.identifier,
+      aud: client_options.token_endpoint,
+      jti: SecureRandom.hex(16),
+      iat: Time.zone.now.to_i,
+      exp: 3.minutes.from_now.to_i,
+    }
+    jwk =
+      ::JWT::JWK.new(
+        client_options.private_key,
+        { alg: "RS256" },
+        kid_generator: ::JWT::JWK::Thumbprint,
+      )
+
+    headers = { kid: jwk.kid, typ: "JWT" }
+
+    token_request_params[:client_assertion] = ::JWT.encode(
+      payload,
+      client_options.private_key,
+      "RS256",
+      headers
+    )
+
+    @access_token = client.access_token!(token_request_params)
+    if configured_response_type == "code"
+      verify_id_token!(@access_token.id_token)
+    end
+
+    @access_token
+  end
+end
