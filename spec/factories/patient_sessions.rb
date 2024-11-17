bb41a495b9766ef1c03697adc6817f0fdbc60943
Thomas Leese <thomas@leese.io>
2024-11-17 10:03:27 +0000

    Don't allow triages on closed sessions
    This prevents users from recording a triage if the session has been
    closed.

--- spec/factories/patient_sessions.rb
+++ spec/factories/patient_sessions.rb
@@ -295,6 +295,10 @@ FactoryBot.define do
       session { association :session, :today, programme: }
     end
 
+    trait :session_closed do
+      session { association :session, :closed, programme: }
+    end
+
     trait :not_gillick_competent do
       after(:create) do |patient_session, _evaluator|
         create(:gillick_assessment, :not_competent, patient_session:)
