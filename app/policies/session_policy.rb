bd40e6909953940665f35442a03f118836038cfd
Thomas Leese <thomas@leese.io>
2024-11-17 09:38:28 +0000

    Add SessionPolicy#edit_close? and update_close?
    This adds policy scope methods which determines if a session can be
    closed.

--- app/policies/session_policy.rb
+++ app/policies/session_policy.rb
@@ -5,6 +5,14 @@ class SessionPolicy < ApplicationPolicy
     super && record.open?
   end
 
+  def edit_close?
+    update_close?
+  end
+
+  def update_close?
+    (user.is_nurse? || user.is_admin?) && record.open? && record.completed?
+  end
+
   class Scope < ApplicationPolicy::Scope
     def resolve
       scope.where(organisation: user.selected_organisation)
