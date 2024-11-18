81c177518291f99fe3feafa7139b8513d75ecbc8
Thomas Leese <thomas@leese.io>
2024-11-18 09:46:00 +0000

    Switch to ActiveRecord session store
    This allows us to store more data in the session than the 4 KB limit of
    the cookie store, which might be useful particularly as we're starting
    to create draft consents and vaccination records in the session.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store,
+ActiveRecord::SessionStore::Session.table_name = "active_record_sessions"
+ActiveRecord::SessionStore::Session.serializer = :json
+
+Rails.application.config.session_store :active_record_store,
+                                       key: "_session_id",
                                        secure: Rails.env.production?,
                                        expire_after: 12.hours
