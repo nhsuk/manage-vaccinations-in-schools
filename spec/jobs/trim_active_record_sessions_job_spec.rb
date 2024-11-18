b386dd962b17404cbcea9f9c6bbaa6414bada8ef
Thomas Leese <thomas@leese.io>
2024-11-18 09:48:23 +0000

    Add TrimSessionsJob
    This mimicks the behaviour of a Rake task which automatically removes
    old sessions from our database. We want to run this on a schedule and
    using a job seemed like the easiest way to do it.

---
+++ spec/jobs/trim_active_record_sessions_job_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+describe TrimActiveRecordSessionsJob do
+  describe "#perform" do
+    subject(:perform) { described_class.new.perform }
+
+    before do
+      ActiveRecord::SessionStore::Session.create!(session_id: "1", data: "abc")
+    end
+
+    let!(:session2) do
+      ActiveRecord::SessionStore::Session.create!(
+        session_id: "2",
+        data: "abc",
+        updated_at: 60.days.ago
+      )
+    end
+
+    it "deletes old sessions" do
+      expect { perform }.to change(
+        ActiveRecord::SessionStore::Session,
+        :count
+      ).from(2).to(1)
+      expect { session2.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
