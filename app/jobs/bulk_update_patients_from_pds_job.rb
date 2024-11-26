14254a287101b91306776cc65f8162005ee3d73a
Thomas Leese <thomas@leese.io>
2024-11-26 15:59:06 +0000

    Use GoodJob::Bulk.enqueue
    This improves the performing of queuing a large number of PDS check jobs
    by using the `GoodJob::Bulk.enqueue` method which does a single bulk
    insertion in to the database.

--- app/jobs/bulk_update_patients_from_pds_job.rb
+++ app/jobs/bulk_update_patients_from_pds_job.rb
@@ -10,17 +10,19 @@ class BulkUpdatePatientsFromPDSJob < ApplicationJob
   def perform
     patients = Patient.with_nhs_number.not_invalidated.not_deceased
 
-    patients
-      .where(updated_from_pds_at: nil)
-      .find_each do |patient|
-        PatientUpdateFromPDSJob.set(priority: 50).perform_later(patient)
-      end
+    GoodJob::Bulk.enqueue do
+      patients
+        .where(updated_from_pds_at: nil)
+        .find_each do |patient|
+          PatientUpdateFromPDSJob.set(priority: 50).perform_later(patient)
+        end
 
-    patients
-      .where("updated_from_pds_at < ?", 6.hours.ago)
-      .order(:updated_from_pds_at)
-      .find_each do |patient|
-        PatientUpdateFromPDSJob.set(priority: 50).perform_later(patient)
-      end
+      patients
+        .where("updated_from_pds_at < ?", 6.hours.ago)
+        .order(:updated_from_pds_at)
+        .find_each do |patient|
+          PatientUpdateFromPDSJob.set(priority: 50).perform_later(patient)
+        end
+    end
   end
 end
