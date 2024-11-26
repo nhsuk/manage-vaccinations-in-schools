2a9eb682aceaf8b8b67e6a6a963a6578532f1a42
Thomas Leese <thomas@leese.io>
2024-11-26 16:06:24 +0000

    Schedule bulk PDS jobs with gaps
    This changes how the jobs to update patient information in PDS are
    scheduled to introduce gaps between the jobs as part of the enqueue
    process rather than relying only on Good Job to handle the concurrency
    throttling.
    
    Due to the way Good Jobs concurrency throttling works, we end up with
    lots of job failures from `ThrottleExceeded` exceptions, where the jobs
    are then enqueued at a later date. This approach works but is very
    resource intensive, given our rate of 5 requests per second and the fact
    that the majority of our jobs will fail.
    
    I'm also seeing race condition issues when we have lots of jobs they
    don't seem to be able to succeed, and I reckon this is down to the fact
    that by the time the jobs are ready to run, they're no longer part of
    the active jobs.

--- app/jobs/bulk_update_patients_from_pds_job.rb
+++ app/jobs/bulk_update_patients_from_pds_job.rb
@@ -13,15 +13,18 @@ class BulkUpdatePatientsFromPDSJob < ApplicationJob
     GoodJob::Bulk.enqueue do
       patients
         .where(updated_from_pds_at: nil)
-        .find_each do |patient|
-          PatientUpdateFromPDSJob.set(priority: 50).perform_later(patient)
-        end
+        .or(patients.where("updated_from_pds_at < ?", 6.hours.ago))
+        .order("updated_from_pds_at ASC NULLS FIRST")
+        .each_with_index do |patient, index|
+          # Schedule with a delay to preemptively handle rate limit issues.
+          # This shouldn't be necessary, but we're finding that Good Job
+          # has occasional race condition issues, and spreading out the jobs
+          # should reduce the risk of this.
 
-      patients
-        .where("updated_from_pds_at < ?", 6.hours.ago)
-        .order(:updated_from_pds_at)
-        .find_each do |patient|
-          PatientUpdateFromPDSJob.set(priority: 50).perform_later(patient)
+          PatientUpdateFromPDSJob.set(
+            priority: 50,
+            wait: 0.21 * index
+          ).perform_later(patient)
         end
     end
   end
