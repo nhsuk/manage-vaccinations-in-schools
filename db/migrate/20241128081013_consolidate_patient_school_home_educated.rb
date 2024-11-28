620acfacb8b567e4f135b8bc0ef238806fb82b10
Thomas Leese <thomas@leese.io>
2024-11-28 08:15:37 +0000

    Add migration to consolidate patient school and home educated values
    Following on from the previous commit this adds a migration which ensures
    that the patients are all valid following the introduction of new
    validation.

---
+++ db/migrate/20241128081013_consolidate_patient_school_home_educated.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class ConsolidatePatientSchoolHomeEducated < ActiveRecord::Migration[7.2]
+  def change
+    # school present and home_educated_nil => known school
+    # school nil and home_educated true => home schooled
+    # school nil and home_educated false => unknown school
+
+    Patient.where.not(school_id: nil).update_all(home_educated: nil)
+    Patient.where(home_educated: true).update_all(school_id: nil)
+    Patient.where(home_educated: false).update_all(school_id: nil)
+
+    if Patient.any?(&:invalid?)
+      raise "Patients are not all valid. Aborting to rollback transaction."
+    end
+  end
+end
