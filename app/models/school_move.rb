55a39d4c86a0e91c071b1c90e63ba58109961ca8
Thomas Leese <thomas@leese.io>
2024-11-28 11:27:18 +0000

    Add SchoolMove#confirm!
    This adds a new method on school moves which will be called when the
    user chooses too confirm a school move, meaning that the patient record
    should be updated and then the patient should be moved around to the
    most approriate session.

--- app/models/school_move.rb
+++ app/models/school_move.rb
@@ -47,7 +47,62 @@ class SchoolMove < ApplicationRecord
               unless: -> { school.nil? }
             }
 
+  def confirm!
+    ActiveRecord::Base.transaction do
+      update_patient!
+      update_sessions!
+      destroy! if persisted?
+    end
+  end
+
   def ignore!
     destroy! if persisted?
+  end
+
+  private
+
+  def update_patient!
+    patient.update!(school:, home_educated:, cohort:)
+  end
+
+  def update_sessions!
+    session = new_session
+
+    patient.patient_sessions.find_each(&:destroy_if_safe!)
+
+    # Patient is moving to a school not managed by an organisation.
+    # All we can do here is remove them from the cohort.
+    return if session.nil?
+
+    PatientSession.find_or_create_by!(patient:, session:)
+  end
+
+  def cohort
+    (school&.organisation || organisation)&.cohorts&.find_or_create_by!(
+      birth_academic_year: patient.date_of_birth.academic_year
+    )
+  end
+
+  def new_session
+    if in_clinic?
+      # If they've been booked in to a clinic we don't move them to a
+      # school session as it's likely they're in a clinic for a reason.
+      # TODO: Ask the user what they want to do here.
+
+      (school&.organisation || organisation)&.generic_clinic_session
+    elsif home_educated || school.nil?
+      organisation.generic_clinic_session
+    elsif school.organisation
+      existing_session =
+        school.organisation.sessions.upcoming.find_by(location: school)
+
+      # There are no upcoming sessions available for their chosen school.
+      # This can happen if the parent fills out the form late.
+      existing_session || school.organisation.generic_clinic_session
+    end
+  end
+
+  def in_clinic?
+    patient.patient_sessions.joins(:location).merge(Location.clinic).exists?
   end
 end
