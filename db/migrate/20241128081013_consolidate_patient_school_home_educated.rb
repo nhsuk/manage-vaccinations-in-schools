afe41aaa099983cca083173bd921a70d242b4633
Misha Gorodnitzky <misaka@dmfg.io>
2024-11-29 14:57:39 +0000

    Add additional logging to failing migration

--- db/migrate/20241128081013_consolidate_patient_school_home_educated.rb
+++ db/migrate/20241128081013_consolidate_patient_school_home_educated.rb
@@ -8,15 +8,23 @@ class ConsolidatePatientSchoolHomeEducated < ActiveRecord::Migration[7.2]
 
     Patient.where.not(school_id: nil).update_all(home_educated: nil)
     Patient.where(school_id: nil, home_educated: nil).update_all(
-      home_educated: false
+      home_educated: false,
     )
     Patient.where(home_educated: true).update_all(school_id: nil)
     Patient.where(home_educated: false).update_all(school_id: nil)
 
     if (patients = Patient.all.select(&:invalid?)).present?
-      errors = patients.sample(5).flat_map { _1.errors.full_messages }
-      raise "Patients are not all valid. Aborting to rollback transaction. " \
-              "Sample of errors: #{errors.to_sentence}"
+      Rails.logger.error("Number of invalid patients: #{patients.count}")
+      some_patients = patients.sample(5)
+      some_patients.each do |patient|
+        Rails.logger.error(
+          "Patient #{patient.id} (school_id: #{patient.school_id} " \
+          "home_educated: #{patient.home_educated}) is invalid" \
+          ": #{patient.errors.full_messages}"
+        )
+      end
+
+      raise "Patients are not all valid. Aborting to rollback transaction."
     end
   end
 end
