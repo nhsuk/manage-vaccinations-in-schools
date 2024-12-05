a6341bd4e5b654be2e2e2be9be0540dcf9e1a660
Thomas Leese <thomas@leese.io>
2024-12-05 09:21:54 +0000

    Add a default name to AppBacklinkComponent
    This component is often used as part of a wizard where we've got
    "previous page" hard coded as the name of the link, so instead we can
    move this in to the component itself to avoid repeating the string in
    lots of places.
    
    I've also changed the first argument to not be a keyword argument as
    it's likely to be obvious that it's the path of the backlink, since it's
    the only required argument.

--- spec/components/app_backlink_component_spec.rb
+++ spec/components/app_backlink_component_spec.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-describe AppBacklinkComponent, type: :component do
-  subject { page }
+describe AppBacklinkComponent do
+  subject(:rendered) { render_inline(component) }
 
-  before { render_inline(component) }
-
-  let(:component) { described_class.new(href:, name:, classes:, attributes:) }
+  let(:component) { described_class.new(href, name:, classes:, attributes:) }
   let(:href) { "/previous_page" }
   let(:name) { "Previous Page" }
   let(:classes) { "additional-class" }
@@ -17,4 +15,10 @@ describe AppBacklinkComponent, type: :component do
   it { should have_css(".nhsuk-width-container.additional-class") }
   it { should have_css('[id="back-link"]') }
   it { should have_css(".nhsuk-u-visually-hidden", text: "to #{name}") }
+
+  context "without a name" do
+    let(:component) { described_class.new(href) }
+
+    it { should have_css(".nhsuk-u-visually-hidden", text: "to previous page") }
+  end
 end
