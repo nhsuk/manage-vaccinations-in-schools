393b09f0b6f6c44db3858e276da1f5684e8577f5
Thomas Leese <thomas@leese.io>
2024-12-05 10:35:54 +0000

    Include pre-screening in triage notes
    When showing the triage notes for a patient session we want to include
    any pre-screening checks that were done if they've got notes attached to
    them.

--- spec/components/app_triage_notes_component_spec.rb
+++ spec/components/app_triage_notes_component_spec.rb
@@ -11,15 +11,11 @@ describe AppTriageNotesComponent do
 
   context "triage notes are not present" do
     it "does not render" do
-      expect(component).not_to be_render
+      expect(component.render?).to be(false)
     end
   end
 
   context "a single triage note is present" do
-    around do |example|
-      travel_to(Time.zone.local(2023, 12, 4, 10, 4)) { example.run }
-    end
-
     let(:performed_by) { create(:user, family_name: "Gear", given_name: "Joe") }
 
     before do
@@ -29,12 +25,13 @@ describe AppTriageNotesComponent do
         programme:,
         notes: "Some notes",
         patient:,
-        performed_by:
+        performed_by:,
+        created_at: Time.zone.local(2023, 12, 4, 10, 4)
       )
     end
 
     it "renders" do
-      expect(component.render?).to be true
+      expect(component.render?).to be(true)
     end
 
     it { should have_css("h3", text: "Triaged decision: Safe to vaccinate") }
@@ -57,9 +54,34 @@ describe AppTriageNotesComponent do
     before { create_list(:triage, 2, programme:, patient:) }
 
     it "renders" do
-      expect(component).to be_render
+      expect(component.render?).to be(true)
     end
 
     it { should have_css("hr") }
+  end
+
+  context "with a pre-screening" do
+    let(:performed_by) { create(:user, family_name: "Gear", given_name: "Joe") }
+
+    before do
+      create(
+        :pre_screening,
+        :allows_vaccination,
+        patient_session:,
+        notes: "Some notes",
+        performed_by:,
+        created_at: Time.zone.local(2023, 12, 4, 10, 4)
+      )
+    end
+
+    it "renders" do
+      expect(component.render?).to be(true)
+    end
+
+    it { should have_css("h3", text: "Completed pre-screening checks") }
+    it { should have_css("p", text: "Some notes") }
+    it { should have_css("p", text: "4 December 2023 at 10:04am") }
+    it { should have_css("p", text: "Joe Gear") }
+    it { should_not have_css("hr") }
   end
 end
