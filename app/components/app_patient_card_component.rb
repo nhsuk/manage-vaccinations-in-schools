8bbdc2ab8dfc2c9ada891a6eec5bb75f25224f5c
Thomas Leese <thomas@leese.io>
2024-12-11 10:39:45 +0000

    Add flags to AppPatientCardComponent
    This adds a number of flags to the component when viewing a "child
    record" allowing the nurses to see clearly if there are any flags on the
    patient.

--- app/components/app_patient_card_component.rb
+++ app/components/app_patient_card_component.rb
@@ -4,6 +4,26 @@ class AppPatientCardComponent < ViewComponent::Base
   erb_template <<-ERB
     <%= render AppCardComponent.new do |card| %>
       <% card.with_heading { "Child record" } %>
+      
+      <% if Flipper.enabled?(:"v1.2.0") %>
+        <% if @patient.date_of_death.present? %>
+          <%= render AppNoticeStatusComponent.new(
+            text: "Record updated with child’s date of death"
+          ) %>
+        <% end %>
+        
+        <% if @patient.invalidated? %>
+          <%= render AppNoticeStatusComponent.new(
+            text: "Record flagged as invalid"
+          ) %>
+        <% end %>
+        
+        <% if @patient.restricted? %>
+          <%= render AppNoticeStatusComponent.new(
+            text: "Record flagged as sensitive"
+          ) %>
+        <% end %>
+      <% end %>
 
       <%= render AppPatientSummaryComponent.new(
                patient,
