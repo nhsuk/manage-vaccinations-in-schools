nhsuk/manage-vaccinations-in-schools

commit ddbd6b96507cbf86388754fa72843c72adcc605d

Remove N+1 on register attendance page
The `.select` was causing a query for `session_attendances` based on
today's `@session_date`.

Instead, we can more directly use two `where` clauses:

- Either for patient sessions with no attendances
- Or for patient sessions with attendances but not on today's date

The patient sessions need to be stored in an intermediate `ps` variable
to maintain the joins from `includes` when using `.or`.

app/controllers/register_attendances_controller.rb
@@ -4,9 +4,9 @@ class RegisterAttendancesController < ApplicationController
   include PatientSortingConcern
 
   before_action :set_session, only: %i[index create]
+  before_action :set_session_date
   before_action :set_patient_sessions, only: :index
   before_action :set_patient, only: :create
-  before_action :set_session_date, only: :create
   before_action :set_patient_session, only: :create
 
   layout "full"
@@ -41,19 +41,21 @@ class RegisterAttendancesController < ApplicationController
   end
 
   def set_patient_sessions
+    ps =
+      @session.patient_sessions.strict_loading.includes(
+        :patient,
+        :vaccination_records,
+        :triages,
+        :consents,
+        session: :session_dates,
+        session_attendances: :session_date
+      )
+
     @patient_sessions =
-      @session
-        .patient_sessions
-        .strict_loading
-        .includes(
-          :patient,
-          :vaccination_records,
-          :triages,
-          :consents,
-          session: :session_dates,
-          session_attendances: :session_date
-        )
-        .select { _1.todays_attendance&.attending.nil? }
+      ps
+        .where
+        .missing(:session_attendances)
+        .or(ps.where.not(session_attendances: { session_date: @session_date }))
   end
 
   def set_patient

commit fbe9b0fb1d982269ec3ba1285c1547edf12d82aa

Fix sort/filter not working on attendances page
This fixes a bug that was inadvertently introduced in
ddbd6b96507cbf86388754fa72843c72adcc605d where users could no longer
sort and filter on the register attendances page. This was happening
because we try and call `select!` on an
`ActiveRecord::AssociationRelation` which doesn't have such a method,
instead we have to first convert the list of patients to an array.

app/controllers/register_attendances_controller.rb
@@ -56,6 +56,7 @@ class RegisterAttendancesController < ApplicationController
         .where
         .missing(:session_attendances)
         .or(ps.where.not(session_attendances: { session_date: @session_date }))
+        .to_a
   end
 
   def set_patient