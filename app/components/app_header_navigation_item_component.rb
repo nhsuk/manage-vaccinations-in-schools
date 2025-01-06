ed605218a85c3419b909765d93523956bd806f9d
Thomas Leese <thomas@leese.io>
2025-01-06 08:54:06 +0000

    Update header styles
    This updates the styles for the header component to match the latest
    styles in the prototype.

--- app/components/app_header_navigation_item_component.rb
+++ app/components/app_header_navigation_item_component.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 class AppHeaderNavigationItemComponent < ViewComponent::Base
-  erb_template <<-ERB
-    <li class="nhsuk-header__navigation-item">
-      <%= link_to @title, @path,
-                  class: "nhsuk-header__navigation-link",
-                  aria: { current: current? ? "true" : nil } %>
-    </li>
-  ERB
-
   def initialize(title, path, request_path:)
     super
 
@@ -17,7 +9,27 @@ class AppHeaderNavigationItemComponent < ViewComponent::Base
     @request_path = request_path
   end
 
+  def call
+    tag.li(class: classes) do
+      link_to(
+        @title,
+        @path,
+        class: "nhsuk-header__navigation-link",
+        aria: {
+          current: current? ? "true" : nil
+        }
+      )
+    end
+  end
+
   def current?
     @request_path.starts_with?(@path)
+  end
+
+  def classes
+    [
+      "nhsuk-header__navigation-item",
+      ("app-header__navigation-item--current" if current?)
+    ].compact.join(" ")
   end
 end
