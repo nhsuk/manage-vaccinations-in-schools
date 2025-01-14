690e6d5f8dcb7eae91d1f1c917d6e561b438820d
Thomas Leese <thomas@leese.io>
2025-01-14 09:04:57 +0000

    Add NotifyDeliveryJob
    This encapsulates some logic related to sending emails and texts via
    GOV.UK Notify in to its own class so that in the future we can use this
    when sending emails. We need to refactor the way emails are sent to use
    the API directly so that we can integrate with the status receipts
    provided by GOV.UK and this is the first step of that.

--- app/jobs/text_delivery_job.rb
+++ app/jobs/text_delivery_job.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-class TextDeliveryJob < ApplicationJob
-  queue_as { Rails.configuration.action_mailer.deliver_later_queue_name }
-
+class TextDeliveryJob < NotifyDeliveryJob
   def perform(
     template_name,
     consent: nil,
@@ -33,21 +31,13 @@ class TextDeliveryJob < ApplicationJob
         vaccination_record:
       )
 
+    args = { personalisation:, phone_number:, template_id: }
+
     delivery_id =
       if self.class.send_via_notify?
-        response =
-          self.class.client.send_sms(
-            phone_number:,
-            template_id:,
-            personalisation:
-          )
-        response.id
+        self.class.client.send_sms(**args).id
       elsif self.class.send_via_test?
-        self.class.deliveries << {
-          phone_number:,
-          template_id:,
-          personalisation:
-        }
+        self.class.deliveries << args
         SecureRandom.uuid
       else
         Rails.logger.info "Sending text message to #{phone_number} with template #{template_id}"
@@ -65,27 +55,5 @@ class TextDeliveryJob < ApplicationJob
       template_id:,
       type: :sms
     )
-  end
-
-  def self.client
-    @client ||=
-      Notifications::Client.new(
-        Rails.configuration.action_mailer.notify_settings[:api_key]
-      )
-  end
-
-  def self.deliveries
-    @deliveries ||= []
-  end
-
-  def self.send_via_notify?
-    Rails.configuration.action_mailer.delivery_method == :notify
-  end
-
-  def self.send_via_test?
-    Rails.configuration.action_mailer.delivery_method == :test
-  end
-
-  class UnknownTemplate < StandardError
   end
 end
