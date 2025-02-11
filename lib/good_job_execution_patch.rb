a8e0c8b7fc03c3a8bc7fec5dd49e6b1f5d08f9d1
Misha Gorodnitzky <misaka@dmfg.io>
2025-02-11 22:22:25 +0000

    WIP: good job patch state and counts

--- lib/good_job_execution_patch.rb
+++ lib/good_job_execution_patch.rb
@@ -7,19 +7,30 @@
 require "good_job"
 
 module GoodJobExecutionPatch
-  def run_stats(since: 1.hour.ago, batch: nil)
-    where("finished_at > ?", since)
-      .select do
-        batch.nil? || it.serialized_params["arguments"][0]["batch"] == batch
-      end
+  def get_batch(batch = nil, since: 1.hour.ago)
+    where("finished_at > ?", since).select do
+      batch.nil? || it.serialized_params["arguments"][0]["batch"] == batch
+    end
+  end
+
+  def batch_stats(batch = nil, since: 1.hour.ago)
+    get_batch(batch, since:)
       .group_by { it.finished_at.change(usec: 0) }
       .transform_values do |executions|
-        { total: executions.count }.tap do
-          errors = executions.select { _1.error.present? }
-          it.merge!(errors: errors.count) if errors.any?
+        { total: executions.count }.tap do |stats|
+          stats[:avg_duration] = executions.sum(&:duration) / stats[:total]
+          executions
+            .partition { it.error.nil? }
+            .tap do |successs, errors|
+              stats.merge! successes: successs.count, errors: errors.pluck(:id)
+            end
         end
       end
       .sort_by(&:first)
+  end
+
+  def batch_count(batch = nil, since: 1.hour.ago)
+    get_batch(batch, since:).count
   end
 end
 
