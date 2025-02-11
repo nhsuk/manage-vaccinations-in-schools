abee1e28396fe5929bbaf96d84bc251c292d9d7a
Thomas Leese <thomas@leese.io>
2025-02-11 11:49:47 +0000

    Add year group selection when importing class lists
    This is a new feature that allows the nurses to choose the year groups
    they are going to import in the class list. Depending on the year groups
    selected, patients not in the year group won't be removed from the
    session.

--- app/controllers/draft_class_imports_controller.rb
+++ app/controllers/draft_class_imports_controller.rb
@@ -9,16 +9,21 @@ class DraftClassImportsController < ApplicationController
   before_action :set_session_options,
                 only: %i[show update],
                 if: -> { current_step == :session }
+  before_action :set_year_group_options,
+                only: %i[show update],
+                if: -> { current_step == :year_groups }
 
   skip_after_action :verify_policy_scoped, only: %i[show update]
 
   def new
-    session = policy_scope(Session).find_by!(slug: params[:session_slug])
-
     @draft_class_import.reset!
-    @draft_class_import.update!(session:)
 
-    redirect_to draft_class_import_path(Wicked::FINISH_STEP)
+    if (session = policy_scope(Session).find_by(slug: params[:session_slug]))
+      @draft_class_import.update!(session:)
+      redirect_to draft_class_import_path("year-groups")
+    else
+      redirect_to draft_class_import_path(Wicked::FIRST_STEP)
+    end
   end
 
   def show
@@ -50,6 +55,16 @@ class DraftClassImportsController < ApplicationController
         .where(location: { type: :school })
   end
 
+  def set_year_group_options
+    @year_group_options =
+      (@session.year_groups & @session.location.year_groups).map do |year_group|
+        OpenStruct.new(
+          value: year_group,
+          label: helpers.format_year_group(year_group)
+        )
+      end
+  end
+
   def set_steps
     self.steps = @draft_class_import.wizard_steps
   end
@@ -59,9 +74,17 @@ class DraftClassImportsController < ApplicationController
   end
 
   def update_params
-    {
-      session_id: params.dig(:draft_class_import, :session_id),
-      wizard_step: current_step
-    }
+    if current_step == :session
+      {
+        session_id: params.dig(:draft_class_import, :session_id),
+        wizard_step: current_step
+      }
+    elsif current_step == :year_groups
+      {
+        year_groups:
+          (params.dig(:draft_class_import, :year_groups) || []).compact_blank,
+        wizard_step: current_step
+      }
+    end
   end
 end
--- spec/features/import_class_lists_spec.rb
+++ spec/features/import_class_lists_spec.rb
@@ -8,6 +8,7 @@ describe "Import class lists" do
 
     when_i_visit_a_session_page_for_the_hpv_programme
     and_i_start_adding_children_to_the_session
+    and_i_select_the_year_groups
     then_i_should_see_the_import_page
 
     when_i_continue_without_uploading_a_file
@@ -30,6 +31,7 @@ describe "Import class lists" do
     then_i_should_the_errors_page_with_invalid_fields
 
     when_i_go_back_to_the_upload_page
+    and_i_select_the_year_groups
     and_i_upload_a_valid_file
     then_i_should_see_the_upload
     and_i_should_see_the_patients
@@ -70,6 +72,14 @@ describe "Import class lists" do
     click_on "Import class list records"
   end
 
+  def and_i_select_the_year_groups
+    check "Year 8"
+    check "Year 9"
+    check "Year 10"
+    check "Year 11"
+    click_on "Continue"
+  end
+
   def then_i_should_see_the_import_page
     expect(page).to have_content("Import class list")
   end
@@ -95,6 +105,9 @@ describe "Import class lists" do
   def then_i_should_see_the_upload
     expect(page).to have_content("Imported on")
     expect(page).to have_content("Imported byTest User")
+    expect(page).to have_content(
+      "Year groupsYear 8, Year 9, Year 10, and Year 11"
+    )
   end
 
   def when_i_click_on_the_imports_tab
