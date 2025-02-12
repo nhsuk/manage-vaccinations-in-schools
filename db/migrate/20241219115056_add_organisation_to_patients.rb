1c4fc62025c297cfae539ee6f494db6b0260b0e3
Thomas Leese <thomas@leese.io>
2025-02-12 11:16:01 +0000

    Update migration to fix cohort assignment
    We cannot rely on the cohort relationship as it's removed in the model.

--- db/migrate/20241219115056_add_organisation_to_patients.rb
+++ db/migrate/20241219115056_add_organisation_to_patients.rb
@@ -3,10 +3,12 @@
 class AddOrganisationToPatients < ActiveRecord::Migration[8.0]
   def up
     add_reference :patients, :organisation, foreign_key: true
+
     Patient
-      .eager_load(:cohort)
+      .where.not(cohort_id: nil)
       .find_each do |patient|
-        patient.update_column(:organisation_id, patient.cohort&.organisation)
+        cohort = Cohort.find(patient.cohort_id)
+        patient.update_column(:organisation_id, cohort.organisation_id)
       end
   end
 
