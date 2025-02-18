9a0ef6615209fa07e74a0af4eea9c61f73a72c4f
Brage Gording <brage.gording@tngtech.com>
2025-02-18 11:00:21 +0000

    MAVIS-2020: Add explicit nullable conditions
    * Makes interpreteation more explicit

--- terraform/app/variables.tf
+++ terraform/app/variables.tf
@@ -7,11 +7,12 @@ variable "region" {
 variable "environment" {
   type        = string
   description = "String literal for the environment"
-  nullable = false
+  nullable    = false
 
   validation {
     condition = contains([
-    "poc", "copilotmigration", "qa", "test", "training", "preview", "production"], var.environment)
+      "poc", "copilotmigration", "qa", "test", "training", "preview", "production"
+    ], var.environment)
     error_message = "Valid values for environment: poc, copilotmigration, qa, test, training, preview, production."
   }
 }
@@ -20,14 +21,14 @@ variable "account_id" {
   type        = string
   default     = "393416225559"
   description = "ID of aws account. Defaults to non-prod account."
-  nullable = false
+  nullable    = false
 }
 
 variable "domain_name" {
   type        = string
   default     = "mavistesting.com"
   description = "Domain for which to create DNS certificate"
-  nullable = false
+  nullable    = false
 }
 
 variable "dns_certificate_arn" {
@@ -39,21 +40,21 @@ variable "firewall_subnet_cidr" {
   type        = string
   description = "CIDR block for the firewall subnet"
   default     = "10.0.5.0/24"
-  nullable = false
+  nullable    = false
 }
 
 variable "enable_firewall" {
   type        = bool
   default     = false
   description = "Boolean toggle to determine whether the firewall should be enabled."
-  nullable = false
+  nullable    = false
 }
 
 variable "firewall_log_retention_days" {
   type        = number
   default     = 3
   description = "Number of days to retain logs for the firewall"
-  nullable = false
+  nullable    = false
 }
 
 
@@ -69,20 +70,20 @@ variable "resource_name" {
     }
   )
   description = "Names of terraform managed resource. Used to import pre-existing infrastructure resources"
-  nullable = false
+  nullable    = false
 }
 
 variable "ecs_log_retention_days" {
   type        = number
   default     = 7
   description = "Number of days to retain logs for ecs instances"
-  nullable = false
+  nullable    = false
 }
 variable "vpc_log_retention_days" {
   type        = number
   default     = 7
   description = "Number of days to retain logs for the vpc traffic"
-  nullable = false
+  nullable    = false
 }
 
 ########## Task definition configuration ##########
@@ -91,9 +92,9 @@ variable "rails_env" {
   type        = string
   default     = "staging"
   description = "The rails environment configuration to use for the mavis application"
-  nullable = false
+  nullable    = false
   validation {
-    condition     = contains(["staging", "production"], var.rails_env)
+    condition = contains(["staging", "production"], var.rails_env)
     error_message = "Incorrect rails environment, allowed values are: {staging, production}"
   }
 }
@@ -102,43 +103,47 @@ variable "rails_master_key_path" {
   type        = string
   default     = "/mavis/development/credentials/RAILS_MASTER_KEY"
   description = "The path of the System Manager Parameter Store secure string for the rails master key."
-  nullable = false
+  nullable    = false
 }
 
 variable "container_name" {
   type        = string
   default     = "mavis"
   description = "Name of essential container in the task definition."
-  nullable = false
+  nullable    = false
 }
 
 variable "docker_image" {
   type        = string
   default     = "mavis/webapp"
   description = "The docker image name for the essential container in the task definition"
-  nullable = false
+  nullable    = false
 }
 
 variable "image_digest" {
   type        = string
   description = "The docker image tag for the essential container in the task definition."
+  nullable    = false
 }
 variable "cis2_enabled" {
   type        = bool
   default     = false
   description = "Boolean toggle to determine whether the CIS2 feature should be enabled."
+  nullable    = false
 }
 
 variable "pds_enabled" {
   type        = bool
   default     = false
   description = "Boolean toggle to determine whether the PDS feature should be enabled."
+  nullable    = false
 }
 
 variable "splunk_enabled" {
   type        = bool
   default     = false
   description = "Boolean toggle to determine whether the Splunk feature should be enabled."
+  nullable    = false
 }
 
 locals {
@@ -186,7 +191,8 @@ locals {
   task_secrets = [
     {
       name      = var.db_secret_arn == null ? "DB_CREDENTIALS" : "DB_SECRET"
-      valueFrom = var.db_secret_arn == null ? aws_rds_cluster.aurora_cluster.master_user_secret[0].secret_arn : var.db_secret_arn
+      valueFrom = var.db_secret_arn == null ? aws_rds_cluster.aurora_cluster.master_user_secret[0].secret_arn :
+        var.db_secret_arn
     },
     {
       name      = "RAILS_MASTER_KEY"
@@ -203,8 +209,8 @@ variable "db_secret_arn" {
 }
 
 variable "backup_retention_period" {
-  type = number
-  default = 7
+  type        = number
+  default     = 7
   description = "The number of days to retain backups for the RDS cluster."
 }
 
