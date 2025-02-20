557ca8ef93ba0754a48a02db1484111319ef9505
Brage Gording <brage.gording@tngtech.com>
2025-02-20 12:48:59 +0000

    MAV-384: Fix validation script
    * In case of no modifications to terraform pipeline the validation
      should be considered successful

--- terraform/scripts/check_task_definition.sh
+++ terraform/scripts/check_task_definition.sh
@@ -5,21 +5,25 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-tfstdout=$1
+tf_stdout=$1
 # Check task definition is replaced
-if [[ $(grep -ce "aws_ecs_task_definition\.task_definition.*replaced" "$tfstdout") -eq 1 ]]; then
+if [[ $(grep -ce "No changes.*Your infrastructure matches the configuration" "$tf_stdout") -eq 1 ]]; then
+  echo "No changes detected, continuing."
+  exit 0
+fi
+if [[ $(grep -ce "aws_ecs_task_definition\.task_definition.*replaced" "$tf_stdout") -eq 1 ]]; then
   echo "Task definition is being replaced"
 else
   echo "Task definition is not being replaced, aborting."
   exit 1
 fi
-if [[ $(grep -ce "aws_s3_object\.appspec_object.*updated in-place" "$tfstdout") -eq 1 ]]; then
+if [[ $(grep -ce "aws_s3_object\.appspec_object.*updated in-place" "$tf_stdout") -eq 1 ]]; then
   echo "S3 bucket object is being replaced"
 else
   echo "S3 bucket object is not being replaced, aborting."
   exit 1
 fi
-if [[ $(grep -c "1 to add, 1 to change, 1 to destroy." "$tfstdout") -eq 1 ]]; then
+if [[ $(grep -c "1 to add, 1 to change, 1 to destroy." "$tf_stdout") -eq 1 ]]; then
   echo "Only 1 change and 1 replace detected"
 else
   echo "Other changes than task definition and s3 bucket object are being made"
