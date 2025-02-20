207429a14a39c92ad815e836d70d556e3cdf6c7e
Brage Gording <brage.gording@tngtech.com>
2025-02-20 15:39:57 +0000

    MAV-384: Make validation script more robust
    * It should be able to handle the case where resources are created for
      the first time

--- terraform/scripts/check_task_definition.sh
+++ terraform/scripts/check_task_definition.sh
@@ -11,22 +11,30 @@ if [[ $(grep -ce "No changes.*Your infrastructure matches the configuration" "$t
   echo "No changes detected, continuing."
   exit 0
 fi
-if [[ $(grep -ce "aws_ecs_task_definition\.task_definition.*replaced" "$tf_stdout") -eq 1 ]]; then
-  echo "Task definition is being replaced"
+if [[ $(grep -cE "aws_ecs_task_definition\.task_definition.*(replaced|created)" "$tf_stdout") -eq 1 ]]; then
+  echo "Task definition is being replaced or created"
 else
   echo "Task definition is not being replaced, aborting."
   exit 1
 fi
-if [[ $(grep -ce "aws_s3_object\.appspec_object.*updated in-place" "$tf_stdout") -eq 1 ]]; then
-  echo "S3 bucket object is being replaced"
+if [[ $(grep -cE "aws_s3_object\.appspec_object.*(updated in-place|created)" "$tf_stdout") -eq 1 ]]; then
+  echo "S3 bucket object is being replaced or created"
 else
   echo "S3 bucket object is not being replaced, aborting."
   exit 1
 fi
-if [[ $(grep -c "1 to add, 1 to change, 1 to destroy." "$tf_stdout") -eq 1 ]]; then
-  echo "Only 1 change and 1 replace detected"
-else
-  echo "Other changes than task definition and s3 bucket object are being made"
+MODIFICATIONS=$(grep -E "[0-9]+ to add, [0-9]+ to change, [0-9]+ to destroy." test_less)
+ADDITIONS=$(echo "$MODIFICATIONS" | sed -E 's/.*([0-9]+) to add.*/\1/')
+CHANGES=$(echo "$MODIFICATIONS" | sed -E 's/.*([0-9]+) to change.*/\1/')
+DELETIONS=$(echo "$MODIFICATIONS" | sed -E 's/.*([0-9]+) to destroy.*/\1/')
+if [[ $DELETIONS -gt $ADDITIONS ]]; then
+  echo "More resources are being destroyed than created."
+  echo "Other resources than task definition and s3 bucket object are being deleted, aborting."
+  exit 1
+fi
+if [[ $((CHANGES + ADDITIONS)) -gt 2 ]]; then
+  echo "More than 2 resources are being changed."
+  echo "Other changes than task definition and s3 bucket object are being made, aborting."
   exit 1
 fi
 echo "Basic checks passed, only task definition and S3 bucket changes observed."
