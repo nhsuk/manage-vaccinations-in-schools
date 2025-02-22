6c8bfdd2ed40491b1ba91fa59947c35fbed387f9
Misha Gorodnitzky <misaka@dmfg.io>
2025-02-22 00:31:24 +0000

    Allow specifying date to use when sending consent
    Also refactor the date matching to try to reduce the possibility of sending
    a consent reminder out if one has already been sent out today

--- lib/herts_consent_reminders.rb
+++ lib/herts_consent_reminders.rb
@@ -17,7 +17,10 @@ module HertsConsentReminders
   def self.send_consent_reminders(session)
     return unless session.open_for_consent?
 
-    filter_patients_to_send_consent(session).each do |patient, programme, type|
+    filter_patients_to_send_consent(
+      session,
+      on_date:
+    ).each do |patient, programme, type|
       ConsentNotification.create_and_send!(
         patient:,
         programme:,
@@ -27,13 +30,20 @@ module HertsConsentReminders
     end
   end
 
-  def self.filter_patients_to_send_consent(session)
+  def self.filter_patients_to_send_consent(session, on_date: nil)
     session.programmes.flat_map do |programme|
       session
         .patients
         .includes(:consents, :consent_notifications)
         .map do |patient|
-          next unless should_send_notification?(patient:, programme:, session:)
+          unless should_send_notification?(
+                   patient:,
+                   programme:,
+                   session:,
+                   on_date:
+                 )
+            next
+          end
 
           sent_initial_reminder =
             patient.consent_notifications.any?(&:initial_reminder?)
@@ -48,20 +58,34 @@ module HertsConsentReminders
     end
   end
 
-  def self.should_send_notification?(patient:, programme:, session:)
+  def self.should_send_notification?(
+    patient:,
+    programme:,
+    session:,
+    on_date: nil
+  )
     return false unless patient.send_notifications?
-
     return false if patient.has_consent?(programme)
-
     return false if patient.consent_notifications.none?(&:request?)
+    return false if session.dates.empty?
+
+    last_sent =
+      patient.consent_notifications.select(&:reminder?).map(&:sent_at).max
+    return false if last_sent&.to_date == Date.current
 
     reminders_sent = patient.consent_notifications.select(&:reminder?).length
-
     return false if reminders_sent >= REMINDERS_BEFORE_SESSION_DAYS.count
 
+    next_reminder_date = next_reminder_for_session(session, reminders_sent)
+    on_date ||= Date.current
+    on_date >= next_reminder_date
+  end
+
+  def self.next_reminder_for_session(session, reminders_sent)
     session_date = session.dates.first
     reminder_dates = REMINDERS_BEFORE_SESSION_DAYS.map { session_date - it }
-    next_reminder_date = reminder_dates[reminders_sent]
+    reminder_dates[reminders_sent]
+  end
 
     Date.current >= next_reminder_date
   end
