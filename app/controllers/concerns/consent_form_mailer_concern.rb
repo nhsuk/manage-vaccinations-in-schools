42c639a7700c4967ffaabd7f16c58d898beebe38
Thomas Leese <thomas@leese.io>
2025-02-25 14:59:50 +0000

    Send consent confirmation emails per programme
    If a user has consent to one vaccine programme, but not the other, we
    need to make sure we sent them two emails, confirming the consent given
    and the consent refused on each.

--- app/controllers/concerns/consent_form_mailer_concern.rb
+++ app/controllers/concerns/consent_form_mailer_concern.rb
@@ -10,34 +10,57 @@ module ConsentFormMailerConcern
         consent_form:
       )
     elsif consent_form.consent_refused?
-      EmailDeliveryJob.perform_later(
-        :consent_confirmation_refused,
-        consent_form:
-      )
-
-      if consent_form.parent_phone_receive_updates
-        SMSDeliveryJob.perform_later(
-          :consent_confirmation_refused,
-          consent_form:
-        )
-      end
-    elsif consent_form.needs_triage?
-      EmailDeliveryJob.perform_later(
-        :consent_confirmation_triage,
-        consent_form:
-      )
-    elsif consent_form.actual_session.clinic? ||
-          consent_form.actual_session.completed?
-      EmailDeliveryJob.perform_later(
-        :consent_confirmation_clinic,
-        consent_form:
-      )
+      send_consent_form_confirmation_refused(consent_form)
     else
-      EmailDeliveryJob.perform_later(:consent_confirmation_given, consent_form:)
+      ProgrammeGrouper
+        .call(consent_form.chosen_programmes)
+        .each_value do |programmes|
+          if consent_form.needs_triage?
+            EmailDeliveryJob.perform_later(
+              :consent_confirmation_triage,
+              consent_form:,
+              programmes:
+            )
+          elsif consent_form.actual_session.clinic? ||
+                consent_form.actual_session.completed?
+            EmailDeliveryJob.perform_later(
+              :consent_confirmation_clinic,
+              consent_form:,
+              programmes:
+            )
+          else
+            EmailDeliveryJob.perform_later(
+              :consent_confirmation_given,
+              consent_form:,
+              programmes:
+            )
 
-      if consent_form.parent_phone_receive_updates
-        SMSDeliveryJob.perform_later(:consent_confirmation_given, consent_form:)
-      end
+            if consent_form.parent_phone_receive_updates
+              SMSDeliveryJob.perform_later(
+                :consent_confirmation_given,
+                consent_form:,
+                programmes:
+              )
+            end
+          end
+        end
+
+      ProgrammeGrouper
+        .call(consent_form.not_chosen_programmes)
+        .each_value do |programmes|
+          send_consent_form_confirmation_refused(consent_form, programmes:)
+        end
+    end
+  end
+
+  def send_consent_form_confirmation_refused(consent_form, programmes: nil)
+    params = { consent_form: }
+    params[:programmes] = programmes if programmes
+
+    EmailDeliveryJob.perform_later(:consent_confirmation_refused, **params)
+
+    if consent_form.parent_phone_receive_updates
+      SMSDeliveryJob.perform_later(:consent_confirmation_refused, **params)
     end
   end
 end
