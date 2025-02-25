d9a25043b57e653f71121c8adc8aa731a31d083c
Brage Gording <brage.gording@tngtech.com>
2025-02-25 13:26:14 +0000

    MAV-332: Future proof configuration
    * Ensure that deletion of log group happens at least 3 minutes after log
      stream

--- terraform/app/monitoring.tf
+++ terraform/app/monitoring.tf
@@ -3,6 +3,7 @@ resource "aws_flow_log" "vpc_flowlogs" {
   log_destination = aws_cloudwatch_log_group.vpc_log_group.arn
   traffic_type    = "ALL"
   vpc_id          = aws_vpc.application_vpc.id
+  depends_on = [time_sleep.wait_to_delete_flowlogs_group]
 }
 
 resource "aws_cloudwatch_log_group" "vpc_log_group" {
@@ -10,11 +11,11 @@ resource "aws_cloudwatch_log_group" "vpc_log_group" {
   retention_in_days = var.vpc_log_retention_days
   skip_destroy      = local.is_production
 }
+
 resource "time_sleep" "wait_to_delete_flowlogs_group" {
   destroy_duration = "3m"
   depends_on = [aws_cloudwatch_log_group.vpc_log_group]
 }
-
 
 resource "aws_cloudwatch_log_group" "ecs_log_group" {
   name              = "mavis-${var.environment}-ecs"
