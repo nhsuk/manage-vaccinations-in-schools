11df3563862a79e674734a2e341deb7bfc072df7
Thomas Leese <thomas@leese.io>
2025-03-04 17:01:23 +0000

    Update session register tab page
    This updates the design of the session register page to match the latest
    designs in the prototype, where a single tab for registration is shown and
    the nurses are able to filter the patients by registration status.

--- app/components/app_patient_session_search_result_card_component.rb
+++ app/components/app_patient_session_search_result_card_component.rb
@@ -1,47 +1,68 @@
 # frozen_string_literal: true
 
 class AppPatientSessionSearchResultCardComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <%= render AppCardComponent.new(link_to:, patient: true) do |card| %>
+      <% card.with_heading { patient.full_name_with_known_as } %>
+
+      <%= govuk_summary_list do |summary_list|
+            summary_list.with_row do |row|
+              row.with_key { "Date of birth" }
+              row.with_value { helpers.patient_date_of_birth(patient) }
+            end
+
+            summary_list.with_row do |row|
+              row.with_key { "Year group" }
+              row.with_value { helpers.patient_year_group(patient) }
+            end
+
+            summary_list.with_row do |row|
+              row.with_key { "Status" }
+              row.with_value { status_tag }
+            end
+          end %>
+      
+      <% if context == :register %>
+        <div class="app-button-group">
+          <%= helpers.govuk_button_to "Attending", create_session_register_path(session, patient, "present", **params.permit(search_form: {})), class: "app-button--secondary app-button--small" %>
+          <%= helpers.govuk_button_to "Absent", create_session_register_path(session, patient, "absent", **params.permit(search_form: {})), class: "app-button--secondary-warning app-button--small" %>
+        </div>
+      <% end %>
+    <% end %>
+  ERB
+
   def initialize(patient_session, link_to:, context:)
     super
 
     @patient_session = patient_session
     @patient = patient_session.patient
+    @session = patient_session.session
     @link_to = link_to
     @context = context
 
-    raise "Unknown context: #{context}" unless context.in?(%i[consent triage])
-  end
-
-  def call
-    render AppCardComponent.new(link_to:, patient: true) do |card|
-      card.with_heading { patient.full_name_with_known_as }
-
-      govuk_summary_list do |summary_list|
-        summary_list.with_row do |row|
-          row.with_key { "Date of birth" }
-          row.with_value { helpers.patient_date_of_birth(patient) }
-        end
-
-        summary_list.with_row do |row|
-          row.with_key { "Year group" }
-          row.with_value { helpers.patient_year_group(patient) }
-        end
-
-        summary_list.with_row do |row|
-          row.with_key { "Status" }
-          row.with_value do
-            render AppProgrammeStatusTagsComponent.new(status, context:)
-          end
-        end
-      end
+    unless context.in?(%i[consent triage register])
+      raise "Unknown context: #{context}"
     end
   end
 
   private
 
-  attr_reader :patient_session, :patient, :link_to, :context
+  attr_reader :patient_session, :patient, :session, :link_to, :context
 
-  def status
-    patient_session.send(context).status
+  def status_tag
+    if context == :register
+      status = patient_session.register.status
+
+      text = I18n.t(status, scope: %i[patient_session status register label])
+
+      colour = I18n.t(status, scope: %i[patient_session status register colour])
+
+      govuk_tag(text:, colour:)
+    else
+      render AppProgrammeStatusTagsComponent.new(
+               patient_session.send(context).status,
+               context:
+             )
+    end
   end
 end
