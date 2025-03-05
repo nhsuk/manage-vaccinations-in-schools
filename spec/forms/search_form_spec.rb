dc18024d4c852e4ab6716d6f730af9c69ba58638
Thomas Leese <thomas@leese.io>
2025-03-05 14:05:56 +0000

    Ensure invalid dates don't raise an error
    This ensures that when entering an invalid date in the search form the
    user won't see an unhandled exception page, instead the date will be
    left blank.

--- spec/forms/search_form_spec.rb
+++ spec/forms/search_form_spec.rb
@@ -21,6 +21,24 @@ describe SearchForm do
   let(:triage_status) { nil }
   let(:year_groups) { %w[8 9 10 11] }
 
+  context "with invalid date parameters" do
+    subject(:form) do
+      described_class.new(
+        "date_of_birth(1i)": "invalid",
+        "date_of_birth(2i)": "value",
+        "date_of_birth(3i)": "12345"
+      )
+    end
+
+    it "doesn't raise an error" do
+      expect { form }.not_to raise_error
+    end
+
+    it "doesn't filter by date" do
+      expect(form.date_of_birth).to be_nil
+    end
+  end
+
   context "for patients" do
     it "doesn't raise an error" do
       expect { form.apply(Patient.all) }.not_to raise_error
