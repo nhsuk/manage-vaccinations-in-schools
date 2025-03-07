8f4023e0c5080a1d8c2a845fa85e2b21393c8575
Thomas Leese <thomas@leese.io>
2025-03-07 16:17:25 +0000

    Attach programme to pre-screening
    When recording a pre-screening we can attach it to the current programme
    that is being recorded so it appears in the activity log.

--- spec/factories/pre_screenings.rb
+++ spec/factories/pre_screenings.rb
@@ -14,20 +14,24 @@
 #  updated_at           :datetime         not null
 #  patient_session_id   :bigint           not null
 #  performed_by_user_id :bigint           not null
+#  programme_id         :bigint           not null
 #
 # Indexes
 #
 #  index_pre_screenings_on_patient_session_id    (patient_session_id)
 #  index_pre_screenings_on_performed_by_user_id  (performed_by_user_id)
+#  index_pre_screenings_on_programme_id          (programme_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (patient_session_id => patient_sessions.id)
 #  fk_rails_...  (performed_by_user_id => users.id)
+#  fk_rails_...  (programme_id => programmes.id)
 #
 FactoryBot.define do
   factory :pre_screening do
     patient_session
+    programme { patient_session.programmes.first }
     performed_by
 
     trait :allows_vaccination do
