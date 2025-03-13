d125b810eba5fb8138dec4c734ecd4259579db8d
Thomas Leese <thomas@leese.io>
2025-03-13 20:06:59 +0100

    Don't invalidate self-consent if vaccinated
    If the patient was vaccinated after getting self-consent, we shouldn't
    invalidate that consent as it was valid when the vaccination was given.
    A future enhancement might be to add a concept of "lifespan" to
    consents, to capture exactly at which point they are invalid.
    
    At the moment, the nurses see a patient that was vaccinated, with
    supposedly no valid consent, which is not accurate.

--- app/jobs/invalidate_self_consents_job.rb
+++ app/jobs/invalidate_self_consents_job.rb
@@ -4,29 +4,33 @@ class InvalidateSelfConsentsJob < ApplicationJob
   queue_as :consents
 
   def perform
-    organisation_ids = Organisation.pluck(:id)
-    programme_ids = Programme.pluck(:id)
+    Programme.find_each do |programme|
+      patients =
+        Patient.has_vaccination_status(
+          %i[none_yet could_not_vaccinate],
+          programme:
+        )
 
-    organisation_ids
-      .product(programme_ids)
-      .each do |organisation_id, programme_id|
+      Organisation.find_each do |organisation|
         consents =
           Consent
             .via_self_consent
-            .where(organisation_id:, programme_id:)
+            .where(organisation:, programme:)
+            .where(patient: patients)
             .where("created_at < ?", Date.current.beginning_of_day)
             .not_withdrawn
 
         triages =
           Triage
-            .where(organisation_id:, programme_id:)
-            .where("created_at < ?", Date.current.beginning_of_day)
+            .where(organisation:, programme:)
             .where(patient_id: consents.pluck(:patient_id))
+            .where("created_at < ?", Date.current.beginning_of_day)
 
         ActiveRecord::Base.transaction do
           consents.invalidate_all
           triages.invalidate_all
         end
       end
+    end
   end
 end
