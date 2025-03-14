770e9abd1c6cbccd8887fae5ef706b42b6ce22c0
Thomas Leese <thomas@leese.io>
2025-03-14 14:55:22 +0100

    Remove PatientSessionStatusConcern
    We no longer have a single status for patient sessions and this is no
    longer be used so it can be safely removed.

--- app/models/patient_session.rb
+++ app/models/patient_session.rb
@@ -26,8 +26,6 @@ class PatientSession < ApplicationRecord
   audited associated_with: :patient
   has_associated_audits
 
-  include PatientSessionStatusConcern
-
   belongs_to :patient
   belongs_to :session
 
