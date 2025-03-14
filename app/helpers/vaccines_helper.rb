6de3988946aeca6cb9b433803b9de850d38a4a5a
Thomas Leese <thomas@leese.io>
2025-03-14 13:56:25 +0100

    Remove VaccinationsHelper
    And move these methods in to the `VaccinesHelper`.

--- app/helpers/vaccines_helper.rb
+++ app/helpers/vaccines_helper.rb
@@ -4,4 +4,16 @@ module VaccinesHelper
   def vaccine_heading(vaccine)
     "#{vaccine.brand} (#{vaccine.programme.name})"
   end
+
+  def vaccine_delivery_methods(vaccine)
+    vaccine.available_delivery_methods.map do |m|
+      [m, VaccinationRecord.human_enum_name("delivery_methods", m)]
+    end
+  end
+
+  def vaccine_delivery_sites(vaccine)
+    vaccine.available_delivery_sites.map do |s|
+      [s, VaccinationRecord.human_enum_name("delivery_sites", s)]
+    end
+  end
 end
