ce6278e931003e4847f4e0c47e8d2a8c47f169ef
Thomas Leese <thomas@leese.io>
2025-03-14 11:04:05 +0100

    Add AppPatientSessionTriageComponent
    This adds a component that renders the triage section of the patient
    session page, containing the consent card, gillick assessments and
    health answers.

--- spec/features/triage_spec.rb
+++ spec/features/triage_spec.rb
@@ -105,7 +105,7 @@ describe "Triage" do
   end
 
   def when_i_record_that_they_are_safe_to_vaccinate
-    click_link "Update triage"
+    click_link "Update triage", match: :first
     choose "Yes, it’s safe to vaccinate"
     click_button "Save triage"
   end
