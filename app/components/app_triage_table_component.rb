8ceae40c8899ff332cafe222f0f6febdbbcccf11
Thomas Leese <thomas@leese.io>
2025-03-14 09:26:31 +0100

    Add AppTriageTableComponent
    This component renders a list of triage notes and replaces the existing
    `AppTriageNotesComponent` to match the latest designs in the prototype.

---
+++ app/components/app_triage_table_component.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class AppTriageTableComponent < ViewComponent::Base
+  def initialize(patient_session:, programme:)
+    super
+
+    @patient_session = patient_session
+    @programme = programme
+  end
+
+  def render?
+    triages.any?
+  end
+
+  private
+
+  attr_reader :patient_session, :programme
+
+  delegate :patient, :session, to: :patient_session
+
+  def triages
+    @triages ||=
+      patient
+        .triages
+        .includes(:performed_by)
+        .where(programme:)
+        .order(created_at: :desc)
+  end
+end
