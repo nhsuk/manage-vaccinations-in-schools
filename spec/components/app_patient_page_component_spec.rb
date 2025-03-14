00424416b0d60ea34af2624c114e1bc6b4b41eb8
Thomas Leese <thomas@leese.io>
2025-03-14 11:04:05 +0100

    Add AppPatientSessionTriageComponent
    This adds a component that renders the triage section of the patient
    session page, containing the consent card, gillick assessments and
    health answers.

--- spec/components/app_patient_page_component_spec.rb
+++ spec/components/app_patient_page_component_spec.rb
@@ -18,35 +18,7 @@ describe AppPatientPageComponent do
   let(:vaccine) { programme.vaccines.first }
 
   let(:component) do
-    described_class.new(
-      patient_session:,
-      programme: programmes.first,
-      triage: nil
-    )
-  end
-
-  context "session in progress, patient in triage" do
-    let(:patient_session) do
-      create(
-        :patient_session,
-        :consent_given_triage_needed,
-        :session_in_progress,
-        programmes:
-      )
-    end
-
-    it { should have_css(".nhsuk-card__heading", text: "Child") }
-    it { should_not have_css(".nhsuk-card__heading", text: "Triage notes") }
-
-    it { should have_content("Is it safe to vaccinate") }
-
-    it { should_not have_content("ready for their HPV vaccination?") }
-
-    context "user is not allowed to triage or vaccinate" do
-      before { stub_authorization(allowed: false) }
-
-      it { should_not have_content("Is it safe to vaccinate") }
-    end
+    described_class.new(patient_session:, programme: programmes.first)
   end
 
   context "session in progress, patient ready to vaccinate" do
@@ -61,9 +33,6 @@ describe AppPatientPageComponent do
     end
 
     it { should have_css(".nhsuk-card__heading", text: "Child") }
-    it { should have_css(".nhsuk-card__heading", text: "Triage notes") }
-
-    it { should_not have_content("Is it safe to vaccinate") }
 
     it { should have_content("ready for their HPV vaccination?") }
 
