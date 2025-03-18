4fa7e2131202f2a08077e0bbcca75ba59b502f9e
Moritz Bogs <moritz.bogs@tngtech.com>
2025-03-18 10:12:47 +0000

    Skip check for running deployments if no application exists

---
+++ terraform/scripts/check-for-running-deployments.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <environment>"
+    exit 1
+fi
+
+environment=$1
+
+terraform init -backend-config="env/${environment}-backend.hcl" -upgrade
+APPLICATION_NAME=$(terraform output -json codedeploy_application_name 2>/dev/null | tr -d '"')
+echo "Application Name: $APPLICATION_NAME"
+APPLICATION_GROUP=$(terraform output -json codedeploy_deployment_group_name 2>/dev/null | tr -d '"')
+echo "Deployment Group Name: $APPLICATION_GROUP"
+if [ -z "$APPLICATION_NAME" ] || [ -z "$APPLICATION_GROUP" ]; then
+    echo "No CodeDeploy application found in the current terraform state. Skipping check for running deployment."
+    exit 0
+fi
+
+running_deployment=$(aws deploy list-deployments --application-name $APPLICATION_NAME \
+    --deployment-group-name $APPLICATION_GROUP --include-only-statuses InProgress \
+    --query 'deployments[0]' --output text)
+if [ "$running_deployment" != "None" ]; then
+  echo "A mavis deployment for ${environment} is currently running: $running_deployment"
+  echo "Aborting infrastructure deployment"
+  exit 1
+fi
