f4c8f352167b9875bb4773ee672be287ff57ca2b
Thomas Leese <thomas@leese.io>
2025-03-20 20:01:03 +0000

    Refactor AppProgrammeSessionTableComponent
    This refactors the component to fetch all the patients sessions for all
    the sessions at once to avoid N+1 issues.

--- app/components/app_programme_session_table_component.rb
+++ app/components/app_programme_session_table_component.rb
@@ -6,27 +6,35 @@ class AppProgrammeSessionTableComponent < ViewComponent::Base
 
     @sessions = sessions
     @programme = programme
+
+    @patient_sessions =
+      PatientSession
+        .where(session: sessions)
+        .eager_load(:patient)
+        .preload(session: :programmes)
+        .group_by(&:session_id)
+
     @outcomes =
       Outcomes.new(patient_sessions: PatientSession.where(session: sessions))
   end
 
   private
 
-  attr_reader :sessions, :programme, :outcomes
+  attr_reader :sessions, :programme, :patient_sessions, :outcomes
 
   def cohort_count(session:)
-    session.patient_sessions.length.to_s
+    patient_sessions.fetch(session.id, []).length
   end
 
   def number_stat(session:)
-    session.patient_sessions.select { yield it }.length.to_s
+    patient_sessions.fetch(session.id, []).select { yield it }.length.to_s
   end
 
   def percentage_stat(session:)
-    total_count = session.patient_sessions.length
+    total_count = patient_sessions.fetch(session.id, []).length
     return nil if total_count.zero?
 
-    count = session.patient_sessions.select { yield it }.length
+    count = patient_sessions.fetch(session.id, []).select { yield it }.length
 
     number_to_percentage(count / total_count.to_f * 100.0, precision: 0)
   end
