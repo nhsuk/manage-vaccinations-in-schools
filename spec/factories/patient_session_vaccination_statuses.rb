174f2745e97a841b70eaad350f911ae9d8811d88
Thomas Leese <thomas@leese.io>
2025-03-25 08:13:04 +0100

    Create PatientSession::VaccinationStatus
    This creates a new model that will represent the vaccination status
    of a patient/programme pair so the value can be queried in the
    database directly rather than needing to generate the status on the
    fly each time.

---
+++ spec/factories/patient_session_vaccination_statuses.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: patient_session_vaccination_statuses
+#
+#  id                 :bigint           not null, primary key
+#  status             :integer          default("none_yet"), not null
+#  patient_session_id :bigint           not null
+#  programme_id       :bigint           not null
+#
+# Indexes
+#
+#  idx_on_patient_session_id_programme_id_520c97058b     (patient_session_id,programme_id) UNIQUE
+#  index_patient_session_vaccination_statuses_on_status  (status)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (patient_session_id => patient_sessions.id) ON DELETE => cascade
+#  fk_rails_...  (programme_id => programmes.id)
+#
+FactoryBot.define do
+  factory :patient_session_vaccination_status,
+          class: "PatientSession::VaccinationStatus" do
+    patient_session
+    programme
+
+    traits_for_enum :status
+  end
+end
