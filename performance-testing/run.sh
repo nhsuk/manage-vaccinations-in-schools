4e825f0cf7d66a4cad7fe56dff2592224b0519ae
Misha Gorodnitzky <misaka@dmfg.io>
2025-04-02 17:30:56 +0100

    Add perf test run.sh

--- performance-testing/run.sh
+++ performance-testing/run.sh
@@ -20,12 +20,24 @@ while [ "$#" -gt 0 ]; do
       echo "  -r, --ramp-up  Ramp-up time in seconds"
       exit 0
       ;;
+    -A|--auth-token)
+      JMETER_PARAMS+=("-JAuthToken=$2")
+      shift 2
+      ;;
     -d|--duration)
       JMETER_PARAMS+=("-JDuration=$2")
       shift 2
       ;;
     -r|--ramp-up)
       JMETER_PARAMS+=("-JRampUp=$2")
+      shift 2
+      ;;
+    -T|--threads)
+      JMETER_PARAMS+=("-JThreads=$2")
+      shift 2
+      ;;
+    -V|--vaccinations)
+      JMETER_PARAMS+=("-JVaccinationLoop=$2")
       shift 2
       ;;
     *)
@@ -40,7 +52,12 @@ if [ -z "$SESSION_SLUG" ]; then
   exit 1
 fi
 
-# bin/jmeter -n -t ../Mavis_NURSE.jmx -l ../mavis-perf-test-2025-04-02-write.jtl -JSessionSlug=GrjmypgJXN -Jsample_varables=PatientInfo_matchNr,PatientId,Authenticity_Token,RandomNumber -JDuration=300 -JRampUp=10
-date=$(date '+%Y%m%d%H%M%S')
+# Warn the user if AuthToken is not set
+if [[ ! " ${JMETER_PARAMS[@]} " =~ " -JAuthToken=" ]]; then
+  echo "!WARNING! AuthToken is not set. Tests will fail if the env is behind basic auth."
+fi
 
-echo bin/jmeter -n -t ../Mavis_NURSE.jmx -l ../mavis-perf-test-${date}.jtl -JSessionSlug=${SESSION_SLUG} "${JMETER_PARAMS[@]}"
+# bin/jmeter -n -t ../Mavis_NURSE.jmx -l ../mavis-perf-test-2025-04-02-write.jtl -JSessionSlug=GrjmypgJXN -Jsample_varables=PatientInfo_matchNr,PatientId,Authenticity_Token,RandomNumber -JDuration=300 -JRampUp=10
+DATE=$(date '+%Y%m%d%H%M%S')
+
+echo bin/jmeter -n -t ../Mavis_NURSE.jmx -l ../mavis-perf-test-${DATE}.jtl -JSessionSlug=${SESSION_SLUG} "${JMETER_PARAMS[@]}"
