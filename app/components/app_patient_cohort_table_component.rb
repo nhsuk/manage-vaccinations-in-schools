10d6c42fd9902e60098c8c3a6f6724210b92d9fd
Thomas Leese <thomas@leese.io>
2025-04-09 07:19:00 +0100

    Update "Remove from cohort" functionality
    This updates the functionality where patients can be removed from
    cohorts to support the new model where patients don't have a single
    explicit organisation, instead it comes from the sessions that the
    patient belongs to.

--- app/components/app_patient_cohort_table_component.rb
+++ app/components/app_patient_cohort_table_component.rb
@@ -1,15 +1,23 @@
 # frozen_string_literal: true
 
 class AppPatientCohortTableComponent < ViewComponent::Base
-  def initialize(patient)
+  def initialize(patient, current_user:)
     super
 
     @patient = patient
+    @current_user = current_user
   end
 
   private
 
-  attr_reader :patient
+  attr_reader :patient, :current_user
 
-  delegate :organisation, :year_group, to: :patient
+  delegate :year_group, to: :patient
+
+  def organisation
+    @organisation ||=
+      if current_user.selected_organisation.patients.include?(patient)
+        current_user.selected_organisation
+      end
+  end
 end
