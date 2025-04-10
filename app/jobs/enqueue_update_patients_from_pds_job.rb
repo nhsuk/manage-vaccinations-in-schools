c578b17bc161b3e04b8705796b0471aa262fbe9f
Thomas Leese <thomas@leese.io>
2025-04-10 16:53:00 +0100

    Add UpdatePatientsFromPDS
    This adds a service class that handles logic related to queuing PDS
    update jobs to avoid code duplication. Currently we run similar code in
    two places, during an import and during the bulk enqueue job.
    
    Instead, we can move this code in to a single class which handles all
    the logic related to scheduling the correct jobs to reduce duplication
    and improve maintainability.
    
    I also think this will make performance testing easier by being able to
    use this class in a Rails console.

--- app/jobs/enqueue_update_patients_from_pds_job.rb
+++ app/jobs/enqueue_update_patients_from_pds_job.rb
@@ -8,28 +8,14 @@ class EnqueueUpdatePatientsFromPDSJob < ApplicationJob
   good_job_control_concurrency_with perform_limit: 1
 
   def perform
-    return unless Settings.pds.enqueue_bulk_updates
+    scope = Patient.with_nhs_number.not_invalidated.not_deceased
 
-    patients = Patient.with_nhs_number.not_invalidated.not_deceased
-    wait_between_jobs = Settings.pds.wait_between_jobs.to_f
-
-    GoodJob::Bulk.enqueue do
-      patients
+    patients =
+      scope
         .where(updated_from_pds_at: nil)
-        .or(patients.where("updated_from_pds_at < ?", 12.hours.ago))
+        .or(scope.where("updated_from_pds_at < ?", 12.hours.ago))
         .order("updated_from_pds_at ASC NULLS FIRST")
-        .find_each
-        .with_index do |patient, index|
-          # Schedule with a delay to preemptively handle rate limit issues.
-          # This shouldn't be necessary, but we're finding that Good Job
-          # has occasional race condition issues, and spreading out the jobs
-          # should reduce the risk of this.
 
-          PatientUpdateFromPDSJob.set(
-            priority: 50,
-            wait: index * wait_between_jobs
-          ).perform_later(patient)
-        end
-    end
+    UpdatePatientsFromPDS.call(patients, priority: 50, queue: :pds)
   end
 end
