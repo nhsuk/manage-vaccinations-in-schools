7b84726e8e2bd06ad6877c7cf79bf43697ea1000
Thomas Leese <thomas@leese.io>
2025-04-11 10:12:07 +0100

    Add FeatureFlagFactory
    This adds a factory that is responsible for ensuring the feature flags
    in the database match the features that we support in the service,
    ensuring we don't need to manually add them when trying to enable
    features in the UI.

---
+++ app/lib/feature_flag_factory.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class FeatureFlagFactory
+  def self.call
+    names =
+      YAML.safe_load(File.read(Rails.root.join("config/feature_flags.yml")))
+
+    names.each { Flipper.add(it) unless Flipper.exist?(it) }
+
+    Flipper.features.each do |feature|
+      feature.remove unless feature.name.to_s.in?(names)
+    end
+  end
+end
