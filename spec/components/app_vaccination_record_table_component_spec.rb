68d19499630d1abdd1d1a67be96b1db3dd24849f
Thomas Leese <thomas@leese.io>
2025-04-16 06:54:05 +0100

    Don't link to vaccination records
    If the user doesn't have permission to see those vaccination records.
    
    If a user clicks on this link they are taken to a 404 error page which
    is not a good user experience, we should instead hide the link.
    
    This follows on from similar functionality that was added to the patient
    table in a055b1a5646e0c5a309dc7430b5e0da9060f4fc5.

--- spec/components/app_vaccination_record_table_component_spec.rb
+++ spec/components/app_vaccination_record_table_component_spec.rb
@@ -3,7 +3,9 @@
 describe AppVaccinationRecordTableComponent do
   subject(:rendered) { render_inline(component) }
 
-  let(:component) { described_class.new(vaccination_records, count:) }
+  let(:component) do
+    described_class.new(vaccination_records, current_user:, count:)
+  end
 
   let(:programme) { create(:programme) }
   let(:vaccination_records) do
@@ -28,6 +30,8 @@ describe AppVaccinationRecordTableComponent do
       create_list(:vaccination_record, 5, :not_administered, programme:)
   end
 
+  let(:current_user) { create(:nurse) }
+
   let(:count) { 10 }
 
   it "renders a heading tab" do
@@ -35,19 +39,6 @@ describe AppVaccinationRecordTableComponent do
       ".nhsuk-table__heading-tab",
       text: "10 vaccination records"
     )
-  end
-
-  context "with new records" do
-    let(:component) do
-      described_class.new(vaccination_records, count:, new_records: true)
-    end
-
-    it "renders a heading tab with the word new" do
-      expect(rendered).to have_css(
-        ".nhsuk-table__heading-tab",
-        text: "10 new vaccination records"
-      )
-    end
   end
 
   it "renders the headers" do
@@ -73,5 +64,13 @@ describe AppVaccinationRecordTableComponent do
     )
     expect(rendered).to have_css(".nhsuk-table__cell", text: "28 May 2000")
     expect(rendered).to have_css(".nhsuk-table__cell", text: "1 September 2020")
+  end
+
+  context "with a vaccination record not performed by the organisation" do
+    before { vaccination_records.first.patient.update!(organisation: nil) }
+
+    it "doesn't render a link" do
+      expect(rendered).not_to have_link("SMITH, John")
+    end
   end
 end
