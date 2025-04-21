a1b142a70b1c78e4f85f4626f0a056b0a173b569
Thomas Leese <thomas@leese.io>
2025-04-21 20:32:15 +0100

    Add a test for uploading twins
    This adds a feature test to cover the scenario where twins are uploaded
    with the same last name, address and date of birth one after the other,
    ensuring that the separate NHS numbers are shown.

---
+++ spec/features/import_child_records_with_twins_spec.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+describe "Child record imports twins" do
+  around { |example| travel_to(Date.new(2024, 12, 1)) { example.run } }
+
+  before { Flipper.enable(:pds_lookup_during_import) }
+  after { Flipper.disable(:pds_lookup_during_import) }
+
+  scenario "User reviews and selects between duplicate records" do
+    stub_pds_get_nhs_number_to_return_a_patient
+    stub_pds_search_to_return_a_patient
+
+    given_i_am_signed_in
+    and_an_hpv_programme_is_underway
+    and_an_existing_patient_record_exists
+
+    when_i_visit_the_import_page
+    and_i_start_adding_children_to_the_cohort
+    and_i_upload_a_file_with_a_twin
+    then_i_should_see_the_import_page_with_successful_import
+    and_the_twins_should_exist
+  end
+
+  def given_i_am_signed_in
+    @programme = create(:programme, :hpv)
+    @team =
+      create(
+        :team,
+        :with_one_nurse,
+        :with_generic_clinic,
+        programmes: [@programme]
+      )
+
+    TeamSessionsFactory.call(@team, academic_year: AcademicYear.current)
+
+    sign_in @team.users.first
+  end
+
+  def and_an_hpv_programme_is_underway
+    @school = create(:school, urn: "123456", team: @team)
+    @session =
+      create(:session, team: @team, location: @school, programmes: [@programme])
+  end
+
+  def and_an_existing_patient_record_exists
+    @existing_patient =
+      create(
+        :patient,
+        given_name: "John",
+        family_name: "Doe",
+        nhs_number: "9000000009",
+        date_of_birth: Date.new(2010, 1, 3),
+        gender_code: :male,
+        address_line_1: "11 Downing Street",
+        address_line_2: "",
+        address_town: "London",
+        address_postcode: "SW1A 1AA",
+        school: @school,
+        team: @team,
+        session: @session
+      )
+  end
+
+  def when_i_visit_the_import_page
+    visit "/"
+    click_link "Import", match: :first
+  end
+
+  def and_i_start_adding_children_to_the_cohort
+    click_button "Import records"
+    choose "Child records"
+    click_button "Continue"
+  end
+
+  def and_i_upload_a_file_with_a_twin
+    attach_file("cohort_import[csv]", "spec/fixtures/cohort_import/valid.csv")
+    click_on "Continue"
+  end
+
+  def then_i_should_see_the_import_page_with_successful_import
+    expect(page).to have_content("1 previously imported record was omitted")
+    expect(Patient.count).to eq(4)
+  end
+
+  def and_the_twins_should_exist
+    patient = Patient.find_by(nhs_number: "9449306168")
+
+    expect(patient).not_to eq(@existing_patient)
+
+    expect(patient.address_postcode).to eq("SW1A 1AA")
+    expect(patient.date_of_birth).to eq(Date.new(2010, 1, 3))
+    expect(patient.family_name).to eq("Doe")
+    expect(patient.given_name).to eq("Mark")
+    expect(patient.pending_changes).to eq({})
+  end
+end
