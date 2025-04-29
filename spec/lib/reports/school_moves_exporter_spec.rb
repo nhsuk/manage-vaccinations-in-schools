f9c304b3ff46c499557ec2b83013a125bdd5aa40
Thomas Leese <thomas@leese.io>
2025-04-29 15:49:42 +0100

    Move database querying to SchoolMovesExporter
    This refactors the code to ensure that the database querying sits closer
    to where the exports are generated to ensure consistency with the other
    exporter classes.

--- spec/lib/reports/school_moves_exporter_spec.rb
+++ spec/lib/reports/school_moves_exporter_spec.rb
@@ -1,18 +1,53 @@
 # frozen_string_literal: true
 
 describe Reports::SchoolMovesExporter do
-  subject(:csv) { described_class.call(SchoolMoveLogEntry.all) }
+  subject(:exporter) do
+    described_class.new(organisation:, start_date:, end_date:)
+  end
 
-  let(:rows) { CSV.parse(csv, headers: true) }
+  let(:organisation) { create(:organisation) }
+  let(:start_date) { nil }
+  let(:end_date) { nil }
 
-  describe "#call" do
+  describe "#row_count" do
+    subject { exporter.row_count }
+
+    let(:one_day_ago) { 1.day.ago }
+    let(:three_days_ago) { 3.days.ago }
+
+    before do
+      3.times do
+        patient = create(:patient, organisation:)
+        create(:school_move_log_entry, patient:, created_at: one_day_ago)
+      end
+
+      2.times do
+        patient = create(:patient, organisation:)
+        create(:school_move_log_entry, patient:, created_at: three_days_ago)
+      end
+    end
+
+    it { should eq(5) }
+
+    context "when date range is specified" do
+      let(:start_date) { one_day_ago }
+
+      it { should eq(3) }
+    end
+  end
+
+  describe "#csv_data" do
+    subject(:csv_data) { exporter.csv_data }
+
+    let(:rows) { CSV.parse(csv_data, headers: true) }
+
     context "with a standard school move" do
       let(:old_school) { create(:school, :secondary) }
       let(:new_school) { create(:school, :secondary) }
 
       before do
-        patient = create(:patient, school: old_school)
-        create(:school_move_log_entry, patient: patient, school: new_school)
+        patient = create(:patient, school: old_school, organisation:)
+        create(:school_move_log_entry, patient:, school: new_school)
       end
 
       it "returns a CSV with the school moves data" do
@@ -45,8 +80,8 @@ describe Reports::SchoolMovesExporter do
 
     context "when moving to home education" do
       before do
-        patient = create(:patient, :home_educated)
-        create(:school_move_log_entry, :home_educated, patient: patient)
+        patient = create(:patient, :home_educated, organisation:)
+        create(:school_move_log_entry, :home_educated, patient:)
       end
 
       it "returns '999999' as the URN" do
@@ -56,8 +91,8 @@ describe Reports::SchoolMovesExporter do
 
     context "when moving to an unknown school" do
       before do
-        patient = create(:patient, school: nil)
-        create(:school_move_log_entry, :unknown_school, patient: patient)
+        patient = create(:patient, school: nil, organisation:)
+        create(:school_move_log_entry, :unknown_school, patient:)
       end
 
       it "returns '888888' as the URN" do
