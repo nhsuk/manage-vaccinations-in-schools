6a34216fb1ef6747ec52dc0a46aa326f4fe738e1
Thomas Leese <thomas@leese.io>
2025-05-01 16:52:55 +0100

    Update patients in bulk

--- app/models/school_move.rb
+++ app/models/school_move.rb
@@ -56,10 +56,6 @@ class SchoolMove < ApplicationRecord
     destroy! if persisted?
   end
 
-  def update_patient!
-    patient.update!(home_educated:, school:)
-  end
-
   def update_sessions!
     patient.patient_sessions.destroy_all_if_safe
 
