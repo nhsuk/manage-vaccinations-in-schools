nhsuk/manage-vaccinations-in-schools

commit 2073e01c1d6db2277cf677c5d4f9d00262c34469

Choose vaccine when choosing a batch
When selecting the batch for a vaccination record we now show all
batches across all active vaccine types, allowing the user to choose
batches from all active vaccines on the programme. For HPV and Td/IPV
this has no effect on functionality as there is only one active vaccine,
but for MenACWY there are three active vaccines.

To support this, the ability to change a vaccine on its own has been
removed, since the vaccine is chosen as part of changing the batch and
this functionality isn't in the prototype.

app/models/draft_vaccination_record.rb
@@ -25,7 +25,6 @@ class DraftVaccinationRecord
   attribute :performed_by_user_id, :integer
   attribute :performed_ods_code, :string
   attribute :programme_id, :integer
-  attribute :vaccine_id, :integer
 
   validates :performed_by_family_name,
             :performed_by_given_name,
@@ -39,7 +38,6 @@ class DraftVaccinationRecord
       :date_and_time,
       (:outcome if can_change_outcome?),
       (:delivery if administered?),
-      (:vaccine if administered?),
       (:batch if administered?),
       (:location if location&.generic_clinic?),
       :confirm
@@ -69,13 +67,8 @@ class DraftVaccinationRecord
               }
   end
 
-  on_wizard_step :vaccine, exact: true do
-    validates :vaccine_id, presence: true
-  end
-
   on_wizard_step :batch, exact: true do
     validates :batch_id, presence: true
-    validate :batch_vaccine_matches_vaccine
   end
 
   on_wizard_step :location, exact: true do
@@ -94,7 +87,6 @@ class DraftVaccinationRecord
               :delivery_method,
               :delivery_site,
               :performed_at,
-              :vaccine_id,
               presence: true
   end
 
@@ -176,40 +168,21 @@ class DraftVaccinationRecord
     self.editing_id = value.id
   end
 
-  def vaccine
-    VaccinePolicy::Scope
-      .new(@current_user, Vaccine)
-      .resolve
-      .find_by(id: vaccine_id)
-  end
+  delegate :vaccine, to: :batch, allow_nil: true
 
-  def vaccine=(value)
-    self.vaccine_id = value.id
-  end
+  def vaccine_id_changed? = batch_id_changed?
 
   private
 
   def reset_unused_fields
-    if administered?
-      if vaccine_id.nil? && programme &&
-           (vaccines = programme.vaccines.active).count == 1
-        self.vaccine_id = vaccines.first.id
-      end
-    else
+    unless administered?
       self.batch_id = nil
       self.delivery_method = nil
       self.delivery_site = nil
-      self.vaccine_id = nil
     end
   end
 
   def can_change_outcome?
     outcome != "already_had" || editing? || session.nil? || session.today?
-  end
-
-  def batch_vaccine_matches_vaccine
-    return if batch&.vaccine_id == vaccine_id
-
-    errors.add(:batch_id, :incorrect_vaccine, vaccine_brand: vaccine&.brand)
   end
 end

spec/models/draft_vaccination_record_spec.rb
@@ -16,7 +16,7 @@ describe DraftVaccinationRecord do
   let(:session) { create(:session, programmes: [programme]) }
   let(:patient) { create(:patient, session:) }
   let(:vaccine) { programme.vaccines.first }
-  let(:batch) { create(:batch, vaccine:) }
+  let(:batch) { create(:batch, organisation:, vaccine:) }
 
   let(:valid_administered_attributes) do
     {
@@ -29,8 +29,7 @@ describe DraftVaccinationRecord do
       outcome: "administered",
       patient_id: patient.id,
       programme_id: programme.id,
-      session_id: session.id,
-      vaccine_id: vaccine.id
+      session_id: session.id
     }
   end
 
@@ -47,24 +46,6 @@ describe DraftVaccinationRecord do
   let(:invalid_attributes) { {} }
 
   describe "validations" do
-    context "vaccine and batch doesn't match" do
-      let(:different_vaccine) { create(:vaccine, programme:) }
-      let(:different_batch) { create(:batch, vaccine: different_vaccine) }
-
-      let(:attributes) do
-        valid_administered_attributes.merge(batch_id: different_batch.id)
-      end
-
-      before { draft_vaccination_record.wizard_step = :batch }
-
-      it "has an error" do
-        expect(draft_vaccination_record.save(context: :update)).to be(false)
-        expect(draft_vaccination_record.errors[:batch_id]).to include(
-          /Choose a batch/
-        )
-      end
-    end
-
     context "when performed_at is in the future" do
       let(:attributes) do
         valid_administered_attributes.merge(performed_at: 1.second.from_now)
@@ -86,16 +67,6 @@ describe DraftVaccinationRecord do
   describe "#reset_unused_fields" do
     subject(:save!) { draft_vaccination_record.save! }
 
-    context "when administered" do
-      let(:attributes) { valid_administered_attributes.merge(vaccine_id: nil) }
-
-      it "sets the vaccine" do
-        expect { save! }.to change(draft_vaccination_record, :vaccine_id).to(
-          vaccine.id
-        )
-      end
-    end
-
     context "when not administered" do
       let(:attributes) do
         valid_not_administered_attributes.merge(
@@ -116,12 +87,6 @@ describe DraftVaccinationRecord do
 
       it "clears the deliver site" do
         expect { save! }.to change(draft_vaccination_record, :delivery_site).to(
-          nil
-        )
-      end
-
-      it "clears the vaccine" do
-        expect { save! }.to change(draft_vaccination_record, :vaccine_id).to(
           nil
         )
       end

app/components/app_vaccinate_form_component.html.erb
@@ -64,7 +64,7 @@
 
     <%= f.hidden_field :delivery_method, value: delivery_method %>
     <%= f.hidden_field :dose_sequence, value: dose_sequence %>
-    <%= f.hidden_field :vaccine_id, value: vaccine.id %>
+    <%= f.hidden_field :programme_id, value: programme.id %>
 
     <%= f.govuk_submit "Continue" %>
   </div>

app/components/app_vaccinate_form_component.rb
@@ -26,15 +26,6 @@ class AppVaccinateFormComponent < ViewComponent::Base
     session_patient_programme_vaccinations_path(session, patient, programme)
   end
 
-  # TODO: this code will need to be revisited in future as it only really
-  # works for HPV, where we only have one vaccine. It is likely to fail for
-  # the flu programme for the SAIS organisations that offer both nasal and
-  # injectable vaccines.
-
-  def vaccine
-    programme.vaccines.active.first
-  end
-
   def delivery_method
     :intramuscular
   end

app/controllers/draft_vaccination_records_controller.rb
@@ -146,8 +146,7 @@ class DraftVaccinationRecordsController < ApplicationController
       delivery: %i[delivery_site delivery_method],
       location: %i[location_name],
       notes: %i[notes],
-      outcome: %i[outcome],
-      vaccine: %i[vaccine_id]
+      outcome: %i[outcome]
     }.fetch(current_step)
 
     params
@@ -190,13 +189,22 @@ class DraftVaccinationRecordsController < ApplicationController
   def set_batches
     scope =
       policy_scope(Batch).includes(:vaccine).where(
-        vaccine: @draft_vaccination_record.vaccine
+        vaccine: {
+          programme: @programme
+        }
       )
+
+    method =
+      if @draft_vaccination_record.delivery_method == "nasal_spray"
+        "nasal_spray"
+      else
+        "injection"
+      end
 
     @batches =
       scope
         .where(id: @draft_vaccination_record.batch_id)
-        .or(scope.not_archived.not_expired)
+        .or(scope.not_archived.not_expired.where(vaccine: { method: }))
         .order_by_name_and_expiration
   end
 

app/forms/vaccinate_form.rb
@@ -18,7 +18,7 @@ class VaccinateForm
   attribute :delivery_method, :string
   attribute :delivery_site, :string
   attribute :dose_sequence, :integer
-  attribute :vaccine_id, :integer
+  attribute :programme_id, :integer
 
   validates :administered, inclusion: [true, false]
 
@@ -39,7 +39,6 @@ class VaccinateForm
   with_options if: :administered do
     validates :delivery_method, presence: true
     validates :delivery_site, presence: true
-    validates :vaccine_id, presence: true
   end
 
   def save(draft_vaccination_record:)
@@ -66,7 +65,6 @@ class VaccinateForm
     draft_vaccination_record.performed_ods_code = organisation.ods_code
     draft_vaccination_record.programme = programme
     draft_vaccination_record.session_id = patient_session.session_id
-    draft_vaccination_record.vaccine_id = vaccine_id
 
     draft_vaccination_record.save # rubocop:disable Rails/SaveBang
   end

app/views/draft_vaccination_records/confirm.html.erb
@@ -22,7 +22,6 @@
      notes: wizard_path("notes"),
      outcome: @draft_vaccination_record.wizard_steps.include?(:outcome) ? wizard_path("outcome") : nil,
      performed_at: wizard_path("date-and-time"),
-     vaccine: wizard_path("vaccine"),
    } %>
 
 <% show_notes = @draft_vaccination_record.editing? %>

app/views/draft_vaccination_records/vaccine.html.erb
@@ -1,20 +1,0 @@
-<% content_for :before_main do %>
-  <%= render AppBacklinkComponent.new(@back_link_path) %>
-<% end %>
-
-<% page_title = "Which vaccine did you use for the #{@programme.name} vaccination?" %>
-<% content_for :page_title, page_title %>
-
-<%= form_with model: @draft_vaccination_record, url: wizard_path, method: :put do |f| %>
-  <%= f.govuk_error_summary %>
-
-  <%= f.govuk_radio_buttons_fieldset :vaccine_id,
-                                     caption: { text: @patient.full_name, size: "l" },
-                                     legend: { size: "l", tag: "h1", text: page_title } do %>
-    <% @programme.vaccines.active.find_each do |vaccine| %>
-      <%= f.govuk_radio_button :vaccine_id, vaccine.id, label: { text: vaccine.brand } %>
-    <% end %>
-  <% end %>
-
-  <%= f.govuk_submit "Continue" %>
-<% end %>

commit 2a50d4d17a63e99b675e6aeae23c11af42212767

Ensure vaccine is set when saving a vaccination record
This fixes a regression that was introduced in
2073e01c1d6db2277cf677c5d4f9d00262c34469 where the vaccine isn't being
set on vaccination records when recorded in the service itself. I've
fixed the regression and then introduced a test to ensure this doesn't
break again in the future.

app/models/draft_vaccination_record.rb
@@ -170,9 +170,15 @@ class DraftVaccinationRecord
 
   delegate :vaccine, to: :batch, allow_nil: true
 
+  delegate :id, to: :vaccine, prefix: true, allow_nil: true
+
   def vaccine_id_changed? = batch_id_changed?
 
   private
+
+  def writable_attribute_names
+    super + %w[vaccine_id]
+  end
 
   def reset_unused_fields
     unless administered?

spec/models/draft_vaccination_record_spec.rb
@@ -64,6 +64,22 @@ describe DraftVaccinationRecord do
     end
   end
 
+  describe "#write_to!" do
+    subject(:write_to!) do
+      draft_vaccination_record.write_to!(vaccination_record)
+    end
+
+    let(:attributes) { valid_administered_attributes }
+
+    let(:vaccination_record) { VaccinationRecord.new }
+
+    it "sets the vaccine" do
+      expect { write_to! }.to change(vaccination_record, :vaccine).to(
+        batch.vaccine
+      )
+    end
+  end
+
   describe "#reset_unused_fields" do
     subject(:save!) { draft_vaccination_record.save! }
 