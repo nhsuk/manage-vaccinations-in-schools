4a550121a4a44ce2ba4270c6e69b04e89064b2be
Moritz Bogs <moritz.bogs@tngtech.com>
2025-05-05 09:46:45 +0100

    Separate backup configs for source account

--- terraform/backup/source/main.tf
+++ terraform/backup/source/main.tf
@@ -8,60 +8,40 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "nhse-mavisbackup-terraform-state"
-    key            = "terraform.tfstate"
+    bucket         = "nhse-mavis-terraform-state"
     region         = "eu-west-2"
-    dynamodb_table = "mavisbackup-state-lock"
-    encrypt = true
+    dynamodb_table = "mavis-terraform-state-lock"
+    encrypt        = true
   }
 }
 
-provider  "aws" {
+provider "aws" {
   region = "eu-west-2"
-}
-
-variable "destination_vault_arn" {
-  description = "ARN of the backup vault in the destination account"
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment name (AWS Account level). Allowed values are 'dev' and 'prod'."
-  type        = string
-    validation {
-        condition     = var.environment == "dev" || var.environment == "prod"
-        error_message = "Environment must be either 'dev' or 'prod'."
-    }
-}
-
-variable "terraform_role_arn" {
-  description = "ARN of Terraform role used to deploy to account"
-  type        = string
-}
-
-data "aws_arn" "destination_vault_arn" {
-  arn = var.destination_vault_arn
 }
 
 data "aws_caller_identity" "current" {}
 
+data "aws_arn" "destination_vault" {
+  arn = var.destination_vault_arn
+}
+
 locals {
-  # Adjust these as required
-  project_name = "mavis-${var.environment}"
-  source_account_id = data.aws_caller_identity.current.account_id
-  destination_account_id = data.aws_arn.destination_vault_arn.account
+  project_name           = "mavis-${var.environment}"
+  source_account_id      = data.aws_caller_identity.current.account_id
+  destination_account_id = data.aws_arn.destination_vault.account
+  access_logs_suffix     = var.environment == "production" ? "-production" : ""
+}
+
+data "aws_s3_bucket" "mavis_logs" {
+  bucket = "nhse-mavis-access-logs${local.access_logs_suffix}"
 }
 
 # First, we create an S3 bucket for compliance reports.
-data "aws_s3_bucket" "mavis_logs" {
-  bucket = "nhse-mavis-logs"
-}
-
 module "s3_reports_bucket" {
-  source = "../../modules/s3"
-  bucket_name = "${local.project_name}-backup-reports"
+  source                   = "../../modules/s3"
+  bucket_name              = "${local.project_name}-backup-reports"
   logging_target_bucket_id = data.aws_s3_bucket.mavis_logs.id
-  logging_target_prefix = "backup-reports/"
+  logging_target_prefix    = "backup-reports/"
 }
 
 resource "aws_s3_bucket_policy" "backup_reports_bucket_policy" {
@@ -71,12 +51,12 @@ resource "aws_s3_bucket_policy" "backup_reports_bucket_policy" {
     Id      = "backup-reports-bucket-policy"
     Statement = [
       {
-        Sid       = "HTTPSOnly"
-        Effect    = "Deny"
+        Sid    = "HTTPSOnly"
+        Effect = "Deny"
         Principal = {
-          "AWS": "*"
+          "AWS" : "*"
         }
-        Action    = "s3:*"
+        Action = "s3:*"
         Resource = [
           module.s3_reports_bucket.arn,
           "${module.s3_reports_bucket.arn}/*",
@@ -86,13 +66,13 @@ resource "aws_s3_bucket_policy" "backup_reports_bucket_policy" {
             "aws:SecureTransport" = "false"
           }
         }
-      }, {
-        Sid       = "AllowBackupReports"
+        }, {
+        Sid    = "AllowBackupReports"
         Effect = "Allow"
         Principal = {
-          "AWS": "arn:aws:iam::393416225559:role/aws-service-role/reports.backup.amazonaws.com/AWSServiceRoleForBackupReports"
+          "AWS" : "arn:aws:iam::${local.source_account_id}:role/aws-service-role/reports.backup.amazonaws.com/AWSServiceRoleForBackupReports"
         }
-        Action = "s3:PutObject"
+        Action   = "s3:PutObject"
         Resource = ["${module.s3_reports_bucket.arn}/*"]
         Condition = {
           StringEquals = {
@@ -103,15 +83,6 @@ resource "aws_s3_bucket_policy" "backup_reports_bucket_policy" {
     ]
   })
 }
-
-
-# We need a key for the SNS topic that will be used for notifications from AWS Backup. This key
-# will be used to encrypt the messages sent to the topic before they are sent to the subscribers,
-# but isn't needed by the recipients of the messages.
-
-# First we need some contextual data
-# data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
 
 # Now we can define the key itself
 resource "aws_kms_key" "backup_notifications" {
@@ -127,16 +98,16 @@ resource "aws_kms_key" "backup_notifications" {
         Principal = {
           AWS = "arn:aws:iam::${local.source_account_id}:root"
         }
-        Action = "kms:*"
+        Action   = "kms:*"
         Resource = "*"
       },
       {
-        Effect    = "Allow"
+        Effect = "Allow"
         Principal = {
           Service = "sns.amazonaws.com"
         }
-        Action    = ["kms:GenerateDataKey*", "kms:Decrypt"]
-        Resource  = "*"
+        Action   = ["kms:GenerateDataKey*", "kms:Decrypt"]
+        Resource = "*"
       },
     ]
   })
@@ -147,41 +118,41 @@ resource "aws_kms_key" "backup_notifications" {
 module "source" {
   source = "../modules/aws-backup-source"
 
-  backup_copy_vault_account_id       = local.destination_account_id
-  backup_copy_vault_arn              = data.aws_arn.destination_vault_arn.arn
-  environment_name                   = var.environment
-  bootstrap_kms_key_arn              = aws_kms_key.backup_notifications.arn
-  project_name                       = local.project_name
-  reports_bucket                     = module.s3_reports_bucket.bucket_id
-  terraform_role_arn                 = var.terraform_role_arn
+  backup_copy_vault_account_id = local.destination_account_id
+  backup_copy_vault_arn        = var.destination_vault_arn
+  environment_name             = var.environment
+  bootstrap_kms_key_arn        = aws_kms_key.backup_notifications.arn
+  project_name                 = local.project_name
+  reports_bucket               = module.s3_reports_bucket.bucket_id
+  terraform_role_arn           = var.vault_owner_role_arn
 
-  backup_plan_config                 = {
-    "compliance_resource_types": [
+  backup_plan_config = {
+    "compliance_resource_types" : [
       "Aurora"
     ],
-    "rules": [
+    "rules" : [
       {
-        "copy_action": {
-          "delete_after": 60
+        "copy_action" : {
+          "delete_after" : 60
         },
-        "lifecycle": {
-          "delete_after": 60
+        "lifecycle" : {
+          "delete_after" : 60
         },
-        "name": "daily_kept_for_2_months",
-        "schedule": "cron(0 0 * * ? *)"
+        "name" : "daily_kept_for_2_months",
+        "schedule" : "cron(0 0 * * ? *)"
       }
     ],
-    "selection_tag": "NHSE-Enable-Backup"
+    "selection_tag" : "NHSE-Enable-Backup"
   }
   # Note here that we need to explicitly disable DynamoDB backups in the source account.
   # The default config in the module enables backups for all resource types.
-  backup_plan_config_dynamodb =  {
-    "compliance_resource_types": [
+  backup_plan_config_dynamodb = {
+    "compliance_resource_types" : [
       "DynamoDB"
     ],
-    "rules": [
+    "rules" : [
     ],
-    "enable": false,
-    "selection_tag": "NHSE-Enable-Backup"
+    "enable" : false,
+    "selection_tag" : "NHSE-Enable-Backup"
   }
 }
