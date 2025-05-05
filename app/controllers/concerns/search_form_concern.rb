0ab693be49fb466729fa00fbb2416f793206ce6f
Thomas Leese <thomas@leese.io>
2025-05-05 23:02:40 +0100

    Add ability to filter on programmes
    This adds the functionality to allow users to filter patients by
    programme when viewing a list of patients on a session tab. By default
    all programmes that a patient is eligible for is shown, but this can be
    filtered using checkboxes.

--- app/controllers/concerns/search_form_concern.rb
+++ app/controllers/concerns/search_form_concern.rb
@@ -4,21 +4,25 @@ module SearchFormConcern
   extend ActiveSupport::Concern
 
   def set_search_form
-    @form =
-      SearchForm.new(
-        params.fetch(:search_form, {}).permit(
-          :consent_status,
-          :date_of_birth_day,
-          :date_of_birth_month,
-          :date_of_birth_year,
-          :missing_nhs_number,
-          :programme_status,
-          :q,
-          :register_status,
-          :session_status,
-          :triage_status,
-          year_groups: []
-        )
-      )
+    @form = SearchForm.new(session: @session, **search_form_params)
+  end
+
+  private
+
+  def search_form_params
+    params.fetch(:search_form, {}).permit(
+      :consent_status,
+      :date_of_birth_day,
+      :date_of_birth_month,
+      :date_of_birth_year,
+      :missing_nhs_number,
+      :programme_status,
+      :q,
+      :register_status,
+      :session_status,
+      :triage_status,
+      programme_types: [],
+      year_groups: []
+    )
   end
 end
--- app/forms/search_form.rb
+++ app/forms/search_form.rb
@@ -5,23 +5,39 @@ class SearchForm
   include ActiveModel::Attributes
   include ActiveRecord::AttributeAssignment
 
+  attr_accessor :session
+
   attribute :consent_status, :string
   attribute :date_of_birth_day, :integer
   attribute :date_of_birth_month, :integer
   attribute :date_of_birth_year, :integer
   attribute :missing_nhs_number, :boolean
   attribute :programme_status, :string
+  attribute :programme_types, array: true
   attribute :q, :string
   attribute :register_status, :string
   attribute :session_status, :string
   attribute :triage_status, :string
   attribute :year_groups, array: true
 
+  def programme_types=(values)
+    super(values&.compact_blank || [])
+  end
+
   def year_groups=(values)
     super(values&.compact_blank&.map(&:to_i)&.compact || [])
   end
 
-  def apply(scope, programme: nil)
+  def programmes
+    @programmes ||=
+      if programme_types.present?
+        Programme.where(type: programme_types)
+      else
+        session&.programmes
+      end
+  end
+
+  def apply(scope)
     scope = scope.search_by_name(q) if q.present?
 
     scope = scope.search_by_year_groups(year_groups) if year_groups.present?
@@ -40,16 +56,18 @@ class SearchForm
 
     scope = scope.search_by_nhs_number(nil) if missing_nhs_number.present?
 
+    scope = scope.in_programmes(programmes) if programmes.present?
+
     if (status = consent_status).present?
-      scope = scope.has_consent_status(status, programme:)
+      scope = scope.has_consent_status(status, programme: programmes)
     end
 
     if (status = programme_status&.to_sym).present?
-      scope = scope.has_vaccination_status(status, programme:)
+      scope = scope.has_vaccination_status(status, programme: programmes)
     end
 
     if (status = session_status&.to_sym).present?
-      scope = scope.has_session_status(status, programme:)
+      scope = scope.has_session_status(status, programme: programmes)
     end
 
     if (status = register_status&.to_sym).present?
@@ -57,7 +75,7 @@ class SearchForm
     end
 
     if (status = triage_status&.to_sym).present?
-      scope = scope.has_triage_status(status, programme:)
+      scope = scope.has_triage_status(status, programme: programmes)
     end
 
     scope.order_by_name
