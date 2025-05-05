45ab313c2e5add551c2b0e03fa35b97004fa7089
Moritz Bogs <moritz.bogs@tngtech.com>
2025-05-05 08:21:23 +0100

    Add destination bootstrap config

---
+++ terraform/backup/destination-bootstrap/main.tf
@@ -0,0 +1,47 @@
+module "terraform_state_bucket" {
+  source      = "../../modules/s3"
+  bucket_name = "mavisbackup-terraform-state"
+}
+
+resource "aws_s3_bucket_policy" "backend_bucket_block_http" {
+  bucket = module.terraform_state_bucket.bucket_id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "block-backend-bucket-http-access"
+    Statement = [
+      {
+        Sid    = "HTTPSOnly"
+        Effect = "Deny"
+        Principal = {
+          "AWS" : "*"
+        }
+        Action = "s3:*"
+        Resource = [
+          module.terraform_state_bucket.arn,
+          "${module.terraform_state_bucket.arn}/*",
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+    ]
+  })
+}
+
+#### Dynamo DB table for terraform state locking
+resource "aws_dynamodb_table" "dynamodb_lock_table" {
+  name         = "mavisbackup-terraform-state-lock"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+}
--- terraform/backup/destination/main.tf
+++ terraform/backup/destination/main.tf
@@ -8,21 +8,15 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "nhse-mavisbackup-destination-terraform-state"
-    key            = "terraform.tfstate"
+    bucket         = "mavisbackup-terraform-state"
     region         = "eu-west-2"
-    dynamodb_table = "mavisbackup-destination-state-lock"
+    dynamodb_table = "mavisbackup-terraform-state-lock"
     encrypt = true
   }
 }
 
 provider  "aws" {
   region = "eu-west-2"
-}
-
-variable "source_account_id" {
-  description = "The Account ID of the source account"
-  type        = string
 }
 
 data "aws_caller_identity" "current" {}
@@ -55,19 +49,9 @@ resource "aws_kms_key" "destination_backup_key" {
 
 module "destination" {
   source = "../modules/aws-backup-destination"
-  source_account_name     = "mavis" # please note that the assigned value would be the prefix in aws_backup_vault.vault.name
+  source_account_name     = "mavis-${var.source_account_environment}"
   account_id              = local.destination_account_id
   source_account_id       = var.source_account_id
   kms_key                 = aws_kms_key.destination_backup_key.arn
   enable_vault_protection = false
-}
-
-###
-# Destination vault ARN output
-###
-
-output "destination_vault_arn" {
-  # The ARN of the backup vault in the destination account is needed by
-  # the source account to copy backups into it.
-  value = module.destination.vault_arn
 }
