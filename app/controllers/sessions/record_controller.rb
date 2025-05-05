94dcb436213830c19f0046e853c9e6c1e9513c13
Thomas Leese <thomas@leese.io>
2025-05-05 23:02:40 +0100

    Add ability to filter on programmes
    This adds the functionality to allow users to filter patients by
    programme when viewing a list of patients on a session tab. By default
    all programmes that a patient is eligible for is shown, but this can be
    filtered using checkboxes.

--- app/controllers/sessions/record_controller.rb
+++ app/controllers/sessions/record_controller.rb
@@ -15,8 +15,6 @@ class Sessions::RecordController < ApplicationController
   before_action :set_batches, except: :show
 
   def show
-    @programmes = @session.programmes
-
     scope =
       @session
         .patient_sessions
@@ -24,14 +22,13 @@ class Sessions::RecordController < ApplicationController
           :latest_note,
           patient: %i[consent_statuses triage_statuses vaccination_statuses]
         )
-        .in_programmes(@session.programmes)
         .has_registration_status(%w[attending completed])
 
-    scope = @form.apply(scope, programme: @programmes)
+    scope = @form.apply(scope)
 
     patient_sessions =
       scope.select do |patient_session|
-        patient_session.programmes.any? do |programme|
+        @form.programmes.any? do |programme|
           patient_session.patient.consent_given_and_safe_to_vaccinate?(
             programme:
           )
--- app/forms/search_form.rb
+++ app/forms/search_form.rb
@@ -14,6 +14,7 @@ class SearchForm
   attribute :date_of_birth_year, :integer
   attribute :missing_nhs_number, :boolean
   attribute :programme_status, :string
+  attribute :programme_types, array: true
   attribute :q, :string
   attribute :register_status, :string
   attribute :session_status, :string
@@ -21,11 +22,15 @@ class SearchForm
   attribute :vaccine_method, :string
   attribute :year_groups, array: true
 
-  attr_accessor :session, :request_path
+  attr_accessor :request_path, :request_session, :session
 
   def initialize(params = {})
     super(params)
-    handle_session_filters
+    handle_request_session_filters
+  end
+
+  def programme_types=(values)
+    super(values&.compact_blank || [])
   end
 
   def consent_statuses=(values)
@@ -36,7 +41,16 @@ class SearchForm
     super(values&.compact_blank&.map(&:to_i)&.compact || [])
   end
 
-  def apply(scope, programme: nil)
+  def programmes
+    @programmes ||=
+      if programme_types.present?
+        Programme.where(type: programme_types)
+      else
+        session&.programmes
+      end
+  end
+
+  def apply(scope)
     scope = scope.search_by_name(q) if q.present?
 
     scope = scope.search_by_year_groups(year_groups) if year_groups.present?
@@ -55,16 +69,18 @@ class SearchForm
 
     scope = scope.search_by_nhs_number(nil) if missing_nhs_number.present?
 
+    scope = scope.in_programmes(programmes) if programmes.present?
+
     if (statuses = consent_statuses).present?
-      scope = scope.has_consent_status(statuses, programme:)
+      scope = scope.has_consent_status(statuses, programme: programmes)
     end
 
     if (status = programme_status&.to_sym).present?
-      scope = scope.has_vaccination_status(status, programme:)
+      scope = scope.has_vaccination_status(status, programme: programmes)
     end
 
     if (status = session_status&.to_sym).present?
-      scope = scope.has_session_status(status, programme:)
+      scope = scope.has_session_status(status, programme: programmes)
     end
 
     if (status = register_status&.to_sym).present?
@@ -72,11 +88,11 @@ class SearchForm
     end
 
     if (status = triage_status&.to_sym).present?
-      scope = scope.has_triage_status(status, programme:)
+      scope = scope.has_triage_status(status, programme: programmes)
     end
 
     if vaccine_method.present?
-      scope = scope.has_vaccine_method(vaccine_method, programme:)
+      scope = scope.has_vaccine_method(vaccine_method, programme: programmes)
     end
 
     scope.order_by_name
@@ -84,38 +100,38 @@ class SearchForm
 
   private
 
-  def handle_session_filters
+  def handle_request_session_filters
     if clear_filters
-      clear_from_session
+      clear_from_request_session
     elsif has_filters?
-      store_in_session
+      store_in_request_session
     else
-      load_from_session
+      load_from_request_session
     end
   end
 
-  def store_in_session
-    return if session.nil?
+  def store_in_request_session
+    return if request_session.nil?
 
     if has_filters?
-      session[SESSION_KEY] ||= {}
-      session[SESSION_KEY][path_key] = attributes
+      request_session[SESSION_KEY] ||= {}
+      request_session[SESSION_KEY][path_key] = attributes
     end
   end
 
-  def load_from_session
-    return if session.nil? || session[SESSION_KEY].blank?
+  def load_from_request_session
+    return if request_session.nil? || request_session[SESSION_KEY].blank?
 
-    stored_filters = session[SESSION_KEY][path_key]
+    stored_filters = request_session[SESSION_KEY][path_key]
     return if stored_filters.blank?
 
     assign_attributes(stored_filters)
   end
 
-  def clear_from_session
-    return if session.nil? || session[SESSION_KEY].blank?
+  def clear_from_request_session
+    return if request_session.nil? || request_session[SESSION_KEY].blank?
 
-    session[SESSION_KEY].delete(path_key)
+    request_session[SESSION_KEY].delete(path_key)
   end
 
   def has_filters?
