3ffb48d7be043f2e4021686e670a52b77e61ebf4
Thomas Leese <thomas@leese.io>
2025-05-05 21:52:24 +0100

    Refactor AppPatientSessionSearchResultCardComponent
    This refactors the component to allow the programmes that we want to
    display to be customisable, opening up the ability to display different
    programmes in different contexts.

--- app/components/app_patient_session_search_result_card_component.rb
+++ app/components/app_patient_session_search_result_card_component.rb
@@ -40,7 +40,7 @@ class AppPatientSessionSearchResultCardComponent < ViewComponent::Base
     <% end %>
   ERB
 
-  def initialize(patient_session, context:)
+  def initialize(patient_session, context:, programmes: [])
     super
 
     unless context.in?(%i[consent triage register record outcome])
@@ -48,15 +48,20 @@ class AppPatientSessionSearchResultCardComponent < ViewComponent::Base
     end
 
     @patient_session = patient_session
-    @context = context
-
     @patient = patient_session.patient
     @session = patient_session.session
+
+    @context = context
+
+    @programmes =
+      programmes
+        .select { it.year_groups.include?(patient.year_group) }
+        .presence || patient_session.programmes
   end
 
   private
 
-  attr_reader :patient_session, :patient, :session, :context
+  attr_reader :patient_session, :patient, :session, :context, :programmes
 
   def can_register_attendance?
     session_attendance =
@@ -68,11 +73,10 @@ class AppPatientSessionSearchResultCardComponent < ViewComponent::Base
   end
 
   def link_to
-    programme = patient_session.programmes.first
     session_patient_programme_path(
       session,
       patient,
-      programme,
+      programmes.first,
       return_to: context
     )
   end
@@ -82,7 +86,7 @@ class AppPatientSessionSearchResultCardComponent < ViewComponent::Base
 
     tag.ul(class: "nhsuk-list nhsuk-list--bullet") do
       safe_join(
-        patient_session.programmes.map do |programme|
+        programmes.map do |programme|
           status = patient_session.next_activity(programme:)
           tag.li("#{I18n.t(status, scope: :activity)} for #{programme.name}")
         end
@@ -100,19 +104,19 @@ class AppPatientSessionSearchResultCardComponent < ViewComponent::Base
              )
     when :consent
       statuses =
-        patient_session.programmes.index_with do |programme|
+        programmes.index_with do |programme|
           patient.consent_status(programme:).status
         end
       render AppProgrammeStatusTagsComponent.new(statuses, outcome: :consent)
     when :triage
       statuses =
-        patient_session.programmes.index_with do |programme|
+        programmes.index_with do |programme|
           patient.triage_status(programme:).status
         end
       render AppProgrammeStatusTagsComponent.new(statuses, outcome: :triage)
     else
       statuses =
-        patient_session.programmes.index_with do |programme|
+        programmes.index_with do |programme|
           patient_session.session_status(programme:).status
         end
       render AppProgrammeStatusTagsComponent.new(statuses, outcome: :session)
