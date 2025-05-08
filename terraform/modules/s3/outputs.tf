2aec0f5a223d4f09274b12b6dbc1c26aba53edc0
Moritz Bogs <moritz.bogs@tngtech.com>
2025-05-08 16:28:08 +0100

    Fix all linting issues
    * Run tflint in all directories except `modules-external`
    * Move backup modules to `modules-external` directory

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,7 +1,9 @@
 output "bucket_id" {
-  value = aws_s3_bucket.this.id
+  value       = aws_s3_bucket.this.id
+  description = "The ID of the S3 bucket"
 }
 
 output "arn" {
-  value = aws_s3_bucket.this.arn
+  value       = aws_s3_bucket.this.arn
+  description = "The ARN of the S3 bucket"
 }
