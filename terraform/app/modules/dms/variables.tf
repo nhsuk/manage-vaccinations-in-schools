7b39b6f3e4b4346ac57d66bc4cdd495176e92071
Brage Gording <brage.gording@tngtech.com>
2025-05-08 11:51:58 +0100

    Create terraform module for db migration
    - Packaging all migration resources into a module makes setup/cleanup
      simple and reusable
    - This migration module is essential for introducing the new kms key for
      for the new database
      - A pre-requisite to remote DB backups
    - Keep the configuration simple and just use the same security group for
      all related resources
      - As old db + migration resources will be deleted after migration this
        preserves the existing security group setup between pre and post
        migration states
    - Use ephemeral values to ensure that we do not put db password as
      plain text in state file

---
+++ terraform/app/modules/dms/variables.tf
@@ -0,0 +1,69 @@
+variable "environment" {
+  type        = string
+  description = "String literal for the environment"
+  nullable    = false
+}
+
+variable "source_endpoint" {
+  type        = string
+  description = "Source database endpoint"
+  nullable    = false
+}
+
+variable "source_port" {
+  type        = number
+  description = "Source database port"
+  nullable    = false
+}
+
+variable "source_database_name" {
+  type        = string
+  description = "Source database name"
+  nullable    = false
+}
+
+variable "source_db_secret_arn" {
+  type        = string
+  description = "The secret arn for the source database"
+  nullable    = false
+}
+
+variable "target_endpoint" {
+  type        = string
+  description = "Target database endpoint"
+  nullable    = false
+}
+
+variable "target_port" {
+  type        = number
+  description = "Target database port"
+  nullable    = false
+}
+
+variable "target_database_name" {
+  type        = string
+  description = "Target database name"
+  nullable    = false
+}
+
+variable "target_db_secret_arn" {
+  type        = string
+  description = "The secret arn for the target database"
+  nullable    = false
+}
+
+variable "engine_name" {
+  description = "Database engine name (aurora or aurora-postgresql)"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for DMS"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "Security group ID for DMS resources"
+  type        = string
+  default     = null
+}
