5b476d070e461894c847c426d18f6db5c7bb63ee
Moritz Bogs <moritz.bogs@tngtech.com>
2025-05-08 13:51:47 +0100

    Apply fix to NHS module
    * The number or report frameworks must be dynamic, depending on how many frameworks are enabled

--- terraform/backup/modules/aws-backup-source/backup_report_plan.tf
+++ terraform/backup/modules/aws-backup-source/backup_report_plan.tf
@@ -48,7 +48,7 @@ resource "aws_backup_report_plan" "resource_compliance" {
 
   report_setting {
     framework_arns       = var.backup_plan_config_dynamodb.enable ? [aws_backup_framework.main.arn, aws_backup_framework.dynamodb[0].arn] : [aws_backup_framework.main.arn]
-    number_of_frameworks = 2
+    number_of_frameworks = var.backup_plan_config_dynamodb.enable ? 2 : 1
     report_template      = "RESOURCE_COMPLIANCE_REPORT"
   }
 }
