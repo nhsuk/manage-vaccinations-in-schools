65507ba404b781c4e9fb5d1e30e4295e00b675eb
Thomas Leese <thomas@leese.io>
2025-05-12 13:57:52 +0100

    Hide "Contains gelatine" reason when not valid
    Only for the Flu programme should this reason for refusal show in the
    list of reasons for refusal, as this is not a valid reason for the other
    programmes.
    
    I've implemented this by passing in whether to show this option when
    using the component. We could be more dynamic and the component to check
    each consent to see if any of the programmes' vaccines may contain
    gelatine, but the performance hit on that (when we may have hundreds of
    thousands of responses) seemed not worth it.

--- app/components/app_consent_refused_table_component.rb
+++ app/components/app_consent_refused_table_component.rb
@@ -1,18 +1,31 @@
 # frozen_string_literal: true
 
 class AppConsentRefusedTableComponent < ViewComponent::Base
-  def initialize(consents)
+  def initialize(consents, vaccine_may_contain_gelatine:)
     super
 
     @grouped_by_reason_for_refusal =
       consents.response_refused.group(:reason_for_refusal).count
     @total_count = @grouped_by_reason_for_refusal.values.sum
+    @vaccine_may_contain_gelatine = vaccine_may_contain_gelatine
   end
 
-  def percentage_for(reason_for_refusal)
-    return 0 if @total_count.zero?
+  private
 
-    @grouped_by_reason_for_refusal.fetch(reason_for_refusal, 0) /
-      @total_count.to_f * 100.0
+  attr_reader :grouped_by_reason_for_refusal,
+              :total_count,
+              :vaccine_may_contain_gelatine
+
+  def percentage_for(reason_for_refusal)
+    return 0 if total_count.zero?
+
+    grouped_by_reason_for_refusal.fetch(reason_for_refusal, 0) /
+      total_count.to_f * 100.0
+  end
+
+  def reasons_for_refusal
+    reasons = Consent.reason_for_refusals.keys
+    reasons -= %w[contains_gelatine] unless vaccine_may_contain_gelatine
+    reasons
   end
 end
