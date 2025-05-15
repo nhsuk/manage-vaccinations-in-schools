acc9e860f7686b3c8873bb0aaa9d37581cce59be
Moritz Bogs <moritz.bogs@tngtech.com>
2025-05-15 16:30:19 +0100

    Use modules directly from GitHub
    * That is much better than copying the source code into our repository

--- terraform/modules-external/aws-backup-destination/backup.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_backup_vault" "vault" {
-  name        = "${var.source_account_name}-backup-vault"
-  kms_key_arn = var.kms_key
-}
-
-output "vault_arn" {
-  value = aws_backup_vault.vault.arn
-}
