nhsuk/manage-vaccinations-in-schools

commit 34361c2b5844ef1156153745a082f89a83f4648d

Allow importing vaccination records without vaccines
This ensures that vaccination records without vaccines can be imported.
This works currently for the existing format but wasn't working
correctly for the new SystmOne import.

app/models/immunisation_import_row.rb
@@ -297,15 +297,17 @@ class ImmunisationImportRow
       end
   end
 
+  def vaccine_nivs_name
+    parsed_vaccination_description_string&.dig(:vaccine_name) ||
+      vaccine_name&.to_s
+  end
+
   def vaccine
     @vaccine ||=
-      begin
-        nivs_name =
-          parsed_vaccination_description_string&.dig(:vaccine_name) ||
-            vaccine_name&.to_s
-
-        organisation.vaccines.includes(:programme).find_by(nivs_name:)
-      end
+      organisation
+        .vaccines
+        .includes(:programme)
+        .find_by(nivs_name: vaccine_nivs_name)
   end
 
   def batch
@@ -956,7 +958,7 @@ class ImmunisationImportRow
           "is not given in the #{programme.name} programme"
         )
       end
-    elsif field.present?
+    elsif vaccine_nivs_name.present?
       errors.add(field.header, "This vaccine is not available in this session.")
     end
   end

spec/models/immunisation_import_row_spec.rb
@@ -182,6 +182,12 @@ describe ImmunisationImportRow do
       end
     end
 
+    context "without a vaccine" do
+      let(:data) { valid_data.except("VACCINE_GIVEN") }
+
+      it { should be_valid }
+    end
+
     context "with an invalid reason not vaccinated" do
       let(:data) do
         { "VACCINATED" => "N", "REASON_NOT_VACCINATED" => "unknown" }
@@ -833,6 +839,18 @@ describe ImmunisationImportRow do
         expect(vaccination_record.performed_at).to eq(
           Time.new(2024, 1, 1, 10, 30, 0, "+00:00")
         )
+      end
+    end
+
+    context "without a vaccine" do
+      let(:data) { valid_data.except("VACCINE_GIVEN") }
+
+      it "doesn't set a vaccine" do
+        expect(vaccination_record.vaccine).to be_nil
+      end
+
+      it "does set a programme" do
+        expect(vaccination_record.programme).not_to be_nil
       end
     end
 

spec/models/immunisation_import_spec.rb
@@ -250,8 +250,8 @@ describe ImmunisationImport do
         # stree-ignore
         expect { process! }
           .to change(immunisation_import, :processed_at).from(nil)
-          .and change(immunisation_import.vaccination_records, :count).by(1)
-          .and change(immunisation_import.patients, :count).by(1)
+          .and change(immunisation_import.vaccination_records, :count).by(2)
+          .and change(immunisation_import.patients, :count).by(2)
           .and change(immunisation_import.batches, :count).by(1)
           .and not_change(immunisation_import.patient_sessions, :count)
 

commit e08f8efcd52f275485271610f87682c2e0c3aa0d

Ensure `VACCINE_GIVEN` is present when recording offline
When uploading vaccinations that were recorded offline the vaccine is a
required column as it would be when recording vaccinations online. This
fixes a regression that was introduced in
34361c2b5844ef1156153745a082f89a83f4648d as part of the SystmOne import
but was applied across the board rather than only for historical
vaccinations.

app/models/immunisation_import_row.rb
@@ -927,6 +927,12 @@ class ImmunisationImportRow
       end
     elsif vaccine_nivs_name.present?
       errors.add(field.header, "This vaccine is not available in this session.")
+    elsif offline_recording? && administered
+      if vaccine_name.nil?
+        errors.add(:base, "<code>VACCINE_GIVEN</code> is required")
+      elsif vaccine_name.blank?
+        errors.add(vaccine_name.header, "is required")
+      end
     end
   end
 end

spec/models/immunisation_import_row_spec.rb
@@ -156,6 +156,27 @@ describe ImmunisationImportRow do
       it { should be_valid }
     end
 
+    context "without a vaccine and recording offline" do
+      let(:data) do
+        valid_data.merge(
+          {
+            "VACCINE_GIVEN" => "",
+            "DATE_OF_VACCINATION" => "#{Date.current.academic_year}0901",
+            "SESSION_ID" => session.id.to_s
+          }
+        )
+      end
+
+      let(:session) { create(:session, organisation:, programmes:) }
+
+      it "has errors" do
+        expect(immunisation_import_row).to be_invalid
+        expect(immunisation_import_row.errors["VACCINE_GIVEN"]).to eq(
+          ["is required"]
+        )
+      end
+    end
+
     context "with an invalid reason not vaccinated" do
       let(:data) do
         { "VACCINATED" => "N", "REASON_NOT_VACCINATED" => "unknown" }