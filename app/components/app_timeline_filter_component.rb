d32e5c93dc09f32501faa9b25af592c07d943afc
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2025-05-16 09:13:07 +0100

    Add ability to show PII related information on the timeline
    A show_pii flag allows optional exclusion of personally-identifiable
    information from the event details and audited changes relating to
    a patient. This has been set to false by default in the controller,
    but can be toggled on or off with a check box in the UI.

--- app/components/app_timeline_filter_component.rb
+++ app/components/app_timeline_filter_component.rb
@@ -11,7 +11,8 @@ class AppTimelineFilterComponent < ViewComponent::Base
     class_imports:,
     cohort_imports:,
     sessions:,
-    reset_url:
+    reset_url:,
+    show_pii:
   )
     super
     @url = url
@@ -24,6 +25,7 @@ class AppTimelineFilterComponent < ViewComponent::Base
     @cohort_imports = cohort_imports
     @sessions = sessions
     @reset_url = reset_url
+    @show_pii = show_pii
   end
 
   attr_reader :url,
@@ -34,5 +36,6 @@ class AppTimelineFilterComponent < ViewComponent::Base
               :class_imports,
               :cohort_imports,
               :sessions,
-              :reset_url
+              :reset_url,
+              :show_pii
 end
--- app/controllers/inspect/timeline/patients_controller.rb
+++ app/controllers/inspect/timeline/patients_controller.rb
@@ -7,6 +7,8 @@ class Inspect::Timeline::PatientsController < ApplicationController
 
   layout "full"
 
+  SHOW_PII = false
+
   DEFAULT_EVENT_NAMES = %w[
     consents
     school_moves
@@ -20,6 +22,8 @@ class Inspect::Timeline::PatientsController < ApplicationController
   ].freeze
 
   def show
+    @show_pii = params[:show_pii] || SHOW_PII
+
     params.reverse_merge!(event_names: DEFAULT_EVENT_NAMES)
     params[:audit_config] ||= {}
 
@@ -43,7 +47,8 @@ class Inspect::Timeline::PatientsController < ApplicationController
       TimelineRecords.new(
         @patient,
         detail_config: build_details_config,
-        audit_config: audit_config
+        audit_config: audit_config,
+        show_pii: @show_pii
       ).load_grouped_events(event_names)
 
     @no_events_message = true if @patient_timeline.empty?
