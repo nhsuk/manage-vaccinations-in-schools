a43d8d3d2939455617aca5cca9a09a093c06e4ac
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-05-28 23:43:02 +0100

    WIP: adding organisation_fhir_concern

---
+++ spec/models/concerns/organisation_fhir_concern_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Normally we test this concern in isolation, but in this case it's bespoke to
+# the VaccinationRecord and has a lot of dependencies on it, so not really
+# worth it.
+describe OrganisationFHIRConcern do
+  include FHIRHelper
+
+  let(:ods_code) { "A9A5A" }
+
+  describe ".fhir_reference" do
+    it "returns a FHIR reference with the correct ODS code" do
+      reference = Organisation.fhir_reference(ods_code:)
+
+      expect(reference.type).to eq "Organization"
+      expect(
+        reference.identifier.system
+      ).to eq "https://fhir.nhs.uk/Id/ods-organization-code"
+      expect(reference.identifier.value).to eq ods_code
+    end
+  end
+end
