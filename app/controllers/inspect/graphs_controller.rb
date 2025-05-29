4245ecd7c4ad19e1ee4dba0949819c70caa87bb1
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-05-29 08:58:49 +0100

    Add toggle for `show_pii` mode in the UI

--- app/controllers/inspect/graphs_controller.rb
+++ app/controllers/inspect/graphs_controller.rb
@@ -7,8 +7,6 @@ module Inspect
 
     layout "full"
 
-    SHOW_PII = false
-
     def show
       @primary_type = safe_get_primary_type
       if @primary_type.nil?
@@ -31,6 +29,7 @@ module Inspect
 
       @traversals_config = build_traversals_config
       @graph_params = build_graph_params
+      @show_pii = params[:show_pii]&.first == "1"
 
       @mermaid =
         GraphRecords
@@ -38,7 +37,7 @@ module Inspect
             traversals_config: @traversals_config,
             primary_type: @primary_type,
             clickable: true,
-            show_pii: SHOW_PII
+            show_pii: @show_pii
           )
           .graph(**@graph_params)
           .join("\n")
