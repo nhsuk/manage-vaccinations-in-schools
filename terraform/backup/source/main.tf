8ca2e67d4e0c15bf2130f92c8415443c0180253a
Moritz Bogs <moritz.bogs@tngtech.com>
2025-05-30 13:42:17 +0100

    Cleanup infrastructure code
    - Remove commented out code
    - Rename terraform instance to drop reduntand "cluster" suffix
    - Fix logic of whether a db has self-managed credentials

--- terraform/backup/source/main.tf
+++ terraform/backup/source/main.tf
@@ -93,9 +93,9 @@ resource "aws_kms_key" "backup_notifications" {
 # Now we can deploy the source and destination modules, referencing the resources we've created above.
 
 module "source" {
-  source = "github.com/NHSDigital/terraform-aws-backup.git//modules/aws-backup-source?ref=v1.1.0"
+  # source = "github.com/NHSDigital/terraform-aws-backup.git//modules/aws-backup-source?ref=v1.1.0"
   # Use SSH to fetch sources locally
-  # source = "git@github.com:NHSDigital/terraform-aws-backup.git//modules/aws-backup-source?ref=v1.1.0"
+  source = "git@github.com:NHSDigital/terraform-aws-backup.git//modules/aws-backup-source?ref=v1.1.0"
 
 
   backup_copy_vault_account_id = local.destination_account_id
