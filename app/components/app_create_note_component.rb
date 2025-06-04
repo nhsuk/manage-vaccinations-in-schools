68c1488e90d66d0792830a61b5d36fae765c1927
Thomas Leese <thomas@leese.io>
2025-06-04 19:12:38 +0100

    Add ability to create notes
    This adds functionality to create notes attached to a patient from the
    session notes and activity log tab when viewing a patient session. It's
    behind a `notes` feature flag so it won't be shown to users until the
    feature flag is toggled on.
    
    Jira-Issue: MAV-1265

---
+++ app/components/app_create_note_component.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class AppCreateNoteComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <%= render AppDetailsComponent.new(summary: "Add a note", open:, expander: true) do %>
+      <%= form_with model: note, url:, builder: do |f| %>
+        <% content_for(:before_content) { f.govuk_error_summary } %>
+
+        <%= f.govuk_text_area :body, label: { text: "Note" } %>
+        <%= f.govuk_submit "Save note", class: "nhsuk-u-margin-bottom-0" %>
+      <% end %>
+    <% end %>
+  ERB
+
+  def initialize(note, open: false)
+    super
+
+    @note = note
+    @open = open
+  end
+
+  private
+
+  attr_reader :note, :open
+
+  delegate :patient, :session, to: :note
+
+  def url = session_patient_activity_path(session, patient)
+
+  def builder = GOVUKDesignSystemFormBuilder::FormBuilder
+end
