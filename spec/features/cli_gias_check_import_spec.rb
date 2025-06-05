726d9e25b1faff02e9cd67adddc86ff8a64c1c91
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-06-05 16:20:43 +0100

    Rename locations to schools in gias check-import

--- spec/features/cli_gias_check_import_spec.rb
+++ spec/features/cli_gias_check_import_spec.rb
@@ -3,9 +3,9 @@
 require_relative "../../app/lib/mavis_cli"
 
 describe "mavis gias check_import" do
-  it "counts the number of locations closed that have future sessions" do
+  it "counts the number of schools closed that have future sessions" do
     given_an_organisation_exists
-    and_there_are_locations_with_future_sessions
+    and_there_are_schools_with_future_sessions
     when_i_run_the_check_import_command
     then_i_should_see_the_correct_counts
   end
@@ -15,33 +15,33 @@ describe "mavis gias check_import" do
     @programme = create(:programme, :hpv)
   end
 
-  def and_there_are_locations_with_future_sessions
-    @location_with_future_session =
+  def and_there_are_schools_with_future_sessions
+    @school_with_future_session =
       create(:school, name: "The Aldgate School", urn: "100000")
     @session_with_future_dates =
       create(
         :session,
-        location: @location_with_future_session,
+        location: @school_with_future_session,
         dates: [Date.tomorrow],
         programmes: [@programme]
       )
 
-    @location2_with_future_session =
+    @school2_with_future_session =
       create(:school, name: "St Paul's Cathedral School", urn: "100002")
     @session_with_future_dates =
       create(
         :session,
-        location: @location2_with_future_session,
+        location: @school2_with_future_session,
         dates: [Date.tomorrow],
         programmes: [@programme]
       )
 
-    @location_without_future_session =
+    @school_without_future_session =
       create(:school, name: "City of London School for Girls", urn: "100001")
     @session_without_future_dates =
       create(
         :session,
-        location: @location_without_future_session,
+        location: @school_without_future_session,
         dates: [Date.yesterday],
         programmes: [@programme]
       )
@@ -64,15 +64,15 @@ describe "mavis gias check_import" do
 
   def then_i_should_see_the_correct_counts
     expect(@output).to eq <<~OUTPUT
-                        New locations (total): 1
-                     Closed locations (total): 1
-      Proposed to be closed locations (total): 1
+                        New schools (total): 1
+                     Closed schools (total): 1
+      Proposed to be closed schools (total): 1
 
-       Existing locations with future sessions: 2
+       Existing schools with future sessions: 2
                      That are closed in import: 1 (50.0%)
       That are proposed to be closed in import: 1 (50.0%)
 
-      URNs of closed locations with future sessions:
+      URNs of closed schools with future sessions:
         100000
     OUTPUT
   end
