702da7789c8d7589f483ba1cba9fb568a2a3f893
Karim Elmestekawy <karim.mestekawy@gmail.com>
2025-06-06 10:25:10 +0100

    Simplifying Unit tests

--- spec/models/immunisation_import_row_spec.rb
+++ spec/models/immunisation_import_row_spec.rb
@@ -780,6 +780,51 @@ describe ImmunisationImportRow do
 
       it { should be_valid }
     end
+
+    context "validating clinic name" do
+      let(:valid_clinic_data) do
+        valid_data.merge(
+          "CARE_SETTING" => "2",
+          "DATE_OF_VACCINATION" => session.dates.first.strftime("%Y%m%d"),
+          "SESSION_ID" => session.id.to_s,
+          "ORGANISATION_CODE" => organisation.ods_code,
+          "PERFORMING_PROFESSIONAL_EMAIL" => create(:user).email
+        )
+      end
+
+      let(:session) { create(:session, organisation:, programmes:) }
+
+      before { create(:community_clinic, name: "A clinic", organisation:) }
+
+      context "with an existing community clinic" do
+        let(:data) { valid_clinic_data.merge("CLINIC_NAME" => "A clinic") }
+
+        it "is matching" do
+          expect(immunisation_import_row).to be_valid
+        end
+      end
+
+      context "with incorrect casing for an existing clinic" do
+        let(:data) { valid_clinic_data.merge("CLINIC_NAME" => "a cLinIC") }
+
+        it "is case insensitive" do
+          expect(immunisation_import_row).to be_valid
+        end
+      end
+
+      context "with a non-existent clinic" do
+        let(:data) do
+          valid_clinic_data.merge("CLINIC_NAME" => "A wrong clinic")
+        end
+
+        it "has errors" do
+          expect(immunisation_import_row).to be_invalid
+          expect(
+            immunisation_import_row.errors["CLINIC_NAME"]
+          ).to contain_exactly("Enter a clinic name")
+        end
+      end
+    end
   end
 
   describe "#to_vaccination_record" do
@@ -1364,69 +1409,6 @@ describe ImmunisationImportRow do
         end
 
         it { should eq("A Clinic") }
-      end
-
-      context "with an existing community clinic" do
-        let(:data) do
-          valid_data.merge(
-            "CLINIC_NAME" => "A clinic",
-            "CARE_SETTING" => "2",
-            "SESSION_ID" => session.id.to_s
-          )
-        end
-
-        let(:session) { create(:session, organisation:, programmes:) }
-
-        before { create(:community_clinic, name: "A clinic", organisation:) }
-
-        it "is matching" do
-          immunisation_import_row.valid?
-          expect(immunisation_import_row.errors.full_messages).not_to include(
-            a_string_including("Enter a clinic name")
-          )
-        end
-      end
-
-      context "with incorrect casing for an existing clinic" do
-        let(:data) do
-          valid_data.merge(
-            "CLINIC_NAME" => "a cLinIC",
-            "CARE_SETTING" => "2",
-            "SESSION_ID" => session.id.to_s
-          )
-        end
-
-        let(:session) { create(:session, organisation:, programmes:) }
-
-        before { create(:community_clinic, name: "A clinic", organisation:) }
-
-        it "is case insensitive" do
-          immunisation_import_row.valid?
-          expect(immunisation_import_row.errors.full_messages).not_to include(
-            a_string_including("Enter a clinic name")
-          )
-        end
-      end
-
-      context "with a non-existent clinic" do
-        let(:data) do
-          valid_data.merge(
-            "CLINIC_NAME" => "A wrong clinic",
-            "CARE_SETTING" => "2",
-            "SESSION_ID" => session.id.to_s
-          )
-        end
-
-        let(:session) { create(:session, organisation:, programmes:) }
-
-        before { create(:community_clinic, name: "A clinic", organisation:) }
-
-        it "has errors" do
-          expect(immunisation_import_row).to be_invalid
-          expect(immunisation_import_row.errors.full_messages).to include(
-            a_string_including("Enter a clinic name")
-          )
-        end
       end
     end
 
