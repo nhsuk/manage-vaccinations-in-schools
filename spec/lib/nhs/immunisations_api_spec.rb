641077822993c40d214b0aa05c37ea1cf8ddf4bc
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-06-09 16:34:49 +0100

    WIP: update immunisation api spec

--- spec/lib/nhs/immunisations_api_spec.rb
+++ spec/lib/nhs/immunisations_api_spec.rb
@@ -3,35 +3,73 @@
 describe NHS::ImmunisationsAPI do
   include FHIRHelper
 
-  let(:patient) { create(:patient) }
-  let(:vaccination_record) { create(:vaccination_record, patient:) }
+  let(:organisation) { create(:organisation, ods_code: "A9A5A") }
+  let(:patient) do
+    create(
+      :patient,
+      id: 31_337,
+      organisation:,
+      address_postcode: "EC1A 1BB",
+      nhs_number: "9449310475",
+      given_name: "Sarah",
+      family_name: "Taylor",
+      date_of_birth: Date.new(2011, 9, 9)
+    )
+  end
+  let(:programme) { create(:programme, :hpv) }
+  let(:location) { create(:community_clinic, organisation:, ods_code: nil) }
+  let(:vaccine) { create(:vaccine, :gardasil, programme:) }
+  let(:session) do
+    create(:session, organisation:, programmes: [programme], location:)
+  end
+  let(:user) do
+    create(
+      :user,
+      id: 31_337,
+      organisation:,
+      family_name: "Nightingale",
+      given_name: "Florence"
+    )
+  end
+  let(:vaccination_record) do
+    create(
+      :vaccination_record,
+      uuid: "11112222-3333-4444-5555-666677778888",
+      organisation:,
+      patient:,
+      programme:,
+      location:,
+      vaccine:,
+      session:,
+      performed_by_user: user,
+      performed_at: Time.zone.parse("2021-02-07T13:28:17.271+00:00"),
+      created_at: Time.zone.parse("2021-02-07T13:28:17.271+00:00")
+    )
+  end
 
   describe "record_immunisation" do
     it "sends the correct JSON payload" do
-      # TODO: We should use the static immunisation JSON fixture and compare
-      #       against that by setting the appropriate attributes on
-      #       vaccination_record
-      expected_body = vaccination_record.to_fhir.to_json
+      expected_body =
+        File.read(Rails.root.join("spec/fixtures/fhir/immunisation.json")).chomp
 
-      stub_request(
-        :post,
-        "https://sandbox.api.service.nhs.uk/immunisation-fhir-api/FHIR/R4/Immunization/"
-      ).with(
-        body: expected_body,
-        headers: {
-          "Accept" => "application/fhir+json",
-          "Content-Type" => "application/fhir+json"
-        }
-      ).to_return(status: 200, body: "", headers: {})
-
-      described_class.record_immunisation(vaccination_record)
-
-      expect(
-        a_request(
+      stubbed_request =
+        stub_request(
           :post,
           "https://sandbox.api.service.nhs.uk/immunisation-fhir-api/FHIR/R4/Immunization/"
         )
-      ).to have_been_made
+          .with do |request|
+            expect(request.headers["Accept"]).to eq "application/fhir+json"
+            expect(
+              request.headers["Content-Type"]
+            ).to eq "application/fhir+json"
+            expect(request.body).to eq expected_body
+            true
+          end
+          .to_return(status: 200, body: "", headers: {})
+
+      described_class.record_immunisation(vaccination_record)
+
+      expect(stubbed_request).to have_been_made
     end
   end
 end
