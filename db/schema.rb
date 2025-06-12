403ff72e40fa3eb7b70e035794ef5565d5da1324
Thomas Leese <thomas@leese.io>
2025-06-12 09:44:26 +0100

    Add vaccine_method to triages
    This adds a new column, `vaccine_method`, to the triage model which will
    be used in the future to allow nurses to choose which vaccine method
    they are triaging the patient as safe to vaccinate.
    
    Jira-Issue: MAV-1285

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_10_154510) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_12_083140) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -755,6 +755,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_10_154510) do
     t.bigint "patient_id", null: false
     t.bigint "organisation_id", null: false
     t.datetime "invalidated_at"
+    t.integer "vaccine_method"
     t.index ["organisation_id"], name: "index_triage_on_organisation_id"
     t.index ["patient_id"], name: "index_triage_on_patient_id"
     t.index ["performed_by_user_id"], name: "index_triage_on_performed_by_user_id"
