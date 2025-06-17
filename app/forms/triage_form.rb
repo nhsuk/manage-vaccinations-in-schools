936bd1d8396f4f15f054a24890636f86cebaba85
Thomas Leese <thomas@leese.io>
2025-06-17 21:13:21 +0100

    Choose a vaccine method when triaging
    When triaging a patient we need to be able to choose the method of
    vaccine if necessary (for example with Flu where nasal spray and
    injection are possible).
    
    Jira-Issue: MAV-1285

--- app/forms/triage_form.rb
+++ app/forms/triage_form.rb
@@ -6,11 +6,29 @@ class TriageForm
 
   attr_accessor :patient_session, :programme, :current_user
 
-  attribute :status, :string
+  attribute :status_and_vaccine_method, :string
   attribute :notes, :string
 
-  validates :status, inclusion: { in: Triage.statuses.keys }
+  validates :status_and_vaccine_method,
+            inclusion: {
+              in: :status_and_vaccine_method_options
+            }
   validates :notes, length: { maximum: 1000 }
+
+  def triage=(triage)
+    self.status_and_vaccine_method =
+      if triage.ready_to_vaccinate?
+        if consented_vaccine_methods.length > 1
+          "safe_to_vaccinate_#{triage.vaccine_method}"
+        else
+          "safe_to_vaccinate"
+        end
+      elsif triage.needs_follow_up?
+        "keep_in_triage"
+      else
+        triage.status
+      end
+  end
 
   def save
     Triage.create!(triage_attributes) if valid?
@@ -20,9 +38,28 @@ class TriageForm
     Triage.create!(triage_attributes)
   end
 
+  def status_and_vaccine_method_options
+    safe_to_vaccinate_choices =
+      if consented_vaccine_methods.length > 1
+        consented_vaccine_methods.map { "safe_to_vaccinate_#{it}" }
+      else
+        ["safe_to_vaccinate"]
+      end
+
+    safe_to_vaccinate_choices +
+      %w[keep_in_triage delay_vaccination do_not_vaccinate]
+  end
+
+  def consented_to_injection? = consented_vaccine_methods.include?("injection")
+
   private
 
   delegate :organisation, :patient, to: :patient_session
+
+  def consented_vaccine_methods
+    @consented_vaccine_methods ||=
+      patient.consent_status(programme:).vaccine_methods
+  end
 
   def triage_attributes
     {
@@ -34,5 +71,28 @@ class TriageForm
       status:,
       vaccine_method: "injection"
     }
+  end
+
+  def status
+    case status_and_vaccine_method
+    when "safe_to_vaccinate", "safe_to_vaccinate_injection",
+         "safe_to_vaccinate_nasal"
+      "ready_to_vaccinate"
+    when "keep_in_triage"
+      "needs_follow_up"
+    else
+      status_and_vaccine_method
+    end
+  end
+
+  def vaccine_method
+    case status_and_vaccine_method
+    when "safe_to_vaccinate"
+      consented_vaccine_methods.first
+    when "safe_to_vaccinate_injection"
+      "injection"
+    when "safe_to_vaccinate_nasal"
+      "nasal"
+    end
   end
 end
