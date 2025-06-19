04b0a2bc4449bccb72657757b7635200b88f5fd5
Thomas Leese <thomas@leese.io>
2025-06-19 07:32:47 +0100

    Refactor UnscheduledSessionsFactory
    This refactors the class to separate the various tasks performed in to
    separate methods to allow it to be easily extended in the future.
    
    Jira-Issue: MAV-1293

--- app/lib/unscheduled_sessions_factory.rb
+++ app/lib/unscheduled_sessions_factory.rb
@@ -8,37 +8,7 @@ class UnscheduledSessionsFactory
   def call
     Organisation
       .includes(:locations, :programmes, :sessions)
-      .find_each do |organisation|
-        sessions =
-          organisation.sessions.select { _1.academic_year == academic_year }
-
-        organisation.locations.find_each do |location|
-          next if sessions.any? { _1.location_id == location.id }
-
-          programmes =
-            if location.generic_clinic?
-              organisation.programmes
-            elsif location.school?
-              organisation.programmes.select do
-                it.year_groups.intersect?(location.year_groups)
-              end
-            else
-              [] # don't create sessions for unhandled location types
-            end
-
-          next if programmes.empty?
-
-          Session.create!(academic_year:, location:, programmes:, organisation:)
-        end
-
-        location_ids = organisation.locations.map(&:id)
-
-        sessions
-          .select(&:unscheduled?)
-          .reject { _1.location_id.in?(location_ids) }
-          .reject { _1.patients.exists? }
-          .each(&:destroy!)
-      end
+      .find_each { create_for_organisation(it) }
   end
 
   def self.call(...) = new(...).call
@@ -48,4 +18,52 @@ class UnscheduledSessionsFactory
   private
 
   attr_reader :academic_year
+
+  def create_for_organisation(organisation)
+    create_clinic_sessions!(organisation)
+    create_school_sessions!(organisation)
+    destroy_orphaned_sessions!(organisation)
+  end
+
+  def create_clinic_sessions!(organisation)
+    ActiveRecord::Base.transaction do
+      organisation.locations.generic_clinic.find_each do |location|
+        session =
+          organisation.sessions.find_or_initialize_by(academic_year:, location:)
+        session.programmes = organisation.programmes
+        session.save!
+      end
+    end
+  end
+
+  def create_school_sessions!(organisation)
+    ActiveRecord::Base.transaction do
+      organisation.schools.find_each do |location|
+        next if organisation.sessions.exists?(academic_year:, location:)
+
+        programmes =
+          organisation.programmes.select do
+            it.year_groups.intersect?(location.year_groups)
+          end
+
+        next if programmes.empty?
+
+        Session.create!(academic_year:, location:, programmes:, organisation:)
+      end
+    end
+  end
+
+  def destroy_orphaned_sessions!(organisation)
+    ActiveRecord::Base.transaction do
+      organisation
+        .sessions
+        .includes(:location, :session_programmes)
+        .unscheduled
+        .where(academic_year:)
+        .where.not(location: organisation.locations)
+        .where
+        .missing(:patient_sessions)
+        .destroy_all
+    end
+  end
 end
