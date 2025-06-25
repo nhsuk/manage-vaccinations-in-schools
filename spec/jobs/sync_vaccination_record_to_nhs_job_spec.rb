c8a27f5ed964ca10b52319257f419b19833785a4
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-06-25 22:39:06 +0100

    Add SyncVaccinationRecordToNHSEJob
    This job will be responsible for syncing a Mavis vaccination record with
    NHS via the Immunisation API. The first task will be to simply create
    the immunisation record if it hasn't already been synced, implemented
    here. Funcionality will be added to update or delete the record as
    necessary to keep the record in sync.
    
    Jira-Issue: MAV-1477

---
+++ spec/jobs/sync_vaccination_record_to_nhs_job_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+describe SyncVaccinationRecordToNHSJob, type: :job do
+  subject(:perform_now) { described_class.perform_now(vaccination_record) }
+
+  before { allow(NHS::ImmunisationsAPI).to receive(:record_immunisation) }
+
+  let(:vaccination_record) do
+    instance_double(
+      VaccinationRecord,
+      id: "123",
+      nhs_immunisations_api_synced_at: nil
+    )
+  end
+
+  it "sends the vaccination record to the NHS Immunisations API" do
+    perform_now
+
+    expect(NHS::ImmunisationsAPI).to have_received(:record_immunisation).with(
+      vaccination_record
+    )
+  end
+
+  context "when the vaccination record has already been synced" do
+    let(:vaccination_record) do
+      instance_double(
+        VaccinationRecord,
+        id: "123",
+        nhs_immunisations_api_synced_at: Time.current
+      )
+    end
+
+    it "does not send the vaccination record to the NHS Immunisations API" do
+      perform_now
+
+      expect(NHS::ImmunisationsAPI).not_to have_received(:record_immunisation)
+    end
+
+    it "logs that the record has already been synced" do
+      allow(Rails.logger).to receive(:info)
+
+      perform_now
+
+      expect(Rails.logger).to have_received(:info).with(
+        "Vaccination record already synced: 123"
+      )
+    end
+  end
+end
