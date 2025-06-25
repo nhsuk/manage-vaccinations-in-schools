19e9e01c8cf9a80259d1fa9b0d08f77cbcfab116
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-06-25 11:19:16 +0100

    Add "delivery method" snomed methods
    ... to VaccinationRecord.
    
    These will be required to generate FHIR records.
    
    Jira-Issue: MAV-1476

--- app/models/vaccination_record.rb
+++ app/models/vaccination_record.rb
@@ -177,6 +177,14 @@ class VaccinationRecord < ApplicationRecord
     programme.seasonal? ? performed_this_academic_year? : true
   end
 
+  def delivery_method_snomed_code
+    DELIVERY_METHOD_SNOMED_CODES_AND_TERMS.fetch(delivery_method).first
+  end
+
+  def delivery_method_snomed_term
+    DELIVERY_METHOD_SNOMED_CODES_AND_TERMS.fetch(delivery_method).second
+  end
+
   private
 
   def requires_location_name?
