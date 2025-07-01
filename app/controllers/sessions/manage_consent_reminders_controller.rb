5fa6b15dac001239fa7bec63fe26e92156e6127e
Martin van IJcken <martin.vanijcken@tngtech.com>
2025-07-01 12:29:20 +0100

    feat: Add button to manually send consent requests
    Jira-Issue: MAV-886

--- app/controllers/sessions/manage_consent_reminders_controller.rb
+++ app/controllers/sessions/manage_consent_reminders_controller.rb
@@ -3,6 +3,15 @@
 class Sessions::ManageConsentRemindersController < ApplicationController
   before_action :set_session
 
+  def create
+    SendSchoolConsentRemindersJob.perform_later(@session)
+
+    redirect_to session_path(@session),
+                flash: {
+                  success: "Manual consent reminders sent."
+                }
+  end
+
   private
 
   def set_session
