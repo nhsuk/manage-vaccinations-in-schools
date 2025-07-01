6daa20c645c9abc93ad91e511d66576ae8a13586
Alistair Davidson <alistair.davidson2@nhs.net>
2025-07-01 10:18:19 +0100

    Use strings for enum fields on reporting tables, to fix Rails behaviour when copying enum attributes

---
+++ db/migrate/20250701090133_change_enums_on_reporting_tables_to_string.rb
@@ -0,0 +1,13 @@
+class ChangeEnumsOnReportingTablesToString < ActiveRecord::Migration[8.0]
+  def change
+    add_column :reportable_vaccination_events, :vaccination_record_delivery_site, :string
+
+    change_column :reportable_vaccination_events, :vaccination_record_delivery_method, :string
+    change_column :reportable_vaccination_events, :vaccination_record_outcome, :string
+    
+    change_column :reportable_consent_events, :consent_response, :string
+    change_column :reportable_consent_events, :consent_reason_for_refusal, :string
+    change_column :reportable_consent_events, :consent_route, :string
+    
+  end
+end
