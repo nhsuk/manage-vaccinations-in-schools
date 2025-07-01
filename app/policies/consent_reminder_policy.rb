5618547f6fe4b05faf159bfaaee440e3dff12dd2
Martin van IJcken <martin.vanijcken@tngtech.com>
2025-07-01 12:27:30 +0100

    feat: Create overview page
    Co-authored-by: Karim Elmestekawy <karim.mestekawy@gmail.com>

---
+++ app/policies/consent_reminder_policy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ConsentReminderPolicy < ApplicationPolicy
+  def show?
+    user.is_nurse? || user.is_admin?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope.where(organisation: user.selected_organisation)
+    end
+  end
+end
