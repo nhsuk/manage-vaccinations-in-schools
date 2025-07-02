072a1a4e619e516419aff580df70803cbc9c7b8d
Alistair Davidson <alistair.davidson2@nhs.net>
2025-07-02 16:10:31 +0100

    Add support for redirecting back to the reporting app after successful login via CIS2, and specs for same. Add one-time-token exchange with the reporting app. Can now demonstrate logging in on Mavis & redirecting back to reporting app with session info
    Add JWT to encode all the info that is currently top-level in the token verification response
    
    Add reporting controller + spec
    
    Add pwd_auth_session_token to the user model. Add Warden callbacks to set & clear it on pwd-login and logout. Make authenticate_user_by_jwt! look for user with not just the given user ID, but also the given session_token and pwd_auth_session_token, return 401 if no match
    
    Add specs for authentication-related methods
    Refactor token auth methods to their own concern
    Delete pwd_auth_session_token on logout also
    
    Remove now-redundant user info from one_time_tokens_controller response - the JWT has all this info in it already

---
+++ app/controllers/users/organisations_controller.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class Users::OrganisationsController < ApplicationController
+  skip_before_action :set_selected_organisation
+  skip_after_action :verify_policy_scoped
+
+  before_action :redirect_to_dashboard_if_cis2_is_enabled
+  before_action :set_organisations
+
+  layout "two_thirds"
+
+  def new
+  end
+
+  def create
+    organisation = current_user.organisations.find(params[:organisation_id])
+
+    if organisation.present?
+      session["cis2_info"] = {
+        "selected_org" => {
+          "name" => organisation.name,
+          "code" => organisation.ods_code
+        },
+        "selected_role" => {
+          "code" => valid_cis2_roles.first,
+          "workgroups" => ["schoolagedimmunisations"]
+        }
+      }
+
+      redirect_after_choosing_org
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def redirect_to_dashboard_if_cis2_is_enabled
+    redirect_to dashboard_path if Settings.cis2.enabled
+  end
+
+  def set_organisations
+    @organisations = current_user.organisations
+  end
+end
