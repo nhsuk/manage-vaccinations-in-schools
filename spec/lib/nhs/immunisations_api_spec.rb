3583ab248d8c05408143d52f151f377a8de4049d
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-07-02 17:10:39 +0100

    Simplify error handling in imms api
    Jira-Issue: MAV-1477

--- spec/lib/nhs/immunisations_api_spec.rb
+++ spec/lib/nhs/immunisations_api_spec.rb
@@ -75,113 +75,66 @@ describe NHS::ImmunisationsAPI do
     end
 
     context "an error is returned by the api" do
-      let(:response) do
-        { issue: [{ severity: "error", code:, diagnostics: }] }.to_json
-      end
+      context "4XX error" do
+        let(:response) do
+          {
+            resourceType: "OperationOutcome",
+            id: "bc2c3c82-4392-4314-9d6b-a7345f82d923",
+            meta: {
+              profile: [
+                "https://simplifier.net/guide/UKCoreDevelopment2/ProfileUKCore-OperationOutcome"
+              ]
+            },
+            issue: [
+              {
+                severity: "error",
+                code: "invalid",
+                details: {
+                  coding: [
+                    {
+                      system: "https://fhir.nhs.uk/Codesystem/http-error-codes",
+                      code: "NOT-FOUND"
+                    }
+                  ]
+                },
+                diagnostics: "Invalid patient ID"
+              }
+            ]
+          }.to_json
+        end
 
-      before do
-        stub_request(
-          :post,
-          "https://sandbox.api.service.nhs.uk/immunisation-fhir-api/FHIR/R4/Immunization"
-        ).to_return(status: 400, body: response, headers: {})
+        before do
+          stub_request(
+            :post,
+            "https://sandbox.api.service.nhs.uk/immunisation-fhir-api/FHIR/R4/Immunization"
+          ).to_return(status: 404, body: response, headers: {})
+        end
 
-        allow(Rails.logger).to receive(:error).and_return(true)
+        it "raises an error with the diagnostic message" do
+          expect {
+            described_class.record_immunisation(vaccination_record)
+          }.to raise_error(
+            StandardError,
+            "Error syncing vaccination #{vaccination_record.id} record to" \
+              " Immunisations API: Invalid patient ID"
+          )
+        end
       end
 
       context "generic error" do
-        let(:code) { "invalid" }
-        let(:diagnostics) { "Invalid patient ID" }
+        before do
+          stub_request(
+            :post,
+            "https://sandbox.api.service.nhs.uk/immunisation-fhir-api/FHIR/R4/Immunization"
+          ).to_return(status: 500, body: nil, headers: {})
+        end
 
-        it "raises an error with the correct message" do
-          begin
+        it "raises an error with the diagnostic message" do
+          expect {
             described_class.record_immunisation(vaccination_record)
-          rescue StandardError
-            nil
-          end
-
-          expect(Rails.logger).to have_received(:error).with(
-            /\[invalid\] Invalid patient ID/
-          )
+          }.to raise_error(Faraday::Error)
         end
       end
-
-      context "the error is invalid NHS number" do
-        let(:code) { "exception" }
-        let(:diagnostics) do
-          "NHS Number: 1234567890 is invalid or it doesn't exist"
-        end
-
-        it "raises an error with the correct message" do
-          begin
-            described_class.record_immunisation(vaccination_record)
-          rescue StandardError
-            nil
-          end
-
-          expect(Rails.logger).to have_received(:error).with(
-            /\[exception\] NHS Number is invalid or it doesn't exist/
-          )
-        end
-      end
-    end
-  end
-
-  describe "extract_error_info" do
-    subject(:error_info) { described_class.extract_error_info(response) }
-
-    context "response body has an error" do
-      let(:response) do
-        {
-          issue: [
-            {
-              severity: "error",
-              code: "invalid",
-              diagnostics: "Invalid patient ID"
-            }
-          ]
-        }.to_json
-      end
-
-      its([:code]) { should eq "invalid" }
-      its([:diagnostics]) { should eq "Invalid patient ID" }
-    end
-
-    context "when the response body is empty" do
-      let(:response) { nil }
-
-      its([:code]) { should be_nil }
-      its([:diagnostics]) { should eq "No response body" }
-    end
-
-    context "when the response body has no issue attribute" do
-      let(:response) { "{}" }
-
-      its([:code]) { should be_nil }
-      its([:diagnostics]) { should eq "No response body" }
-    end
-
-    context "when the response body has no issues" do
-      let(:response) { '{"issues": [] }' }
-
-      its([:code]) { should be_nil }
-      its([:diagnostics]) { should eq "No issues in response" }
-    end
-
-    context "the issue severity is not 'error'" do
-      let(:response) do
-        {
-          issue: [
-            {
-              severity: "warning",
-              code: "not-found",
-              diagnostics: "Patient not found"
-            }
-          ]
-        }.to_json
-      end
-
-      its([:code]) { should be_nil }
-      its([:diagnostics]) { should eq "Issue is not an error" }
     end
   end
 end
