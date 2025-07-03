6557283e63b10588d0a08703fb21abcb89700f3a
Brage Gording <brage.gording@tngtech.com>
2025-07-03 14:28:02 +0100

    Add mavis-application dashboard
    - Also refactor tf setup to automatically apply all files in folder
    - Standardize naming <application_name>-<scope>
    - For now use hard-coded values for data-source uids, this will
      potentially be refactored in the future

--- terraform/monitoring/grafana/main.tf
+++ terraform/monitoring/grafana/main.tf
@@ -19,13 +19,3 @@ provider "grafana" {
   url  = var.workspace_url
   auth = var.service_account_token
 }
-
-resource "grafana_data_source" "cloudwatch" {
-  name = "CloudWatch"
-  type = "cloudwatch"
-  json_data_encoded = jsonencode({
-    authType      = "default"
-    defaultRegion = var.region
-  })
-}
-
