64751e36c77b270ce66b913f431f33728658fe4b
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-07-14 00:20:20 +0100

    Update vaccination record to nhs immunisations api
    Jira-Issue: MAV-1012

--- spec/jobs/sync_vaccination_record_to_nhs_job_spec.rb
+++ spec/jobs/sync_vaccination_record_to_nhs_job_spec.rb
@@ -3,14 +3,14 @@
 describe SyncVaccinationRecordToNHSJob, type: :job do
   subject(:perform_now) { described_class.perform_now(vaccination_record) }
 
-  before { allow(NHS::ImmunisationsAPI).to receive(:record_immunisation) }
+  before do
+    allow(NHS::ImmunisationsAPI).to receive(:record_immunisation)
+    allow(NHS::ImmunisationsAPI).to receive(:update_immunisation)
+  end
 
+  let(:nhs_immunisations_api_synced_at) { nil }
   let(:vaccination_record) do
-    instance_double(
-      VaccinationRecord,
-      id: "123",
-      nhs_immunisations_api_synced_at: nil
-    )
+    create(:vaccination_record, id: 123, nhs_immunisations_api_synced_at:)
   end
 
   it "sends the vaccination record to the NHS Immunisations API" do
@@ -21,14 +21,20 @@ describe SyncVaccinationRecordToNHSJob, type: :job do
     )
   end
 
-  context "when the vaccination record has already been synced" do
-    let(:vaccination_record) do
-      instance_double(
-        VaccinationRecord,
-        id: "123",
-        nhs_immunisations_api_synced_at: Time.current
+  context "when the vaccination record has been synced before" do
+    let(:nhs_immunisations_api_synced_at) { 1.second.ago }
+
+    it "updates the vaccination record with the NHS Immunisations API" do
+      perform_now
+
+      expect(NHS::ImmunisationsAPI).to have_received(:update_immunisation).with(
+        vaccination_record
       )
     end
+  end
+
+  context "when the vaccination record is already in-sync" do
+    let(:nhs_immunisations_api_synced_at) { 1.second.from_now }
 
     it "does not send the vaccination record to the NHS Immunisations API" do
       perform_now
@@ -44,6 +50,54 @@ describe SyncVaccinationRecordToNHSJob, type: :job do
       expect(Rails.logger).to have_received(:info).with(
         "Vaccination record already synced: 123"
       )
+    end
+  end
+
+  context "when the vaccination record has been discarded" do
+    let(:vaccination_record) do
+      create(:vaccination_record, :discarded, id: 123)
+    end
+
+    it "does not send the vaccination record to the NHS Immunisations API" do
+      begin
+        perform_now
+      rescue StandardError
+        nil
+      end
+
+      expect(NHS::ImmunisationsAPI).not_to have_received(:record_immunisation)
+    end
+
+    it "raises an error" do
+      expect { perform_now }.to raise_error(
+        "Vaccination record is discarded: #{vaccination_record.id}"
+      )
+    end
+  end
+
+  VaccinationRecord.defined_enums["outcome"].each_key do |outcome|
+    next if outcome == "administered"
+
+    context "when the vaccination record outcome is #{outcome}" do
+      let(:vaccination_record) do
+        create(:vaccination_record, id: 123, outcome:)
+      end
+
+      it "does not send the vaccination record to the NHS Immunisations API" do
+        begin
+          perform_now
+        rescue StandardError
+          nil
+        end
+
+        expect(NHS::ImmunisationsAPI).not_to have_received(:record_immunisation)
+      end
+
+      it "raises an error" do
+        expect { perform_now }.to raise_error(
+          "Vaccination record is not administered: #{vaccination_record.id}"
+        )
+      end
     end
   end
 end
