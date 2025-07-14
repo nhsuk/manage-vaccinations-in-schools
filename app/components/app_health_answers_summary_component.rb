ec05c4a630547d4f7e4f4a168d524e0d974af753
Thomas Leese <thomas@leese.io>
2025-07-14 22:53:25 +0100

    Add AppHealthAnswersSummaryComponent
    This extracts the logic related to rendering a summary list of health
    answers from the `AppHealthAnswersCardComponent` in to this new
    component so it can be used outside the context of a card.
    
    Jira-Issue: MAV-1474

---
+++ app/components/app_health_answers_summary_component.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class AppHealthAnswersSummaryComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <dl class="nhsuk-summary-list app-summary-list--full-width">
+      <% health_answers.each do |question, answers| %>
+        <div class="nhsuk-summary-list__row">
+          <dt class="nhsuk-summary-list__key">
+            <%= question %>
+          </dt>
+          <dd class="nhsuk-summary-list__value">
+            <% answers.each do |answer| %>
+              <p>
+                <%= answer[:responder] %> responded: <%= answer[:answer].humanize %><% if answer[:notes].present? %>:<% end %>
+              </p>
+              <% if answer[:notes].present? %>
+                <blockquote><%= answer[:notes] %></blockquote>
+              <% end %>
+            <% end %>
+          </dd>
+        </div>
+      <% end %>
+    </dl>
+  ERB
+
+  def initialize(objects)
+    super
+
+    @objects = objects.is_a?(Array) ? objects : [objects]
+  end
+
+  private
+
+  attr_reader :objects
+
+  def health_answers
+    ConsolidatedHealthAnswers.new(objects).to_h
+  end
+end
