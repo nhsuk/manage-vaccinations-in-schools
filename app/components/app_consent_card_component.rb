db255545a5f2421b7b1b21316143d17b227ee6c9
Thomas Leese <thomas@leese.io>
2025-07-15 08:51:41 +0100

    Add AppConsentCardComponent
    This adds a new component which renders a small card containing
    information about a consent response. This will be used in the new
    designs for the consent responses on the patient session page.
    
    Jira-Issue: MAV-1474

---
+++ app/components/app_consent_card_component.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+class AppConsentCardComponent < ViewComponent::Base
+  def initialize(consent, session:)
+    super
+
+    @consent = consent
+    @session = session
+  end
+
+  def call
+    render AppCardComponent.new(**card_options) do |card|
+      card.with_heading { heading }
+      govuk_summary_list(rows:)
+    end
+  end
+
+  private
+
+  attr_reader :consent, :session
+
+  delegate :patient, :programme, to: :consent
+
+  def link_to
+    session_patient_programme_consent_path(session, patient, programme, consent)
+  end
+
+  def card_options = { link_to:, colour: "offset", compact: true }
+
+  def heading
+    if consent.via_self_consent?
+      consent.who_responded
+    else
+      "#{consent.name} (#{consent.who_responded})"
+    end
+  end
+
+  def rows
+    [
+      if (phone = consent.parent&.phone).present?
+        { key: { text: "Phone number" }, value: { text: phone } }
+      end,
+      if (email = consent.parent&.email).present?
+        { key: { text: "Email address" }, value: { text: email } }
+      end,
+      {
+        key: {
+          text: "Date"
+        },
+        value: {
+          text: consent.responded_at.to_fs(:long)
+        }
+      },
+      {
+        key: {
+          text: "Decision"
+        },
+        value: {
+          text: helpers.consent_status_tag(consent)
+        }
+      }
+    ].compact
+  end
+end
---
+++ spec/components/app_consent_card_component_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+describe AppConsentCardComponent do
+  subject { render_inline(component) }
+
+  let(:component) { described_class.new(consent, session:) }
+
+  let(:programme) { create(:programme) }
+  let(:organisation) { create(:organisation, programmes: [programme]) }
+
+  let(:consent) do
+    create(
+      :consent,
+      patient:,
+      parent:,
+      programme:,
+      organisation:,
+      submitted_at: Time.zone.local(2024, 1, 1)
+    )
+  end
+  let(:school) { create(:school, name: "Waterloo Road", organisation:) }
+  let(:session) do
+    create(:session, programmes: [programme], organisation:, location: school)
+  end
+  let(:parent) { create(:parent) }
+  let(:patient) { create(:patient) }
+
+  it { should have_content(parent.full_name) }
+
+  it { should have_content("Phone number") }
+  it { should have_content(parent.phone) }
+
+  it { should have_content("Email address") }
+  it { should have_content(parent.email) }
+
+  it { should have_content("Date") }
+  it { should have_content("1 January 2024 at 12:00am") }
+
+  it { should have_content("Decision") }
+  it { should have_content("Consent given") }
+end
