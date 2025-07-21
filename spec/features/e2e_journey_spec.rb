d91268cb7111cb4685e2d4df23f1f882c69c599e
Thomas Leese <thomas@leese.io>
2025-07-21 21:19:24 +0100

    Add comma to long day of week date format
    This adds a comma between the day of the week and the date to match the
    latest designs in the prototype.

--- spec/features/e2e_journey_spec.rb
+++ spec/features/e2e_journey_spec.rb
@@ -150,14 +150,14 @@ describe "End-to-end journey" do
     expect(page).to have_content("Edit session")
 
     expect(page).to have_content("ProgrammesHPV")
-    expect(page).to have_content("Session datesFriday 1 March 2024")
+    expect(page).to have_content("Session datesFriday, 1 March 2024")
     expect(page).to have_content(
-      "Consent requestsSend on Friday 9 February 2024"
+      "Consent requestsSend on Friday, 9 February 2024"
     )
     expect(page).to have_content(
       "Consent remindersSend 1 week before each session"
     )
-    expect(page).to have_content("Next: Friday 23 February 2024")
+    expect(page).to have_content("Next: Friday, 23 February 2024")
 
     click_on "Continue"
   end
