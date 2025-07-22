3bdf16a5f0e63d893d17fb9dce0b80f3803b561a
Thomas Leese <thomas@leese.io>
2025-07-22 15:01:16 +0100

    Add academic_year to SessionSearchForm
    This allows us to filter sessions by their academic year which is a
    feature of the new designs.
    
    Jira-Issue: MAV-1508

--- app/forms/session_search_form.rb
+++ app/forms/session_search_form.rb
@@ -1,16 +1,23 @@
 # frozen_string_literal: true
 
 class SessionSearchForm < SearchForm
+  attribute :academic_year, :integer
   attribute :programmes, array: true
   attribute :q, :string
   attribute :status, :string
   attribute :type, :string
+
+  def academic_year
+    super || AcademicYear.current
+  end
 
   def programmes=(values)
     super(values&.compact_blank || [])
   end
 
   def apply(scope)
+    scope = scope.where(academic_year:)
+
     scope =
       scope.has_programmes(
         Programme.where(type: programmes)
@@ -24,7 +31,6 @@ class SessionSearchForm < SearchForm
 
     scope.sort
   end
-
 
   VALID_STATUS_SCOPES = %w[in_progress unscheduled scheduled completed].freeze
 end
