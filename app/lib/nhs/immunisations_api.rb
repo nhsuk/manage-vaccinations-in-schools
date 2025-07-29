e26dde67108dc4bc3029089f556f174684780607
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-07-29 12:31:42 +0100

    Edit status logic, add new help text
    If a child has requested that their parents aren't notified of the vaccination event, then this shouldn't be synced to the API. This also requires a new help text under when the vaccination record is viewed
    
    Also, use this logic in the vaccination mailer to decide whether a patient's parents should be notified

--- app/lib/nhs/immunisations_api.rb
+++ app/lib/nhs/immunisations_api.rb
@@ -173,7 +173,8 @@ module NHS::ImmunisationsAPI
       vaccination_record.kept? && vaccination_record.recorded_in_service? &&
         vaccination_record.administered? &&
         vaccination_record.programme.type.in?(PROGRAMME_TYPES) &&
-        (ignore_nhs_number || vaccination_record.patient.nhs_number.present?)
+        (ignore_nhs_number || vaccination_record.patient.nhs_number.present?) &&
+        vaccination_record.notify_parents
     end
 
     private
