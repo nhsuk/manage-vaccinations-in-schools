cf3c6715e632b7413c76ee9630b7acbb7642af1b
Martin van IJcken <martin.vanijcken@tngtech.com>
2025-08-07 14:12:58 +0100

    feat: show warning on consent page for children without nhs number

---
+++ app/components/app_session_needs_review_warning_component.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class AppSessionNeedsReviewWarningComponent < ViewComponent::Base
+  def call
+    render AppWarningCalloutComponent.new(heading: "Needs review") do
+      tag.ul do
+        safe_join(
+          warning_counts.filter_map do |warning, _c|
+            tag.li { make_row_from_warning(warning) }
+          end
+        )
+      end
+    end
+  end
+
+  def initialize(session:)
+    super
+    @session = session
+  end
+
+  def render?
+    warning_counts.values.any?(&:positive?)
+  end
+
+  private
+
+  def warning_href
+    {
+      children_without_nhs_number:
+        session_consent_path(@session, missing_nhs_number: true)
+    }
+  end
+
+  def warning_counts
+    @warning_counts ||= {
+      children_without_nhs_number:
+        patient_sessions.merge(Patient.without_nhs_number).count
+    }
+  end
+
+  def make_row_from_warning(warning)
+    return if warning_counts[warning].zero?
+    link_to(t(warning, count: warning_counts[warning]), warning_href[warning])
+  end
+
+  def patient_sessions
+    @session
+      .patient_sessions
+      .joins(:patient, :session)
+      .appear_in_programmes(@session.programmes)
+  end
+end
