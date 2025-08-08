95a175e995ae66f7e9b34f4ffc93460228acc5b4
John Henderson <john@logixworks.co.uk>
2025-08-08 10:27:02 +0100

    Show eligible programmes on patient page
    This adds a new section to the patient page showing which
    programmes the patient is eligible for at which time and
    their vaccination status if it exists.
    
    Jira-Issue: MAV-1516

---
+++ spec/components/app_patient_programmes_table_component_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+describe AppPatientProgrammesTableComponent do
+  subject(:rendered_component) { render_inline(component) }
+
+  let(:component) { described_class.new(patient, programmes:) }
+  let(:team) { create(:team, programmes:) }
+  let(:session) { create(:session, team:, programmes:) }
+
+  let(:today) { Date.new(2025, 9, 1) }
+
+  around { |example| travel_to(today) { example.run } }
+
+  context "for seasonal programmes" do
+    let(:patient) { create(:patient, session:) }
+    let(:programmes) { create_list(:programme, 1, :flu) }
+
+    it { should have_content("Vaccination programmes") }
+    it { should have_content("Flu (Winter 2025)") }
+    it { should_not have_content("Vaccinated") }
+    it { should have_content("Selected for the Year 2025 to 2026 Flu cohort") }
+
+    context "when vaccinated" do
+      let(:patient) { create(:patient, :vaccinated, session:) }
+
+      it { should have_content("Vaccinated") }
+    end
+
+    context "when vaccinated last year" do
+      let(:patient) { create(:patient, session:) }
+
+      before do
+        create(
+          :vaccination_record,
+          patient:,
+          programme: programmes.first,
+          performed_at: Time.zone.local(2024, 9, 1)
+        )
+        StatusUpdater.call(patient:)
+      end
+
+      it { should_not have_content("Vaccinated") }
+    end
+  end
+
+  context "for non-seasonal programmes" do
+    let(:patient) { create(:patient, session:, year_group: 8) }
+    let(:programmes) do
+      [
+        create(:programme, :hpv),
+        create(:programme, :menacwy),
+        create(:programme, :td_ipv)
+      ]
+    end
+
+    it { should have_content("Vaccination programmes") }
+    it { should have_content("HPV") }
+    it { should have_content("Td/IPV") }
+    it { should have_content("MenACWY") }
+
+    it do
+      expect(rendered_component).to have_content(
+        "Selected for the Year 2025 to 2026 HPV cohort"
+      ).once
+    end
+
+    it { should have_content("Eligibility starts 1 September 2026").twice }
+
+    context "when vaccinated" do
+      let(:patient) { create(:patient, :vaccinated, session:) }
+
+      it { should have_content("Vaccinated") }
+    end
+
+    context "when vaccinated last year" do
+      let(:patient) { create(:patient, session:) }
+
+      before do
+        create(
+          :vaccination_record,
+          patient:,
+          programme: programmes.first,
+          performed_at: Time.zone.local(2024, 9, 1)
+        )
+        StatusUpdater.call(patient:)
+      end
+
+      it { should have_content("Vaccinated") }
+    end
+  end
+end
