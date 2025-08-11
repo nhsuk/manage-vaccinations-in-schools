4792d2ccfd15c9b817eb1a863972ecb8cfed2f79
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-08-11 11:17:26 +0100

    Make rate limiting configurable
    This is necessary as we'll be adding rate limiting for another NHS API
    service, the Immunisations API, and it has a different rate limit than
    PDS.
    
    Jira-Issue: MAV-1739

--- app/jobs/patient_nhs_number_lookup_job.rb
+++ app/jobs/patient_nhs_number_lookup_job.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class PatientNHSNumberLookupJob < ApplicationJob
+  def self.concurrent_jobs_per_second = 5
+  def self.concurrency_key = :pds
+
   include NHSAPIConcurrencyConcern
 
   queue_as :imports
