efda1c17fe44f7573f77c92ab09e359bca9fc85c
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-08-12 15:38:19 +0100

    Add models for cohort and class import join tables
    These are used to simplify the import code so that they don't have to
    construct a new class on the fly to represent the join table, in order
    to simplify the import-related code.

---
+++ app/models/class_imports_parent.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: class_imports_parents
+#
+#  class_import_id :bigint           not null
+#  parent_id       :bigint           not null
+#
+# Indexes
+#
+#  index_class_imports_parents_on_class_import_id_and_parent_id  (class_import_id,parent_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (class_import_id => class_imports.id)
+#  fk_rails_...  (parent_id => parents.id)
+#
+class ClassImportsParent < ApplicationRecord
+  belongs_to :class_import
+  belongs_to :parent
+end
