87d43c14b5ae0952e6d789c9c641348450b1aa2c
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-08-14 17:22:17 +0100

    Remove invalid nhs number test from extravaganza
    This was testing a very narrow edge-case which we will probably
    shut-down in a following PR.
    
    Specifically, when we find a patient using
    their NHS number with PDS, and it is invalid, this was testing that the
    import would invalidate the patient. However this is unlikely to happen
    since the patient would have already been invalidated as a result of the
    daily PDS update job. Additionally, in the future we'll remove the
    find-by-nhs-number lookup as part of the new PDS lookup flows.
    
    Jira-Issue: MAV-1390

--- spec/features/import_child_pds_lookup_extravaganza_spec.rb
+++ spec/features/import_child_pds_lookup_extravaganza_spec.rb
@@ -9,17 +9,11 @@ describe "Import child records" do
 
     when_i_visit_the_import_page
     and_i_upload_the_pds_extravaganza_import_file
-    and_i_wait_for_the_import_to_complete
     then_i_should_see_the_import_page
     and_i_should_see_the_existing_patient_as_imported
 
     when_i_click_on_the_invalidated_patient
     then_i_should_see_invalidated_patient_record
-
-    and_i_go_back_to_the_import_page
-
-    when_i_click_on_the_invalid_nhs_number_patient
-    then_i_should_see_invalid_nhs_number_patient_record
   end
 
   def given_i_am_signed_in
@@ -72,21 +66,6 @@ describe "Import child records" do
         school: nil, # Unknown school, should be silently updated
         session: @session
       )
-    @invalid_nhs_number_patient =
-      create(
-        :patient,
-        given_name: "James",
-        family_name: "Smith",
-        nhs_number: nil, # Second row of valid.csv
-        date_of_birth: Date.new(2010, 1, 2),
-        gender_code: :male,
-        address_line_1: "10 Downing Street",
-        address_line_2: "",
-        address_town: "London",
-        address_postcode: "SW11 1AA",
-        school: @school,
-        session: @session
-      )
   end
 
   def and_pds_lookup_during_import_is_enabled
@@ -103,17 +82,6 @@ describe "Import child records" do
 
     stub_pds_get_nhs_number_to_return_an_invalidated_patient(
       @invalidated_patient.nhs_number
-    )
-
-    stub_pds_get_nhs_number_to_return_an_invalid_nhs_number_response(
-      "9990056846"
-    )
-
-    stub_pds_search_to_return_no_patients(
-      "family" => @invalid_nhs_number_patient.family_name,
-      "given" => @invalid_nhs_number_patient.given_name,
-      "birthdate" => "eq#{@invalid_nhs_number_patient.date_of_birth}",
-      "address-postalcode" => @invalid_nhs_number_patient.address_postcode
     )
   end
 
@@ -133,18 +101,6 @@ describe "Import child records" do
     click_on "Continue"
   end
 
-  def and_i_wait_for_the_import_to_complete
-    perform_enqueued_jobs
-    perform_enqueued_jobs
-    perform_enqueued_jobs
-    perform_enqueued_jobs
-  end
-
-  def and_i_go_back_to_the_import_page
-    click_on "Import"
-    click_on Date.current.to_fs(:long)
-  end
-
   def then_i_should_see_the_import_page
     expect(page).to have_content("Imported on")
     expect(page).to have_content("Imported byUSER, Test")
@@ -158,22 +114,11 @@ describe "Import child records" do
     click_link "SAMSON, Betty"
   end
 
-  def when_i_click_on_the_invalid_nhs_number_patient
-    click_link "SMITH, James"
-  end
-
   def then_i_should_see_invalidated_patient_record
     expect(page).to have_content("Record flagged as invalid")
     expect(page).to have_content("SAMSON, Betty")
     expect(page).to have_content("NHS number999 352 4689")
     expect(page).to have_content("Date of birth1 January 2010 (aged 15)")
-    expect(page).to have_content("SW11 1AA")
-  end
-
-  def then_i_should_see_invalid_nhs_number_patient_record
-    expect(page).to have_content("SMITH, James")
-    expect(page).to have_content("NHS numberNot provided")
-    expect(page).to have_content("Date of birth2 January 2010 (aged 15)")
     expect(page).to have_content("SW11 1AA")
   end
 end
