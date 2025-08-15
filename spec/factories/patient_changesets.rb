027a51e1748d4e4921cda76962f960c2e54154fd
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2025-08-15 21:30:07 +0100

    Add tests for ProcessPatientchangesetJob
    Introduces a complete test suite for `ProcessPatientChangesetsJob` to verify the cascading search flow, job enquieing process and NHS number retrieval based on various search results.

--- spec/factories/patient_changesets.rb
+++ spec/factories/patient_changesets.rb
@@ -28,8 +28,8 @@
 #
 FactoryBot.define do
   factory :patient_changeset do
-    pending_changes { {} }
-    row_number { 0 }
+    row_number { 1 }
+    status { :pending }
 
     trait :class_import do
       import { association(:class_import) }
@@ -37,6 +37,39 @@ FactoryBot.define do
 
     trait :cohort_import do
       import { association(:cohort_import) }
+    end
+
+    pending_changes do
+      {
+        child: {
+          "given_name" => "John",
+          "family_name" => "Doe",
+          "date_of_birth" => "2010-01-01",
+          "address_postcode" => "SW1A 1AA",
+          "nhs_number" => nil
+        },
+        parent_1: {
+        },
+        parent_2: {
+        },
+        pds: {
+        },
+        search_results: []
+      }
+    end
+
+    after(:build) do |changeset|
+      changeset.import_type = changeset.import.class.name
+    end
+
+    trait :with_nhs_number do
+      after(:build) do |changeset|
+        changeset.pending_changes["child"]["nhs_number"] = "1234567890"
+      end
+    end
+
+    trait :processed do
+      status { :processed }
     end
   end
 end
