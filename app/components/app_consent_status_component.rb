864057b17cfe4c714b451ceaaa3f1b78e42190cc
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-08-15 15:58:40 +0100

    Update inline SVG icon paths

--- app/components/app_consent_status_component.rb
+++ app/components/app_consent_status_component.rb
@@ -32,15 +32,8 @@ class AppConsentStatusComponent < ViewComponent::Base
   def icon_tick(content, color)
     template = <<-ERB
       <p class="app-status app-status--#{color}">
-        <svg class="nhsuk-icon nhsuk-icon__tick"
-             xmlns="http://www.w3.org/2000/svg"
-             viewBox="0 0 24 24"
-             aria-hidden="true">
-          <path d="M18.4 7.8l-8.5 8.4L5.6 12"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="4"
-                stroke-linecap="round"></path>
+        <svg class="nhsuk-icon nhsuk-icon--tick" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" focusable="false" aria-hidden="true">
+          <path d="M11.4 18.8a2 2 0 0 1-2.7.1h-.1L4 14.1a1.5 1.5 0 0 1 2.1-2L10 16l8.1-8.1a1.5 1.5 0 1 1 2.2 2l-8.9 9Z"/>
         </svg>
         #{content}
       </p>
@@ -51,15 +44,8 @@ class AppConsentStatusComponent < ViewComponent::Base
   def icon_cross(content, color)
     template = <<-ERB
       <p class="app-status app-status--#{color}">
-        <svg class="nhsuk-icon nhsuk-icon__cross"
-             xmlns="http://www.w3.org/2000/svg"
-             viewBox="0 0 24 24"
-             aria-hidden="true">
-          <path d="M18.6 6.5c.5.5.5 1.5 0 2l-4 4 4 4c.5.6.5 1.4 0 2-.4.4-.7.4-1
-                  .4-.5 0-.9 0-1.2-.3l-3.9-4-4 4c-.3.3-.5.3-1
-                  .3a1.5 1.5 0 0 1-1-2.4l3.9-4-4-4c-.5-.5-.5-1.4 0-2
-                  .6-.7 1.5-.7 2.2 0l3.9 3.9 4-4c.6-.6 1.4-.6 2 0Z"
-                fill="currentColor"></path>
+        <svg class="nhsuk-icon nhsuk-icon--cross" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" focusable="false" aria-hidden="true">
+          <path d="M17 18.5c-.4 0-.8-.1-1.1-.4l-10-10c-.6-.6-.6-1.6 0-2.1.6-.6 1.5-.6 2.1 0l10 10c.6.6.6 1.5 0 2.1-.3.3-.6.4-1 .4z M7 18.5c-.4 0-.8-.1-1.1-.4-.6-.6-.6-1.5 0-2.1l10-10c.6-.6 1.5-.6 2.1 0 .6.6.6 1.5 0 2.1l-10 10c-.3.3-.6.4-1 .4z"/>
         </svg>
         #{content}
       </p>
