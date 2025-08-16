d5bce2f8dbf9b2314c8cf3ed35644fb997884f2f
Thomas Leese <thomas@leese.io>
2025-08-16 12:28:48 +0100

    Add command to create schools
    This adds a command that makes it possible to create a school. This is
    needed because some schools will not be in the DfE data, specificially
    schools which use the same URN as another but a different site code.
    
    Jira-Issue: MAV-1384

---
+++ app/lib/mavis_cli/schools/create.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+module MavisCLI
+  module Schools
+    class Create < Dry::CLI::Command
+      desc "Create a new school"
+
+      argument :urn, required: true, desc: "School URN"
+      argument :name, required: true, desc: "Name of the school"
+      argument :gias_establishment_number,
+               required: true,
+               desc: "GIAS establishment number"
+      argument :gias_local_authority_code,
+               required: true,
+               desc: "GIAS local authority code"
+
+      option :site, desc: "Additional site "
+      option :status, desc: "Status of the school", default: "open"
+      option :url, desc: "URL of the school"
+
+      option :address_line_1, desc: "First line of address"
+      option :address_line_2, desc: "Second line of address"
+      option :address_town, desc: "Town of the address"
+      option :address_postcode, desc: "Postcode of the address"
+
+      option :year_groups,
+             type: :array,
+             desc: "Year groups taught at the school"
+
+      def call(
+        urn:,
+        name:,
+        gias_establishment_number:,
+        gias_local_authority_code:,
+        url: nil,
+        status: "open",
+        address_line_1: nil,
+        address_line_2: nil,
+        address_town: nil,
+        address_postcode: nil,
+        year_groups: [],
+        site: nil
+      )
+        MavisCLI.load_rails
+
+        location =
+          Location.create!(
+            address_line_1:,
+            address_line_2:,
+            address_postcode:,
+            address_town:,
+            gias_establishment_number:,
+            gias_local_authority_code:,
+            name:,
+            site:,
+            status:,
+            type: "school",
+            url:,
+            urn:,
+            year_groups:
+          )
+
+        puts "Location #{location.id} has been created."
+      end
+    end
+  end
+
+  register "schools" do |prefix|
+    prefix.register "create", Schools::Create
+  end
+end
