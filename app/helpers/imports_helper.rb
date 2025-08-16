2a46bbf6d9dc5cc088bf418e550db911b3c11859
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2025-08-16 22:17:55 +0100

    Import issues text based on NHS number match
    If a match is found based on NHS number, the text shown to users on the main
    import page will be different.

--- app/helpers/imports_helper.rb
+++ app/helpers/imports_helper.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 module ImportsHelper
+  FIELD_GROUPS = {
+    %w[address_line_1 address_line_2 address_town address_postcode] => :address,
+    %w[date_of_birth] => :date_of_birth,
+    %w[nhs_number] => :nhs_number,
+    %w[gender_code] => :gender,
+    %w[given_name family_name preferred_given_name preferred_family_name] =>
+      :name,
+    %w[birth_academic_year] => :year_group,
+    %w[registration] => :registration
+  }.freeze
+
   def import_issues_count
     vaccination_records_with_issues =
       policy_scope(VaccinationRecord).with_pending_changes.distinct
@@ -13,5 +24,11 @@ module ImportsHelper
       end
 
     unique_import_issues.count
+  end
+
+  def issue_categories_for(pending_changes)
+    FIELD_GROUPS.filter_map do |(keys, group)|
+      group.to_s.humanize if (pending_changes & keys).any?
+    end
   end
 end
