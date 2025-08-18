1ef2222b5e4bfd4368b2a5ff4c9464f3e124ce38
Martin van IJcken <martin.vanijcken@tngtech.com>
2025-08-18 11:50:45 +0100

    design: follow design guidance for conflict with deleted vaccination record
    Finalize duplicate record page

--- spec/features/import_vaccination_records_with_duplicates_spec.rb
+++ spec/features/import_vaccination_records_with_duplicates_spec.rb
@@ -158,11 +158,11 @@ describe "Immunisation imports duplicates" do
   end
 
   def when_i_choose_to_keep_the_duplicate_record
-    choose "Use duplicate record"
+    choose "Use uploaded vaccination record"
   end
 
   def when_i_choose_to_keep_the_previously_uploaded_record
-    choose "Keep previously uploaded record"
+    choose "Keep existing vaccination record"
   end
 
   def when_i_submit_the_form_without_choosing_anything
@@ -180,7 +180,6 @@ describe "Immunisation imports duplicates" do
   end
 
   def then_i_should_see_the_first_duplicate_record
-    expect(page).to have_content("This record needs reviewing")
     expect(page).to have_content("Batch ID123013325")
     expect(page).to have_content("Batch IDSomethingElse")
     expect(page).to have_content("MethodIntramuscular")
@@ -190,7 +189,6 @@ describe "Immunisation imports duplicates" do
   end
 
   def then_i_should_see_the_second_duplicate_record
-    expect(page).to have_content("This record needs reviewing")
     expect(page).to have_content("Batch ID123013325")
     expect(page).to have_content("Batch IDCervarixBatch")
     expect(page).to have_content("MethodNasal spray")
