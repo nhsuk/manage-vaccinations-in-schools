74d147877c8736687ab40b8e351a2a5fdcf75034
Thomas Leese <thomas@leese.io>
2025-08-20 22:00:22 +0100

    Set `location_id` on vaccination records in clinic
    This ensures that when we're recording a vaccination from a community
    clinic session, we link the location of the community clinic or school
    using the `location_id` foreign key rather than storing the name of the
    location in `location_name`. This enables us to have more structured data,
    including access to various other bits of information on the location.
    
    We still need the `location_name` column to handle importing unknown
    locations.
    
    Jira-Issue: MAV-550

--- app/helpers/address_helper.rb
+++ app/helpers/address_helper.rb
@@ -8,10 +8,4 @@ module AddressHelper
   def format_address_single_line(addressable)
     addressable.address_parts.join(", ")
   end
-
-  def format_location_name_and_address_single_line(location)
-    [location.name, format_address_single_line(location)].compact_blank.join(
-      ", "
-    )
-  end
 end
--- spec/helpers/address_helper_spec.rb
+++ spec/helpers/address_helper_spec.rb
@@ -22,26 +22,4 @@ describe AddressHelper do
 
     it { should eq("10 Downing Street, London, SW1A 1AA") }
   end
-
-  describe "#format_location_name_and_address_single_line" do
-    subject(:formatted_string) do
-      helper.format_location_name_and_address_single_line(location)
-    end
-
-    it { should eq("School of Politics, 10 Downing Street, London, SW1A 1AA") }
-
-    context "with a nil address" do
-      let(:location) do
-        create(
-          :school,
-          name: "School of Politics",
-          address_line_1: "",
-          address_town: "",
-          address_postcode: ""
-        )
-      end
-
-      it { should eq("School of Politics") }
-    end
-  end
 end
