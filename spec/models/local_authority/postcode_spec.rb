50db911bc3286836de837b33d05609e53969bebe
Al Davidson <alistair.davidson2@nhs.net>
2025-08-21 10:24:05 +0100

    Update spec/models/local_authority/postcode_spec.rb
    Co-authored-by: Thomas Leese <thomas@leese.io>

--- spec/models/local_authority/postcode_spec.rb
+++ spec/models/local_authority/postcode_spec.rb
@@ -14,7 +14,7 @@
 #  index_local_authority_postcodes_on_gss_code  (gss_code)
 #  index_local_authority_postcodes_on_value     (value) UNIQUE
 #
-describe LocalAuthority::Postcode, type: :model do
+describe LocalAuthority::Postcode do
   it { should normalize(:value).from(" Sw111AA ").to("SW11 1AA") }
   it { should normalize(:value).from(" Sw11.   1aA ").to("SW11 1AA") }
   it { should normalize(:value).from(" ,dH1.  3lh. ").to("DH1 3LH") }
