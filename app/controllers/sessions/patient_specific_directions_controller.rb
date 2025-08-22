398271fd27bbe1c401c44f6cf297dd75c038a23b
John Henderson <john@logixworks.co.uk>
2025-08-22 09:40:28 +0100

    Prescribers can create PSD instructions in bulk
    This PR implements a new feature that allows prescribers (nurses,
    pharmacists) to bulk create Patient Specific Directions (PSDs) for
    patients who have given consent and don't require individual triage
    assessment. This significantly reduces manual work by enabling batch
    processing of PSDs instead of creating them one by one.
    
    What's changed?
    - New (basic) controller and page for the PSDs tab (no filters yet)
      - PSDs tab only appears if delegation feature is enabled
    - New controller to manage the bulk add PSDs

---
+++ app/controllers/sessions/patient_specific_directions_controller.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+class Sessions::PatientSpecificDirectionsController < ApplicationController
+  include PatientSearchFormConcern
+
+  before_action :set_session
+  before_action :set_patient_search_form
+
+  layout "full"
+
+  def show
+    scope =
+      @session.patient_sessions.includes_programmes.includes(
+        patient: {
+          patient_specific_directions: :programme
+        }
+      )
+    @eligible_for_bulk_psd_count = patient_sessions_allowed_psd.count
+    patient_sessions = @form.apply(scope)
+    @pagy, @patient_sessions = pagy(patient_sessions)
+  end
+
+  def create
+    ActiveRecord::Base.transaction do
+      PatientSpecificDirection.import!(
+        psds_to_create,
+        on_duplicate_key_ignore: true
+      )
+    end
+
+    redirect_to session_patient_specific_directions_path(@session),
+                flash: {
+                  success: "PSDs added"
+                }
+  end
+
+  def bulk_add
+    @eligible_for_bulk_psd_count = patient_sessions_allowed_psd.count
+  end
+
+  private
+
+  def set_session
+    @session = policy_scope(Session).find_by!(slug: params[:session_slug])
+  end
+
+  def programme
+    @session.programmes.includes(:vaccines).first
+  end
+
+  def psds_to_create
+    patient_sessions_allowed_psd.map do |patient_session|
+      PatientSpecificDirection.new(
+        academic_year: @session.academic_year,
+        patient_id: patient_session.patient_id,
+        programme_id: programme.id,
+        vaccine_id: programme.vaccines.first.id,
+        created_by_user_id: current_user.id,
+        vaccine_method: :nasal,
+        delivery_site: :nose,
+        full_dose: true
+      )
+    end
+  end
+
+  def patient_sessions_allowed_psd
+    @patient_sessions_allowed_psd ||=
+      @session
+        .patient_sessions
+        .has_consent_status(:given, programme:)
+        .without_patient_specific_direction(programme:)
+  end
+end
