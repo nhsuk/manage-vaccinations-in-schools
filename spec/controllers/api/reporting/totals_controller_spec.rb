c76404f586271023164af8a7cc33eb10ed93efad
Alistair Davidson <apdavidson@gmail.com>
2025-08-22 16:10:56 +0100

    spec fix after rebase

--- spec/controllers/api/reporting/totals_controller_spec.rb
+++ spec/controllers/api/reporting/totals_controller_spec.rb
@@ -11,10 +11,9 @@ RSpec.describe API::Reporting::TotalsController do
       data: {
         user: user.as_json,
         cis2_info: {
-          organisation_name: team.name,
           organisation_code: team.organisation.ods_code,
-          role_code: "S8000:G8000:R8001",
-          workgroups: ["schoolagedimmunisations"],
+          workgroups: [team.workgroup],
+          role_code: CIS2Info::NURSE_ROLE
         }
       }
     }
