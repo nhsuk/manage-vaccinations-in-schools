635243f91755bc346683fe2869dc5b6b2b3953a4
Thomas Leese <thomas@leese.io>
2025-08-22 11:42:27 +0100

    Track which users should be shown in list of suppliers
    Our users log in using CIS2, and it's only at the point of logging in do
    we know what permissions a user has, so at that point we will update the
    this value for them. For non-CIS2 environments, we could in theory rely
    on the `fallback_role` column instead, but I think to make the code
    consistent it makes sense to use same column in both cases.
    
    Jira-Issue: MAV-1353

--- spec/features/user_cis2_authentication_spec.rb
+++ spec/features/user_cis2_authentication_spec.rb
@@ -1,14 +1,37 @@
 # frozen_string_literal: true
 
 describe "User CIS2 authentication", :cis2 do
-  scenario "from start page" do
-    given_a_test_team_is_setup_in_mavis_and_cis2
+  before { given_a_test_team_is_setup_in_mavis_and_cis2 }
+
+  scenario "as an admin" do
+    given_an_admin_user_is_mocked
+
     when_i_go_to_the_start_page
     then_i_should_see_the_cis2_login_button
 
     when_i_click_the_cis2_login_button
-    and_i_am_logged_in
+    and_i_am_logged_in_as_an_admin
     and_i_am_added_to_the_team
+    and_i_am_not_marked_as_a_pgd_supplier
+
+    when_i_click_the_change_role_button
+    then_i_see_the_dashboard
+
+    when_i_log_out
+    then_i_am_on_the_start_page
+    and_i_am_logged_out
+  end
+
+  scenario "as a nurse" do
+    given_a_nurse_user_is_mocked
+
+    when_i_go_to_the_start_page
+    then_i_should_see_the_cis2_login_button
+
+    when_i_click_the_cis2_login_button
+    and_i_am_logged_in_as_a_nurse
+    and_i_am_added_to_the_team
+    and_i_am_marked_as_a_pgd_supplier
 
     when_i_click_the_change_role_button
     then_i_see_the_dashboard
@@ -21,11 +44,25 @@ describe "User CIS2 authentication", :cis2 do
   def given_a_test_team_is_setup_in_mavis_and_cis2
     @user = create(:user, uid: "123")
     @team = create(:team, users: [@user])
+  end
 
+  def given_a_nurse_user_is_mocked
     mock_cis2_auth(
-      uid: "123",
+      uid: @user.uid,
       given_name: "Nurse",
       family_name: "Test",
+      org_code: @team.organisation.ods_code,
+      org_name: @team.name,
+      workgroups: [@team.workgroup]
+    )
+  end
+
+  def given_an_admin_user_is_mocked
+    mock_cis2_auth(
+      uid: @user.uid,
+      given_name: "Admin",
+      family_name: "Test",
+      role_code: CIS2Info::ADMIN_ROLE,
       org_code: @team.organisation.ods_code,
       org_name: @team.name,
       workgroups: [@team.workgroup]
@@ -48,7 +85,12 @@ describe "User CIS2 authentication", :cis2 do
     expect(page).to have_current_path dashboard_path
   end
 
-  def and_i_am_logged_in
+  def and_i_am_logged_in_as_an_admin
+    expect(page).to have_content("TEST, Admin")
+    expect(page).to have_button "Log out"
+  end
+
+  def and_i_am_logged_in_as_a_nurse
     expect(page).to have_content("TEST, Nurse")
     expect(page).to have_button "Log out"
   end
@@ -57,6 +99,14 @@ describe "User CIS2 authentication", :cis2 do
     user = User.first
     expect(user).not_to be_nil
     expect(user.teams).to include(@team)
+  end
+
+  def and_i_am_not_marked_as_a_pgd_supplier
+    expect(@user.reload).not_to be_show_in_suppliers
+  end
+
+  def and_i_am_marked_as_a_pgd_supplier
+    expect(@user.reload).to be_show_in_suppliers
   end
 
   def when_i_click_the_change_role_button
@@ -72,6 +122,7 @@ describe "User CIS2 authentication", :cis2 do
   end
 
   def and_i_am_logged_out
+    expect(page).not_to have_content("TEST, Admin")
     expect(page).not_to have_content("TEST, Nurse")
     expect(page).not_to have_button("Log out")
   end
