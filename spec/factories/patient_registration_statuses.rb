6dbb021e9da497e0a36dd03c30d488bc97bc2b69
Thomas Leese <thomas@leese.io>
2025-08-22 18:20:03 +0100

    Replace registration status patient session foreign key
    This replaces the foreign key association between registration statuses
    and patient sessions to instead link directly to the patient. This is
    needed as we eventually want to replace the PatientSession model and to
    do that we need to make sure all foreign keys to it have been replaced.
    
    The functionality should be the same before and after.
    
    Jira-Issue: MAV-1821

--- spec/factories/patient_registration_statuses.rb
+++ spec/factories/patient_registration_statuses.rb
@@ -4,23 +4,27 @@
 #
 # Table name: patient_registration_statuses
 #
-#  id                 :bigint           not null, primary key
-#  status             :integer          default("unknown"), not null
-#  patient_session_id :bigint           not null
+#  id         :bigint           not null, primary key
+#  status     :integer          default("unknown"), not null
+#  patient_id :bigint           not null
+#  session_id :bigint           not null
 #
 # Indexes
 #
-#  index_patient_registration_statuses_on_patient_session_id  (patient_session_id) UNIQUE
-#  index_patient_registration_statuses_on_status              (status)
+#  idx_on_patient_id_session_id_2ff02d8889            (patient_id,session_id) UNIQUE
+#  index_patient_registration_statuses_on_patient_id  (patient_id)
+#  index_patient_registration_statuses_on_session_id  (session_id)
+#  index_patient_registration_statuses_on_status      (status)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (patient_session_id => patient_sessions.id) ON DELETE => cascade
+#  fk_rails_...  (patient_id => patients.id) ON DELETE => cascade
+#  fk_rails_...  (session_id => sessions.id) ON DELETE => cascade
 #
 FactoryBot.define do
   factory :patient_registration_status, class: "Patient::RegistrationStatus" do
-    patient_session
-
+    patient
+    session
     traits_for_enum :status
   end
 end
