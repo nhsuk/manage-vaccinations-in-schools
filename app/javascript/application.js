f018d92520ac0bd4cf92199820ed498de0d6526c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-08-23 10:27:42 +0100

    Remove service worker
    This was intended for offline recording, but has been disabled prior to the pilot.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
 import "@hotwired/turbo-rails";
-// import { initServiceWorker } from "./serviceworker-companion";
 import "./controllers";
 
 Turbo.session.drive = false;
-
-// initServiceWorker();
