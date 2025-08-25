4a5481189d119491ef29f56499e1e44bf07adab6
Thomas Leese <thomas@leese.io>
2025-08-25 19:56:57 +0100

    Refactor recording already vaccinated
    This moves the method related to determining whether the user is able to
    take this action in to the policy method, closer to where it would be
    used.
    
    Jira-Issue: MAV-1353

--- app/policies/vaccination_record_policy.rb
+++ app/policies/vaccination_record_policy.rb
@@ -9,6 +9,11 @@ class VaccinationRecordPolicy < ApplicationPolicy
     create?
   end
 
+  def record_already_vaccinated?
+    user.is_nurse? && !session.today? &&
+      patient.vaccination_status(programme:, academic_year:).none_yet?
+  end
+
   def edit?
     user.is_nurse? && record.session_id.present? &&
       record.performed_ods_code == user.selected_organisation.ods_code
@@ -21,6 +26,9 @@ class VaccinationRecordPolicy < ApplicationPolicy
   def destroy?
     user.is_superuser?
   end
+
+  delegate :patient, :session, :programme, to: :record
+  delegate :academic_year, to: :session
 
   class Scope < ApplicationPolicy::Scope
     def resolve
