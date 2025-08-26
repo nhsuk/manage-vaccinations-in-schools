2a562853027ece32c479089a4dd0f80be769ce26
John Henderson <john@logixworks.co.uk>
2025-08-26 11:31:42 +0100

    Refactor AppRegisterStatusTagComponent to be reusable
    The upcoming PSDs tab page will need to render the PSD status for each
    patient. The html markup will be the same as what this component renders
    for the register status.

---
+++ app/components/app_status_tag_component.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class AppStatusTagComponent < ViewComponent::Base
+  def initialize(status, context:)
+    super
+
+    @status = status
+    @context = context
+  end
+
+  def call = tag.strong(text, class: ["nhsuk-tag nhsuk-tag--#{colour}"])
+
+  private
+
+  def text
+    I18n.t(@status, scope: [:status, @context, :label])
+  end
+
+  def colour
+    I18n.t(@status, scope: [:status, @context, :colour])
+  end
+end
