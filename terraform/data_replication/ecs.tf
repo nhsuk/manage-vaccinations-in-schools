4ea8febfa3c31ea30df7c3810694b981b5d86e52
Brage Gording <brage.gording@tngtech.com>
2025-08-28 13:40:04 +0100

    Implement template task definition mechanism
    - Replace the task definition creation with a teamplate version
      - The template version will be picked up and modified during the ECS deployment workflow
    - Remove redundant resources/outputs
      - We no longer need outputs/s3 buckets since we will be using AWS provided github actions for deployment
    - Move application-specific variables into a config file (to be used in deployment)
      - A new SSM resources is introduced that will allow tweaking
        parameters without redeployment

--- terraform/data_replication/ecs.tf
+++ terraform/data_replication/ecs.tf
@@ -33,7 +33,6 @@ module "db_access_service" {
     secrets              = local.task_secrets
     cpu                  = 1024
     memory               = 2048
-    docker_image         = "${var.account_id}.dkr.ecr.eu-west-2.amazonaws.com/${var.docker_image}@${var.image_digest}"
     execution_role_arn   = aws_iam_role.ecs_task_execution_role.arn
     task_role_arn        = aws_iam_role.ecs_task_role.arn
     log_group_name       = aws_cloudwatch_log_group.ecs_log_group.name
