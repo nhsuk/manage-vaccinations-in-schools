069e8a64647648039004fc6f6e4a8437beb279bf
Thomas Leese <thomas@leese.io>
2025-09-01 07:41:39 +0100

    Avoid `validate_inclusion_of` for booleans
    Using this for booleans results in a warning explaining that it can't
    fully be tested due to type conversions.

--- spec/forms/triage_form_spec.rb
+++ spec/forms/triage_form_spec.rb
@@ -16,12 +16,7 @@ describe TriageForm do
     it { should_not validate_presence_of(:notes) }
     it { should_not validate_presence_of(:vaccine_methods) }
     it { should validate_length_of(:notes).is_at_most(1000) }
-
-    it do
-      expect(form).to validate_inclusion_of(:add_psd).in_array(
-        [true, false]
-      ).allow_nil
-    end
+    it { should allow_values(true, false).for(:add_psd) }
   end
 
   describe "when the patient is safe to vaccinate for HPV" do
