0b74f890dd9e41c3b86e6784fdd6494a035fd367
Sam Coy <sam.coy@tngtech.com>
2025-09-02 14:36:30 +0100

    Adjust vaccination source backfilling logic
    This is a small change to the migration which adds source as a column to the VaccinationRecord table.
    
    For historically uploaded vaccination records with the outcome `already_had` created before the migration, we now backfill the source value as `historical_upload` rather than `consent_refusal`. This is consistent with how those records are treated after the migration.
    
    Jira-Issue: MAV-1603

--- db/migrate/20250829172505_add_source_to_vaccination_record.rb
+++ db/migrate/20250829172505_add_source_to_vaccination_record.rb
@@ -4,11 +4,19 @@ class AddSourceToVaccinationRecord < ActiveRecord::Migration[8.0]
   def up
     add_column :vaccination_records, :source, :integer
 
-    # Backfill values based on presence of session_id
     VaccinationRecord.update_all(source: "historical_upload")
-    VaccinationRecord.where(outcome: "already_had").update_all(
-      source: "consent_refusal"
-    )
+    # This handles records which were created when a parent refused consent because the patient was already vaccinated
+    consent_refusal_vaccination_records =
+      VaccinationRecord
+        .includes(:immunisation_imports)
+        .where(outcome: "already_had")
+        .select { |record| record.immunisation_imports.empty? }
+
+    if consent_refusal_vaccination_records.any?
+      VaccinationRecord.where(
+        id: consent_refusal_vaccination_records.map(&:id)
+      ).update_all(source: "consent_refusal")
+    end
     VaccinationRecord.recorded_in_service.update_all(source: "service")
 
     change_column_null :vaccination_records, :source, false
