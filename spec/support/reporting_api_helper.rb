a242b702a2b25cdf70e20046c5fb338d965b6b81
Alistair Davidson <alistair.davidson2@nhs.net>
2025-09-03 12:07:59 +0100

    Factor-out a shared example for common Reporting API controller behaviour - disabled by :reporting_api feature flag, and requiring a valid JWT to access :index

---
+++ spec/support/reporting_api_helper.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module ReportingAPIHelper
+  def valid_payload
+    team = create(:team, :with_one_nurse)
+    user = team.users.first
+    {
+      data: {
+        user: user.as_json,
+        cis2_info: {
+          organisation_code: team.organisation.ods_code,
+          workgroups: [team.workgroup],
+          role_code: CIS2Info::NURSE_ROLE
+        }
+      }
+    }
+  end
+
+  def valid_jwt
+    JWT.encode(valid_payload, Settings.reporting_api.client_app.secret, "HS512")
+  end
+end
