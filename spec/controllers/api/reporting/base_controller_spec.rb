5b3cfd31cfe8b431245a012284689d710e598c67
Alistair Davidson <alistair.davidson2@nhs.net>
2025-09-05 10:52:26 +0100

    Add ReportingAPI VaccinationEvents endpoint

---
+++ spec/controllers/api/reporting/base_controller_spec.rb
@@ -0,0 +1,177 @@
+# frozen_string_literal: true
+
+describe API::Reporting::BaseController do
+  let(:mock_headers) { {} }
+  let(:mock_response) do
+    instance_double(ActionDispatch::Response, headers: mock_headers)
+  end
+
+  describe "#set_json_headers" do
+    before { allow(controller).to receive(:response).and_return(mock_response) }
+
+    it "sets the Last-Modified header to now" do
+      controller.send(:set_json_headers)
+      last_modified = controller.send(:response).headers["Last-Modified"]
+      expect(Time.zone.parse(last_modified)).to be_within(1.second).of(
+        Time.zone.now
+      )
+    end
+  end
+
+  describe "#set_csv_headers" do
+    let(:filename) { "my-test.csv" }
+
+    before { allow(controller).to receive(:response).and_return(mock_response) }
+
+    it "sets the Content-Type header to text/csv" do
+      controller.send(:set_csv_headers, filename:)
+      expect(controller.send(:response).headers["Content-Type"]).to eq(
+        "text/csv"
+      )
+    end
+
+    it "sets the Last-Modified header to now" do
+      controller.send(:set_csv_headers, filename:)
+      last_modified = controller.send(:response).headers["Last-Modified"]
+      expect(Time.zone.parse(last_modified)).to be_within(1.second).of(
+        Time.zone.now
+      )
+    end
+
+    it "sets the Content-Disposition header to be an attachment, with the given filename" do
+      controller.send(:set_csv_headers, filename:)
+      expect(controller.send(:response).headers["Content-Disposition"]).to eq(
+        "attachment; filename=#{filename}"
+      )
+    end
+
+    it "sets the Cache-Control header to no-cache" do
+      controller.send(:set_csv_headers, filename:)
+      expect(controller.send(:response).headers["Cache-Control"]).to eq(
+        "no-cache"
+      )
+    end
+  end
+
+  describe "#csv_filename" do
+    it "ends in .csv" do
+      expect(controller.send(:csv_filename)).to end_with(".csv")
+    end
+
+    context "given a prefix" do
+      it "starts with the given prefix" do
+        expect(
+          controller.send(:csv_filename, prefix: "myprefix")
+        ).to start_with("myprefix")
+      end
+    end
+
+    context "given no prefix" do
+      it "starts with data" do
+        expect(controller.send(:csv_filename, prefix: "data")).to start_with(
+          "data"
+        )
+      end
+    end
+
+    it "includes the @filters" do
+      controller.send(
+        :instance_variable_set,
+        "@filters",
+        "string-representation-of-filters-instance-variable"
+      )
+      expect(controller.send(:csv_filename)).to include(
+        "string-representation-of-filters-instance-variable"
+      )
+    end
+
+    it "includes the given timestamp in iso8601, with any non-alphanumeric characters removed" do
+      timestamp = Time.zone.parse("2025-07-01T12:13:14 +01:00")
+      expect(controller.send(:csv_filename, timestamp:)).to include(
+        "20250701T1213140100"
+      )
+    end
+  end
+
+  describe "#to_csv" do
+    context "given some records" do
+      let!(:flu) { Programme.flu.first || create(:programme, type: "flu") }
+      let(:location) { create(:school, name: "St Vaxes") }
+      let(:sample_session) do
+        create(:session, location: location, programmes: [flu])
+      end
+      let(:patient_a) do
+        create(:patient, random_nhs_number: true, year_group: 7)
+      end
+      let(:records) { ReportingAPI::VaccinationEvent.where("id > 0") }
+      let(:result) { controller.send(:to_csv, records:, header_mappings:) }
+
+      before do
+        create_list(
+          :reporting_api_vaccination_event,
+          2,
+          for_patient: patient_a,
+          year_group: 7,
+          outcome: "administered",
+          session: sample_session,
+          location: location
+        )
+      end
+
+      context "and some header mappings" do
+        let(:header_mappings) do
+          {
+            "Patient ID" => :patient_id,
+            "Outcome" => :vaccination_record_outcome,
+            "Year Group" => :patient_year_group
+          }
+        end
+
+        describe "the returned CSV" do
+          let(:returned_csv_rows) { result.split("\n") }
+
+          it "has a header row matching the given header mapping names" do
+            expect(returned_csv_rows.first).to eq(
+              header_mappings.keys.join(",")
+            )
+          end
+
+          it "has a row for each record" do
+            expect(returned_csv_rows.size).to eq(3)
+          end
+
+          describe "the row for each record" do
+            it "has each attribute named in the header mappings in the right order" do
+              expect(returned_csv_rows[1]).to eq(
+                "#{records[0].patient_id},administered,7"
+              )
+              expect(returned_csv_rows[2]).to eq(
+                "#{records[1].patient_id},administered,7"
+              )
+            end
+
+            context "if the record does not have one of the attributes in the header mappings" do
+              let(:header_mappings) do
+                {
+                  "Patient ID" => :patient_id,
+                  "Outcome" => :vaccination_record_outcome,
+                  "Non-existent attribute" => :non_existent_attribute
+                }
+              end
+
+              it "does not raise an error" do
+                expect { result }.not_to raise_error
+              end
+
+              it "has an empty value in the result for that attribute" do
+                expect(returned_csv_rows[1]).to eq(
+                  "#{records[0].patient_id},administered,"
+                )
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/reporting_api/event_filter_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+describe ReportingAPI::EventFilter do
+  subject(:event_filter) do
+    described_class.new(params: params, filters: filters)
+  end
+
+  context "given params" do
+    let(:params) do
+      {
+        thing_name: "thing_name value",
+        some_other_param: "some_other_param_value"
+      }
+    end
+
+    context "and filters (a mapping of param names to attribute names)" do
+      let(:filters) do
+        {
+          thing_name: "attribute_name_for_thing_name",
+          some_other_param: "attribute_name_for_some_other_param"
+        }
+      end
+
+      describe "#to_where_clause" do
+        it "returns a hash of attribute name (stringified) to corresponding given param value" do
+          expect(event_filter.to_where_clause).to eq(
+            {
+              "attribute_name_for_thing_name" => "thing_name value",
+              "attribute_name_for_some_other_param" => "some_other_param_value"
+            }
+          )
+        end
+
+        context "when params are given but not present in filters" do
+          let(:params) do
+            {
+              thing_name: "thing_name value",
+              some_other_param: "some_other_param_value",
+              param_not_in_filters: "value of param_not_in_filters"
+            }
+          end
+
+          it "does not include those params which are not present in filters" do
+            expect(event_filter.to_where_clause).to eq(
+              {
+                "attribute_name_for_thing_name" => "thing_name value",
+                "attribute_name_for_some_other_param" =>
+                  "some_other_param_value"
+              }
+            )
+          end
+        end
+      end
+
+      describe "to_s" do
+        it "returns a string with all pairs of param & value, concatenated by" do
+          expect(event_filter.to_s).to eq(
+            "thing_name_thing_name_value_some_other_param_some_other_param_value"
+          )
+        end
+
+        it "replaces any number of non-ASCII-alphanumeric characters with underscores" do
+          event_filter.params = {
+            thing_name: "thing! \tvalue",
+            some_other_param: "i like 小笼包"
+          }
+          expect(event_filter.to_s).to eq(
+            "thing_name_thing_value_some_other_param_i_like_"
+          )
+        end
+      end
+    end
+  end
+end
