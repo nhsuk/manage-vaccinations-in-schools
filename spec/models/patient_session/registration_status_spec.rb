nhsuk/manage-vaccinations-in-schools

commit 7539761dbf475effc54f2f9feab0ab7b030e39a2

Replace session attendance patient session foreign key
This replaces the foreign key association between session attendances
and patient sessions to instead link directly to the patient. This is
needed as we eventually want to replace the PatientSession model and to
do that we need to make sure all foreign keys to it have been replaced.

The functionality should be the same before and after.

Jira-Issue: MAV-1820

app/models/patient_session/registration_status.rb
@@ -21,21 +21,24 @@ class PatientSession::RegistrationStatus < ApplicationRecord
   belongs_to :patient_session
 
   has_one :patient, through: :patient_session
+  has_one :session, through: :patient_session
 
   has_many :vaccination_records,
            -> { kept.order(performed_at: :desc) },
            through: :patient
 
-  has_many :session_attendances,
-           -> { includes(:session_date) },
-           through: :patient_session
+  has_one :session_date, -> { today }, through: :session, source: :session_dates
+
+  has_many :session_attendances, through: :patient
 
   enum :status,
        { unknown: 0, attending: 1, not_attending: 2, completed: 3 },
        default: :unknown,
        validate: true
 
-  def session_attendance = session_attendances.find(&:today?)
+  def session_attendance
+    session_attendances.find { it.session_date_id == session_date.id }
+  end
 
   def assign_status
     self.status = generator.status

spec/models/patient_session/registration_status_spec.rb
@@ -57,7 +57,7 @@ describe PatientSession::RegistrationStatus do
         create(
           :session_attendance,
           :present,
-          patient_session:,
+          patient:,
           session_date: session.session_dates.find_by(value: Date.current)
         )
       end
@@ -66,7 +66,7 @@ describe PatientSession::RegistrationStatus do
         create(
           :session_attendance,
           :absent,
-          patient_session:,
+          patient:,
           session_date: session.session_dates.find_by(value: Date.yesterday)
         )
       end
@@ -87,7 +87,7 @@ describe PatientSession::RegistrationStatus do
         create(
           :session_attendance,
           :present,
-          patient_session:,
+          patient:,
           session_date: session.session_dates.first
         )
       end
@@ -100,7 +100,7 @@ describe PatientSession::RegistrationStatus do
         create(
           :session_attendance,
           :present,
-          patient_session:,
+          patient:,
           session_date: session.session_dates.second
         )
       end
@@ -113,7 +113,7 @@ describe PatientSession::RegistrationStatus do
         create(
           :session_attendance,
           :absent,
-          patient_session:,
+          patient:,
           session_date: session.session_dates.second
         )
       end

app/components/app_patient_session_search_result_card_component.rb
@@ -97,8 +97,8 @@ class AppPatientSessionSearchResultCardComponent < ViewComponent::Base
   def can_register_attendance?
     session_attendance =
       SessionAttendance.new(
-        patient_session:,
-        session_date: SessionDate.new(value: Date.current)
+        patient:,
+        session_date: SessionDate.new(session:, value: Date.current)
       )
 
     policy(session_attendance).new?

app/controllers/patient_sessions/session_attendances_controller.rb
@@ -43,10 +43,12 @@ class PatientSessions::SessionAttendancesController < PatientSessions::BaseContr
 
   def set_session_attendance
     @session_attendance =
-      authorize @patient_session
-                  .session_attendances
-                  .includes(:patient, :session_date)
-                  .find_or_initialize_by(session_date: @session_date)
+      authorize(
+        @patient
+          .session_attendances
+          .includes(:patient, session_date: { session: :programmes })
+          .find_or_initialize_by(session_date: @session_date)
+      )
   end
 
   def session_attendance_params

app/lib/generate/vaccination_records.rb
@@ -23,17 +23,20 @@ class Generate::VaccinationRecords
     vaccination_records = []
 
     random_patient_sessions.each do |patient_session|
-      patient_session_id = patient_session.id
-      session_date_ids = patient_session.session.session_dates.pluck(:id)
+      patient = patient_session.patient
+      session = patient_session.session
 
-      unless SessionAttendance.exists?(
-               patient_session_id:,
-               session_date_id: session_date_ids
+      unless SessionAttendance.joins(:session_date).exists?(
+               patient:,
+               session_date: {
+                 session:
+               }
              )
         session_attendances << FactoryBot.build(
           :session_attendance,
           :present,
-          patient_session:
+          patient:,
+          session:
         )
       end
 

app/lib/status_updater.rb
@@ -59,6 +59,7 @@ class StatusUpdater
       .where(patient_session_id: patient_sessions.select(:id))
       .includes(
         :session_attendances,
+        :session_date,
         :vaccination_records,
         patient_session: {
           session: :programmes

app/models/patient.rb
@@ -65,6 +65,7 @@ class Patient < ApplicationRecord
   has_many :consent_notifications
   has_many :consent_statuses
   has_many :consents
+  has_many :gillick_assessments
   has_many :notes
   has_many :notify_log_entries
   has_many :parent_relationships, -> { order(:created_at) }
@@ -73,6 +74,7 @@ class Patient < ApplicationRecord
   has_many :pre_screenings
   has_many :school_move_log_entries
   has_many :school_moves
+  has_many :session_attendances
   has_many :session_notifications
   has_many :triage_statuses
   has_many :triages
@@ -80,10 +82,8 @@ class Patient < ApplicationRecord
   has_many :vaccination_statuses
   has_many :patient_specific_directions
 
-  has_many :gillick_assessments
   has_many :parents, through: :parent_relationships
   has_many :patient_specific_directions
-  has_many :session_attendances, through: :patient_sessions
   has_many :sessions, through: :patient_sessions
   has_many :teams, -> { distinct }, through: :sessions
 

app/models/patient_session.rb
@@ -44,7 +44,6 @@ class PatientSession < ApplicationRecord
   belongs_to :patient
   belongs_to :session
 
-  has_many :session_attendances, dependent: :destroy
   has_one :registration_status
 
   has_one :location, through: :session
@@ -64,6 +63,10 @@ class PatientSession < ApplicationRecord
           source: :notes
 
   has_many :pre_screenings,
+           -> { where(patient_id: it.patient_id) },
+           through: :session
+
+  has_many :session_attendances,
            -> { where(patient_id: it.patient_id) },
            through: :session
 
@@ -315,9 +318,10 @@ class PatientSession < ApplicationRecord
 
   def todays_attendance
     if (session_date = session.session_dates.today.first)
-      session_attendances.includes(:session_date).find_or_initialize_by(
-        session_date:
-      )
+      patient
+        .session_attendances
+        .includes(:patient, session_date: { session: :programmes })
+        .find_or_initialize_by(session_date:)
     end
   end
 

commit 50816b85e6694aed8cedc37298cdae578c23a86b

Fix updating registration status when there isn't a date
This fixes an issue where the registration status cannot be generated
when there isn't a session date today. This fixes a regression from
7539761dbf475effc54f2f9feab0ab7b030e39a2.

Sentry-Issue: 6860955503

app/models/patient_session/registration_status.rb
@@ -37,7 +37,7 @@ class PatientSession::RegistrationStatus < ApplicationRecord
        validate: true
 
   def session_attendance
-    session_attendances.find { it.session_date_id == session_date.id }
+    session_attendances.find { it.session_date_id == session_date&.id }
   end
 
   def assign_status

spec/models/patient_session/registration_status_spec.rb
@@ -41,18 +41,31 @@ describe PatientSession::RegistrationStatus do
 
   describe "associations" do
     it { should belong_to(:patient_session) }
+  end
 
-    describe "#session_attendance" do
-      subject do
-        described_class
-          .includes(:session_attendances)
-          .find(patient_session_registration_status.id)
-          .session_attendance
-      end
+  describe "#session_attendance" do
+    subject do
+      described_class
+        .includes(:session_attendances)
+        .find(patient_session_registration_status.id)
+        .session_attendance
+    end
 
-      let(:patient_session_registration_status) do
-        create(:patient_session_registration_status, patient_session:)
-      end
+    let(:patient_session_registration_status) do
+      create(:patient_session_registration_status, patient_session:)
+    end
+
+    context "with no attendances" do
+      it { should be_nil }
+    end
+
+    context "with no session date today" do
+      let(:session) { create(:session, date: Date.yesterday, programmes:) }
+
+      it { should be_nil }
+    end
+
+    context "with an attendance today and yesterday" do
       let(:today_session_attendance) do
         create(
           :session_attendance,
@@ -76,7 +89,7 @@ describe PatientSession::RegistrationStatus do
   end
 
   describe "#status" do
-    subject(:status) { patient_session_registration_status.assign_status }
+    subject { patient_session_registration_status.assign_status }
 
     context "with no session attendance" do
       it { should be(:unknown) }