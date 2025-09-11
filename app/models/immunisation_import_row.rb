847f9a4f6e049570812aa5af535df10e431d5121
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-09-11 16:36:59 +0100

    Prevent Imms API records being edited by an offline spreadsheet
    Vaccination records which are received from the Imms API are considered
    immutable in Mavis, because we are not the vaccination record's primary
    source. As such, users should not be allowed to edit the records by any
    means, including an offline spreadsheet upload.
    
    We (Thomas, Mike, Alistair) considered exposing this as a validation
    error on the import, but that approach was discarded. This is because we
    would have needed to duplicate a lot of the import logic to decide
    whether it was appropriate to throw a validation error. This led to
    circular dependencies in the validation logic.
    
    Instead this approach was taken, where an adjusted import issue page is
    shown, forcing the user to discard the incoming changes. 
    
    Manually set `apply_changes` in the `.html`
    
    Instead of depending on custom logic in the form.

--- app/models/immunisation_import_row.rb
+++ app/models/immunisation_import_row.rb
@@ -95,7 +95,12 @@ class ImmunisationImportRow
     return unless valid?
 
     outcome = (administered ? "administered" : reason_not_administered_value)
-    source = (offline_recording? ? "service" : "historical_upload")
+    source =
+      if imms_api_record?
+        "nhs_immunisations_api"
+      else
+        offline_recording? ? "service" : "historical_upload"
+      end
 
     attributes = {
       dose_sequence: dose_sequence_value,
@@ -135,8 +140,7 @@ class ImmunisationImportRow
     vaccination_record =
       if uuid.present?
         VaccinationRecord
-          .joins(:team)
-          .find_by!(teams: { id: team.id }, uuid: uuid.to_s)
+          .find_by!(uuid: uuid.to_s)
           .tap { it.stage_changes(attributes) }
       else
         VaccinationRecord.find_or_initialize_by(attributes)
@@ -379,6 +383,13 @@ class ImmunisationImportRow
   delegate :default_dose_sequence, :maximum_dose_sequence, to: :programme
 
   def offline_recording? = session_id.present?
+
+  def imms_api_record?
+    uuid.present? &&
+      VaccinationRecord.find_by!(
+        uuid: uuid.to_s
+      ).sourced_from_nhs_immunisations_api?
+  end
 
   def academic_year = date_of_vaccination.to_date.academic_year
 
@@ -970,15 +981,13 @@ class ImmunisationImportRow
   def validate_uuid
     return if uuid.blank?
 
-    scope =
-      VaccinationRecord.joins(:team).where(
-        teams: {
-          id: team.id
-        },
-        uuid: uuid.to_s
-      )
+    scope = VaccinationRecord.includes(:team).find_by(uuid: uuid.to_s)
 
-    errors.add(uuid.header, "Enter an existing record.") unless scope.exists?
+    return if scope&.sourced_from_nhs_immunisations_api?
+
+    return if !scope.nil? && scope.team == team
+
+    errors.add(uuid.header, "Enter an existing record.")
   end
 
   def validate_vaccine
