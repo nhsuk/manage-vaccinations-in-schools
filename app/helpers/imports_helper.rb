39a1ffa4ef7138de2ad3fb33847772bd01f2c0d9
Thomas Leese <thomas@leese.io>
2025-09-16 11:04:12 +0100

    Remove `distinct` in `import_issues_count`
    We make a call to `uniq` later on in the method so there's no need to
    select on distinct entries here, which should slightly improve the
    performance of the query, the downside being an increase in memory
    usage.
    
    Jira-Issue: MAV-2001

--- app/helpers/imports_helper.rb
+++ app/helpers/imports_helper.rb
@@ -14,9 +14,7 @@ module ImportsHelper
 
   def import_issues_count
     vaccination_records_with_issues =
-      policy_scope(VaccinationRecord).with_pending_changes.distinct.pluck(
-        :patient_id
-      )
+      policy_scope(VaccinationRecord).with_pending_changes.pluck(:patient_id)
 
     patients_with_issues = policy_scope(Patient).with_pending_changes.pluck(:id)
 
