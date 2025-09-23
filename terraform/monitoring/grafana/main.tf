93931bf86bc9b59436da0fd7f50a5d74040e8e1e
Brage Gording <brage.gording@tngtech.com>
2025-09-23 14:21:44 +0100

    Add postgres data source
    - Leave password/url as manual steps to fill for now

--- terraform/monitoring/grafana/main.tf
+++ terraform/monitoring/grafana/main.tf
@@ -30,4 +30,30 @@ resource "grafana_data_source" "cloudwatch" {
   uid = "cloudwatch"
 }
 
-# resouce "grafana_data_source" "postgres" {}
+resource "grafana_data_source" "postgresql" {
+  name = "postgresql"
+  type = "grafana-postgresql-datasource"
+  json_data_encoded = jsonencode({
+    sslmode                = "require"
+    connMaxLifetime        = 14400
+    database               = "manage_vaccinations"
+    maxIdleConns           = 5
+    maxIdleConnsAuto       = true
+    maxOpenConns           = 5
+    tlsConfigurationMethod = "file-path"
+    timescaledb            = false
+    postgresVersion        = 1500
+  })
+  secure_json_data_encoded = jsonencode({
+    password = "CHANGE_ME"
+  })
+  url      = "CHANGE_ME"
+  uid      = "postgres"
+  username = "grafana_ro"
+  lifecycle {
+    ignore_changes = [
+      secure_json_data_encoded,
+      url,
+    ]
+  }
+}
