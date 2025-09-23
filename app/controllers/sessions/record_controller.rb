00e6b79b81283ea4f355aa0a99d38d40e41527cc
Thomas Leese <thomas@leese.io>
2025-09-23 13:22:37 +0100

    Gracefully handle page overflow errors
    This configures Pagy to ensure that if a user tries to access a page
    that doesn't exist, rather than seeing an error page (and an unhandled
    exception going to Sentry), the user is shown the last possible page.
    
    To support this I've also reconfigured how we include the Pagy extras to
    make it globally configured across the codebase. This is because the
    Pagy behaviour was dependent on which files had been executed and which
    `pagy/extra` files had been required, meaning the tests were flaky
    depending on the order.

--- app/controllers/sessions/record_controller.rb
+++ app/controllers/sessions/record_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "pagy/extras/array"
-
 class Sessions::RecordController < ApplicationController
   include PatientSearchFormConcern
   include TodaysBatchConcern
