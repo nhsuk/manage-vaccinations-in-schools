a2d07669fd75ce3681b112fea1beeb02b4b380f7
Brage Gording <brage.gording@tngtech.com>
2025-09-24 16:35:12 +0100

    Create independent password for data replication
    - Avoids spreading of production credentials
    - Reduces concerns/dependensies between real production and data
      replication

--- terraform/data_replication/rds.tf
+++ terraform/data_replication/rds.tf
@@ -38,19 +38,20 @@ resource "aws_security_group_rule" "rds_inbound_grafana" {
   description       = "Allow Grafana workspace access to PostgreSQL"
 }
 
-resource "aws_rds_cluster" "cluster" {
-  cluster_identifier     = "${local.name_prefix}-rds-${formatdate("hh-mm-ss", timestamp())}"
-  engine                 = "aurora-postgresql"
-  engine_mode            = "provisioned"
-  database_name          = "manage_vaccinations"
-  master_username        = "postgres"
-  snapshot_identifier    = var.imported_snapshot
-  db_subnet_group_name   = aws_db_subnet_group.dbsg.name
-  vpc_security_group_ids = [aws_security_group.rds.id]
-  storage_encrypted      = true
-  skip_final_snapshot    = true
-  deletion_protection    = false
-  engine_version         = var.db_engine_version
+resource "aws_rds_cluster" "this" {
+  cluster_identifier          = "${local.name_prefix}-rds-${formatdate("hh-mm-ss", timestamp())}"
+  engine                      = "aurora-postgresql"
+  engine_mode                 = "provisioned"
+  database_name               = "manage_vaccinations"
+  master_username             = "postgres"
+  snapshot_identifier         = var.imported_snapshot
+  db_subnet_group_name        = aws_db_subnet_group.dbsg.name
+  vpc_security_group_ids      = [aws_security_group.rds.id]
+  storage_encrypted           = true
+  skip_final_snapshot         = true
+  deletion_protection         = false
+  manage_master_user_password = true
+  engine_version              = var.db_engine_version
 
   serverlessv2_scaling_configuration {
     max_capacity = var.max_aurora_capacity_units
@@ -62,12 +63,12 @@ resource "aws_rds_cluster" "cluster" {
   }
 }
 
-resource "aws_rds_cluster_instance" "instance" {
-  cluster_identifier   = aws_rds_cluster.cluster.id
+resource "aws_rds_cluster_instance" "this" {
+  cluster_identifier   = aws_rds_cluster.this.id
   identifier           = "${local.name_prefix}-rds-instance-${formatdate("hh-mm-ss", timestamp())}"
   instance_class       = "db.serverless"
-  engine               = aws_rds_cluster.cluster.engine
-  engine_version       = aws_rds_cluster.cluster.engine_version
+  engine               = aws_rds_cluster.this.engine
+  engine_version       = aws_rds_cluster.this.engine_version
   db_subnet_group_name = aws_db_subnet_group.dbsg.name
   promotion_tier       = 1
 
