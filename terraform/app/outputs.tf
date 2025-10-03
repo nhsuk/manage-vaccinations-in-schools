5bb4a0fe51fc6f4779638956d1394c0790fd5b6b
Moritz Bogs <moritz.bogs@tngtech.com>
2025-10-03 13:00:00 +0100

    Reintroduce approval step before deployment

--- terraform/app/outputs.tf
+++ terraform/app/outputs.tf
@@ -1,13 +1,3 @@
-# output "codedeploy_application_name" {
-#   description = "The name of the CodeDeploy application"
-#   value       = aws_codedeploy_app.mavis.name
-# }
-#
-# output "codedeploy_deployment_group_name" {
-#   description = "The name of the CodeDeploy deployment group"
-#   value       = aws_codedeploy_deployment_group.blue_green_deployment_group.deployment_group_name
-# }
-
 output "db_secret_arn" {
   description = "The ARN of the secret containing the DB credentials."
   value       = aws_rds_cluster.core.master_user_secret[0].secret_arn
