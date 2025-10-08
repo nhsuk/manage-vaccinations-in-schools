c567a703220d7892b93eb19471e9f16580f1cd6c
Thomas Leese <thomas@leese.io>
2025-10-08 06:56:32 +0100

    Add option to show NHS numbers
    This updates the `AppPatientSearchResultCardComponent` to allow showing
    the patient's NHS number which we need on the childrens page.
    
    Jira-Issue: MAV-2143

--- app/components/app_patient_search_result_card_component.rb
+++ app/components/app_patient_search_result_card_component.rb
@@ -6,6 +6,7 @@ class AppPatientSearchResultCardComponent < ViewComponent::Base
     link_to:,
     programmes: [],
     academic_year: nil,
+    show_nhs_number: false,
     show_parents: false,
     show_postcode: false,
     show_school: false,
@@ -17,6 +18,7 @@ class AppPatientSearchResultCardComponent < ViewComponent::Base
     @programmes = programmes
     @academic_year = academic_year || AcademicYear.pending
 
+    @show_nhs_number = show_nhs_number
     @show_parents = show_parents
     @show_postcode = show_postcode
     @show_school = show_school
@@ -29,6 +31,12 @@ class AppPatientSearchResultCardComponent < ViewComponent::Base
       card.with_heading(level: 4) { patient.full_name_with_known_as }
 
       govuk_summary_list do |summary_list|
+        if show_nhs_number
+          summary_list.with_row do |row|
+            row.with_key { "NHS number" }
+            row.with_value { patient_nhs_number(patient) }
+          end
+        end
         summary_list.with_row do |row|
           row.with_key { "Date of birth" }
           row.with_value { patient_date_of_birth(patient) }
@@ -88,6 +96,7 @@ class AppPatientSearchResultCardComponent < ViewComponent::Base
               :programmes,
               :academic_year,
               :triage_status,
+              :show_nhs_number,
               :show_parents,
               :show_postcode,
               :show_school,
@@ -96,6 +105,7 @@ class AppPatientSearchResultCardComponent < ViewComponent::Base
 
   delegate :govuk_summary_list,
            :patient_date_of_birth,
+           :patient_nhs_number,
            :patient_parents,
            :patient_school,
            :patient_year_group,
