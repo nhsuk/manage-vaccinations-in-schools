a6664ff80f0210a56a5544d2db9aac7d1c899d01
Thomas Leese <thomas@leese.io>
2025-10-10 15:19:56 +0100

    Make `ManifestController` an API controller
    This should improve the performance of this controller by avoiding the
    overhead of the full `ApplicationController`, particularly as this just
    returns a JSON response.

--- app/controllers/manifest_controller.rb
+++ app/controllers/manifest_controller.rb
@@ -1,22 +1,16 @@
 # frozen_string_literal: true
 
-class ManifestController < ApplicationController
-  skip_before_action :authenticate_user!
-  skip_before_action :store_user_location!
-  skip_after_action :verify_policy_scoped
+class ManifestController < ActionController::API
+  include ApplicationHelper
 
   before_action :set_assets_name
 
   def show
     render_block = -> do
-      respond_to do |format|
-        format.json do
-          render "manifest/show", content_type: "application/manifest+json"
-        end
-      end
+      render "manifest/show", content_type: "application/manifest+json"
     end
 
-    if params[:digest] == helpers.manifest_digest
+    if params[:digest] == manifest_digest
       http_cache_forever(public: true, &render_block)
     else
       expires_in 1.hour, public: true
