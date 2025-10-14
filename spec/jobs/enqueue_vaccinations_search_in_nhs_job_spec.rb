14ea7ad3f64e53bd7a9ace81a7375017e47bb3fc
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-10-14 09:27:33 +0100

    Refactor enqueue vaccs search jobs spec
    To reduce overlap and improve comprehensibility.
    
    Jira-Issue: MAV-1995

--- spec/jobs/enqueue_vaccinations_search_in_nhs_job_spec.rb
+++ spec/jobs/enqueue_vaccinations_search_in_nhs_job_spec.rb
@@ -3,235 +3,166 @@
 describe EnqueueVaccinationsSearchInNHSJob do
   include ActiveJob::TestHelper
 
-  let(:team) { create(:team) }
-  let(:flu) { Programme.flu }
-  let(:location) { create(:school, team:, programmes: [flu]) }
-  let(:school) { location }
-  let!(:patient) { create(:patient, team:, school:, session:) }
-
   describe "#perform", :within_academic_year do
-    subject { SearchVaccinationRecordsInNHSJob }
+    before do
+      Flipper.enable(:imms_api_enqueue_session_searches)
+      allow(SearchVaccinationRecordsInNHSJob).to receive(:perform_bulk)
 
-    before { allow(SearchVaccinationRecordsInNHSJob).to receive(:perform_bulk) }
+      # Ensure expectation patients and sessions are created before the job runs
+      searchable_patients
+
+      described_class.perform_now
+    end
+
+    let(:team) { create(:team) }
+    let(:flu) { Programme.flu }
+
+    let(:programmes) { [flu] }
+    let(:gias_year_groups) { (0..11).to_a }
+    let(:school) { create(:school, team:, programmes:, gias_year_groups:) }
+    let(:clinic) { create(:generic_clinic, team:, programmes:) }
+
+    let(:days_before_consent_reminders) { 7 }
+    let(:session) do
+      if location.generic_clinic?
+        send_consent_requests_at = nil
+        send_invitations_at = send_consent_requests_or_invitations_at
+      else
+        send_consent_requests_at = send_consent_requests_or_invitations_at
+        send_invitations_at = nil
+      end
+
+      create(
+        :session,
+        programmes:,
+        academic_year: AcademicYear.pending,
+        dates:,
+        send_consent_requests_at:,
+        send_invitations_at:,
+        days_before_consent_reminders:,
+        team:,
+        location:
+      )
+    end
+    let(:searchable_patients) { [[create(:patient, team:, school:, session:)]] }
+
+    shared_examples "behaviour before, during or after consent/invitation period" do
+      subject { SearchVaccinationRecordsInNHSJob }
+
+      context "before the consent or invitation period for the session",
+              within_academic_year: {
+                from_start: 30.days
+              } do
+        let(:dates) { [30.days.from_now] }
+        let(:send_consent_requests_or_invitations_at) { 7.days.from_now }
+
+        it { should_not have_received(:perform_bulk) }
+      end
+
+      context "within the consent or invitation period for the session",
+              within_academic_year: {
+                from_start: 7.days
+              } do
+        let(:dates) { [7.days.from_now] }
+        let(:send_consent_requests_or_invitations_at) { 14.days.ago }
+
+        it "performs searches on the session patients" do
+          searchable_patients.each do |patients|
+            expect(SearchVaccinationRecordsInNHSJob).to have_received(
+              :perform_bulk
+            ).once.with(patients.map(&:id).zip)
+          end
+        end
+      end
+
+      context "after the session dates",
+              within_academic_year: {
+                from_start: 7.days
+              } do
+        let(:dates) { [7.days.ago] }
+        let(:send_consent_requests_or_invitations_at) { 30.days.ago }
+
+        it { should_not have_received(:perform_bulk) }
+      end
+
+      context "in between multiple session dates",
+              within_academic_year: {
+                from_start: 7.days
+              } do
+        let(:dates) { [7.days.ago, 7.days.from_now] }
+        let(:send_consent_requests_or_invitations_at) { 14.days.ago }
+
+        it "performs searches on the session patients" do
+          searchable_patients.each do |patients|
+            expect(SearchVaccinationRecordsInNHSJob).to have_received(
+              :perform_bulk
+            ).once.with(patients.map(&:id).zip)
+          end
+        end
+      end
+
+      context "on the threshold of the consent or invitation period",
+              within_academic_year: {
+                from_start: 7.days
+              } do
+        let(:dates) { [30.days.from_now] }
+        let(:send_consent_requests_or_invitations_at) { 2.days.from_now }
+
+        it "performs searches on the session patients" do
+          searchable_patients.each do |patients|
+            expect(SearchVaccinationRecordsInNHSJob).to have_received(
+              :perform_bulk
+            ).once.with(patients.map(&:id).zip)
+          end
+        end
+      end
+    end
 
     context "with a normal school session" do
-      let(:send_consent_requests_at) {}
-      let(:days_before_consent_reminders) { 7 }
-      let(:session) do
-        create(
-          :session,
-          programmes: [flu],
-          academic_year: AcademicYear.pending,
-          dates:,
-          send_consent_requests_at:,
-          days_before_consent_reminders:,
-          team:,
-          location:
-        )
-      end
+      let(:location) { school }
 
-      context "with a specific unscheduled session" do
-        before { described_class.perform_now([session]) }
-
-        let(:dates) { [] }
-        let(:days_before_consent_reminders) { nil }
-
-        it { should have_received(:perform_bulk).once.with([[patient.id]]) }
-      end
-
-      context "session with dates in the future" do
-        before { described_class.perform_now }
-
-        let(:dates) { [7.days.from_now] }
-        let(:send_consent_requests_at) { 14.days.ago }
-
-        it { should have_received(:perform_bulk).once.with([[patient.id]]) }
-
-        context "clinic session" do
-          let(:location) { create(:generic_clinic, team:, programmes: [flu]) }
-          let(:school) { create(:school, team:, programmes: [flu]) }
-
-          it { should have_received(:perform_bulk).exactly(:once) }
-        end
-      end
-
-      context "session with dates in the past",
-              within_academic_year: {
-                from_start: 7.days
-              } do
-        before { described_class.perform_now }
-
-        let(:dates) { [7.days.ago] }
-        let(:send_consent_requests_at) { 30.days.ago }
-
-        it { should_not have_received(:perform_bulk) }
-      end
-
-      context "session with dates in the past and the future",
-              within_academic_year: {
-                from_start: 7.days
-              } do
-        before { described_class.perform_now }
-
-        let(:send_consent_requests_at) { 28.days.ago }
-        let(:dates) { [7.days.ago, 7.days.from_now] }
-
-        it { should have_received(:perform_bulk).exactly(:once) }
-      end
-
-      context "session with send_invitations_at in the future" do
-        before { described_class.perform_now }
-
-        let(:send_consent_requests_at) { 2.days.from_now }
-        let(:dates) { [17.days.from_now] }
-
-        it { should have_received(:perform_bulk).exactly(:once) }
-      end
-
-      context "session with send_consent_requests_at too far in the future" do
-        let(:send_consent_requests_at) { 3.days.from_now }
-        let(:dates) { [17.days.from_now] }
-
-        it { should_not have_received(:perform_bulk) }
-      end
+      include_examples "behaviour before, during or after consent/invitation period"
     end
 
-    shared_examples "notification date logic" do |notification_date_field|
-      context "with notification date within threshold" do
-        let(:session) do
-          session_attributes = {
-            programmes: [flu],
-            academic_year: AcademicYear.pending,
-            dates:,
-            team:,
-            location:
-          }
+    context "clinic session" do
+      let(:location) { clinic }
 
-          # Set the appropriate notification date field
-          if notification_date_field == :send_invitations_at
-            session_attributes[:send_invitations_at] = 1.day.from_now
-            session_attributes[:send_consent_requests_at] = nil
-          else
-            session_attributes[:send_invitations_at] = nil
-            session_attributes[:send_consent_requests_at] = 1.day.from_now
-            session_attributes[:days_before_consent_reminders] = 7
-          end
-
-          create(:session, session_attributes)
-        end
-
-        before { described_class.perform_now }
-
-        it "includes the session" do
-          expect(SearchVaccinationRecordsInNHSJob).to have_received(
-            :perform_bulk
-          ).with([patient.id].zip)
-        end
-      end
-
-      context "with notification date too far in future" do
-        let(:session) do
-          session_attributes = {
-            programmes: [flu],
-            academic_year: AcademicYear.pending,
-            dates:,
-            team:,
-            location:
-          }
-
-          # Set the appropriate notification date field
-          if notification_date_field == :send_invitations_at
-            session_attributes[:send_invitations_at] = 3.days.from_now
-            session_attributes[:send_consent_requests_at] = nil
-          else
-            session_attributes[:send_invitations_at] = nil
-            session_attributes[:send_consent_requests_at] = 3.days.from_now
-            session_attributes[:days_before_consent_reminders] = 7
-          end
-
-          create(:session, session_attributes)
-        end
-
-        before { described_class.perform_now }
-
-        it "excludes the session" do
-          expect(SearchVaccinationRecordsInNHSJob).not_to have_received(
-            :perform_bulk
-          )
-        end
-      end
+      include_examples "behaviour before, during or after consent/invitation period"
     end
 
-    context "testing notification date logic for different location types" do
-      let(:dates) { [30.days.from_now] }
-
-      context "generic clinic sessions" do
-        let(:location) { create(:generic_clinic, team:, programmes: [flu]) }
-        let(:school) { create(:school, team:, programmes: [flu]) }
-
-        include_examples "notification date logic", :send_invitations_at
-      end
-
-      context "school sessions" do
-        let(:location) { create(:school, team:, programmes: [flu]) }
-        let(:school) { location }
-
-        include_examples "notification date logic", :send_consent_requests_at
-      end
-
-      context "mixed session types" do
-        let(:generic_clinic) do
-          create(:generic_clinic, team:, programmes: [flu])
-        end
-        let(:school_location) { create(:school, team:, programmes: [flu]) }
-
-        let(:generic_clinic_session) do
+    context "mixed session types" do
+      let(:searchable_patients) do
+        clinic_session =
           create(
             :session,
-            programmes: [flu],
+            programmes:,
             academic_year: AcademicYear.pending,
             dates:,
-            send_invitations_at: 1.day.from_now,
+            send_invitations_at: send_consent_requests_or_invitations_at,
             send_consent_requests_at: nil,
             days_before_consent_reminders: nil,
             team:,
-            location: generic_clinic
+            location: clinic
           )
-        end
-
-        let(:school_session) do
+        school_session =
           create(
             :session,
-            programmes: [flu],
+            programmes:,
             academic_year: AcademicYear.pending,
             dates:,
             send_invitations_at: nil,
-            send_consent_requests_at: 1.day.from_now,
+            send_consent_requests_at: send_consent_requests_or_invitations_at,
             days_before_consent_reminders: 7,
             team:,
-            location: school_location
+            location: school
           )
-        end
-
-        let!(:patient) do
-          create(:patient, team:, school:, session: generic_clinic_session)
-        end
-        let!(:second_patient) do
-          create(:patient, team:, school:, session: school_session)
-        end
-
-        before { described_class.perform_now }
-
-        it "includes all sessions when their respective notification dates are within threshold" do
-          expect(SearchVaccinationRecordsInNHSJob).to have_received(
-            :perform_bulk
-          ).with([patient.id].zip)
-
-          expect(SearchVaccinationRecordsInNHSJob).to have_received(
-            :perform_bulk
-          ).with([second_patient.id].zip)
-        end
+        [
+          [create(:patient, team:, school:, session: clinic_session)],
+          [create(:patient, team:, school:, session: school_session)]
+        ]
       end
+
+      include_examples "behaviour before, during or after consent/invitation period"
     end
   end
 end
