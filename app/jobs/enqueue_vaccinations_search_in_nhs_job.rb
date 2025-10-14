cf9c1f45067928d9e8c06a721f6a6ae8c76e583c
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-10-14 18:55:59 +0100

    Add rolling searches
    This allows us to ensure that patient vaccination information is
    up-to-date even if they are not in a session, by searching for patients
    that haven't been updated over the last 28 days.
    
    Jira-Issue: MAV-1995

--- app/jobs/enqueue_vaccinations_search_in_nhs_job.rb
+++ app/jobs/enqueue_vaccinations_search_in_nhs_job.rb
@@ -3,23 +3,56 @@
 class EnqueueVaccinationsSearchInNHSJob < ApplicationJob
   queue_as :immunisations_api_search
 
-  def perform(sessions = nil)
-    scope =
-      if sessions
-        Session.where(id: sessions.map(&:id))
-      else
-        scope = Session.has_any_programmes_of([Programme.flu]).scheduled
+  def perform(programme_types: default_programme_types)
+    patient_ids = Set.new
 
-        scope.where("sessions.send_invitations_at <= ?", 2.days.from_now).or(
-          scope.where("sessions.send_consent_requests_at <= ?", 2.days.from_now)
-        )
-      end
-
-    scope.find_each do |session|
-      ids = session.patients.pluck(:id)
-      next if ids.empty?
-
-      SearchVaccinationRecordsInNHSJob.perform_bulk(ids.zip)
+    if Flipper.enabled?(:imms_api_enqueue_session_searches)
+      patient_ids += patient_ids_session_searches(programme_types:)
     end
+
+    if Flipper.enabled?(:imms_api_enqueue_rolling_searches)
+      patient_ids += patient_ids_for_rolling_searches
+    end
+
+    if patient_ids.any?
+      SearchVaccinationRecordsInNHSJob.perform_bulk(patient_ids.zip)
+    end
+  end
+
+  private
+
+  def default_programme_types = ["flu"].freeze
+  def daily_enqueue_size_limit = 1000
+  def rolling_search_period_in_days = 28
+
+  def patient_ids_session_searches(programme_types:)
+    scope =
+      Session
+        .has_any_programme_types_of(programme_types)
+        .scheduled
+        .then do
+          it.where("sessions.send_invitations_at <= ?", 2.days.from_now).or(
+            it.where("sessions.send_consent_requests_at <= ?", 2.days.from_now)
+          )
+        end
+
+    scope.find_each.flat_map { |session| session.patients.pluck(:id) }
+  end
+
+  def patient_ids_for_rolling_searches
+    patient_ids ||=
+      Patient
+        .where.not(
+          id:
+            PatientProgrammeVaccinationsSearch.where(
+              "last_searched_at > ?",
+              rolling_search_period_in_days.days.ago
+            ).select(:patient_id)
+        )
+        .ids
+
+    batch_size = (patient_ids.count.to_f / rolling_search_period_in_days).ceil
+    batch_size = [batch_size, daily_enqueue_size_limit].max
+    patient_ids.first(batch_size)
   end
 end
--- spec/jobs/enqueue_vaccinations_search_in_nhs_job_spec.rb
+++ spec/jobs/enqueue_vaccinations_search_in_nhs_job_spec.rb
@@ -4,165 +4,352 @@ describe EnqueueVaccinationsSearchInNHSJob do
   include ActiveJob::TestHelper
 
   describe "#perform", :within_academic_year do
-    before do
-      Flipper.enable(:imms_api_enqueue_session_searches)
-      allow(SearchVaccinationRecordsInNHSJob).to receive(:perform_bulk)
-
-      # Ensure expectation patients and sessions are created before the job runs
-      searchable_patients
-
-      described_class.perform_now
-    end
-
-    let(:team) { create(:team) }
     let(:flu) { Programme.flu }
-
     let(:programmes) { [flu] }
+    let(:team) { create(:team) }
     let(:gias_year_groups) { (0..11).to_a }
-    let(:school) { create(:school, team:, programmes:, gias_year_groups:) }
-    let(:clinic) { create(:generic_clinic, team:, programmes:) }
 
-    let(:days_before_consent_reminders) { 7 }
-    let(:session) do
-      if location.generic_clinic?
-        send_consent_requests_at = nil
-        send_invitations_at = send_consent_requests_or_invitations_at
-      else
-        send_consent_requests_at = send_consent_requests_or_invitations_at
-        send_invitations_at = nil
+    describe "session searches" do
+      before do
+        setup_feature_flag
+        allow(SearchVaccinationRecordsInNHSJob).to receive(:perform_bulk)
+
+        # Ensure expectation patients and sessions are created before the job runs
+        searchable_patients
+
+        described_class.perform_now
       end
 
-      create(
-        :session,
-        programmes:,
-        academic_year: AcademicYear.pending,
-        dates:,
-        send_consent_requests_at:,
-        send_invitations_at:,
-        days_before_consent_reminders:,
-        team:,
-        location:
-      )
-    end
-    let(:searchable_patients) { [[create(:patient, team:, school:, session:)]] }
-
-    shared_examples "behaviour before, during or after consent/invitation period" do
-      subject { SearchVaccinationRecordsInNHSJob }
-
-      context "before the consent or invitation period for the session",
-              within_academic_year: {
-                from_start: 30.days
-              } do
-        let(:dates) { [30.days.from_now] }
-        let(:send_consent_requests_or_invitations_at) { 7.days.from_now }
-
-        it { should_not have_received(:perform_bulk) }
+      def setup_feature_flag
+        Flipper.enable(:imms_api_enqueue_session_searches)
       end
 
-      context "within the consent or invitation period for the session",
-              within_academic_year: {
-                from_start: 7.days
-              } do
-        let(:dates) { [7.days.from_now] }
-        let(:send_consent_requests_or_invitations_at) { 14.days.ago }
-
-        it "performs searches on the session patients" do
-          searchable_patients.each do |patients|
-            expect(SearchVaccinationRecordsInNHSJob).to have_received(
-              :perform_bulk
-            ).once.with(patients.map(&:id).zip)
-          end
-        end
-      end
-
-      context "after the session dates",
-              within_academic_year: {
-                from_start: 7.days
-              } do
-        let(:dates) { [7.days.ago] }
-        let(:send_consent_requests_or_invitations_at) { 30.days.ago }
-
-        it { should_not have_received(:perform_bulk) }
-      end
-
-      context "in between multiple session dates",
-              within_academic_year: {
-                from_start: 7.days
-              } do
-        let(:dates) { [7.days.ago, 7.days.from_now] }
-        let(:send_consent_requests_or_invitations_at) { 14.days.ago }
-
-        it "performs searches on the session patients" do
-          searchable_patients.each do |patients|
-            expect(SearchVaccinationRecordsInNHSJob).to have_received(
-              :perform_bulk
-            ).once.with(patients.map(&:id).zip)
-          end
-        end
-      end
-
-      context "on the threshold of the consent or invitation period",
-              within_academic_year: {
-                from_start: 7.days
-              } do
-        let(:dates) { [30.days.from_now] }
-        let(:send_consent_requests_or_invitations_at) { 2.days.from_now }
-
-        it "performs searches on the session patients" do
-          searchable_patients.each do |patients|
-            expect(SearchVaccinationRecordsInNHSJob).to have_received(
-              :perform_bulk
-            ).once.with(patients.map(&:id).zip)
-          end
-        end
-      end
-    end
-
-    context "with a normal school session" do
+      let(:school) { create(:school, team:, programmes:, gias_year_groups:) }
+      let(:clinic) { create(:generic_clinic, team:, programmes:) }
       let(:location) { school }
 
-      include_examples "behaviour before, during or after consent/invitation period"
+      let(:days_before_consent_reminders) { 7 }
+      let(:session) do
+        if location.generic_clinic?
+          send_consent_requests_at = nil
+          send_invitations_at = send_consent_requests_or_invitations_at
+        else
+          send_consent_requests_at = send_consent_requests_or_invitations_at
+          send_invitations_at = nil
+        end
+
+        create(
+          :session,
+          programmes:,
+          academic_year: AcademicYear.pending,
+          dates:,
+          send_consent_requests_at:,
+          send_invitations_at:,
+          days_before_consent_reminders:,
+          team:,
+          location:
+        )
+      end
+      let(:searchable_patients) { [create(:patient, team:, school:, session:)] }
+
+      context "with feature flag disabled" do
+        def setup_feature_flag
+          Flipper.disable(:imms_api_enqueue_session_searches)
+        end
+
+        let(:searchable_patients) { [] }
+
+        it "does not perform searches on the session patients" do
+          expect(SearchVaccinationRecordsInNHSJob).not_to have_received(
+            :perform_bulk
+          )
+        end
+      end
+
+      shared_examples "behaviour before, during or after consent/invitation period" do
+        subject { SearchVaccinationRecordsInNHSJob }
+
+        context "before the consent or invitation period for the session",
+                within_academic_year: {
+                  from_start: 30.days
+                } do
+          let(:dates) { [30.days.from_now] }
+          let(:send_consent_requests_or_invitations_at) { 7.days.from_now }
+
+          it { should_not have_received(:perform_bulk) }
+        end
+
+        context "within the consent or invitation period for the session",
+                within_academic_year: {
+                  from_start: 7.days
+                } do
+          let(:dates) { [7.days.from_now] }
+          let(:send_consent_requests_or_invitations_at) { 14.days.ago }
+
+          it "performs searches on the session patients" do
+            expect(SearchVaccinationRecordsInNHSJob).to have_received(
+              :perform_bulk
+            ).once.with(searchable_patients.map(&:id).zip)
+          end
+        end
+
+        context "after the session dates",
+                within_academic_year: {
+                  from_start: 7.days
+                } do
+          let(:dates) { [7.days.ago] }
+          let(:send_consent_requests_or_invitations_at) { 30.days.ago }
+
+          it { should_not have_received(:perform_bulk) }
+        end
+
+        context "in between multiple session dates",
+                within_academic_year: {
+                  from_start: 7.days
+                } do
+          let(:dates) { [7.days.ago, 7.days.from_now] }
+          let(:send_consent_requests_or_invitations_at) { 14.days.ago }
+
+          it "performs searches on the session patients" do
+            expect(SearchVaccinationRecordsInNHSJob).to have_received(
+              :perform_bulk
+            ).once.with(searchable_patients.map(&:id).zip)
+          end
+        end
+
+        context "on the threshold of the consent or invitation period",
+                within_academic_year: {
+                  from_start: 7.days
+                } do
+          let(:dates) { [30.days.from_now] }
+          let(:send_consent_requests_or_invitations_at) { 2.days.from_now }
+
+          it "performs searches on the session patients" do
+            expect(SearchVaccinationRecordsInNHSJob).to have_received(
+              :perform_bulk
+            ).once.with(searchable_patients.map(&:id).zip)
+          end
+        end
+      end
+
+      context "with a normal school session" do
+        let(:location) { school }
+
+        include_examples "behaviour before, during or after consent/invitation period"
+      end
+
+      context "clinic session" do
+        let(:location) { clinic }
+
+        include_examples "behaviour before, during or after consent/invitation period"
+      end
+
+      context "mixed session types" do
+        let(:searchable_patients) do
+          clinic_session =
+            create(
+              :session,
+              programmes:,
+              academic_year: AcademicYear.pending,
+              dates:,
+              send_invitations_at: send_consent_requests_or_invitations_at,
+              send_consent_requests_at: nil,
+              days_before_consent_reminders: nil,
+              team:,
+              location: clinic
+            )
+          school_session =
+            create(
+              :session,
+              programmes:,
+              academic_year: AcademicYear.pending,
+              dates:,
+              send_invitations_at: nil,
+              send_consent_requests_at: send_consent_requests_or_invitations_at,
+              days_before_consent_reminders: 7,
+              team:,
+              location: school
+            )
+          [
+            create(:patient, team:, school:, session: clinic_session),
+            create(:patient, team:, school:, session: school_session)
+          ]
+        end
+
+        include_examples "behaviour before, during or after consent/invitation period"
+      end
     end
 
-    context "clinic session" do
-      let(:location) { clinic }
+    describe "doing the rolling searches" do
+      before do
+        setup_feature_flag
+        setup_stubs
 
-      include_examples "behaviour before, during or after consent/invitation period"
-    end
+        # Ensure expectation patients are created before the job runs
+        searchable_patients
 
-    context "mixed session types" do
-      let(:searchable_patients) do
-        clinic_session =
-          create(
-            :session,
-            programmes:,
-            academic_year: AcademicYear.pending,
-            dates:,
-            send_invitations_at: send_consent_requests_or_invitations_at,
-            send_consent_requests_at: nil,
-            days_before_consent_reminders: nil,
-            team:,
-            location: clinic
-          )
-        school_session =
-          create(
-            :session,
-            programmes:,
-            academic_year: AcademicYear.pending,
-            dates:,
-            send_invitations_at: nil,
-            send_consent_requests_at: send_consent_requests_or_invitations_at,
-            days_before_consent_reminders: 7,
-            team:,
-            location: school
-          )
+        described_class.perform_now
+      end
+
+      def setup_stubs
+        allow(SearchVaccinationRecordsInNHSJob).to receive(:perform_bulk)
+      end
+
+      def setup_feature_flag
+        Flipper.enable(:imms_api_enqueue_rolling_searches)
+      end
+
+      let(:school) { create(:school, team:, programmes:, gias_year_groups:) }
+      let(:patient_programme_vaccinations_searches) do
         [
-          [create(:patient, team:, school:, session: clinic_session)],
-          [create(:patient, team:, school:, session: school_session)]
+          PatientProgrammeVaccinationsSearch.new(
+            programme: flu,
+            last_searched_at:
+          )
+        ]
+      end
+      let(:searchable_patients) do
+        [
+          create(
+            :patient,
+            team:,
+            school:,
+            patient_programme_vaccinations_searches:
+          )
         ]
       end
 
-      include_examples "behaviour before, during or after consent/invitation period"
+      context "with the feature flag disabled" do
+        def setup_feature_flag
+          Flipper.disable(:imms_api_enqueue_rolling_searches)
+        end
+
+        let(:patient_programme_vaccinations_searches) { [] }
+
+        it "does not perform a search on the patient" do
+          expect(SearchVaccinationRecordsInNHSJob).not_to have_received(
+            :perform_bulk
+          )
+        end
+      end
+
+      context "with a patient that has no previous searches" do
+        let(:patient_programme_vaccinations_searches) { [] }
+
+        it "performs a search on the patients" do
+          expect(SearchVaccinationRecordsInNHSJob).to have_received(
+            :perform_bulk
+          ).once.with(searchable_patients.map(&:id).zip)
+        end
+      end
+
+      context "with a patient that has searches 28 days ago or older" do
+        let(:last_searched_at) { 28.days.ago }
+
+        it "performs a search on the patient" do
+          expect(SearchVaccinationRecordsInNHSJob).to have_received(
+            :perform_bulk
+          ).once.with(searchable_patients.map(&:id).zip)
+        end
+      end
+
+      context "with a patient that has searches less-than 28 days" do
+        let(:last_searched_at) { 27.days.ago }
+
+        it "does not perform a search on the patient" do
+          expect(SearchVaccinationRecordsInNHSJob).not_to have_received(
+            :perform_bulk
+          )
+        end
+      end
+
+      context "with many patients that have searches 28 days ago or older" do
+        let(:searchable_patients) do
+          Patient.import(build_list(:patient, 50, team:, school:))
+          Patient.all
+        end
+
+        context "and the batch size is less than the daily limit" do
+          def setup_stubs
+            allow(SearchVaccinationRecordsInNHSJob).to receive(:perform_bulk)
+
+            allow_any_instance_of(EnqueueVaccinationsSearchInNHSJob).to receive(
+              :daily_enqueue_size_limit
+            ).and_return(1)
+          end
+
+          it "searches for 1/28th of the elligible patients" do
+            expect(SearchVaccinationRecordsInNHSJob).to have_received(
+              :perform_bulk
+            ).once.with(searchable_patients.first(2).map(&:id).zip)
+          end
+        end
+
+        context "and the batch size is greater than the daily limit" do
+          let(:daily_limit) { 10 }
+
+          def setup_stubs
+            allow(SearchVaccinationRecordsInNHSJob).to receive(:perform_bulk)
+
+            allow_any_instance_of(EnqueueVaccinationsSearchInNHSJob).to receive(
+              :daily_enqueue_size_limit
+            ).and_return(daily_limit)
+          end
+
+          it "searches for the daily limit of patients" do
+            expect(SearchVaccinationRecordsInNHSJob).to have_received(
+              :perform_bulk
+            ).once.with(searchable_patients.first(daily_limit).map(&:id).zip)
+          end
+        end
+      end
+    end
+
+    describe "when a patient shows up on both session and rolling searches" do
+      before do
+        Flipper.enable(:imms_api_enqueue_session_searches)
+        Flipper.enable(:imms_api_enqueue_rolling_searches)
+
+        allow(SearchVaccinationRecordsInNHSJob).to receive(:perform_bulk)
+
+        patient
+
+        described_class.perform_now
+      end
+
+      let(:school) { create(:school, team:, programmes:, gias_year_groups:) }
+      let(:session) do
+        create(
+          :session,
+          programmes:,
+          academic_year: AcademicYear.pending,
+          dates: [7.days.from_now],
+          send_consent_requests_at: 7.days.ago,
+          days_before_consent_reminders: 7,
+          team:,
+          location: school
+        )
+      end
+      let(:patient) do
+        create(
+          :patient,
+          team:,
+          school:,
+          session:,
+          patient_programme_vaccinations_searches: [
+            PatientProgrammeVaccinationsSearch.new(
+              programme: flu,
+              last_searched_at: 28.days.ago
+            )
+          ]
+        )
+      end
+
+      it "only performs one search for the patient" do
+        expect(SearchVaccinationRecordsInNHSJob).to have_received(
+          :perform_bulk
+        ).once.with([[patient.id]])
+      end
     end
   end
 end
