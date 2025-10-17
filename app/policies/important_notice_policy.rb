30dfa2fd109df654a89d245747f012cf92b39c68
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2025-10-17 15:12:41 +0100

    Create ImportantNotice model and policy
    This supports the dismiss notice feature. The policy restricts notices to superusers in the select team only.

---
+++ app/policies/important_notice_policy.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class ImportantNoticePolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope
+        .joins(:patient)
+        .active(team: user.selected_team)
+        .where(team_id: user.selected_team.id)
+    end
+  end
+
+  def index?
+    user.can_access_sensitive_flagged_records?
+  end
+end
