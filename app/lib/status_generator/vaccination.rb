51f663f6d06b4ecf0f6a94c70815ec355033c985
Thomas Leese <thomas@leese.io>
2025-10-17 07:48:49 +0100

    Refactor `StatusGenerator::Vaccination`
    This refactors the class responsible for determining the vaccination
    status in preparation for additional work related to MMR vaccinations.
    
    Jira-Issue: MAV-2082

--- app/lib/status_generator/vaccination.rb
+++ app/lib/status_generator/vaccination.rb
@@ -18,7 +18,15 @@ class StatusGenerator::Vaccination
     @consents = consents
     @triages = triages
     @attendance_record = attendance_record
-    @vaccination_records = vaccination_records
+
+    @vaccination_records = vaccination_records.select do
+      it.patient_id == patient.id && it.programme_id == programme_id &&
+        if programme.seasonal?
+          it.academic_year == academic_year
+        else
+          it.academic_year <= academic_year
+        end
+    end
   end
 
   def status
@@ -40,26 +48,26 @@ class StatusGenerator::Vaccination
     return unless status_should_be_due? || status_should_be_eligible?
 
     latest_dose_sequence =
-      relevant_vaccination_records.filter_map(&:dose_sequence).max || 0
+      valid_vaccination_records.filter_map(&:dose_sequence).max || 0
 
     latest_dose_sequence + 1
   end
 
   def latest_date
     if status_should_be_vaccinated?
-      vaccination_record.performed_at.to_date
+      vaccinated_vaccination_record.performed_at.to_date
     elsif latest_session_status_should_be_absent?
       [
-        relevant_vaccination_records.map(&:performed_at).max&.to_date,
+        vaccination_records.map(&:performed_at).max&.to_date,
         attendance_record&.created_at&.to_date
       ].compact.max
     else
-      relevant_vaccination_records.map(&:performed_at).max&.to_date
+      vaccination_records.map(&:performed_at).max&.to_date
     end
   end
 
   def latest_location_id
-    vaccination_record&.location_id if status_should_be_vaccinated?
+    vaccinated_vaccination_record&.location_id if status_should_be_vaccinated?
   end
 
   def latest_session_status
@@ -87,53 +95,88 @@ class StatusGenerator::Vaccination
               :attendance_record,
               :vaccination_records
 
+  def status_should_be_vaccinated?
+    vaccinated_vaccination_record != nil
+  end
+
+  def status_should_be_due?
+    return false unless is_eligible?
+
+    return false unless consent_generator.status == :given
+
+    triage_generator.status.in?(%i[safe_to_vaccinate not_required])
+  end
+
+  def status_should_be_eligible? = is_eligible?
+
+  def latest_session_status_should_be_contraindicated?
+    vaccination_records.last&.contraindications?
+  end
+
+  def latest_session_status_should_be_refused?
+    vaccination_records.last&.refused?
+  end
+
+  def latest_session_status_should_be_absent?
+    vaccination_records.last&.absent_from_session? ||
+      attendance_record&.attending == false
+  end
+
+  def latest_session_status_should_be_unwell?
+    vaccination_records.last&.not_well?
+  end
+
   def programme_id = programme.id
 
   def year_group = patient.year_group(academic_year:)
 
-  def relevant_vaccination_records
-    @relevant_vaccination_records ||=
-      vaccination_records.select do
-        it.patient_id == patient.id && it.programme_id == programme_id &&
-          if programme.seasonal?
-            it.academic_year == academic_year
-          else
-            it.academic_year <= academic_year
+  def valid_vaccination_records
+    @valid_vaccination_records ||=
+      if programme.seasonal?
+        vaccination_records.select { it.administered? || it.already_had? }
+      else
+        if (
+          already_had_records = vaccination_records.select(&:already_had?)
+        ).present?
+          return already_had_records
+        end
+
+        administered_records = vaccination_records.select(&:administered?)
+
+        if programme.hpv?
+          administered_records
+        elsif programme.menacwy?
+          administered_records.select do
+            patient.age(now: it.performed_at) >= 10
           end
+        elsif programme.mmr?
+          administered_records # TODO: Implement vaccination criteria
+        elsif programme.td_ipv?
+          administered_records.select do
+            patient.age(now: it.performed_at) >= 10
+          end
+        else
+          raise UnsupportedProgramme, programme
+        end
       end
   end
 
-  def vaccination_record
-    if programme.seasonal?
-      relevant_vaccination_records.find { it.administered? || it.already_had? }
-    elsif programme.mmr?
-      nil # TODO: Implement vaccination criteria
-    else
-      if (
-           already_had_record =
-             relevant_vaccination_records.find(&:already_had?)
-         )
-        return already_had_record
-      end
+  def vaccinated_vaccination_record
+    @vaccinated_vaccination_record ||=
+      begin
+        if (already_had_record = valid_vaccination_records.find(&:already_had?))
+          return already_had_record
+        end
 
-      administered_records =
-        relevant_vaccination_records.select(&:administered?)
-
-      if programme.hpv?
-        administered_records.first
-      elsif programme.menacwy?
-        administered_records.find { patient.age(now: it.performed_at) >= 10 }
-      elsif programme.td_ipv?
-        administered_records.find do
-          (
+        if programme.vaccinated_dose_sequence > 1
+          valid_vaccination_records.find do
             it.dose_sequence == programme.vaccinated_dose_sequence ||
               (it.dose_sequence.nil? && it.recorded_in_service?)
-          ) && patient.age(now: it.performed_at) >= 10
+          end
+        else
+          valid_vaccination_records.first
         end
-      else
-        raise UnsupportedProgramme, programme
       end
-    end
   end
 
   def is_eligible?
@@ -141,11 +184,11 @@ class StatusGenerator::Vaccination
       patient_locations
         .select { it.academic_year == academic_year }
         .any? do |patient_location|
-          location = patient_location.location
-          year_group.in?(
-            location.programme_year_groups(academic_year:)[programme]
-          )
-        end
+        location = patient_location.location
+        year_group.in?(
+          location.programme_year_groups(academic_year:)[programme]
+        )
+      end
   end
 
   def consent_generator
@@ -169,36 +212,5 @@ class StatusGenerator::Vaccination
         triages:,
         vaccination_records:
       )
-  end
-
-  def status_should_be_vaccinated?
-    vaccination_record != nil
-  end
-
-  def status_should_be_due?
-    return false unless is_eligible?
-
-    return false unless consent_generator.status == :given
-
-    triage_generator.status.in?(%i[safe_to_vaccinate not_required])
-  end
-
-  def status_should_be_eligible? = is_eligible?
-
-  def latest_session_status_should_be_contraindicated?
-    relevant_vaccination_records.last&.contraindications?
-  end
-
-  def latest_session_status_should_be_refused?
-    relevant_vaccination_records.last&.refused?
-  end
-
-  def latest_session_status_should_be_absent?
-    relevant_vaccination_records.last&.absent_from_session? ||
-      attendance_record&.attending == false
-  end
-
-  def latest_session_status_should_be_unwell?
-    relevant_vaccination_records.last&.not_well?
   end
 end
