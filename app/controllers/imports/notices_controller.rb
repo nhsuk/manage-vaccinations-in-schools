15992d8d150f4b531fecab75cd47de5915b625be
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2025-10-17 16:01:44 +0100

    Add Dismiss link on Important notices table
    This allows users to dismiss the notice once they have acknowledged it. It is only for notices that cannot be dismissed by fixing the issue, eg invalid records.

--- app/controllers/imports/notices_controller.rb
+++ app/controllers/imports/notices_controller.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
 class Imports::NoticesController < ApplicationController
+  before_action :set_notice, only: %i[dismiss destroy]
+
   layout "full"
 
   def index
     authorize ImportantNotice
 
     @notices = policy_scope(ImportantNotice).order(recorded_at: :desc)
+  end
+
+  def dismiss
+  end
+
+  def destroy
+    @notice.dismiss!(user: current_user)
+
+    redirect_to imports_notices_path, flash: { success: "Notice dismissed" }
+  end
+
+  private
+
+  def set_notice
+    @notice = authorize policy_scope(ImportantNotice).find(params[:id])
   end
 end
