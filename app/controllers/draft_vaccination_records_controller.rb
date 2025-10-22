d1e928158c1f8a53d032dbb8219911e6321fb56a
Martin van IJcken <martin.vanijcken@tngtech.com>
2025-10-22 11:13:03 +0100

    log: include patient_id in logs
    Jira-Issue: MAV-1069

--- app/controllers/draft_vaccination_records_controller.rb
+++ app/controllers/draft_vaccination_records_controller.rb
@@ -11,6 +11,7 @@ class DraftVaccinationRecordsController < ApplicationController
   before_action :set_programme
   before_action :set_vaccination_record
 
+  include PatientLoggingConcern
   include WizardControllerConcern
 
   before_action :validate_params, only: :update
@@ -298,5 +299,9 @@ class DraftVaccinationRecordsController < ApplicationController
       )
 
     @vaccination_record.update!(next_dose_delay_triage:)
+  end
+
+  def patient_id_for_logging
+    @patient.id
   end
 end
