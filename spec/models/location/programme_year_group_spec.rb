8c400c578070d176f7fe686eafb978e885911191
Thomas Leese <thomas@leese.io>
2025-10-22 22:46:38 +0100

    Remove ignored columns
    This removes columns which have been marked as ignored and therefore can
    be safely removed without any impact.

--- spec/models/location/programme_year_group_spec.rb
+++ spec/models/location/programme_year_group_spec.rb
@@ -10,14 +10,12 @@
 #
 # Indexes
 #
-#  idx_on_location_id_academic_year_programme_id_year__6ad5e2b67d  (location_id,academic_year,programme_id,year_group) UNIQUE
 #  idx_on_location_year_group_id_programme_id_405f51181e           (location_year_group_id,programme_id) UNIQUE
 #  index_location_programme_year_groups_on_location_year_group_id  (location_year_group_id)
 #  index_location_programme_year_groups_on_programme_id            (programme_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (location_id => locations.id) ON DELETE => cascade
 #  fk_rails_...  (location_year_group_id => location_year_groups.id) ON DELETE => cascade
 #  fk_rails_...  (programme_id => programmes.id) ON DELETE => cascade
 #
