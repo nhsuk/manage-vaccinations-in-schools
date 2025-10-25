8380559b89d0624b074f9ce4a1d40831f2a42ef4
Thomas Leese <thomas@leese.io>
2025-10-25 14:54:52 +0100

    Run `app:update`
    This updates the default configuration to Rails 8.1.
    
    Jira-Issue: MAV-2386

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "rails/test_unit/railtie"
-require "cgi"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
