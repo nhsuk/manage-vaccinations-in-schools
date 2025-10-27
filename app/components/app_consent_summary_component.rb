9f31a435025dfa0c3c375dc67cab498575cde35c
Thomas Leese <thomas@leese.io>
2025-10-27 14:39:54 +0000

    Refactor `AppConsentSummaryComponent`
    This refactors the component to use a pattern we've got in other
    components where each row of the summary list is its own method, which
    should improve readability and allow for more flexibility in the future.
    
    Jira-Issue: MAV-2425

--- app/components/app_consent_summary_component.rb
+++ app/components/app_consent_summary_component.rb
@@ -6,97 +6,7 @@ class AppConsentSummaryComponent < ViewComponent::Base
     @change_links = change_links
   end
 
-  def call
-    govuk_summary_list(actions: @change_links.present?) do |summary_list|
-      summary_list.with_row do |row|
-        row.with_key { "Programme" }
-        row.with_value do
-          tag.strong(
-            programme.name,
-            class: "nhsuk-tag app-tag--attached nhsuk-tag--white"
-          )
-        end
-      end
-
-      if consent.responded_at.present?
-        summary_list.with_row do |row|
-          row.with_key { "Date" }
-          row.with_value { consent.responded_at.to_fs(:long) }
-        end
-      end
-
-      summary_list.with_row do |row|
-        row.with_key { "Method" }
-        row.with_value { consent.human_enum_name(:route).humanize }
-        if (href = change_links[:route])
-          row.with_action(text: "Change", visually_hidden_text: "method", href:)
-        end
-      end
-
-      summary_list.with_row do |row|
-        row.with_key { "Response" }
-        row.with_value { consent_response_tag(consent) }
-        if (href = change_links[:response])
-          row.with_action(
-            text: "Change",
-            visually_hidden_text: "decision",
-            href:
-          )
-        end
-      end
-
-      if consent.vaccine_method_nasal?
-        summary_list.with_row do |row|
-          row.with_key { "Consent also given for injected vaccine?" }
-          row.with_value { consent.vaccine_method_injection? ? "Yes" : "No" }
-        end
-      end
-
-      unless consent.without_gelatine.nil?
-        summary_list.with_row do |row|
-          row.with_key { "Consent given for gelatine-free vaccine only?" }
-          row.with_value { consent.without_gelatine ? "Yes" : "No" }
-        end
-      end
-
-      unless consent.notify_parents_on_vaccination.nil?
-        summary_list.with_row do |row|
-          row.with_key { "Confirmation of vaccination sent to parent?" }
-          row.with_value do
-            consent.notify_parents_on_vaccination ? "Yes" : "No"
-          end
-          if (href = change_links[:notify_parents_on_vaccination])
-            row.with_action(
-              text: "Change",
-              visually_hidden_text: "decision",
-              href:
-            )
-          end
-        end
-      end
-
-      if consent.reason_for_refusal.present?
-        summary_list.with_row do |row|
-          row.with_key { "Reason for refusal" }
-          row.with_value { consent.human_enum_name(:reason_for_refusal) }
-        end
-      end
-
-      unless consent.notify_parent_on_refusal.nil?
-        summary_list.with_row do |row|
-          row.with_key { "Confirmation of decision sent to parent?" }
-          row.with_value { consent.notify_parent_on_refusal ? "Yes" : "No" }
-        end
-      end
-
-      if consent.notes.present?
-        summary_list.with_row do |row|
-          row.with_key { "Notes" }
-          row.with_value { consent.notes }
-        end
-      end
-    end
-  end
+  def call = govuk_summary_list(rows:, actions: @change_links.present?)
 
   private
 
@@ -104,4 +14,158 @@ class AppConsentSummaryComponent < ViewComponent::Base
 
   delegate :programme, to: :consent
   delegate :consent_response_tag, :govuk_summary_list, to: :helpers
+
+  def rows
+    [
+      programme_row,
+      date_row,
+      method_row,
+      response_row,
+      injection_alternative_row,
+      without_gelatine_row,
+      notify_parents_on_vaccination_row,
+      reason_for_refusal_row,
+      notify_parent_on_refusal_row,
+      notes_row
+    ].compact
+  end
+
+  def programme_row
+    {
+      key: {
+        text: "Programme"
+      },
+      value: {
+        text:
+          tag.strong(
+            programme.name,
+            class: "nhsuk-tag app-tag--attached nhsuk-tag--white"
+          )
+      }
+    }
+  end
+
+  def date_row
+    return if consent.responded_at.nil?
+
+    {
+      key: {
+        text: "Date"
+      },
+      value: {
+        text: consent.responded_at.to_fs(:long)
+      }
+    }
+  end
+
+  def method_row
+    {
+      key: {
+        text: "Method"
+      },
+      value: {
+        text: consent.human_enum_name(:route).humanize
+      },
+      actions: [
+        if (href = change_links[:route])
+          { href:, visually_hidden_text: "method" }
+        end
+      ].compact
+    }
+  end
+
+  def response_row
+    {
+      key: {
+        text: "Response"
+      },
+      value: {
+        text: consent_response_tag(consent)
+      },
+      actions: [
+        if (href = change_links[:response])
+          { href:, visually_hidden_text: "decision" }
+        end
+      ].compact
+    }
+  end
+
+  def injection_alternative_row
+    return unless consent.vaccine_method_nasal?
+
+    {
+      key: {
+        text: "Consent also given for injected vaccine?"
+      },
+      value: {
+        text: consent.vaccine_method_injection? ? "Yes" : "No"
+      }
+    }
+  end
+
+  def without_gelatine_row
+    return if consent.without_gelatine.nil?
+
+    {
+      key: {
+        text: "Consent given for gelatine-free vaccine only?"
+      },
+      value: {
+        text: consent.without_gelatine ? "Yes" : "No"
+      }
+    }
+  end
+
+  def notify_parents_on_vaccination_row
+    return if consent.notify_parents_on_vaccination.nil?
+
+    {
+      key: {
+        text: "Confirmation of vaccination sent to parent?"
+      },
+      value: {
+        text: consent.notify_parents_on_vaccination ? "Yes" : "No"
+      },
+      actions: [
+        if (href = change_links[:notify_parents_on_vaccination])
+          {
+            href:,
+            visually_hidden_text: "confirmation of vaccination sent to parent"
+          }
+        end
+      ].compact
+    }
+  end
+
+  def reason_for_refusal_row
+    return if consent.reason_for_refusal.nil?
+
+    {
+      key: {
+        text: "Reason for refusal"
+      },
+      value: {
+        text: consent.human_enum_name(:reason_for_refusal)
+      }
+    }
+  end
+
+  def notify_parent_on_refusal_row
+    return if consent.notify_parent_on_refusal.nil?
+
+    {
+      key: {
+        text: "Confirmation of decision sent to parent?"
+      },
+      value: {
+        text: consent.notify_parent_on_refusal ? "Yes" : "No"
+      }
+    }
+  end
+
+  def notes_row
+    return if consent.notes.blank?
+
+    { key: { text: "Notes" }, value: { text: consent.notes } }
+  end
 end
