58c545c5669b64a68b99136b29de94329599a47a
Mike Thompson <mike@artifactual.co.uk>
2025-11-04 17:42:11 +0000

    Move session timeout concern functions to users sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  include SessionTimeoutConcern
   prepend_before_action :skip_activity_tracking, only: [:time_remaining]
 
   skip_before_action :authenticate_user!,
@@ -21,5 +20,26 @@ class Users::SessionsController < Devise::SessionsController
 
   def refresh
     render json: { time_remaining_seconds: calc_time_remaining_seconds }
+  end
+
+  private
+
+  def calc_time_remaining_seconds
+    last_activity = session.dig("warden.user.user.session", "last_request_at")
+    if last_activity.class != Integer
+      raise "Last request at value from session is not an integer"
+    end
+    expires_at = last_activity + Devise.timeout_in
+    [(expires_at - Time.current.to_i), 0].max
+  end
+
+  def return_unauthorized_if_not_signed_in
+    unless user_signed_in?
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
+  end
+
+  def skip_activity_tracking
+    request.env["devise.skip_trackable"] = true
   end
 end
