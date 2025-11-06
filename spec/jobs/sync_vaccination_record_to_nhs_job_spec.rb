d55fc8d6b338b3d58ed0467f69b0a85d43cf12d4
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-11-06 17:28:25 +0000

    Add per-programme feature flagging
    This uses the feature flagging `actor`s
    
    Jira-Issue: MAV-2487

--- spec/jobs/sync_vaccination_record_to_nhs_job_spec.rb
+++ spec/jobs/sync_vaccination_record_to_nhs_job_spec.rb
@@ -1,18 +1,60 @@
 # frozen_string_literal: true
 
 describe SyncVaccinationRecordToNHSJob, type: :job do
-  before do
-    allow(NHS::ImmunisationsAPI).to receive(:sync_immunisation)
-    Flipper.enable(:imms_api_sync_job)
-  end
+  subject(:perform) { described_class.new.perform(vaccination_record.id) }
+
+  before { allow(NHS::ImmunisationsAPI).to receive(:sync_immunisation) }
 
   after { Flipper.disable(:imms_api_sync_job) }
 
-  let(:vaccination_record) { create(:vaccination_record) }
+  let(:programme) { CachedProgramme.flu }
+  let(:vaccination_record) { create(:vaccination_record, programme:) }
 
-  it "syncs the vaccination" do
-    described_class.new.perform(vaccination_record.id)
+  context "with the feature flag fully on" do
+    before { Flipper.enable(:imms_api_sync_job) }
 
-    expect(NHS::ImmunisationsAPI).to have_received(:sync_immunisation)
+    it "syncs the vaccination" do
+      perform
+
+      expect(NHS::ImmunisationsAPI).to have_received(:sync_immunisation)
+    end
+  end
+
+  context "with the feature flag off" do
+    before { Flipper.disable(:imms_api_sync_job) }
+
+    it "doesn't sync the record" do
+      perform
+
+      expect(NHS::ImmunisationsAPI).not_to have_received(:sync_immunisation)
+    end
+  end
+
+  context "with the feature flag on for the correct programme" do
+    before do
+      Flipper.disable(:imms_api_sync_job)
+      Flipper.enable(:imms_api_sync_job, programme)
+    end
+
+    it "syncs the vaccination" do
+      perform
+
+      expect(NHS::ImmunisationsAPI).to have_received(:sync_immunisation)
+    end
+  end
+
+  context "with the feature flag on for the wrong programme" do
+    before do
+      Flipper.disable(:imms_api_sync_job)
+      Flipper.enable(:imms_api_sync_job, other_programme)
+    end
+
+    let(:other_programme) { CachedProgramme.hpv }
+
+    it "doesn't sync the vaccination" do
+      perform
+
+      expect(NHS::ImmunisationsAPI).not_to have_received(:sync_immunisation)
+    end
   end
 end
