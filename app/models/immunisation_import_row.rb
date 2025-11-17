8146a184f99da5f4ef06303730c43951e6a2ba6e
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-11-17 15:29:34 +0000

    Search for vaccine by `nivs_name` as well as `upload_name`
    Jira-Issue: MAV-2778

--- app/models/immunisation_import_row.rb
+++ app/models/immunisation_import_row.rb
@@ -400,7 +400,11 @@ class ImmunisationImportRow
   end
 
   def vaccine
-    @vaccine ||= team.vaccines.find_by(upload_name: vaccine_upload_name)
+    return if vaccine_upload_name.nil?
+
+    @vaccine ||=
+      team.vaccines.find_by(upload_name: vaccine_upload_name) ||
+        team.vaccines.find_by(nivs_name: vaccine_upload_name)
   end
 
   def batch
