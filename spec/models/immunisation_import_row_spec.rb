14af4b241268f22c615e1bda69dbdde0a79253b7
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-11-27 11:09:51 +0000

    Require `ORGANISATION_CODE` for bulk uploads
    Jira-Issue: MAV-

--- spec/models/immunisation_import_row_spec.rb
+++ spec/models/immunisation_import_row_spec.rb
@@ -911,7 +911,8 @@ describe ImmunisationImportRow do
               "<code>PERSON_FORENAME</code> or <code>First name</code> is required",
               "<code>PERSON_GENDER_CODE</code>, <code>PERSON_GENDER</code> or <code>Sex</code> is required",
               "<code>PERSON_SURNAME</code> or <code>Surname</code> is required",
-              "<code>PERSON_POSTCODE</code> or <code>Postcode</code> is required"
+              "<code>PERSON_POSTCODE</code> or <code>Postcode</code> is required",
+              "<code>ORGANISATION_CODE</code> is required"
             )
           end
         end
