748fd60df888b098e7333f9f7747239045592dcd
Thomas Leese <thomas@leese.io>
2025-11-29 07:17:37 +0000

    Send consent form confirmation in background job
    We want to start sending the confirmation emails after the consent form
    is matched with a patient to be able to fetch the year group.
    
    Jira-Issue: MAV-2663

--- app/controllers/parent_interface/consent_forms_controller.rb
+++ app/controllers/parent_interface/consent_forms_controller.rb
@@ -62,9 +62,6 @@ module ParentInterface
 
       TeamCachedCounts.new(@team).reset_unmatched_consent_responses!
 
-      @consent_form.notifier.send_confirmation
-      @consent_form.confirmation_sent!
-
       ProcessConsentFormJob.perform_later(@consent_form)
     end
 
