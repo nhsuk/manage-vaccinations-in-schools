28e4997a41b0ca9a232086667a15e8b28366926d
Thomas Leese <thomas@leese.io>
2025-11-29 13:25:55 +0000

    Update `AppSessionCardComponent`
    This updates the component to support some new features which are going
    to be shown on the new sessions page.
    
    Jira-Issue: MAV-2645

--- spec/components/app_session_card_component_spec.rb
+++ spec/components/app_session_card_component_spec.rb
@@ -14,9 +14,25 @@ describe AppSessionCardComponent do
     create(:session, academic_year: 2025, date:, programmes: [programme])
   end
 
-  it { should have_text("Cohort100 children") }
+  it { should have_text("Children100 children") }
   it { should have_text("ProgrammesFlu") }
-  it { should have_text("StatusScheduled") }
-  it { should have_text("Session dates1 September 2025") }
+
+  it do
+    expect(rendered).to have_text(
+      "Year groupsReception, Years 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, and 11"
+    )
+  end
+
+  it { should have_text("Date1 September 2025") }
   it { should have_text("Consent periodOpens 11 August") }
+
+  it { should_not have_text("Status") }
+
+  context "when showing status" do
+    let(:component) do
+      described_class.new(session, patient_count: 100, show_status: true)
+    end
+
+    it { should have_text("StatusScheduled") }
+  end
 end
