97017be4be7ed39b07970fcae14c5c74e3f0e1a1
Thomas Leese <thomas@leese.io>
2025-12-03 10:58:19 +0000

    Add `AppSessionButtonsComponent`
    This adds a new component which renders the edit button and others for a
    session. We need this to be able to show it on different sections
    depending on the feature flag.
    
    Jira-Issue: MAV-2645

---
+++ app/components/app_session_buttons_component.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class AppSessionButtonsComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <div class="nhsuk-button-group">
+      <% if policy(session).edit? %>
+        <%= govuk_button_link_to edit_button_text, edit_session_path(session), secondary: true %>
+
+        <%= link_to "Record offline", session_path(session, format: :xlsx) %>
+
+        <% if session.clinic? && session.can_send_clinic_invitations? %>
+          <%= link_to "Send booking reminders", edit_session_invite_to_clinic_path(@session) %>
+        <% elsif session.school? && session.can_send_clinic_invitations? %>
+          <%= link_to "Send clinic invitations", edit_session_invite_to_clinic_path(@session) %>
+        <% end %>
+      <% end %>
+    </div>
+  ERB
+
+  def initialize(session)
+    @session = session
+  end
+
+  private
+
+  attr_reader :session
+
+  delegate :policy, :govuk_button_link_to, to: :helpers
+
+  def edit_button_text
+    session.dates.empty? ? "Schedule sessions" : "Edit session"
+  end
+end
