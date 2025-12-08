3304644416b62c87e6a3c5a2c5890c0cb64fa232
Thomas Leese <thomas@leese.io>
2025-12-08 11:42:14 +0000

    Add schema comment to `ConsentFormProgramme`
    This is generated automatically.

--- spec/models/consent_form_programme_spec.rb
+++ spec/models/consent_form_programme_spec.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: consent_form_programmes
+#
+#  id                 :bigint           not null, primary key
+#  notes              :text             default(""), not null
+#  programme_type     :enum             not null
+#  reason_for_refusal :integer
+#  response           :integer
+#  vaccine_methods    :integer          default([]), not null, is an Array
+#  without_gelatine   :boolean
+#  consent_form_id    :bigint           not null
+#
+# Indexes
+#
+#  idx_on_programme_type_consent_form_id_805eb5d685  (programme_type,consent_form_id) UNIQUE
+#  index_consent_form_programmes_on_consent_form_id  (consent_form_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (consent_form_id => consent_forms.id) ON DELETE => cascade
+#
 describe ConsentFormProgramme do
   let(:programme) { Programme.mmr }
   let(:session) { create(:session, programmes: [programme]) }
