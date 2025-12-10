nhsuk/manage-vaccinations-in-schools

commit 7bc4e63ec6f465088d29851afbe34d75a8e8c40e

Add new session header design
In this design the session header will have a darker blue background to
highlight the difference between being in a session and viewing a
school.

Jira-Issue: MAV-2645

app/views/sessions/_header.html.erb
@@ -1,56 +1,120 @@
-<%= h1 @session.location.name, class: "nhsuk-u-margin-bottom-2" %>
+<% content_for :before_container do %>
+  <% if Flipper.enabled?(:schools_and_sessions) %>
+    <div class="app-session-banner">
+      <div class="nhsuk-width-container">
+        <div class="nhsuk-main-wrapper nhsuk-main-wrapper--s">
+          <%= h1 session_title(@session), class: "nhsuk-u-margin-bottom-2" %>
 
-<p class="nhsuk-caption-l nhsuk-u-margin-bottom-4">
-  <%= @session.programmes(patient: @patient).map(&:name).to_sentence %>
-  (<%= format_academic_year(@session.academic_year) %>)
-</p>
+          <p class="nhsuk-caption-l nhsuk-u-margin-bottom-0">
+            <%= format_year_groups(@session.year_groups) %>
+          </p>
+        </div>
 
-<%= render AppSecondaryNavigationComponent.new do |nav|
-      nav.with_item(
-        href: session_path(@session),
-        text: t("sessions.tabs.overview"),
-        selected: request.path == session_path(@session),
-      )
-    
-      nav.with_item(
-        href: session_patients_path(@session),
-        text: t("sessions.tabs.patients"),
-        selected: request.path == session_patients_path(@session),
-      )
-    
-      unless Flipper.enabled?(:programme_status, current_team)
+        <%= render AppSecondaryNavigationComponent.new(reverse: true) do |nav|
+              nav.with_item(
+                href: session_path(@session),
+                text: t("sessions.tabs.overview"),
+                selected: request.path == session_path(@session),
+              )
+            
+              nav.with_item(
+                href: session_patients_path(@session),
+                text: t("sessions.tabs.patients"),
+                selected: request.path == session_patients_path(@session),
+              )
+            
+              if @session.psd_enabled?
+                nav.with_item(
+                  href: session_patient_specific_directions_path(@session),
+                  text: t("sessions.tabs.patient_specific_directions"),
+                  selected: request.path == session_patient_specific_directions_path(@session),
+                )
+              end
+            
+              if @session.requires_registration?
+                nav.with_item(
+                  href: session_register_path(@session),
+                  text: t("sessions.tabs.register"),
+                  selected: request.path == session_register_path(@session),
+                )
+              end
+            
+              nav.with_item(
+                href: session_record_path(@session),
+                text: t("sessions.tabs.record"),
+                selected: request.path == session_record_path(@session),
+              )
+            end %>
+      </div>
+    </div>
+  <% end %>
+<% end %>
+
+<% content_for :before_main do %>
+  <% unless Flipper.enabled?(:schools_and_sessions) %>
+    <%= render AppBreadcrumbComponent.new(items: [
+                                            { text: t("dashboard.index.title"), href: dashboard_path },
+                                            { text: t("sessions.index.title"), href: sessions_path },
+                                            { text: @session.location.name, href: session_path(@session) },
+                                          ]) %>
+  <% end %>
+<% end %>
+
+<% unless Flipper.enabled?(:schools_and_sessions) %>
+  <%= h1 @session.location.name, class: "nhsuk-u-margin-bottom-2" %>
+
+  <p class="nhsuk-caption-l nhsuk-u-margin-bottom-4">
+    <%= @session.programmes(patient: @patient).map(&:name).to_sentence %>
+    (<%= format_academic_year(@session.academic_year) %>)
+  </p>
+
+  <%= render AppSecondaryNavigationComponent.new do |nav|
         nav.with_item(
-          href: session_consent_path(@session),
-          text: t("sessions.tabs.consent"),
-          selected: request.path == session_consent_path(@session),
+          href: session_path(@session),
+          text: t("sessions.tabs.overview"),
+          selected: request.path == session_path(@session),
         )
-    
+      
         nav.with_item(
-          href: session_triage_path(@session),
-          text: t("sessions.tabs.triage"),
-          selected: request.path == session_triage_path(@session),
+          href: session_patients_path(@session),
+          text: t("sessions.tabs.patients"),
+          selected: request.path == session_patients_path(@session),
         )
-      end
-    
-      if @session.psd_enabled?
+      
+        unless Flipper.enabled?(:programme_status, current_team)
+          nav.with_item(
+            href: session_consent_path(@session),
+            text: t("sessions.tabs.consent"),
+            selected: request.path == session_consent_path(@session),
+          )
+      
+          nav.with_item(
+            href: session_triage_path(@session),
+            text: t("sessions.tabs.triage"),
+            selected: request.path == session_triage_path(@session),
+          )
+        end
+      
+        if @session.psd_enabled?
+          nav.with_item(
+            href: session_patient_specific_directions_path(@session),
+            text: t("sessions.tabs.patient_specific_directions"),
+            selected: request.path == session_patient_specific_directions_path(@session),
+          )
+        end
+      
+        if @session.requires_registration?
+          nav.with_item(
+            href: session_register_path(@session),
+            text: t("sessions.tabs.register"),
+            selected: request.path == session_register_path(@session),
+          )
+        end
+      
         nav.with_item(
-          href: session_patient_specific_directions_path(@session),
-          text: t("sessions.tabs.patient_specific_directions"),
-          selected: request.path == session_patient_specific_directions_path(@session),
+          href: session_record_path(@session),
+          text: t("sessions.tabs.record"),
+          selected: request.path == session_record_path(@session),
         )
-      end
-    
-      if @session.requires_registration?
-        nav.with_item(
-          href: session_register_path(@session),
-          text: t("sessions.tabs.register"),
-          selected: request.path == session_register_path(@session),
-        )
-      end
-    
-      nav.with_item(
-        href: session_record_path(@session),
-        text: t("sessions.tabs.record"),
-        selected: request.path == session_record_path(@session),
-      )
-    end %>
+      end %>
+<% end %>

app/views/sessions/show.html.erb
@@ -1,8 +1,10 @@
 <% content_for :before_main do %>
-  <%= render AppBreadcrumbComponent.new(items: [
-                                          { text: t("dashboard.index.title"), href: dashboard_path },
-                                          { text: t("sessions.index.title"), href: sessions_path },
-                                        ]) %>
+  <% unless Flipper.enabled?(:schools_and_sessions) %>
+    <%= render AppBreadcrumbComponent.new(items: [
+                                            { text: t("dashboard.index.title"), href: dashboard_path },
+                                            { text: t("sessions.index.title"), href: sessions_path },
+                                          ]) %>
+  <% end %>
 <% end %>
 
 <%= render "header" %>

app/views/sessions/consent/show.html.erb
@@ -1,11 +1,3 @@
-<% content_for :before_main do %>
-  <%= render AppBreadcrumbComponent.new(items: [
-                                          { text: t("dashboard.index.title"), href: dashboard_path },
-                                          { text: t("sessions.index.title"), href: sessions_path },
-                                          { text: @session.location.name, href: session_path(@session) },
-                                        ]) %>
-<% end %>
-
 <%= render "sessions/header" %>
 
 <div class="nhsuk-grid-row">

app/views/sessions/manage_consent_reminders/show.html.erb
@@ -1,9 +1,11 @@
 <% content_for :before_main do %>
-  <%= render AppBreadcrumbComponent.new(items: [
-                                          { text: t("dashboard.index.title"), href: dashboard_path },
-                                          { text: t("sessions.index.title"), href: sessions_path },
-                                          { text: @session.location.name, href: session_path(@session) },
-                                        ]) %>
+  <% unless Flipper.enabled?(:schools_and_sessions) %>
+    <%= render AppBreadcrumbComponent.new(items: [
+                                            { text: t("dashboard.index.title"), href: dashboard_path },
+                                            { text: t("sessions.index.title"), href: sessions_path },
+                                            { text: @session.location.name, href: session_path(@session) },
+                                          ]) %>
+  <% end %>
 <% end %>
 
 <%= h1 t(".title") %>

app/views/sessions/patient_specific_directions/show.html.erb
@@ -1,11 +1,3 @@
-<% content_for :before_main do %>
-  <%= render AppBreadcrumbComponent.new(items: [
-                                          { text: t("dashboard.index.title"), href: dashboard_path },
-                                          { text: t("sessions.index.title"), href: sessions_path },
-                                          { text: @session.location.name, href: session_path(@session) },
-                                        ]) %>
-<% end %>
-
 <%= render "sessions/header" %>
 
 <div class="nhsuk-grid-row">

app/views/sessions/patients/show.html.erb
@@ -1,11 +1,3 @@
-<% content_for :before_main do %>
-  <%= render AppBreadcrumbComponent.new(items: [
-                                          { text: t("dashboard.index.title"), href: dashboard_path },
-                                          { text: t("sessions.index.title"), href: sessions_path },
-                                          { text: @session.location.name, href: session_path(@session) },
-                                        ]) %>
-<% end %>
-
 <%= render "sessions/header" %>
 
 <div class="nhsuk-grid-row">

app/views/sessions/record/show.html.erb
@@ -1,11 +1,3 @@
-<% content_for :before_main do %>
-  <%= render AppBreadcrumbComponent.new(items: [
-                                          { text: t("dashboard.index.title"), href: dashboard_path },
-                                          { text: t("sessions.index.title"), href: sessions_path },
-                                          { text: @session.location.name, href: session_path(@session) },
-                                        ]) %>
-<% end %>
-
 <%= render "sessions/header" %>
 
 <% if @session.today? %>

app/views/sessions/register/show.html.erb
@@ -1,11 +1,3 @@
-<% content_for :before_main do %>
-  <%= render AppBreadcrumbComponent.new(items: [
-                                          { text: t("dashboard.index.title"), href: dashboard_path },
-                                          { text: t("sessions.index.title"), href: sessions_path },
-                                          { text: @session.location.name, href: session_path(@session) },
-                                        ]) %>
-<% end %>
-
 <%= render "sessions/header" %>
 
 <% if @session.today? %>

commit a0eff58f5654f2f99606c1d7afbeae61eebfe3cb

Fix breadcrumb items for session
This fixes a regression from 7bc4e63ec6f465088d29851afbe34d75a8e8c40e
where the breadcrumbs on the session overview tab would be duplicates if
the schools and sessions feature flag was turned off.

Jira-Issue: MAV-2645

app/controllers/sessions/base_controller.rb
@@ -3,6 +3,7 @@
 class Sessions::BaseController < ApplicationController
   before_action :set_session
   before_action :set_programme_statuses
+  before_action :set_breadcrumb_items
 
   private
 
@@ -23,5 +24,13 @@ class Sessions::BaseController < ApplicationController
           needs_consent_request_failed
           needs_consent_follow_up_requested
         ]
+  end
+
+  def set_breadcrumb_items
+    @breadcrumb_items = [
+      { text: t("dashboard.index.title"), href: dashboard_path },
+      { text: t("sessions.index.title"), href: sessions_path },
+      { text: @session.location.name, href: session_path(@session) }
+    ]
   end
 end

app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 
   before_action :set_session_search_form, only: :index
   before_action :set_session, except: :index
+  before_action :set_breadcrumb_items, except: :index
 
   def index
     @programmes = current_user.selected_team.programmes
@@ -85,6 +86,13 @@ class SessionsController < ApplicationController
 
   def set_session
     @session = authorize policy_scope(Session).find_by!(slug: params[:slug])
+  end
+
+  def set_breadcrumb_items
+    @breadcrumb_items = [
+      { text: t("dashboard.index.title"), href: dashboard_path },
+      { text: t("sessions.index.title"), href: sessions_path }
+    ]
   end
 
   def patient_counts_for_sessions(sessions)

app/views/sessions/_header.html.erb
@@ -44,11 +44,7 @@
 
 <% content_for :before_main do %>
   <% unless Flipper.enabled?(:schools_and_sessions) %>
-    <%= render AppBreadcrumbComponent.new(items: [
-                                            { text: t("dashboard.index.title"), href: dashboard_path },
-                                            { text: t("sessions.index.title"), href: sessions_path },
-                                            { text: @session.location.name, href: session_path(@session) },
-                                          ]) %>
+    <%= render AppBreadcrumbComponent.new(items: @breadcrumb_items) %>
   <% end %>
 <% end %>
 

app/views/sessions/show.html.erb
@@ -1,12 +1,3 @@
-<% content_for :before_main do %>
-  <% unless Flipper.enabled?(:schools_and_sessions) %>
-    <%= render AppBreadcrumbComponent.new(items: [
-                                            { text: t("dashboard.index.title"), href: dashboard_path },
-                                            { text: t("sessions.index.title"), href: sessions_path },
-                                          ]) %>
-  <% end %>
-<% end %>
-
 <%= render "header" %>
 
 <%= render AppSessionOverviewComponent.new(@session) %>