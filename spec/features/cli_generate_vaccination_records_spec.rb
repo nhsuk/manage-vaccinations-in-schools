8d45d8687f00704397e8a21203835b2e78d55f9c
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-12-11 09:26:55 +0000

    Refactor vaccination records generator cli
    These were broken as a result of recent data refactoring, and also contained
    bugs from the initial implementation.
    
    For example when calculating if there were enough patients to record
    vaccinations, it only considered one session. So if the user requested 5000
    vaccinations, and the first qualifying session didn't have 5000 patients that
    could be vaccinated in it, it would raise an error. This has been fixed to
    consider all the sessions that qualify.

--- spec/features/cli_generate_vaccination_records_spec.rb
+++ spec/features/cli_generate_vaccination_records_spec.rb
@@ -3,35 +3,82 @@
 require_relative "../../app/lib/mavis_cli"
 
 describe "mavis generate vaccination-records" do
-  it "generates vaccination records" do
-    given_an_team_exists
-    and_there_is_a_patient_in_a_session
-    when_i_run_the_generate_vaccination_records_command
-    then_vaccination_administered_records_are_created
+  it "generates vaccination records in all sessions" do
+    given_a_team_with_sessions_exists
+    and_there_are_patients_in_each_session
+    when_i_generate_four_vaccination_records
+    then_four_vaccination_records_are_created
   end
 
-  def given_an_team_exists
+  it "generates vaccination records" do
+    given_a_team_with_sessions_exists
+    and_there_is_a_patient_in_each_session
+    when_i_generate_a_vaccination_record_for_a_specific_session
+    then_the_administered_vaccination_records_is_created_for_that_session
+  end
+
+  def given_a_team_with_sessions_exists
     @programme = Programme.hpv
     @team = create(:team, programmes: [@programme])
+
+    @session1 =
+      create(
+        :session,
+        team: @team,
+        programmes: [@programme],
+        location: create(:generic_clinic, team: @team)
+      )
+    @session2 = create(:session, team: @team, programmes: [@programme])
+    @session3 = create(:session, team: @team, programmes: [@programme])
+    @session4 = create(:session, team: @team, programmes: [@programme])
   end
 
-  def and_there_is_a_patient_in_a_session
-    subteam = create(:subteam, team: @team)
-    location = create(:generic_clinic, subteam:)
-    @session =
-      create(:session, team: @team, programmes: [@programme], location:)
-    parent = create(:parent)
+  def and_there_are_patients_in_each_session
+    [@session1, @session2, @session3, @session4].each do |session|
+      create_list(
+        :patient,
+        3,
+        :consent_given_triage_not_needed,
+        team: @team,
+        session:,
+        programmes: [@programme]
+      )
+    end
+  end
+
+  def and_there_is_a_patient_in_each_session
     create(
       :patient,
       :consent_given_triage_not_needed,
       team: @team,
-      session: @session,
-      programmes: [@programme],
-      parents: [parent]
+      session: @session1,
+      programmes: [@programme]
+    )
+    create(
+      :patient,
+      :consent_given_triage_not_needed,
+      team: @team,
+      session: @session2,
+      programmes: [@programme]
     )
   end
 
-  def when_i_run_the_generate_vaccination_records_command
+  def when_i_generate_four_vaccination_records
+    Dry::CLI.new(MavisCLI).call(
+      arguments: [
+        "generate",
+        "vaccination-records",
+        "-w",
+        @team.workgroup,
+        "-p",
+        @programme.type,
+        "-A",
+        "4"
+      ]
+    )
+  end
+
+  def when_i_generate_a_vaccination_record_for_a_specific_session
     @vaccination_records_count_before = @team.vaccination_records.count
 
     Dry::CLI.new(MavisCLI).call(
@@ -43,28 +90,27 @@ describe "mavis generate vaccination-records" do
         "-p",
         @programme.type,
         "-s",
-        @session.id.to_s,
+        @session1.id.to_s,
         "-A",
         "1"
       ]
     )
   end
 
-  def then_vaccination_administered_records_are_created
-    expect(
-      @team.reload.vaccination_records.count
-    ).to eq @vaccination_records_count_before + 1
+  def then_four_vaccination_records_are_created
+    expect(vaccination_records_for(@team).count).to eq 4
+  end
 
-    expect(
-      @team
-        .reload
-        .patients
-        .has_vaccination_status(
-          :vaccinated,
-          programme: @programme,
-          academic_year: AcademicYear.current
-        )
-        .count
-    ).to eq 1
+  def then_the_administered_vaccination_records_is_created_for_that_session
+    expect(vaccination_records_for(@session1).count).to eq 1
+    expect(vaccination_records_for(@session2).count).to eq 0
+  end
+
+  def vaccination_records_for(team_or_session)
+    team_or_session.reload.patients.has_vaccination_status(
+      :vaccinated,
+      programme: @programme,
+      academic_year: AcademicYear.current
+    )
   end
 end
