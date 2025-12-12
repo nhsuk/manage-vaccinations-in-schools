4c1e3e809afa879a2cc5d473e88744628f72c544
John Henderson <john@logixworks.co.uk>
2025-12-12 10:53:49 +0000

    Ensure disease types in consent status is updated
    This ensures that the consent status of each patient is updated with
    the disease types for the vaccine so we can show the correct vaccine
    batches during the vaccination journey.
    
    Jira-Issue: MAV-2811

--- app/lib/status_updater.rb
+++ app/lib/status_updater.rb
@@ -40,7 +40,7 @@ class StatusUpdater
           batch,
           on_duplicate_key_update: {
             conflict_target: [:id],
-            columns: %i[status vaccine_methods without_gelatine]
+            columns: %i[status vaccine_methods without_gelatine disease_types]
           }
         )
       end
