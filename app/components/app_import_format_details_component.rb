8daf547b241095a56ccff2d7a178ae1aaaa64ff9
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-12-16 16:35:12 +0000

    Refactor to use `has_upload_access_only?`
    Previously, had been using `type_upload_only?`. This approach better
    matches the `has_poc_access?` function. It can also accommodate the
    potential re-introduction of a new `Team.type` for teams which used
    to have access to the national reporting functionality, but now uses
    Mavis as a point-of-care system.

--- app/components/app_import_format_details_component.rb
+++ app/components/app_import_format_details_component.rb
@@ -17,7 +17,7 @@ class AppImportFormatDetailsComponent < ViewComponent::Base
     when CohortImport
       "How to format your Mavis CSV file for child records"
     when ImmunisationImport
-      if team.type_upload_only?
+      if team.has_upload_access_only?
         "How to format your CSV file for vaccination records"
       else
         "How to format your Mavis CSV file for vaccination records"
