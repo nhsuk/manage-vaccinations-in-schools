nhsuk/manage-vaccinations-in-schools

commit 51dfd09618ee5430460ec65bba802ce069ee75f1

Add ability to create draft sessions
This allows for an "Add a new session" button to exist on the sessions
page.

Jira-Issue: MAV-2645

app/controllers/sessions_controller.rb
@@ -4,8 +4,10 @@ class SessionsController < ApplicationController
   include SessionSearchFormConcern
 
   before_action :set_session_search_form, only: :index
-  before_action :set_session, except: :index
+  before_action :set_session, except: %i[index new]
   before_action :set_breadcrumb_items, except: :index
+
+  skip_after_action :verify_policy_scoped, only: :new
 
   def index
     @programmes = current_user.selected_team.programmes
@@ -20,6 +22,17 @@ class SessionsController < ApplicationController
     @patient_count_by_session_id = patient_counts_for_sessions(@sessions)
 
     render layout: "full"
+  end
+
+  def new
+    @draft_session = DraftSession.new(request_session: session, current_user:)
+
+    @draft_session.clear_attributes
+    @draft_session.assign_attributes(create_params)
+
+    @draft_session.save!
+
+    redirect_to draft_session_path(Wicked::FIRST_STEP)
   end
 
   def show
@@ -102,5 +115,16 @@ class SessionsController < ApplicationController
       .where("sessions.id = ANY(ARRAY[?]::bigint[])", sessions.map(&:id))
       .group("sessions.id")
       .count("DISTINCT patients.id")
+  end
+
+  def create_params
+    {
+      academic_year: AcademicYear.pending,
+      session_dates: [DraftSessionDate.new],
+      team_id: current_team.id,
+      national_protocol_enabled: false,
+      psd_enabled: false,
+      requires_registration: false
+    }
   end
 end

app/controllers/draft_sessions_controller.rb
@@ -21,7 +21,7 @@ class DraftSessionsController < ApplicationController
   skip_after_action :verify_policy_scoped
 
   def show
-    authorize @session, :edit?
+    authorize @session, @session.new_record? ? :new? : :edit?
 
     if (current_step == :dates_check && should_skip_dates_check?) ||
          (current_step == :programmes_check && should_skip_programmes_check?)
@@ -32,7 +32,7 @@ class DraftSessionsController < ApplicationController
   end
 
   def update
-    authorize @session, :update?
+    authorize @session, @session.new_record? ? :create? : :update?
 
     case current_step
     when :dates
@@ -55,7 +55,7 @@ class DraftSessionsController < ApplicationController
   end
 
   def set_session
-    @session = @draft_session.session
+    @session = @draft_session.session || Session.new
   end
 
   def set_steps

app/views/sessions/index.html.erb
@@ -2,6 +2,10 @@
 
 <%= h1 t(".title"), size: "xl" %>
 
+<% if Flipper.enabled?(:schools_and_sessions) %>
+  <%= govuk_button_link_to "Add a new session", new_session_path, secondary: true %>
+<% end %>
+
 <div class="nhsuk-grid-row">
   <div class="app-grid-column-filters">
     <%= render AppSessionSearchFormComponent.new(

config/routes.rb
@@ -250,7 +250,7 @@ Rails.application.routes.draw do
     get "sessions"
   end
 
-  resources :sessions, only: %i[index show edit], param: :slug do
+  resources :sessions, only: %i[index new show edit], param: :slug do
     resource :patients, only: :show, controller: "sessions/patients"
     resource :patient_specific_directions,
              path: "patient-specific-directions",

commit eedcdb41e0cae0b6eeddc90fad51523dc57a9d85

Ensure session requires registration by default
This fixes a regression that was introduced in
51dfd09618ee5430460ec65bba802ce069ee75f1.

Jira-Issue: MAV-2741

app/controllers/sessions_controller.rb
@@ -125,7 +125,7 @@ class SessionsController < ApplicationController
       team_id: current_team.id,
       national_protocol_enabled: false,
       psd_enabled: false,
-      requires_registration: false
+      requires_registration: true
     }
   end
 end

spec/features/sessions_school_spec.rb
@@ -295,6 +295,7 @@ describe "School sessions" do
 
   def then_i_see_the_confirmation_page
     expect(page).to have_content("Check and confirm")
+    expect(page).to have_content("Register attendanceYes")
   end
 
   def when_i_press_back_enough_time_to_see_the_dates