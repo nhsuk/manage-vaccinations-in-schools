8265533214fb0f220e2ea8df46b76a35378b5cf2
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2026-01-05 11:18:44 +0000

    Rename "Change parent" to "Edit parent"
    This reduces ambiguity and prevents users from using this feature to change details to a different parent entirely.

--- app/components/app_child_summary_component.rb
+++ app/components/app_child_summary_component.rb
@@ -124,7 +124,7 @@ class AppChildSummaryComponent < ViewComponent::Base
                          )
                      )
                     row.with_action(
-                      text: "Change",
+                      text: "Edit",
                       href:,
                       visually_hidden_text: parent_relationship.ordinal_label
                     )
