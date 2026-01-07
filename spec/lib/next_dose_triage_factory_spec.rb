540cc57a61580eea3c7ccffe4512f5712cd561b3
Thomas Leese <thomas@leese.io>
2026-01-07 18:05:03 +0000

    Add `NextDoseTriageFactory`
    This adds a service class that is responsible for creating a triage for
    the next dose of a vaccination record for multi-dose programmes. The
    logic already existed but I've moved it in to a separate class so we can
    use it elsewhere.
    
    Jira-Issue: MAV-2881

---
+++ spec/lib/next_dose_triage_factory_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+describe NextDoseTriageFactory do
+  subject(:call) { described_class.call(vaccination_record:, current_user:) }
+
+  let(:vaccination_record) { create(:vaccination_record, programme:) }
+  let(:current_user) { create(:nurse) }
+
+  context "with a single dose programme" do
+    let(:programme) { Programme.hpv }
+
+    it "does not create a triage" do
+      expect { call }.not_to(change(Triage, :count))
+    end
+  end
+
+  context "with the MMR programme" do
+    let(:programme) { Programme.mmr }
+
+    it "creates a triage" do
+      expect { call }.to change(Triage, :count).by(1)
+
+      triage = vaccination_record.reload.next_dose_delay_triage
+      expect(triage).to be_delay_vaccination
+      expect(triage.delay_vaccination_until).to eq(28.days.from_now.to_date)
+    end
+  end
+end
