f7cff36e3dadebb85f115a6ba7a1963a24301ed3
Thomas Leese <thomas@leese.io>
2026-01-09 11:18:29 +0000

    Delete `CohortPolicy`
    This is not being used so it can be safely removed.

--- app/policies/cohort_policy.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CohortPolicy < ApplicationPolicy
-  class Scope < ApplicationPolicy::Scope
-    def resolve
-      scope.where(team: user.selected_team)
-    end
-  end
-end
