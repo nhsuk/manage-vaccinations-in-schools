nhsuk/manage-vaccinations-in-schools

commit 0787c8ced06ba346b090b6cf623bcddcbd22a790

Update programme variant selection logic
This slightly changes the logic related to how the MMR or MMRV variant
is chosen to support being able to select the more general MMR(V)
variant if the disease types are not set.

Specifically, this changes the variant that's selected in the scenario
where the `disease_types` is an empty array and the `patient` is
eligible for MMRV. In the old logic, the returned programme would be the
MMRV variant, whereas in the new logic we return the MMR(V) programme.
If instead the `disease_types` were set to `nil`, that means that the
patient's eligibility should instead be used and it would return the
MMRV variant.

We need this change to support triage instances where the programme is
either MMR(V), MMR or MMRV, combined with the fact that a `patient`
exists which we want ignored.

Jira-Issue: MAV-2989

app/models/programme.rb
@@ -137,20 +137,20 @@ class Programme
   def name_in_sentence = flu? ? name.downcase : name
 
   def variant_for(disease_types: nil, patient: nil)
-    return self if (disease_types.blank? && patient.blank?) || !mmr?
+    return self unless mmr?
 
     # We don't use both `patient` and `disease_types` because the patient
-    # might be eligible for a particular programme variant, but the
-    # `disease_types` indicate a different variant was used.
+    #  might be eligible for a particular programme variant, but the disease
+    #  types indicate a different variant was used.
 
     eligible_for_mmrv =
       if disease_types.present?
         disease_types.include?("varicella")
-      elsif patient
+      elsif disease_types.nil? && patient
         patient.eligible_for_mmrv?
-      else
-        false
       end
+
+    return self if eligible_for_mmrv.nil?
 
     variant_type =
       if eligible_for_mmrv && Flipper.enabled?(:mmrv)

spec/factories/consents.rb
@@ -61,7 +61,6 @@ FactoryBot.define do
     end
 
     programme { Programme.sample }
-    disease_types { Programme::DISEASE_TYPES[programme.type] }
 
     team do
       Team.has_all_programmes_of([programme]).first ||

spec/factories/patient_consent_statuses.rb
@@ -28,7 +28,6 @@ FactoryBot.define do
     patient
     programme { Programme.sample }
     academic_year { AcademicYear.current }
-    disease_types { Programme::DISEASE_TYPES[programme.type] }
 
     traits_for_enum :status
 

spec/factories/patient_programme_statuses.rb
@@ -31,7 +31,6 @@ FactoryBot.define do
     patient
     academic_year { AcademicYear.current }
     programme { Programme.sample }
-    disease_types { Programme::DISEASE_TYPES[programme.type] }
 
     traits_for_enum :status
 

spec/features/mmr_vaccination_administered_spec.rb
@@ -110,26 +110,34 @@ describe "MMR vaccination" do
   end
 
   def and_there_is_a_session_today_with_patients_safe_to_vaccinate
+    mmr_variant =
+      @programme.variant_for(
+        disease_types: Programme::Variant::DISEASE_TYPES.fetch("mmr")
+      )
+
     @without_gelatine_only_patient =
       create(
         :patient,
         :consent_given_without_gelatine_triage_not_needed,
         :in_attendance,
-        session: @session
+        session: @session,
+        programmes: [mmr_variant]
       )
     @without_gelatine_patient =
       create(
         :patient,
         :consent_given_triage_not_needed,
         :in_attendance,
-        session: @session
+        session: @session,
+        programmes: [mmr_variant]
       )
     @with_gelatine_patient =
       create(
         :patient,
         :consent_given_triage_not_needed,
         :in_attendance,
-        session: @session
+        session: @session,
+        programmes: [mmr_variant]
       )
   end
 

spec/features/mmrv_vaccination_administered_spec.rb
@@ -96,11 +96,13 @@ describe "MMRV vaccination" do
         :in_attendance,
         session: @session,
         parents: [@parent],
-        date_of_birth: Programme::MIN_MMRV_ELIGIBILITY_DATE + 1.month
+        date_of_birth: Programme::MIN_MMRV_ELIGIBILITY_DATE + 1.month,
+        programmes: [
+          @programme.variant_for(
+            disease_types: Programme::Variant::DISEASE_TYPES["mmrv"]
+          )
+        ]
       )
-    @patient.consents.last.update!(
-      disease_types: Programme::Variant::DISEASE_TYPES["mmrv"]
-    )
     StatusUpdater.call(patient: @patient)
     @community_clinic = create(:community_clinic, team: @team)
   end

spec/jobs/email_delivery_job_spec.rb
@@ -123,9 +123,15 @@ describe EmailDeliveryJob do
       expect(notify_log_entry_programme.programme_type).to eq(
         programmes.first.type
       )
-      expect(notify_log_entry_programme.disease_types).to eq(
-        programmes.first.disease_types
-      )
+
+      disease_types =
+        if programmes.first.mmr?
+          Programme::Variant::DISEASE_TYPES.fetch("mmr")
+        else
+          programmes.first.disease_types
+        end
+
+      expect(notify_log_entry_programme.disease_types).to eq(disease_types)
     end
 
     context "when the parent doesn't have an email address" do

commit 8a945319ec918479d4a9099f85676b12b0bf4150

Fix regression and prepare for MMRV being enabled
This fixes a small regression that was introduced in
0787c8ced06ba346b090b6cf623bcddcbd22a790 which meant that patients would
always see "MMR(V)" before receiving consent as their programme status
rather than the approriate "MMR" or "MMRV" variant according to which
one they are eligible for.

I've also updated some tests to run with the feature flag enabled to
prepare for when the flag is going to be enabled and eventually removed.

Jira-Issue: MAV-2989

app/lib/status_generator/programme.rb
@@ -104,8 +104,11 @@ class StatusGenerator::Programme
   def disease_types
     return nil if vaccination_generator.status == :not_eligible
 
-    vaccination_generator.disease_types.presence ||
+    if vaccination_generator.disease_types
+      vaccination_generator.disease_types
+    elsif consent_generator.status.in?(%i[given refused conflicts])
       consent_generator.disease_types
+    end
   end
 
   def date

spec/features/triage_without_gelatine_spec.rb
@@ -46,7 +46,13 @@ describe "Triage" do
   end
 
   def given_an_mmr_programme_is_underway
+    Flipper.enable(:mmrv)
+
     programmes = [Programme.mmr]
+    @programme_variant =
+      Programme.mmr.variant_for(
+        disease_types: Programme::Variant::DISEASE_TYPES.fetch("mmr")
+      )
 
     team = create(:team, programmes:)
     @user = create(:nurse, team:)
@@ -55,7 +61,13 @@ describe "Triage" do
   end
 
   def given_a_patient_exists_needing_triage
-    @patient = create(:patient, :consent_given_triage_needed, session: @session)
+    @patient =
+      create(
+        :patient,
+        :consent_given_triage_needed,
+        session: @session,
+        programmes: [@programme_variant]
+      )
   end
 
   def given_a_patient_exists_needing_triage_without_gelatine
@@ -63,7 +75,8 @@ describe "Triage" do
       create(
         :patient,
         :consent_given_without_gelatine_triage_needed,
-        session: @session
+        session: @session,
+        programmes: [@programme_variant]
       )
   end
 

spec/lib/patient_status_resolver_spec.rb
@@ -14,6 +14,8 @@ describe PatientStatusResolver do
   let(:academic_year) { AcademicYear.current }
   let(:context_location_id) { nil }
 
+  before { Flipper.enable(:mmrv) }
+
   describe "#consent" do
     subject { patient_status_resolver.consent }
 
@@ -115,9 +117,16 @@ describe PatientStatusResolver do
 
     context "for MMR programme" do
       let(:programme) { Programme.mmr }
+      let(:programme_variant) do
+        programme.variant_for(
+          disease_types: Programme::Variant::DISEASE_TYPES.fetch("mmr")
+        )
+      end
+
+      let(:date_of_birth) { Date.new(2019, 12, 31) }
 
       context "and eligible for 1st dose" do
-        let(:patient) { create(:patient, session:) }
+        let(:patient) { create(:patient, date_of_birth:, session:) }
 
         before do
           StatusUpdater.call(patient:)
@@ -138,7 +147,13 @@ describe PatientStatusResolver do
 
       context "and due 1st dose" do
         let(:patient) do
-          create(:patient, :consent_given_triage_not_needed, session:)
+          create(
+            :patient,
+            :consent_given_triage_not_needed,
+            date_of_birth:,
+            session:,
+            programmes: [programme_variant]
+          )
         end
 
         before do
@@ -163,7 +178,9 @@ describe PatientStatusResolver do
           create(
             :patient,
             :consent_given_without_gelatine_triage_not_needed,
-            session:
+            date_of_birth:,
+            session:,
+            programmes: [programme_variant]
           )
         end
 

spec/lib/status_generator/programme_spec.rb
@@ -61,7 +61,7 @@ describe StatusGenerator::Programme do
 
     its(:status) { should be(:needs_consent_no_response) }
     its(:date) { should eq(vaccination_record.performed_at.to_date) }
-    its(:disease_types) { should be_empty }
+    its(:disease_types) { should be_nil }
     its(:dose_sequence) { should be_nil }
     its(:vaccine_methods) { should be_nil }
     its(:without_gelatine) { should be_nil }
@@ -256,7 +256,7 @@ describe StatusGenerator::Programme do
   context "when consent is needed" do
     its(:status) { should be(:needs_consent_no_response) }
     its(:date) { should be_nil }
-    its(:disease_types) { should be_empty }
+    its(:disease_types) { should be_nil }
     its(:dose_sequence) { should be_nil }
     its(:vaccine_methods) { should be_nil }
     its(:without_gelatine) { should be_nil }