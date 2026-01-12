0241cf78c9954e0cabe793033435c417cfef9f72
Thomas Leese <thomas@leese.io>
2026-01-12 15:03:40 +0000

    Add missing methods to `CohortImportPolicy`
    This adds missing methods to the policy which don't seem to be covered
    currently by the tests.
    
    Sentry-Issue: 7182723291

--- app/policies/cohort_import_policy.rb
+++ app/policies/cohort_import_policy.rb
@@ -3,6 +3,10 @@
 class CohortImportPolicy < ApplicationPolicy
   def approve? = edit?
 
+  def re_review? = edit?
+
+  def cancel? = edit?
+
   class Scope < ApplicationPolicy::Scope
     def resolve
       scope.where(team: user.selected_team)
