22dfde54c3d7c80b010982eb15c3ff8a11c5e047
Mike Thompson <mike@artifactual.co.uk>
2026-01-14 09:57:23 +0000

    Hide parent info in other relevant places
    Including the "Add parent or guardian" button.
    
    This covers
    
    - when editing a patient
    - when view a vaccination record
    - when viewing a patient before merging following NHS
      number change
    
    Jira: MAV-3031

--- spec/features/upload_only_team_spec.rb
+++ spec/features/upload_only_team_spec.rb
@@ -27,10 +27,22 @@ describe "Upload-only team homepage and navigation" do
     given_i_am_signed_in_as_an_upload_only_team
     and_i_upload_a_valid_file
     when_i_visit_the_children_page
-    and_i_find_a_child
+    and_i_find_a_child(given_name: "Harry", family_name: "Potter")
     then_i_should_see_vaccinations_then_child_details
     and_child_cannot_be_archived
     and_child_does_not_look_archived
+  end
+
+  scenario "Parent details are not shown for a child with parents" do
+    given_i_am_signed_in_as_an_upload_only_team
+    and_there_is_a_child_with_parents
+    when_i_visit_the_children_page
+    and_i_find_a_child(given_name: "Draco", family_name: "Malfoy")
+    then_i_should_not_see_parent_details
+
+    when_i_edit_the_child_record
+    then_i_should_not_see_parent_details
+    and_i_should_not_see_add_parent_button
   end
 
   def given_i_am_signed_in_as_an_upload_only_team
@@ -45,6 +57,23 @@ describe "Upload-only team homepage and navigation" do
       )
     create(:school, team: @team, urn: 100_000)
     sign_in @team.users.first
+  end
+
+  def and_there_is_a_child_with_parents
+    @child_with_parents =
+      create(:patient, given_name: "Draco", family_name: "Malfoy", team: @team)
+    create(
+      :parent_relationship,
+      patient: @child_with_parents,
+      parent: create(:parent)
+    )
+    # Create a vaccination record to link this patient with the team
+    create(
+      :vaccination_record,
+      programme: Programme.flu,
+      patient: @child_with_parents,
+      team: @team
+    )
   end
 
   def when_i_visit_the_dashboard
@@ -137,10 +166,10 @@ describe "Upload-only team homepage and navigation" do
     expect(page).to have_content("2 imported records")
   end
 
-  def and_i_find_a_child
-    fill_in "Search", with: "harry"
+  def and_i_find_a_child(given_name:, family_name:)
+    fill_in "Search", with: given_name
     click_on "Search"
-    click_on "POTTER, Harry"
+    click_on "#{family_name.upcase}, #{given_name}"
   end
 
   def then_i_should_see_vaccinations_then_child_details
@@ -158,5 +187,17 @@ describe "Upload-only team homepage and navigation" do
 
   def and_child_does_not_look_archived
     expect(page).not_to have_content("Archived")
+  end
+
+  def when_i_edit_the_child_record
+    click_on "Edit child record"
+  end
+
+  def then_i_should_not_see_parent_details
+    expect(page).not_to have_content("First parent or guardian")
+  end
+
+  def and_i_should_not_see_add_parent_button
+    expect(page).not_to have_content("Add parent or guardian")
   end
 end
