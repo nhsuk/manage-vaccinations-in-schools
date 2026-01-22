9431682633a7c5023fb0fad458aa78c73799b24c
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2026-01-22 11:31:04 +0000

    Create new columns on `VaccinationRecord`
    Creates `batch_number` and `batch_expiry`
    
    These will hold the denormalised
    batch values after the refactor, instead of
    `VaccinationRecord.batch.name` and `VaccinationRecord.batch.expiry`.
    
    Jira-Issue: MAV-3146

--- db/schema.rb
+++ db/schema.rb
@@ -930,7 +930,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_26_113704) do
   end
 
   create_table "vaccination_records", force: :cascade do |t|
+    t.date "batch_expiry"
     t.bigint "batch_id"
+    t.string "batch_number"
     t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.integer "delivery_method"
