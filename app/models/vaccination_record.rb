059503dc3565e8e2567af4ddb84783a52393b844
Thomas Leese <thomas@leese.io>
2026-01-23 15:44:44 +0000

    Remove `performed_at` column
    This column is no longer being used so it can be safely removed.
    
    Jira-Issue: MAV-3158

--- app/models/vaccination_record.rb
+++ app/models/vaccination_record.rb
@@ -75,8 +75,6 @@
 #  fk_rails_...  (vaccine_id => vaccines.id)
 #
 class VaccinationRecord < ApplicationRecord
-  self.ignored_columns = %w[performed_at]
-
   include BelongsToProgramme
   include Confirmable
   include Discard::Model
