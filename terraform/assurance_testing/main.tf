7b0392694fbd71b01a4b826894c7a92573bf20a2
Joshua Frost <joshuafrost.work@gmail.com>
2026-01-26 17:53:56 +0000

    Add S3 bucket and cloudfront for end-to-end test reports

--- terraform/assurance_testing/main.tf
+++ terraform/assurance_testing/main.tf
@@ -82,3 +82,81 @@ module "s3_performance_reports" {
   logging_target_bucket_id = data.aws_s3_bucket.access_logs.id
   logging_target_prefix    = "performancetest-reports/"
 }
+
+module "s3_endtoend_reports" {
+  source                   = "../modules/s3"
+  bucket_name              = "endtoendtest-reports"
+  logging_target_bucket_id = data.aws_s3_bucket.access_logs.id
+  logging_target_prefix    = "endtoendtest-reports/"
+
+  additional_policy_statements = [
+    {
+      sid    = "AllowCloudFrontRead"
+      effect = "Allow"
+      actions = [
+        "s3:GetObject",
+      ]
+      resources = [
+        "arn:aws:s3:::endtoendtest-reports/*",
+      ]
+      principals = [
+        {
+          type        = "Service"
+          identifiers = ["cloudfront.amazonaws.com"]
+        }
+      ]
+      condition = {
+        test     = "StringEquals"
+        variable = "AWS:SourceArn"
+        values   = [aws_cloudfront_distribution.endtoend_reports.arn]
+      }
+    }
+  ]
+}
+
+resource "aws_cloudfront_origin_access_control" "endtoend_reports" {
+  name                              = "endtoend-reports-oac"
+  description                       = "OAC for end-to-end test reports bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "endtoend_reports" {
+  enabled             = true
+  comment             = "End-to-end test reports"
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = module.s3_endtoend_reports.bucket_regional_domain_name
+    origin_id                = "s3-endtoend-reports"
+    origin_access_control_id = aws_cloudfront_origin_access_control.endtoend_reports.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-endtoend-reports"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -7,3 +7,8 @@ output "arn" {
   value       = aws_s3_bucket.this.arn
   description = "The ARN of the S3 bucket"
 }
+
+output "bucket_regional_domain_name" {
+  value       = aws_s3_bucket.this.bucket_regional_domain_name
+  description = "The Regional domain name of the S3 bucket"
+}
