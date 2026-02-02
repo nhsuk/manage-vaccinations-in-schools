908cf9e73baa7f5698dc92f191c0b7d6edb9e623
Mike Thompson <mike@artifactual.co.uk>
2026-02-02 13:31:46 +0000

    Generate v06 of Reporting API Totals view
    This is just the view file with no changes.

---
+++ db/migrate/20260202124516_update_reporting_api_totals_to_version_6.rb
@@ -0,0 +1,8 @@
+class UpdateReportingAPITotalsToVersion6 < ActiveRecord::Migration[8.1]
+  def change
+    update_view :reporting_api_totals,
+      version: 6,
+      revert_to_version: 5,
+      materialized: true
+  end
+end
