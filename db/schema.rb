2f61ff6a20930320dbe1aa4e325e25914664ffbe
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2026-02-03 14:25:43 +0000

    Remove unused `Batch.name` column
    This column is now unused; instead `Batch.number` is used.
    
    This change also removes the
    `index_batches_on_team_id_and_name_and_expiry_and_vaccine_id` on
    `Batch`. This is acceptable; as of
    https://github.com/nhsuk/manage-vaccinations-in-schools/pull/5950,
    `Batch`es are no longer `find_or_initialize`d by these attributes,
    so this index is no longer used.
    
    Jira-Issue: MAV-3151

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_05_080017) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_05_081730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -94,7 +94,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_05_080017) do
     t.datetime "archived_at"
     t.datetime "created_at", null: false
     t.date "expiry"
-    t.string "name"
     t.string "number", null: false
     t.bigint "team_id"
     t.datetime "updated_at", null: false
