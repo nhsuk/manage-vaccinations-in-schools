f75abf43b69ef15195ff0e26b8f4bc599321471f
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2026-02-04 14:16:25 +0000

    Remove unique index on ArchiveReason We plan to add an `unarchived_at` column to the reasons to track if a patient has been unarchived, for better display in the acitivity log. This means that a patient can have multiple archive reasons for a single team if they were archived, unarchvied and then archived again for example.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_04_135340) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_04_141544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -53,9 +53,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_04_135340) do
     t.bigint "unarchived_by_user_id"
     t.datetime "updated_at", null: false
     t.index ["created_by_user_id"], name: "index_archive_reasons_on_created_by_user_id"
-    t.index ["patient_id", "team_id"], name: "index_archive_reasons_on_patient_id_and_team_id", unique: true
     t.index ["patient_id"], name: "index_archive_reasons_on_patient_id"
-    t.index ["team_id", "patient_id"], name: "index_archive_reasons_on_team_id_and_patient_id", unique: true
     t.index ["team_id"], name: "index_archive_reasons_on_team_id"
     t.index ["unarchived_by_user_id"], name: "index_archive_reasons_on_unarchived_by_user_id"
   end
