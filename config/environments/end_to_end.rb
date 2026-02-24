efb9953f62785684d5e09fbc3cfdd4af2e847ec5
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2026-02-24 10:43:16 +0000

    Configure end_to_end env to serve assets
    This replaces the use of pre-compiled assets, which can be a pain.

--- config/environments/end_to_end.rb
+++ config/environments/end_to_end.rb
@@ -2,4 +2,8 @@
 
 require_relative "development"
 
-Rails.application.configure { config.web_console.development_only = false }
+Rails.application.configure do
+  config.web_console.development_only = false # Allow dev console on errors
+  config.assets.compile = true # Allow Rails to serve assets dynamically
+  config.assets.server = true # Enable the asset server middleware
+end
