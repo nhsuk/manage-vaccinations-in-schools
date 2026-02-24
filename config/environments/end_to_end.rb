e16e1b2b36533010592c901edf0bc10eb61aeb9c
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2026-02-24 10:43:16 +0000

    Disable reloading in end_to_end env

--- config/environments/end_to_end.rb
+++ config/environments/end_to_end.rb
@@ -3,6 +3,9 @@
 require_relative "development"
 
 Rails.application.configure do
+  config.enable_reloading = false # Don't reload code changes
+  config.eager_load = true # Eager load code on boot for better performance
+
   config.web_console.development_only = false # Allow dev console on errors
   config.assets.compile = true # Allow Rails to serve assets dynamically
   config.assets.server = true # Enable the asset server middleware
