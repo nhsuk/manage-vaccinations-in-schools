d3016d0b9a42642a6199e1908fb12f0797f9acce
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2026-02-26 09:13:46 +0000

    Add authorization for imported_records action
    Adds imported_records? method to ClassImportPolicy and
    CohortImportPolicy to authorize the imported_records controller action.
    This was missing before and means users could not access the
    Imported records tab when an import is in re-review. Since re-review is
    very uncommon, we hadn't encountered this error until now.

--- spec/policies/class_import_policy_spec.rb
+++ spec/policies/class_import_policy_spec.rb
@@ -16,6 +16,7 @@ describe ClassImportPolicy do
               :cancel?,
               :create?,
               :edit?,
+              :imported_records?,
               :new?,
               :re_review?,
               :show?,
