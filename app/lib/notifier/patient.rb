1f4abecc12c31aea47705f06af2c9374c0ce4959
Thomas Leese <thomas@leese.io>
2026-03-04 09:52:10 +0000

    Add customisations to `Notifier::Patient`
    This adds two new options when sending clinic invitations for a patient
    that allows users of this method to decide how the invitations should be
    sent.
    
    In particular, we'll be using this in a follow up commit to enable
    sending bulk invitations, but only for programmes that have not already
    received an invitation, using the new
    `include_already_invited_programmes` parameter.
    
    Jira-Issue: MAV-3886

--- app/lib/notifier/patient.rb
+++ app/lib/notifier/patient.rb
@@ -7,25 +7,41 @@ class Notifier::Patient
     @patient = patient
   end
 
-  def send_clinic_invitation(programme_types:, team:, academic_year:, sent_by:)
+  ##
+  # Send a clinic initiation email and SMS to the parents of this patient.
+  #
+  # This determines the correct type of invitation to use (either an initial
+  # invitation or a subsequent invitation) based on the previous invitations
+  # which have been sent:
+  #
+  # +include_vaccinated_programmes+ allows for the sending of invitations for
+  # programmes where the patient has already been vaccinated.
+  #
+  # +include_already_invited_programmes+ allows for the sending of invitations
+  # for programmes where the patient has already been invited for in the past.
+  #
+  def send_clinic_invitation(
+    programmes,
+    team:,
+    academic_year:,
+    sent_by:,
+    include_vaccinated_programmes: false,
+    include_already_invited_programmes: true
+  )
     return unless send_notification?(team:)
 
-    programme_types.reject! do |programme_type|
-      patient.programme_status(
-        Programme.find(programme_type),
-        academic_year:
-      ).vaccinated_fully?
-    end
+    programme_types =
+      programme_types_to_send_for(
+        programmes,
+        team:,
+        academic_year:,
+        include_vaccinated_programmes:,
+        include_already_invited_programmes:
+      )
 
     return if programme_types.empty?
 
-    already_sent =
-      patient.clinic_notifications.any? do
-        it.team_id == team.id && it.academic_year == academic_year &&
-          (programme_types - it.programme_types).empty? # is subset
-      end
-
-    type = already_sent ? :subsequent_invitation : :initial_invitation
+    type = clinic_invitation_type(programme_types, team:, academic_year:)
 
     # We create a record in the database first to avoid sending duplicate emails/texts.
     # If a problem occurs while the emails/texts are sent, they will be in the job
@@ -41,7 +57,7 @@ class Notifier::Patient
       sent_by:
     )
 
-    template_name = determine_template_name(type, team:)
+    template_name = find_template_name(type, team:)
 
     params = { academic_year:, patient:, programme_types:, sent_by:, team: }
 
@@ -64,7 +80,56 @@ class Notifier::Patient
     @parents ||= patient.parents.select(&:contactable?).uniq
   end
 
-  def determine_template_name(type, team:)
+  def programme_types_to_send_for(
+    programmes,
+    team:,
+    academic_year:,
+    include_vaccinated_programmes: false,
+    include_already_invited_programmes: true
+  )
+    programmes_to_send_for =
+      programmes.select do |programme|
+        unless include_vaccinated_programmes
+          is_vaccinated =
+            patient.programme_status(programme, academic_year:).vaccinated?
+
+          next false if is_vaccinated
+        end
+
+        unless include_already_invited_programmes
+          already_invited =
+            patient.clinic_notifications.any? do
+              it.team_id == team.id && it.academic_year == academic_year &&
+                it.programme_types.include?(programme.type)
+            end
+
+          next false if already_invited
+        end
+
+        true
+      end
+
+    programmes_to_send_for.map(&:type)
+  end
+
+  def clinic_invitation_type(programme_types, team:, academic_year:)
+    already_sent_initial_invitation_to_all_programmes =
+      programme_types.all? do |programme_type|
+        patient.clinic_notifications.any? do
+          it.team_id == team.id && it.academic_year == academic_year &&
+            it.initial_invitation? &&
+            it.programme_types.include?(programme_type)
+        end
+      end
+
+    if already_sent_initial_invitation_to_all_programmes
+      :subsequent_invitation
+    else
+      :initial_invitation
+    end
+  end
+
+  def find_template_name(type, team:)
     template_names = [
       :"clinic_#{type}_#{team.organisation.ods_code.downcase}",
       :"clinic_#{type}"
