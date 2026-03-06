08b38129c3ef102df09a5da60adeac4ad5447a81
Jake Benilov <benilov@gmail.com>
2026-03-06 18:36:28 +0000

    Migrate consent_clinic_request email to local template
    Jira-Issue: MAV-4111

--- spec/features/parental_consent_send_request_spec.rb
+++ spec/features/parental_consent_send_request_spec.rb
@@ -80,6 +80,26 @@ describe "Parental consent" do
     and_an_activity_log_entry_is_visible_for_the_text("HPV")
   end
 
+  scenario "Send clinic request for doubles" do
+    given_programmes_exists(:td_ipv, :menacwy)
+    and_a_patient_without_consent_exists
+    and_i_am_signed_in
+
+    when_i_go_to_a_patient_without_consent
+    then_i_see_no_requests_sent
+
+    when_i_click_send_consent_request
+    then_i_see_the_confirmation_banner
+    and_i_see_a_consent_request_has_been_sent
+    and_a_clinic_request_email_is_sent_to_the_parent
+    and_a_clinic_request_sms_is_sent_to_the_parent
+
+    when_i_click_on_session_activity_and_notes
+    then_an_activity_log_entry_is_visible_for_the_email("MenACWY Td/IPV")
+    and_an_activity_log_entry_is_visible_for_the_text("MenACWY Td/IPV")
+  end
+
+
   scenario "Send clinic request where patient is eligible for MMRV" do
     given_a_programme_exists(:mmr)
     and_a_patient_without_consent_exists
@@ -264,7 +284,16 @@ describe "Parental consent" do
   end
 
   def and_a_clinic_request_email_is_sent_to_the_parent
-    expect_email_to(@parent.email, :consent_clinic_request)
+    expect(email_deliveries).to include(
+      matching_notify_email(
+        to: @parent.email,
+        template: :consent_clinic_request
+      ).with_content_including(
+        "You recently booked",
+        "Please give consent",
+        "If you need to contact us"
+      )
+    )
   end
 
   def and_a_clinic_request_sms_is_sent_to_the_parent
